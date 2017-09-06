c9bb464cbc27c1edaf3224355fc46926290a1965
Mark Caron <mcaron@redhat.com>
2017-09-06 13:24:37 -0400

    Adding attribute to overpass template

--- packages/rh-overpass/rh-overpass.js
+++ packages/rh-overpass/rh-overpass.js
@@ -1,7 +1,8 @@
 const overpassTemplate = document.createElement('div');
+overpassTemplate.setAttribute('style', 'display: none;');
+
 overpassTemplate.innerHTML = `
   <link rel="stylesheet" type="text/css" href="http://overpass-30e2.kxcdn.com/overpass.css">
 `;
 
-// window.customElements.define('rh-overpass', RhOverpass);
 document.head.appendChild(overpassTemplate);

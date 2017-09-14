632caa908a7889a061c4d99fa5e2b8a32c97b8d1
Kyle Buchanan <kylebuch8@gmail.com>
2017-09-14 13:58:52 -0400

    fixing compiled version and adding an IIFE to the js file

--- packages/rh-overpass/rh-overpass.js
+++ packages/rh-overpass/rh-overpass.js
@@ -1,8 +1,10 @@
-const overpassTemplate = document.createElement('div');
-overpassTemplate.setAttribute('style', 'display: none;');
+(function () {
+  const overpassTemplate = document.createElement('div');
+  overpassTemplate.setAttribute('style', 'display: none;');
 
-overpassTemplate.innerHTML = `
-  <link rel="stylesheet" type="text/css" href="http://overpass-30e2.kxcdn.com/overpass.css">
-`;
+  overpassTemplate.innerHTML = `
+    <link rel="stylesheet" type="text/css" href="http://overpass-30e2.kxcdn.com/overpass.css">
+  `;
 
-document.head.appendChild(overpassTemplate);
+  document.head.appendChild(overpassTemplate);
+}());

ca8a43f8b0d42cb667e88d72d6516b02df1f24d6
Kyle Buchanan <kylebuch8@gmail.com>
2017-11-28 10:47:21 -0500

    updating structure

--- packages/cp-tooltip/gulpfile.js
+++ packages/cp-tooltip/gulpfile.js
@@ -1,58 +1,59 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const replace = require('gulp-replace');
-const sass = require('gulp-sass');
-const stripCssComments = require('gulp-strip-css-comments');
-const trim = require('gulp-trim');
-const del = require('del');
-const fs = require('fs');
-let watcher;
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const replace = require("gulp-replace");
+const stripCssComments = require("strip-css-comments");
+const trim = require("trim");
+const fs = require("fs");
+const sass = require("node-sass");
 
-gulp.task('clean', () => {
-  return del(['./*.compiled.*'])
-});
-
-gulp.task('sass', () => {
-  return gulp.src(['./*.scss'])
-    .pipe(sass())
-    .pipe(stripCssComments())
-    .pipe(trim())
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('replaceStyles', () => {
-  return gulp.src('./cp-tooltip.js')
-    .pipe(replace(/<style>[\s\S]*<\/style>/g, '<style>' + fs.readFileSync('./cp-tooltip.css') + '</style>'))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('compile', () => {
-  return gulp.src(['./*.js', '!./gulpfile.js'])
-    .pipe(replace(/^(import .*?)(['"]\.\.\/(?!\.\.\/).*)(\.js['"];)$/gm, '$1$2.compiled$3'))
+gulp.task("compile", () => {
+  return gulp
+    .src("./cp-tooltip.js")
+    .pipe(
+      replace(
+        /^(import .*?)(['"]\.\.\/(?!\.\.\/).*)(\.js['"];)$/gm,
+        "$1$2.compiled$3"
+      )
+    )
     .pipe(babel())
     .pipe(uglify())
-    .pipe(rename({
-      suffix: ".compiled"
-    }))
-    .pipe(gulp.dest('./'));
+    .pipe(
+      rename({
+        suffix: ".compiled"
+      })
+    )
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('stopwatch', done => {
-  watcher.close();
-  done();
+gulp.task("watch", () => {
+  return gulp.watch("./src/*", gulp.series("merge", "compile"));
 });
 
-gulp.task('watch', () => {
-  watcher = gulp.watch(['./cp-tooltip.js', './*.scss'], gulp.series('stopwatch', 'sass', 'replaceStyles', 'clean', 'compile', 'watch'));
-  return watcher;
+gulp.task("merge", () => {
+  return gulp
+    .src("./src/cp-tooltip.js")
+    .pipe(
+      replace(/(template\.innerHTML = `)(`;)/, (match, p1, p2) => {
+        const html = fs
+          .readFileSync("./src/cp-tooltip.html")
+          .toString()
+          .trim();
+
+        const cssResult = sass.renderSync({
+          file: "./src/cp-tooltip.scss"
+        }).css;
+
+        return `${p1}
+<style>${stripCssComments(cssResult).trim()}</style>
+${html}
+${p2}`;
+      })
+    )
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('default',
-  gulp.series('clean', 'sass', 'replaceStyles', 'compile')
-);
+gulp.task("default", gulp.series("merge", "compile"));
 
-gulp.task('dev',
-  gulp.series('default', 'watch')
-);
+gulp.task("dev", gulp.series("merge", "compile", "watch"));

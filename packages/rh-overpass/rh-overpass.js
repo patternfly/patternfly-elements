260f82a89024c6696a5968391b1beddf8e6604c2
Kyle Buchanan <kylebuch8@gmail.com>
2017-11-30 09:41:40 -0500

    removing hardcoded reference to http and using // instead

--- packages/rh-overpass/rh-overpass.js
+++ packages/rh-overpass/rh-overpass.js
@@ -3,7 +3,7 @@
   overpassTemplate.setAttribute('style', 'display: none;');
 
   overpassTemplate.innerHTML = `
-    <link rel="stylesheet" type="text/css" href="http://overpass-30e2.kxcdn.com/overpass.css">
+    <link rel="stylesheet" type="text/css" href="//overpass-30e2.kxcdn.com/overpass.css">
   `;
 
   document.head.appendChild(overpassTemplate);

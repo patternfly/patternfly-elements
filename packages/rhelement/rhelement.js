23c58c37d5db1d884f1b1a51546db740f4e07c9b
Kyle Buchanan <kylebuch8@gmail.com>
2018-01-03 15:37:52 -0500

    Making the template parameter optional

--- packages/rhelement/rhelement.js
+++ packages/rhelement/rhelement.js
@@ -2,12 +2,15 @@ class Rhelement extends HTMLElement {
   constructor(id, template) {
     super();
 
-    if (window.ShadyCSS) {
+    if (window.ShadyCSS && template) {
       ShadyCSS.prepareTemplate(template, id);
     }
 
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    if (template) {
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
   }
 
   connectedCallback() {

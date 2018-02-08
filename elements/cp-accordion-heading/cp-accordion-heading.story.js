f85e3dac21b28de07df6db5dfb074400e4320960
Kyle Buchanan <kylebuch8@gmail.com>
2018-02-08 16:35:06 -0500

    updating a few stories

--- elements/cp-accordion-heading/cp-accordion-heading.story.js
+++ elements/cp-accordion-heading/cp-accordion-heading.story.js
@@ -1,12 +1,17 @@
 import { storiesOf } from "@storybook/polymer";
-import { withKnobs, text } from "@storybook/addon-knobs/polymer";
+import { withKnobs, text, select } from "@storybook/addon-knobs/polymer";
 import "./cp-accordion-heading";
 
-storiesOf("Accordion", module).add(
-  "cp-accordion-heading",
-  () => `
+const stories = storiesOf("Accordion", module);
+
+stories.addDecorator(withKnobs);
+
+stories.add("cp-accordion-heading", () => {
+  const heading = text("Heading", "Heading");
+
+  return `
     <cp-accordion-heading>
-      <h1>Heading 1</h1>
+      <h1>${heading}</h1>
     </cp-accordion-heading>
-  `
-);
+  `;
+});
--- elements/cp-accordion-panel/cp-accordion-panel.story.js
+++ elements/cp-accordion-panel/cp-accordion-panel.story.js
@@ -5,7 +5,7 @@ import "./cp-accordion-panel";
 storiesOf("Accordion", module).add(
   "cp-accordion-panel",
   () => `
-    <cp-accordion-panel>
+    <cp-accordion-panel expanded>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </cp-accordion-panel>
   `

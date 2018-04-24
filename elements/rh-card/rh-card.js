e15e419713866bc1ef9e38364b94219083ff75c2
Mark Caron <mcaron@redhat.com>
2018-04-24 16:04:07 -0400

    Adding test themeset, updating card demo.

--- elements/rh-card/rh-card.js
+++ elements/rh-card/rh-card.js
@@ -9,22 +9,27 @@ const template = document.createElement("template");
 template.innerHTML = `
 <style>:host {
   display: block;
-  padding: var(--rh-global--spacer, 1rem);
-  border: var(--rh-global--BorderWidth, 1px) var(--rh-global--BorderStyle, solid) transparent; }
+  padding: var(--rhe-theme--spacer, 1rem);
+  border: var(--rhe-theme--border--BorderWidth, 1px) var(--rhe-theme--border--BorderStyle, solid) transparent; }
 
 :host([theme="dark"]) {
-  background: var(--rh-card--theme-dark--Background, #1a1a1a);
-  color: var(--rh-card--theme-dark--Color, #fff); }
+  background: var(--rhe-theme--bg-color--shade7, #252527);
+  color: var(--rhe-theme--text-color--shade7, #fff); }
 
 :host([theme="light"]) {
-  background: var(--rh-card--theme-light--Background, #e7e7e7);
-  color: var(--rh-card--theme-light--Color, #333); }
+  background: var(--rhe-theme--bg-color--shade2, #e7e7e7);
+  color: var(--rhe-theme--text-color--shade2, #333); }
 
-:host ::slotted(h2:first-child) {
-  margin-top: 0; }</style>
-<slot name="header"></slot>
-<slot></slot>
-<slot name="footer"></slot>
+:host .rh-card__header::slotted(h1:first-child),
+:host .rh-card__header::slotted(h2:first-child),
+:host .rh-card__header::slotted(h3:first-child),
+:host .rh-card__header::slotted(h4:first-child),
+:host .rh-card__header::slotted(h5:first-child),
+:host .rh-card__header::slotted(h6:first-child) {
+  margin-top: 0 !important; }</style>
+<slot class="rh-card__header" name="header"></slot>
+<slot class="rh-card__body"></slot>
+<slot class="rh-card__footer" name="footer"></slot>
 `;
 /* end DO NOT EDIT */
 

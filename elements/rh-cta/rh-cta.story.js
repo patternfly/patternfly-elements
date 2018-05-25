288bade873efd9178478051a4c4af2715de9865d
Kyle Buchanan <kylebuch8@gmail.com>
2018-05-25 10:52:43 -0400

    switching the title from cp-dialog Demo to rh-dialog Demo

--- elements/rh-cta/rh-cta.story.js
+++ elements/rh-cta/rh-cta.story.js
@@ -1,11 +1,21 @@
 import { storiesOf } from "@storybook/polymer";
 import "./rh-cta";
 
-storiesOf("Cta", module).add(
+storiesOf("Call To Action", module).add(
   "rh-cta",
   () => `
-  <rh-cta>
-    Cta
-  </rh-cta>
+    <p>
+      <rh-cta class="solid"><a href="#">Become a Member</a></rh-cta>
+      <rh-cta class="outlined"><a href="#">Sign Up</a></rh-cta>
+      <rh-cta class="ghost"><a href="#">More info</a></rh-cta>
+    </p>
+
+    <p>
+      <rh-cta><a href="#">Learn more</a></rh-cta>
+    </p>
+
+    <p>
+      <rh-cta class="ghost"><a href="#">More info</a></rh-cta><rh-cta class="solid"><a href="#">Become a Member</a></rh-cta>
+    </p>
   `
 );

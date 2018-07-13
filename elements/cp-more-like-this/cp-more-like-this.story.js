02252648cf60267840e30843f527339180cda2dd
Michael Clayton <mclayton@redhat.com>
2018-07-13 11:11:03 -0400

    run prettier on everything

--- elements/cp-more-like-this/cp-more-like-this.story.js
+++ elements/cp-more-like-this/cp-more-like-this.story.js
@@ -1,12 +1,15 @@
-import { storiesOf } from '@storybook/polymer';
-import { withKnobs, text } from '@storybook/addon-knobs/polymer';
-import './cp-more-like-this';
+import { storiesOf } from "@storybook/polymer";
+import { withKnobs, text } from "@storybook/addon-knobs/polymer";
+import "./cp-more-like-this";
 
 const stories = storiesOf("More Like This", module);
 stories.addDecorator(withKnobs);
 
-stories.add('cp-more-like-this', () => {
-  const apiUrl = text("API URL", "https://api.access.redhat.com/rs/search?q=id:1336663&mltDocSearch=true&rows=3");
+stories.add("cp-more-like-this", () => {
+  const apiUrl = text(
+    "API URL",
+    "https://api.access.redhat.com/rs/search?q=id:1336663&mltDocSearch=true&rows=3"
+  );
   const contentType = text("Content Type", "Solution");
 
   return `

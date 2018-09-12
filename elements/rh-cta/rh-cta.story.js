ea1ec9c3e0b5fbb49e9dd86074d013f799124a5f
Tyler Martin <tmartin@redhat.com>
2018-09-11 22:34:03 -0400

    move actions to the right, update knobs for theme

--- elements/rh-cta/rh-cta.story.js
+++ elements/rh-cta/rh-cta.story.js
@@ -6,23 +6,34 @@ const stories = storiesOf("Call To Action", module);
 stories.addDecorator(withKnobs);
 
 stories.add("rh-cta", () => {
-  const type = "Priority";
-  const options = {
+  const priorityType = "Priority";
+  const colorType = "Color";
+
+  const priorityOptions = {
     default: "default",
     primary: "primary",
     secondary: "secondary"
   };
 
-  const defaultValue = "Solid";
+  const colorOptions = {
+    default: "default",
+    base: "base",
+    complement: "complement",
+    accent: "accent"
+  };
 
-  const value = select(type, options, defaultValue);
+  const priorityDefault = "default";
+  const colorDefault = "default";
+
+  const value = select(priorityType, priorityOptions, priorityDefault);
+  const colorValue = select(colorType, colorOptions, colorDefault);
+
+  let valueAttr = value != "default" ? ` priority="${value}"` : "";
+  let colorAttr = colorValue != "default" ? ` color="${colorValue}"` : "";
 
   return `
   <p>
-    <rh-cta><a href="#">Become a Member</a></rh-cta>
-  </p>
-  <p>
-    <rh-cta priority="${value}"><a href="#">Become a Member</a></rh-cta>
+    <rh-cta${valueAttr}${colorAttr}><a href="#">Become a Member</a></rh-cta>
   </p>
 
   `;

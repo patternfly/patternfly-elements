9a9578067f71c6b4ca55908bb1e487aa15d81410
Tyler Martin <tmartin@redhat.com>
2018-09-21 14:55:10 -0400

    Add circled option

--- elements/rh-icon/rh-icon.story.js
+++ elements/rh-icon/rh-icon.story.js
@@ -1,11 +1,21 @@
 import { storiesOf } from "@storybook/polymer";
-import { withKnobs, text, select } from "@storybook/addon-knobs/polymer";
+import {
+  withKnobs,
+  text,
+  select,
+  boolean
+} from "@storybook/addon-knobs/polymer";
 import "./rh-icon";
 
 const stories = storiesOf("Icon", module);
 stories.addDecorator(withKnobs);
 
 stories.add("rh-icon", () => {
+  let colorSelect = "";
+  let circledSelect = "";
+  let circled = "";
+  let iconColor = "";
+
   const iconOptions = {
     server: "server",
     "server-stack": "server-stack"
@@ -27,29 +37,48 @@ stories.add("rh-icon", () => {
     "2x": "2x",
     "3x": "3x",
     "4x": "4x",
-    sm: "sm",
-    md: "md",
-    lg: "lg",
-    xl: "xl"
+    sm: "Small",
+    md: "Medium",
+    lg: "Large",
+    xl: "Extra Large"
+  };
+
+  const circledOptions = {
+    default: "default",
+    base: "base",
+    lightest: "lightest",
+    light: "light",
+    dark: "dark",
+    darkest: "darkest",
+    complement: "complement",
+    accent: "accent"
   };
 
   const valueSelect = select("Icon", iconOptions, "server");
-  const colorSelect = select("Color", colorOptions, "default");
   const sizeSelect = select("Size", sizeOptions, "default");
+  const circledBoolean = boolean("Circled", false);
+
+  if (circledBoolean != false) {
+    circledSelect = select("Circle Color", circledOptions, "default");
+    circled =
+      circledSelect != "default" ? ` circled="${circledSelect}"` : " circled";
+  } else {
+    colorSelect = select("Color", colorOptions, "default");
+    iconColor = colorSelect != "default" ? ` color="${colorSelect}"` : "";
+  }
 
   let value = valueSelect != "default" ? ` icon="rh-icon-${valueSelect}"` : "";
-  let iconColor = colorSelect != "default" ? ` color="${colorSelect}"` : "";
   let size = sizeSelect != "default" ? ` size="${sizeSelect}"` : "";
 
   return `
 
   <section>
     <h2>Your RHElement</h2>
-    <rh-icon${value}${iconColor}${size}</rh-icon>
+    <rh-icon${value}${circled}${iconColor}${size}></rh-icon>
   </section>
   <section>
     <h2>Markup</h2>
-    <pre><code>&lt;rh-icon${value}${iconColor}${size}&gt;&lt;/rh-icon&gt;</code></pre>
+    <pre><code>&lt;rh-icon${value}${circled}${iconColor}${size}&gt;&lt;/rh-icon&gt;</code></pre>
   </section>
 
   `;

4aeda7cf7d4d77e6a9ccd4d0b5cd706cc0ad81c3
Tyler Martin <tmartin@redhat.com>
2018-09-21 13:48:27 -0400

    update template and add icon and color knobs

--- elements/rh-icon/rh-icon.story.js
+++ elements/rh-icon/rh-icon.story.js
@@ -1,11 +1,41 @@
 import { storiesOf } from "@storybook/polymer";
+import { withKnobs, text, select } from "@storybook/addon-knobs/polymer";
 import "./rh-icon";
 
-storiesOf("Icon", module).add(
-  "rh-icon",
-  () => `
-    <h2><rh-icon icon="rh-icon-server" data-size="2x"></rh-icon> Content beside it.</h2>
-    <p><rh-icon icon="rh-icon-server-alt1" data-color="black" data-size="2x"></rh-icon> More content</p>
-    <div><rh-icon icon="rh-icon-server-stack" data-bg data-size="large"></rh-icon> Feature-ish?</div>
-  `
-);
+const stories = storiesOf("Icon", module);
+stories.addDecorator(withKnobs);
+
+stories.add("rh-icon", () => {
+  const iconOptions = {
+    server: "server",
+    "server-stack": "server-stack"
+  };
+
+  const colorOptions = {
+    default: "default",
+    base: "base",
+    complement: "complement",
+    accent: "accent",
+    critical: "critical",
+    important: "important",
+    moderate: "moderate",
+    success: "success",
+    info: "info"
+  };
+
+  const valueSelect = select("Icon", iconOptions, "server");
+  const colorSelect = select("Color", colorOptions, "default");
+
+  return `
+
+  <section>
+    <h2>Your RHElement</h2>
+    <rh-icon icon="rh-icon-${valueSelect}" size="4x">
+  </section>
+  <section>
+    <h2>Markup</h2>
+    <pre><code>&lt;rh-icon&gt;&lt;/rh-icon&gt;</code></pre>
+  </section>
+
+  `;
+});

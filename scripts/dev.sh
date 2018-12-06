e58e8926e5e535c4da5a03c04f0cccfd5d5ba537
Michael Clayton <mclayton@redhat.com>
2018-12-06 15:51:47 -0500

    go back to '*' to make dev script resilient to name change

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,7 +3,7 @@
 CMD="npm run lerna -- run dev --parallel --no-bail --include-filtered-dependencies"
 
 for el in "$@"; do
-  CMD="$CMD --scope \"@rhelements/$el\""
+  CMD="$CMD --scope \"*/$el\""
 done
 
 eval $CMD

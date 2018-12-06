bffc49294f223727d18ca5f32e6ebfcedaaed464
Kyle Buchanan <kylebuch8@gmail.com>
2018-12-06 13:41:20 -0500

    changing from RHElements to PFElements
    This is a monster commit that has all name and prefix changes for all of our elements.

--- elements/rh-icon-panel/gulpfile.js
+++
@@ -1,5 +0,0 @@
-// rollup.config.js
-const gulpFactory = require("../../scripts/gulpfile.factory.js");
-const rhelementPackage = require("./package.json");
-
-gulpFactory(rhelementPackage.rhelement);

ce9acaa4ad9a7f15e6891517158d53d8e08f825c
[ Cassondra ] <carobert@redhat.com>
2018-12-10 21:57:06 -0500

    Create a fork testing the hide-show-set approach

--- elements/rh-hide-show/rh-hide-show.story.js
+++ elements/rh-hide-show/rh-hide-show.story.js
@@ -1,11 +1,48 @@
 import { storiesOf } from "@storybook/polymer";
+import { withKnobs, text, select } from "@storybook/addon-knobs/polymer";
+import { escapeHTML } from "../../.storybook/utils.js";
 import "./rh-hide-show";
 
-storiesOf("Hide", module).add(
-  "rh-hide-show",
-  () => `
-  <rh-hide-show>
-    Hide
-  </rh-hide-show>
-  `
-);
+const stories = storiesOf("Hide-show", module);
+stories.addDecorator(withKnobs);
+
+stories.add("rh-hide-show", () => {
+  // Pointer to theme file
+  // const themeLabel = "Theme file";
+  // const themeValue = text(themeLabel, "");
+
+  // Attributes
+  // const colorLabel = "Foo";
+  // const colorOptions = {
+  //   lightest: "lightest",
+  //   light: "light",
+  //   base: "base",
+  //   dark: "dark",
+  //   darkest: "darkest",
+  //   complement: "complement",
+  //   accent: "accent"
+  // };
+  // const colorDefault = "light";
+  // const colorValue = select(colorLabel, colorOptions, colorDefault);
+  // let colorAttr = colorValue != "base" ? ` color="${colorValue}"` : "";
+
+  // Slots
+  // const headerLabel = "Header";
+  // const headerDefault = "<h2>Foo bar</h2>";
+  // const headerValue = text(headerLabel, headerDefault);
+
+  return `
+  <section>
+    <h2>Your Element</h2>
+    <rh-hide-show>
+    </rh-hide-show>
+  </section>
+  <section>
+    <h2>Markup</h2>
+    <pre>
+    ${escapeHTML(`<rh-hide-show>`)}
+    ${escapeHTML("</rh-hide-show>")}
+    </pre>
+  </section>
+  `;
+});
--- elements/rh-hide-show/src/rh-hide-show.js
+++ elements/rh-hide-show/src/rh-hide-show.js
@@ -27,16 +27,80 @@ class RhHideShow extends RHElement {
     return "rh-hide-show";
   }
 
+  get styleUrl() {
+    return "rh-hide-show.css";
+  }
+
   get templateUrl() {
     return "rh-hide-show.html";
   }
 
+  isTab() {
+    return this.parentNode.offsetWidth > 768;
+  }
+
+  static get observedAttributes() {
+    return ["vertical", "selected-index", "rh-variant", "theme", "color"];
+  }
+
+  static get cascadingAttributes() {
+    return {
+      vertical: "rh-tabs",
+      "selected-index": "rh-tabs",
+      "rh-variant": "rh-tabs",
+      theme: "rh-accordion",
+      color: "rh-accordion",
+      "render-as": "rh-hide-show-set"
+    };
+  }
+
+  // Declare the type of this component
+  static get rhType() {
+    return RHElement.rhType.combo;
+  }
+
+  constructor() {
+    super(RhHideShow);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    let tag = document.createElement("rh-accordion");
+    if (this.isTab()) {
+      tag = document.createElement("rh-tabs");
+    }
+
+    if (this.children.length > 0) {
+      Array.from(this.children).forEach(child => {
+        tag.appendChild(child);
+      });
+    }
+    this.shadowRoot.innerHTML = "";
+    // Add the element to the shadow DOM
+    this.shadowRoot.appendChild(tag);
+  }
+
+  // disconnectedCallback() {}
+
+  // attributeChangedCallback(attr, oldValue, newValue) {}
+}
+
+class RhHideShowSet extends RHElement {
+  static get tag() {
+    return "rh-hide-show-set";
+  }
+
   get styleUrl() {
-    return "rh-hide-show.scss";
+    return "rh-hide-show.css";
+  }
+
+  get templateUrl() {
+    return "rh-hide-show-set.html";
   }
 
   isTab() {
-    return this.parentNode.offsetWidth > 768;
+    return this.parentNode.parentNode.offsetWidth > 768;
   }
 
   // static get observedAttributes() {
@@ -45,15 +109,64 @@ class RhHideShow extends RHElement {
 
   // Declare the type of this component
   static get rhType() {
-    return RHElement.rhType.combo;
+    return RHElement.rhType.component;
   }
 
   constructor() {
-    super(RhHideShow, { delayRender: true });
+    super(RhHideShowSet);
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    if (this.children.length > 0) {
+      let tag_header;
+      let tag_panel;
+
+      // Create an array from the child nodes
+      let header = Array.from(this.children).filter(
+        child => child.slot === "rh-hide-show-set--header"
+      );
+      let panels = Array.from(this.children).filter(
+        child => child.slot !== "rh-hide-show-set--header"
+      );
+
+      // Check if this should be a tab or accordion
+      if (this.isTab()) {
+        // Build the header section
+        tag_header = document.createElement("rh-tab");
+        tag_header.setAttribute("slot", "tab");
+        tag_header.setAttribute("role", "heading");
+        // Build the panel section
+        tag_panel = document.createElement("rh-tab-panel");
+        tag_panel.setAttribute("slot", "panel");
+        tag_panel.setAttribute("role", "region");
+
+        if (header.length > 0) {
+          tag_header.innerText = header[0].innerText;
+        }
+      } else {
+        tag_header = document.createElement("rh-accordion-header");
+        tag_panel = document.createElement("rh-accordion-panel");
+        if (header.length > 0) {
+          header.forEach(child => {
+            tag_header.appendChild(child);
+          });
+        }
+      }
+
+      if (panels.length > 0) {
+        panels.forEach(child => {
+          tag_panel.appendChild(child);
+        });
+      }
+
+      this.shadowRoot.innerHTML = "";
+      // Add the header to the shadow DOM
+      this.shadowRoot.appendChild(tag_header);
+      // Add the panel to the shadow DOM
+      this.shadowRoot.appendChild(tag_panel);
+    }
   }
 
   // disconnectedCallback() {}
@@ -61,6 +174,8 @@ class RhHideShow extends RHElement {
   // attributeChangedCallback(attr, oldValue, newValue) {}
 }
 
+RHElement.create(RhHideShowSet);
 RHElement.create(RhHideShow);
 
-export default RhHideShow;
+// export default RhHideShow;
+// export default RhHideShowSet;

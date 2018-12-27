aab020ee03e3dab76fb8b1955c4b4ebaebed0a17
homa <hseyedal@redhat.com>
2018-12-27 16:54:22 -0500

    centered close button

--- elements/rh-autocomplete/rh-autocomplete.js
+++ elements/rh-autocomplete/rh-autocomplete.js
@@ -384,21 +384,23 @@ button {
   position: absolute;
   top: 0px;
   bottom: 0px;
-  padding: 0px; }
+  padding: 0px;
+  margin: 0px;
+  cursor: pointer; }
 
 button.clear-search {
   right: 31px;
   width: 20px;
-  top: 45%;
-  transform: translate(-50%, -50%);
-  margin-right: 5px;
-  font-size: 28px;
-  font-weight: 600;
-  line-height: 20px; }
+  margin-right: 5px; }
 
-button.clear-search:hover {
+button.clear-search svg {
+  width: 12px;
+  stroke: #ccc; }
+
+button.clear-search:hover svg,
+button.clear-search:focus svg {
   opacity: 1;
-  color: #06c; }
+  stroke: #06c; }
 
 button.search-button {
   right: 1px;
@@ -439,10 +441,10 @@ button.search-button:disabled svg {
 
 <span class="loading" aria-hidden="true" hidden>
   <svg viewBox="0 0 40 40" enable-background="new 0 0 40 40">
-  <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+    <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
     s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
     c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
-  <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+    <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
     C22.32,8.481,24.301,9.057,26.013,10.047z">
     <animateTransform attributeType="xml"
       attributeName="transform"
@@ -456,7 +458,10 @@ button.search-button:disabled svg {
 </span>
 
 <button type="button" class="clear-search" aria-label="clear search query">
-  &times;
+  <svg viewBox="0 0 40 40" enable-background="new 0 0 40 40">
+    <line x1="5" y1="5" x2="35" y2="35" stroke-width="10" stroke-linecap="round" stroke-miterlimit="10"></line>
+    <line x1="35" y1="5" x2="5" y2="35" stroke-width="10" stroke-linecap="round" stroke-miterlimit="10"></line>
+  </svg>
 </button>
 <button class="search-button" type="button" aria-label="Search">
   <svg viewBox="0 0 512 512">

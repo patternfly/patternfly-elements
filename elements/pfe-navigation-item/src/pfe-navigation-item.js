38c7fd2f14a04e145cc25425010f94972df989b9
castastrophe <casnroberts+github@gmail.com>
2019-02-21 09:19:53 -0500

    Move the story to the demo folder and add the schema

--- elements/pfe-navigation-item/src/pfe-navigation-item.js
+++ elements/pfe-navigation-item/src/pfe-navigation-item.js
@@ -5,6 +5,10 @@ class PfeNavigationItem extends PFElement {
     return "pfe-navigation-item";
   }
 
+  get schemaUrl() {
+    return "pfe-navigation-item.json";
+  }
+
   get templateUrl() {
     return "pfe-navigation-item.html";
   }
@@ -17,13 +21,31 @@ class PfeNavigationItem extends PFElement {
   //   return [];
   // }
 
-  constructor() {
-    super(PfeNavigationItem);
+  // Declare the type of this component
+  static get PfeType() {
+    return PFElement.PfeTypes.Container;
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // }
+  constructor() {
+    super(PfeNavigationItem, { type: PfeNavigationItem.PfeType });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+    // Strip the prefix from the attribute
+    attr = attr.replace("pfe-", "");
+    // If the observer is defined in the attribute properties
+    if (this[attr] && this[attr].observer) {
+      // Get the observer function
+      let observer = this[this[attr].observer].bind(this);
+      // If it's a function, allow it to run
+      if (typeof observer === "function") observer(attr, oldValue, newValue);
+    }
+  }
 
   // disconnectedCallback() {}
 

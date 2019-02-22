b0bb7dd58990abbf6f16764d43123954a1502dfb
castastrophe <casnroberts+github@gmail.com>
2019-02-22 13:04:39 -0500

    Add copy of light DOM into shadowDOM for the navigation slots

--- elements/pfe-navigation/src/pfe-navigation.js
+++ elements/pfe-navigation/src/pfe-navigation.js
@@ -41,39 +41,25 @@ class PfeNavigation extends PFElement {
   connectedCallback() {
     super.connectedCallback();
 
-    // Get the trigger slot
-    // const trigger =
-    // Register click event on the trigger slot
-    // this.addEventListener("click", this._clickHandler);
-
-    // this.addEventListener(`${PfeNavigation.tag}:active`, this._changeHandler);
-    // this.addEventListener("keydown", this._keydownHandler);
-
-
-    // this.setAttribute("role", "presentation");
-    // this.setAttribute("defined", "");
-  }
-
-  _clickHandler(event) {
-    event.preventDefault();
-    // Check that the event fired is from the trigger slot
-    if (event.target.getAttribute("slot") === "pfe-navigation--trigger") {
-
-      // Open the tray if it's not open, close if it is
-    }
-    // If the event is fired from the tray
-
-    this.dispatchEvent(
-      new CustomEvent(`${PfeNavigation.tag}:active`, {
-        detail: { expanded: !this.expanded },
-        bubbles: true
-      })
-    );
-  }
-
-  disconnectedCallback() {
-    this.removeEventListener("click", this._changeHandler);
-    this.removeEventListener("keydown", this._keydownHandler);
+    // Define the name of the slots
+    const slots = {
+        main: "pfe-navigation--main",
+        utility: "pfe-navigation--utility"
+    };
+    // Copy the content of the main and utility slots into the ShadowDOM
+    Object.values(slots).forEach(slotName => {
+        const fraggle = document.createDocumentFragment();
+        // Get the content and the slots
+        const contents = [...this.querySelectorAll(`[slot="${slotName}"]`)];
+        const slot = this.shadowRoot.querySelector(`[pfe-id="${slotName}"]`);
+        contents.forEach(content => {
+            fraggle.appendChild(content);
+        });
+        // If the slot and contents exist, append the fragment to the DOM
+        if(slot && contents.length) {
+            slot.appendChild(fraggle);
+        }
+    });
   }
 
   attributeChangedCallback(attr, oldValue, newValue) {
@@ -86,26 +72,6 @@ class PfeNavigation extends PFElement {
       let observer = this[this[attr].observer].bind(this);
       // If it's a function, allow it to run
       if (typeof observer === "function") observer(attr, oldValue, newValue);
-    }
-  }
-
-  _changeHandler(evt) {
-
-  }
-
-  // Update the icon attribute and return the SVG
-  _updateIcon(attr, oldValue, newValue){
-    switch (newValue) {
-      case "Search":
-        // Get the search SVG
-      case "Globe":
-        // Get the globe SVG
-      case "Person":
-        // Get the person SVG
-      case "App":
-        // Get the person SVG
-      default:
-        // @TODO is there a default icon?
     }
   }
 }

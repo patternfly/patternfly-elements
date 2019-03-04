5613ed64af76e686bdc37109355cf31074974be3
Kyle Buchanan <kylebuch8@gmail.com>
2019-03-04 09:54:18 -0500

    fixing pfe-content-set so light dom styles come through to the shadow dom

--- elements/pfe-content-set/src/pfe-content-set.js
+++ elements/pfe-content-set/src/pfe-content-set.js
@@ -1,16 +1,22 @@
 import PFElement from "../pfelement/pfelement.js";
+import PfeAccordion from "../pfe-accordion/pfe-accordion.js";
+import PfeTabs from "../pfe-tabs/pfe-tabs.js";
 
 class PfeContentSet extends PFElement {
   static get tag() {
     return "pfe-content-set";
   }
 
-  get styleUrl() {
-    return "pfe-content-set.css";
-  }
-
   get templateUrl() {
     return "pfe-content-set.html";
+  }
+
+  get styleUrl() {
+    return "pfe-content-set.scss";
+  }
+
+  static get pfeType() {
+    return PFElement.pfeType.combo;
   }
 
   get isTab() {
@@ -19,9 +25,14 @@ class PfeContentSet extends PFElement {
       : window.outerWidth > 768;
   }
 
+  get orientation() {
+    return this.hasAttribute("vertical") ? "vertical" : null;
+  }
+
   get settings() {
     let settings = {};
     const variant = this.getAttribute("pfe-variant");
+
     if (variant === "primary") {
       settings.variant = variant;
       settings.color = "striped";
@@ -31,69 +42,84 @@ class PfeContentSet extends PFElement {
     } else {
       settings.color = "lightest";
     }
+
     return settings;
   }
 
-  get orientation() {
-    if (this.hasAttribute("vertical")) {
-      return " vertical";
-    }
-  }
-
-  static get observedAttributes() {
-    return ["pfe-vertical", "selected-index", "pfe-variant", "theme", "color"];
-  }
-
-  static get cascadingAttributes() {
-    return {
-      vertical: "pfe-tabs",
-      "selected-index": "pfe-tabs",
-      "pfe-variant": "pfe-tabs",
-      theme: "pfe-accordion",
-      color: "pfe-accordion"
-    };
-  }
-
-  // Declare the type of this component
-  static get pfeType() {
-    return PFElement.pfeType.combo;
-  }
-
   constructor() {
-    super(PfeContentSet, {
-      delayRender: true
-    });
-
-    this.groupings = [];
-
-    this._observer = new MutationObserver(() => {
-      const tempGrouping = [...this.querySelectorAll("pfe-content-set-group")];
-
-      tempGrouping.forEach(group => {
-        const tempGroup = {
-          heading: group.querySelector("[pfe-heading]"),
-          body: [...group.querySelectorAll(":not([pfe-heading])")]
-        };
-
-        this.groupings.push(tempGroup);
-      });
-
-      // this._observer.disconnect();
-      this.render();
-    });
-
-    this._observer.observe(this, {
-      attributes: true,
-      childList: true
-    });
+    super(PfeContentSet, { delayRender: true });
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    if (this.isTab) {
+      this._buildTabs();
+    } else {
+      this._buildAccordion();
+    }
+
+    this.render();
   }
 
-  disconnectedCallback() {
-    this._observer.disconnect();
+  _buildAccordion() {
+    const fragment = document.createDocumentFragment();
+    const accordion = document.createElement(PfeAccordion.tag);
+
+    [...this.children].forEach(child => {
+      if (child.hasAttribute("pfe-content-set--header")) {
+        const header = document.createElement("pfe-accordion-header");
+        header.appendChild(child);
+        accordion.appendChild(header);
+      }
+
+      if (child.hasAttribute("pfe-content-set--panel")) {
+        const panel = document.createElement("pfe-accordion-panel");
+        panel.appendChild(child);
+        accordion.appendChild(panel);
+      }
+    });
+
+    fragment.appendChild(accordion);
+
+    if (this.settings.color) {
+      accordion.setAttribute("color", this.settings.color);
+    }
+
+    this.appendChild(fragment);
+  }
+
+  _buildTabs() {
+    const fragment = document.createDocumentFragment();
+    const tabs = document.createElement(PfeTabs.tag);
+
+    [...this.children].forEach(child => {
+      if (child.hasAttribute("pfe-content-set--header")) {
+        const header = document.createElement("pfe-tab");
+        header.setAttribute("slot", "tab");
+        header.appendChild(child);
+        tabs.appendChild(header);
+      }
+
+      if (child.hasAttribute("pfe-content-set--panel")) {
+        const panel = document.createElement("pfe-tab-panel");
+        panel.setAttribute("slot", "panel");
+        panel.appendChild(child);
+        tabs.appendChild(panel);
+      }
+    });
+
+    fragment.appendChild(tabs);
+
+    if (this.orientation) {
+      tabs.setAttribute("vertical", true);
+    }
+
+    if (this.settings.variant) {
+      tabs.setAttribute("pfe-variant", this.settings.variant);
+    }
+
+    this.appendChild(fragment);
   }
 }
 

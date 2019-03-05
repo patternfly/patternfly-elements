ce6bfe09c8fb2775704d66c02755d86097dc3724
Michael Clayton <mclayton@redhat.com>
2019-03-05 15:03:57 -0500

    in dev task, run hugo server if hugo is installed

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-HUGO_BIN=${HUGO_BIN:="hugo"}
 
-# check for hugo
+source hugo-check.sh
 
-if ! [ -x "$(command -v $HUGO_BIN)" ]; then
-  echo "Error: could not find Hugo (looked for '$HUGO_BIN')" >&2
-  echo
-  echo "If Hugo is installed but the command is not named 'hugo', set HUGO_BIN to the correct command name:" >&2
-  echo
-  echo "  export HUGO_BIN=\"my-hugo\"" >&2
-  echo
-  echo "If Hugo is not installed, see https://gohugo.io/getting-started/quick-start/" >&2
-  exit 1
-fi
+hugoNoisyCheck || exit 1
 
 # clean and run build
 
 pushd $DIR > /dev/null
 pushd ../docs/ > /dev/null
 
+echo "Cleaning public/ dir."
+rm -rf public
+
 echo "Building hugo docs."
 hugo
 
 echo "Building storybook."
-mkdir public/demo
 npm run build-storybook
+mkdir public/demo
 cp -r ../.storybook_out/* public/demo
 
 echo "Pushing to github pages."

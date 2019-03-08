96c2ff95d4faa568603492401e80f7762fa1ed71
castastrophe <casnroberts+github@gmail.com>
2019-03-08 17:03:12 -0500

    Working on tests for pfe-cta

--- elements/pfe-cta/src/pfe-cta.js
+++ elements/pfe-cta/src/pfe-cta.js
@@ -13,8 +13,21 @@ class PfeCta extends PFElement {
     return "pfe-cta.html";
   }
 
+  static get observedAttributes() {
+    return [
+      "pfe-priority",
+      "pfe-color",
+      "on"
+    ];
+  }
+
+  // Declare the type of this component
+  static get PfeType() {
+    return PFElement.PfeTypes.Content;
+  }
+
   constructor() {
-    super(PfeCta);
+    super(PfeCta, { type: PfeCta.PfeType });
   }
 
   connectedCallback() {
@@ -35,9 +48,24 @@ class PfeCta extends PFElement {
     }
   }
 
-  disconnectedCallback() {}
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+    // Strip the prefix form the attribute
+    attr = attr.replace("pfe-", "");
+    // If the observer is defined in the attribute properties
+    if (this[attr] && this[attr].observer) {
+      // Get the observer function
+      let observer = this[this[attr].observer].bind(this);
+      // If it's a function, allow it to run
+      if (typeof observer === "function") observer(attr, oldValue, newValue);
+    }
+  }
+
+  _basicAttributeChanged(attr, oldValue, newValue) {
+    this[attr].value = newValue;
+  }
 }
 
 PFElement.create(PfeCta);
 
-export default PfeCta;
+export { PfeCta as default };

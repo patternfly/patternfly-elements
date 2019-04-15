843df4ec7c9612fa7e36c7864c8fb41ce4c12249
Michael Clayton <mclayton@redhat.com>
2019-04-15 13:46:10 -0400

    improve phrasing of the deletion suggestion

--- scripts/leftover-check.sh
+++ scripts/leftover-check.sh
@@ -15,7 +15,7 @@ for e in elements/*; do
 done
 
 if [[ $LEFTOVERS > 0 ]]; then
-  echo -e "\n$([ $LEFTOVERS == 1 ] && echo "This element" || echo "These elements") were likely created on another branch, but do not exist in your current branch.  Git did not delete $([ $LEFTOVERS == 1 ] && echo "it" || echo "them") when you switched branches because they contain a \`node_modules\` directory which is not tracked by git.  Feel free to delete them if they're bothering you."
+  echo -e "\n$([ $LEFTOVERS == 1 ] && echo "This element" || echo "These elements") were likely created on another branch, but do not exist in your current branch.  Git did not delete $([ $LEFTOVERS == 1 ] && echo "it" || echo "them") when you switched branches because they contain a \`node_modules\` directory which is not tracked by git.  They can be safely deleted if they are causing you any issues."
 fi
 
 exit 0 # always exist 0 to prevent npm from printing a giant error blurb that takes attention away from these warnings

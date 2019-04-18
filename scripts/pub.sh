013a9f31685f1932ebb96599e30cb1bdb5e02c90
Michael Clayton <mclayton@redhat.com>
2019-04-18 15:10:43 -0400

    update draft of publish script

--- scripts/pub.sh
+++ scripts/pub.sh
@@ -7,12 +7,12 @@
 PRERELEASE_PREFIX="prerelease"
 OLD_VERSION=""
 NEW_VERSION=""
+TAG_NAME=""
+RELEASE_BRANCH=""
 
-# 1. go to the root dir
 checkDir() {
   if [[ -f ./package.json ]]; then
     if grep '"@patternfly/patternfly-elements"' package.json >/dev/null; then
-      echo "correct dir"
       return
     fi
   fi
@@ -20,40 +20,84 @@ checkDir() {
   echo "Error: publish must be run from the root directory.  Use \`npm run publish-elements\`"
   exit 1
 }
-# 2. make sure working dir is completely clean and up to date with origin
+
 cleanWorkspace() {
-  echo 'hi'
-  git status --untracked-files=no --porcelain
+  git status --untracked-files=no --porcelain || exit 1
 }
-# 3. git checkout master
-resetMaster() {
-  git checkout master
-  git pull origin master
+
+checkoutMaster() {
+  git checkout master || exit 1
+  git pull origin master || exit 1
 
 }
-# 4. npm run lerna version -- --no-git-tag-version --no-push --preid prerelease
+
 #    - Choose the appropriate version bump type for the release you’re publishing.
 #    - if bumping a prerelease version (example: from 1.0.0-prerelease.2 to 1.0.0-prerelease.3), choose Custom Prerelease
 bumpVersion() {
-  npm run lerna version -- --no-git-tag-version --no-push --preid="$PRERELEASE_PREFIX"
+  OLD_VERSION=$(node -e "console.log(require('./lerna.json').version)")
+  ./node_modules/.bin/lerna version -- --no-git-tag-version --no-push --preid="$PRERELEASE_PREFIX" || exit 1
+  NEW_VERSION=$(node -e "console.log(require('./lerna.json').version)")
 }
-# 5. create a branch named “release/$NEW_VERSION” (example: “release/1.0.0-prerelease.3”)
-# 6. npm install && npm run build && git add elements/*/*.{js,map,css} -f
-# 7. git commit -am “$NEW_VERSION”
-# 8. git tag “v$NEW_VERSION”
-# 9. Remove the bundle files with this monstrosity:
-#    - for e in elements/*; do find $e -maxdepth 1 \( -not -name "gulpfile.js" -not -name "rollup.config.js" \) \( -name "*.js" -or -name "*.css" -or -name "*.map" \) -exec git rm -f {} \; ;done
-# 10. git commit -am “remove bundles from $NEW_VERSION”
-# 11. git push origin release/$NEW_VERSION -u
-# 12. git push --tags
-# 13. git checkout master
-# 14. git reset --hard origin/master
-# 15. create a PR for the branch you just created
-# 16. delete branch after merging PR
-# 17. To publish to npm:
-#    - git checkout v$NEW_VERSION
-#    - npm run lerna publish from-git
+
+createBranch() {
+  TAG_NAME="v$NEW_VERSION"
+  RELEASE_BRANCH="release/$TAG_NAME"
+  git co -b $RELEASE_BRANCH || exit 1
+}
+
+npmInstall() {
+  npm install || exit 1
+}
+
+commitIgnoredFiles() {
+  git add elements/*/*.{js,map,css} -f || exit 1
+  git commit -am "v$NEW_VERSION" || exit 1
+}
+
+gitTag() {
+  git tag $TAG_NAME || exit 1
+}
+
+removeIgnoredFiles() {
+   for e in elements/*; do find $e -maxdepth 1 \( -not -name "gulpfile.js" -not -name "rollup.config.js" \) \( -name "*.js" -or -name "*.css" -or -name "*.map" \) -exec git rm -f {} \; ;done
+   git commit -am "remove bundles from $NEW_VERSION" || exit 1
+}
+
+pushToOrigin() {
+  git push origin $RELEASE_BRANCH -u || exit 1
+  git push --tags || exit 1
+}
+
+resetMaster() {
+  git checkout master || exit 1
+  git reset --hard origin/master || exit 1
+}
+
+npmPublish() {
+  git checkout $TAG_NAME
+  # npm run lerna publish from-git
+  echo 'lerna publish to npm'
+}
+
+goodbye() {
+  echo "Returning you to the master branch."
+  git checkout master
+  echo " 15. create a PR for the branch you just created"
+  echo " 16. delete branch after merging PR"
+}
+
 
 
 checkDir
 cleanWorkspace
+#checkoutMaster
+bumpVersion
+createBranch
+npmInstall
+commitIgnoredFiles
+gitTag
+removeIgnoredFiles
+pushToOrigin
+resetMaster
+npmPublish
+goodbye

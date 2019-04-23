201e78f9f92cbb544642db99d666906e3310f3dd
Michael Clayton <mclayton@redhat.com>
2019-04-23 09:48:51 -0400

    add friendly logging to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,9 +17,15 @@ ctrl_c() {
   exit 1
 }
 
+log() {
+  echo "[RELEASE] $1"
+}
+
 checkDir() {
+  log "verifying CWD is monorepo root"
   if [[ -f ./package.json ]]; then
     if grep '"@patternfly/patternfly-elements"' package.json >/dev/null; then
+      log "CWD is monorepo root"
       return
     fi
   fi
@@ -29,55 +35,67 @@ checkDir() {
 }
 
 cleanWorkspace() {
+  log "verifying clean workspace"
   git status --untracked-files=no --porcelain > /dev/null || exit 1
 }
 
 checkoutMaster() {
+  log "checkout master branch & pull"
   git checkout master || exit 1
   git pull origin master || exit 1
 }
 
 bumpVersion() {
+  log "bumping version"
   OLD_VERSION=$(node -e "console.log(require('./lerna.json').version)")
   ./node_modules/.bin/lerna version --no-git-tag-version --no-push --preid="$PRERELEASE_PREFIX" || exit 1
   NEW_VERSION=$(node -e "console.log(require('./lerna.json').version)")
 }
 
 createBranch() {
+  log "creating release branch with new version numbers"
   TAG_NAME="v$NEW_VERSION"
   RELEASE_BRANCH="release/$TAG_NAME"
   git co -b $RELEASE_BRANCH || exit 1
 }
 
 npmInstall() {
+  log "installing NPM dependencies"
   npm ci || exit 1
 }
 
 commitIgnoredFiles() {
+  log "committing compiled bundles to release branch"
   git add elements/*/*.{js,map,css} -f || exit 1
   git commit -am "v$NEW_VERSION" || exit 1
 }
 
 gitTag() {
+  log "creating a git tag"
   git tag $TAG_NAME || exit 1
 }
 
 removeIgnoredFiles() {
+  log "removing the compiled bundles from release branch (they should only exist in the tag)"
    for e in elements/*; do find $e -maxdepth 1 \( -not -name "gulpfile.js" -not -name "rollup.config.js" \) \( -name "*.js" -or -name "*.css" -or -name "*.map" \) -exec git rm -f {} \; ;done
    git commit -am "remove bundles from $NEW_VERSION" || exit 1
 }
 
 pushToOrigin() {
+  log "pushing release branch to origin"
   git push origin $RELEASE_BRANCH -u || exit 1
+  log "pushing tags to origin"
   git push --tags || exit 1
 }
 
 resetMaster() {
+  log "resetting master branch to origin/master"
   git checkout master || exit 1
   git reset --hard origin/master || exit 1
 }
 
 npmPublish() {
+  log "publishing newly versioned elements to NPM"
   git checkout $TAG_NAME || exit 1
   npm run lerna publish from-git || exit 1
   git checkout .
@@ -85,22 +103,22 @@ npmPublish() {
 
 handlePR() {
   if command -v hub > /dev/null; then
-    echo "Hub installation found, creating a PR."
+    log "Hub installation found, creating a PR."
     git checkout $RELEASE_BRANCH
     hub pull-request --browse --message "version bumps from release $RELEASE_BRANCH"
   else
-    echo
-    echo "FINAL STEP:"
-    echo "Follow this link to create a pull request, merging the release branch ($RELEASE_BRANCH) into master."
-    echo
-    echo "  https://github.com/patternfly/patternfly-elements/compare/$RELEASE_BRANCH?expand=1"
-    echo
-    echo "Note, if you install Hub (https://hub.github.com/) then this step will be automated in the future."
+    log
+    log "FINAL STEP:"
+    log "Follow this link to create a pull request, merging the release branch ($RELEASE_BRANCH) into master."
+    log
+    log "  https://github.com/patternfly/patternfly-elements/compare/$RELEASE_BRANCH?expand=1"
+    log
+    log "Note, if you install Hub (https://hub.github.com/) then this step will be automated in the future."
   fi
 }
 
 goodbye() {
-  echo "Returning you to the master branch."
+  log "Returning you to the master branch."
   git checkout master
 }
 

d4d7cf21bd46fd96d3c8f6714145b9e8c962d59f
Daniel Faucette <dfaucett@redhat.com>
2019-07-12 15:42:54 -0400

    added some init logic

--- elements/pfe-progress-indicator/src/pfe-progress-indicator.js
+++ elements/pfe-progress-indicator/src/pfe-progress-indicator.js
@@ -19,13 +19,32 @@ class PfeProgressIndicator extends PFElement {
 
   constructor() {
     super(PfeProgressIndicator);
+    this._init = this._init.bind(this);
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // }
+  connectedCallback() {
+    super.connectedCallback();
+    this._slot = this.shadowRoot.querySelector("slot");
+    this._slot.addEventListener("slotchange", this._init);
+  }
 
-  // disconnectedCallback() {}
+  disconnectedCallback() {
+    this._slot.removeEventListener("slotchange", this._init);
+  }
+
+  _init() {
+    const firstChild = this.children[0];
+
+    if (!firstChild) {
+      console.warn(
+        `${
+        PfeProgressIndicator.tag
+        }: You do not have a backup loading message.`
+      );
+    } else {
+      // do nothing
+    }
+  }
 
   // attributeChangedCallback(attr, oldValue, newValue) {}
 }

323f84ab018e44d3a81e710574217ebafb576637
Daniel Faucette <dfaucett@redhat.com>
2019-08-06 16:23:49 -0400

    updated story file

--- elements/pfe-progress-indicator/demo/pfe-progress-indicator.story.js
+++ elements/pfe-progress-indicator/demo/pfe-progress-indicator.story.js
@@ -1,11 +1,35 @@
 import { storiesOf } from "@storybook/polymer";
-import "../pfe-progress-indicator";
+import * as storybookBridge from "@storybook/addon-knobs/polymer";
+import * as tools from "../../../.storybook/utils.js";
 
-storiesOf("Progress", module).add(
-  "pfe-progress-indicator",
-  () => `
-  <pfe-progress-indicator>
-    Progress
-  </pfe-progress-indicator>
-  `
-);
+import PfeProgressIndicator from "../pfe-progress-indicator.js";
+
+const stories = storiesOf("Progress Indicator", module);
+
+stories.addDecorator(storybookBridge.withKnobs);
+
+const template = (data = {}) =>
+  tools.component(PfeProgressIndicator.tag, data.prop, data.slots);
+
+stories.add(PfeProgressIndicator.tag, () => {
+  let config = {};
+
+  const slots = PfeProgressIndicator.slots;
+
+  // Build the knobs and read in their selections
+  config.prop = tools.autoPropKnobs(props, storybookBridge);
+
+  // Set a default value for the user-entered content
+  slots.content.default = "My loading message...";
+
+  // Build the knobs and read in their selections
+  config.has = tools.autoContentKnobs(slots, storybookBridge);
+
+  config.slots = [{
+    content: config.has.content
+  }];
+
+  let rendered = template(config);
+
+  return tools.preview(rendered);
+});

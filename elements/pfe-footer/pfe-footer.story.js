482894ff43a70845af04390bcb05c67bdc17e9bd
Kyle Buchanan <kylebuch8@gmail.com>
2019-09-09 13:05:30 -0400

    v1.0.0-prerelease.24

---
+++ elements/pfe-footer/pfe-footer.story.js
@@ -0,0 +1,11 @@
+import { storiesOf } from "@storybook/polymer";
+import "../pfe-footer";
+
+storiesOf("Footer", module).add(
+  "pfe-footer",
+  () => `
+  <pfe-footer>
+    Footer
+  </pfe-footer>
+  `
+);

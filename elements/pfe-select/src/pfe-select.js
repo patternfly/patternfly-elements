d525810962d23f16b1da1a30cad2258c5cede0a6
Diwanshi Pandey <dpandey@redhat.com>
2019-09-25 16:18:41 -0400

    Implements pfe-select with JS options

--- elements/pfe-select/src/pfe-select.js
+++ elements/pfe-select/src/pfe-select.js
@@ -13,15 +13,33 @@ class PfeSelect extends PFElement {
     return "pfe-select.scss";
   }
 
+  get pfeOptions() {
+    return this._pfeOptions;
+  }
+
+  set pfeOptions(options) {
+    this._pfeOptions = options.filter(el => el.selected).length > 1 ? this.handleMultipleSelectedValues(options) : options;
+    this.modifyDOM();
+  }
+
+  get pfeInvalid() {
+    return this.getAttribute('pfe-invalid');
+  }
+
+  set pfeInvalid(invalidAttr) {
+    if (!invalidAttr) {
+      return;
+    }
+    this.querySelector('select').setAttribute('aria-invalid', invalidAttr);
+  }
+
   static get observedAttributes() {
-    return [
-      "pfe-invalid"
-    ];
+    return ["pfe-invalid"];
   }
 
   constructor() {
     super(PfeSelect);
-
+    this._pfeOptions;
     this._init = this._init.bind(this);
     this._inputChanged = this._inputChanged.bind(this);
 
@@ -29,17 +47,19 @@ class PfeSelect extends PFElement {
   }
 
   connectedCallback() {
-    super.connectedCallback();
-
-    if (this.children.length) {
+    super.connectedCallback();    
+    if (this._pfeOptions) {
+      this.modifyDOM()
       this._init();
+    } else {
+      if (this.children.length) {
+        this._init();
+      }
     }
-
+    
     this.observer.observe(this, { childList: true });
-
-    let invalidAttr = this.getAttribute('pfe-invalid');
-    if (invalidAttr) {
-      this.querySelector('select').setAttribute('aria-invalid', invalidAttr);
+    if (this.pfeInvalid) {
+      this.pfeInvalid = this.pfeInvalid;
     }
     
   }
@@ -56,11 +76,50 @@ class PfeSelect extends PFElement {
       if (typeof observer === "function") observer(attr, oldValue, newValue);
     }
 
-    let invalidAttr = this.getAttribute('pfe-invalid');
-    if (this.getAttribute('pfe-invalid')) {
-      this.querySelector('select').setAttribute('aria-invalid', invalidAttr);
+    if (this.pfeInvalid) {
+      this.pfeInvalid = this.pfeInvalid;
     }
+  }
 
+  disconnectedCallback() {
+    this.observer.disconnect();
+    this._input.removeEventListener("input", this._inputChanged);
+  }
+
+  addOptions(options) {
+    // Reset the pfeOptions by concatenating newly added options with _pfeOptions
+    this.pfeOptions = this._pfeOptions.concat(options);
+  }
+
+  handleMultipleSelectedValues(options) {
+    // Warn if options array has more than one selected value set as true
+    console.warn("The first 'selected' option will take precedence over others incase of multiple 'selected' options");
+    // Get the index of the first element with selected "true"
+    const firstIndex = options.findIndex(el => el.selected);
+    // Update the options array with precedence to first element with selected value as true
+    return options.map((el, idx) => {
+      el.selected = firstIndex == idx;
+      return el;
+    });
+  }
+
+  modifyDOM() {
+    // Create select element
+    let pfeSelect = document.createElement('select');
+    pfeSelect.setAttribute('slot', 'pfe-select');
+    // Create option element for each element in _pfeOptions array
+    this._pfeOptions.map(el => {
+      const option = Object.assign(document.createElement('option') , el);      
+      pfeSelect.add(option, null);      
+    });
+    // if select already exists in the DOM then replace the old select with the new _pfeOptions array
+    if (this.children.length) {
+      const select = this.querySelector('select');
+      select.parentNode.replaceChild(pfeSelect, select);
+    } else {
+      // Otherwise create a new select element
+      this.appendChild(pfeSelect);
+    }
   }
 
   _init() {
@@ -69,7 +128,6 @@ class PfeSelect extends PFElement {
       console.warn("The first child needs to be a select element");
       return;
     }
-
     this._input.addEventListener("input", this._inputChanged);
   }
 
@@ -81,10 +139,6 @@ class PfeSelect extends PFElement {
     }));
   }
 
-  disconnectedCallback() {
-    this.observer.disconnect();
-    this._input.removeEventListener("input", this._inputChanged);
-  }
 }
 
 PFElement.create(PfeSelect);

bd0149f2ba436c9cd045b2b587a6dfbdcbf24b3b
Ivana Rodriguez <ivrodrig@redhat.com>
2019-10-24 16:02:49 -0500

    Add open, close, and toggle functions

--- elements/pfe-toast/src/pfe-toast.js
+++ elements/pfe-toast/src/pfe-toast.js
@@ -9,25 +9,95 @@ class PfeToast extends PFElement {
     return "pfe-toast.html";
   }
 
+  // get schemaUrl() {
+  //   return "pfe-toast.json"; //TO DO
+  // }
+
   get styleUrl() {
     return "pfe-toast.scss";
   }
 
-  // static get observedAttributes() {
-  //   return [];
-  // }
+  static get PfeType() {
+    return PFElement.PfeTypes.Container;
+  }
+
+  static get observedAttributes() {
+    return [];
+  }
 
   constructor() {
     super(PfeToast);
+    // state
+    this.isOpen = false;
+
+    // elements
+    this._container = this.shadowRoot.querySelector(`.${this.tag}__container`);
+    this._toastCloseButton = this.shadowRoot.querySelector(`.${this.tag}__close`);
+    
+    // events
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // }
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldVal, newVal);
+  }
 
-  // disconnectedCallback() {}
+  connectedCallback() {
+    super.connectedCallback();
 
-  // attributeChangedCallback(attr, oldValue, newValue) {}
+    // attach listeners
+    this._toastCloseButton.addEventListener("click", this.close);
+  }
+
+  disconnectedCallback() {
+    this._toastCloseButton.removeEventListener("click", this.close);
+  }
+
+  open(event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.isOpen = true;
+    this.setAttribute("role", "alert");
+    this.removeAttribute("hidden");
+    setTimeout(() => {
+      this.style.right = "25px";
+    }, 500);
+     // to update
+    this.dispatchEvent(
+      new CustomEvent(`${this.tag}:open`, {
+        detail: {},
+        bubbles: true
+      })
+    );
+    return this;
+  }
+
+  close(event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.isOpen = false;
+    this.removeAttribute("role");
+    this.style.right = `-${this.getBoundingClientRect().width + 25}px`; // to update
+    setTimeout(() => {
+      this.setAttribute("hidden", true);
+    }, 500);
+    this.dispatchEvent(
+      new CustomEvent(`${this.tag}:close`, {
+        detail: {},
+        bubbles: true
+      })
+    );
+    return this;
+  }
+
+  toggle(event) {
+    this.isOpen ? this.close(event) : this.open(event);
+    return this;
+  }
 }
 
 PFElement.create(PfeToast);

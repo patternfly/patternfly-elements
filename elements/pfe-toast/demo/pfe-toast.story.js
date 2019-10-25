c4218ce36b110e6ea1f586bb11982aa80a9ed322
Ivana Rodriguez <ivrodrig@redhat.com>
2019-10-25 11:49:23 -0500

    Update storybook

--- elements/pfe-toast/demo/pfe-toast.story.js
+++ elements/pfe-toast/demo/pfe-toast.story.js
@@ -1,11 +1,30 @@
 import { storiesOf } from "@storybook/polymer";
-import "../pfe-toast";
+import * as storybookBridge from "@storybook/addon-knobs/polymer";
+import * as tools from "../../../.storybook/utils.js";
+import PfeToast from "../pfe-toast";
 
-storiesOf("Toast", module).add(
-  "pfe-toast",
-  () => `
-  <pfe-toast>
-    Toast
-  </pfe-toast>
-  `
-);
+const stories = storiesOf("Toast", module);
+
+// Add the readme
+import readme from "../README.md";
+stories.addParameters({
+  notes: {
+    markdown: readme
+  }
+});
+
+// Define the template to be used
+const template = (data = {}) => {
+  return tools.component(PfeToast.tag, data.prop, data.slots);
+};
+
+stories.addDecorator(storybookBridge.withKnobs);
+
+stories.add(PfeToast.tag, () => {
+  let config = {};
+  config.slots = [{
+    content: "Get toasted!"
+  }];
+  const render = template(config);
+  return tools.preview(render);
+});

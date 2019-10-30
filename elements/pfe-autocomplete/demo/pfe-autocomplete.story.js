347ce10bd94d4e1f26d087b015567d6549928b9e
Kyle Buchanan <kylebuch8@gmail.com>
2019-10-30 09:45:05 -0400

    updating pfe-autocomplete story

--- elements/pfe-autocomplete/demo/pfe-autocomplete.story.js
+++ elements/pfe-autocomplete/demo/pfe-autocomplete.story.js
@@ -1,14 +1,11 @@
 import { storiesOf } from "@storybook/polymer";
-<<<<<<< HEAD
 import {
   withKnobs,
   text,
   select,
   boolean
 } from "@storybook/addon-knobs/polymer";
-import addons from '@storybook/addons';
 import * as storybookBridge from "@storybook/addon-knobs/polymer";
-import CoreEvents from '@storybook/core-events';
 import PfeAutocomplete from "../dist/pfe-autocomplete";
 import * as tools from "../../../.storybook/utils.js";
 
@@ -42,22 +39,16 @@ stories.addParameters({
 });
 
 stories.addDecorator(withKnobs);
-stories.addDecorator(story => {
-    addons.getChannel().emit(CoreEvents.REGISTER_SUBSCRIPTION, subscribe);
-    return story();
-  });
 
 function subscribe() {
   setTimeout(() => {
-    const staticAutocomplete = document.querySelector("#static");
+    const staticAutocomplete = document.querySelector("pfe-autocomplete");
 
     staticAutocomplete.autocompleteRequest = function(params, callback) {
       const regx = new RegExp("\^" + params.query, "i");
       callback(items.filter(item => regx.test(item)));
     };
   }, 0);
-
-  return function unsubscribe() {};
 }
 
 stories.add("pfe-autocomplete", () => {
@@ -87,6 +78,9 @@ stories.add("pfe-autocomplete", () => {
 
   let preview = tools.code(pfeAutocompleteMarkup);
 
+  // this hooks everything back up when the knobs change
+  subscribe();
+
   return `
     ${pfeAutocompleteMarkup}
     ${preview}
@@ -95,50 +89,4 @@ stories.add("pfe-autocomplete", () => {
       ${items.map(item => `<li>${item}</li>`).join("")}
     </ul>
   `;
-=======
-import * as storybookBridge from "@storybook/addon-knobs/polymer";
-import * as tools from "../../../.storybook/utils.js";
-
-import PfeAutocomplete from "../pfe-autocomplete";
-
-const stories = storiesOf("Autocomplete", module);
-
-// Define the template to be used
-const template = (data = {}) => {
-  return tools.component(PfeAutocomplete.tag, data.prop, data.slots);
-};
-
-stories.addDecorator(storybookBridge.withKnobs);
-
-stories.add(PfeAutocomplete.tag, () => {
-    let config = {};
-
-    const props = {
-        "is-disabled": {
-            title: "Disabled",
-            type: "boolean",
-            default: false
-        },
-        "init-value": {
-            title: "Initial value",
-            type: "string"
-        }
-    };
-
-    // Trigger the auto generation of the knobs for attributes
-    config.prop = tools.autoPropKnobs(props, storybookBridge);
-
-    config.slots = [{
-        content: tools.customTag({
-            tag: "input",
-            attributes: {
-                placeholder: "Enter your search term"
-            }
-        })
-    }];
-
-    const render = template(config);
-    const output = tools.preview(render);
-    return output;
->>>>>>> issue-325/css-enhancement-js-bugfix
 });

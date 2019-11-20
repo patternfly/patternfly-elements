dac632adb5ed0d62a248e380a777cbed435078e2
Christopher Doherty <cdoherty@redhat.com>
2019-11-20 17:04:34 -0500

    US234004 fixed accordion caret size in edge, removed overflow scroll on body.

--- elements/pfe-navigation/src/pfe-navigation.js
+++ elements/pfe-navigation/src/pfe-navigation.js
@@ -152,58 +152,19 @@ class PfeNavigation extends PFElement {
     return "pfe-navigation.json";
   }
 
-  // get stuck() {
-  //   return this.classList.contains("pfe-sticky");
-  // }
-
-  // set stuck(state) {
-  //   if(state) {
-  //     this.classList.add("pfe-sticky");
-  //   } else {
-  //     this.classList.remove("pfe-sticky");
-  //   }
-  // }
-
   get overlay() {
     return !this._overlay.hasAttribute("hidden");
   }
 
   set overlay(state) {
-    // const parent = this.parentElement;
-    // const hasScrollbar = this.scrollbarWidth > 0;
     if (state) {
-      // this.setAttribute("expanded", true);
       // Add the overlay to the page
       this._overlay.removeAttribute("hidden");
       this._wrapper.setAttribute("expanded", "");
-      // This prevents background scroll while nav is open
-      document.body.style.overflowY = "hidden";
-      // Lock the background from scrolling
-      // this.parentElement.style.overflow = "hidden";
-      // this.parentElement.style.paddingRight = this.scrollbarWidth + "px";
-      // if (this.connected) this._wrapper.style.paddingRight = this.scrollbarWidth + "px";
-      // parent.style.overflow = "hidden";
-      // parent.style.position = "relative";
-      // parent.style.paddingRight = this.scrollbarWidth + "px";
-      // if (this.connected && hasScrollbar) {
-      //   this._wrapper.style.paddingRight = this.scrollbarWidth + "px";
-      // }
     } else {
-      // this.removeAttribute("expanded");
       // Remove the overlay from the page
       this._overlay.setAttribute("hidden", "");
       this._wrapper.removeAttribute("expanded");
-      // Allow background to scroll again
-      document.body.style.overflowY = "auto";
-      // Release the background for scrolling
-      // this.parentElement.style.overflow = null;
-      // this.parentElement.style.paddingRight = null;
-      // if (this.connected) this._wrapper.style.paddingRight = null;
-      // parent.style.overflow = null;
-      // parent.style.paddingRight = null;
-      // if (this.connected && hasScrollbar) {
-      //   this._wrapper.style.paddingRight = null;
-      // }
     }
   }
 

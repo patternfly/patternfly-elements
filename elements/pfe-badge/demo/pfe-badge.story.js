b66790b8836587a0efe61ea38bfc49d961889172
Diwanshi Pandey <diwanshipandey@gmail.com>
2019-12-04 21:21:43 +0530

    updating the storybook for pfe-badge

--- elements/pfe-badge/demo/pfe-badge.story.js
+++ elements/pfe-badge/demo/pfe-badge.story.js
@@ -1,11 +1,100 @@
 import { storiesOf } from "@storybook/polymer";
+import * as storybookBridge from "@storybook/addon-knobs/polymer";
+import { escapeHTML } from "../../../.storybook/utils.js";
+
 import  PfeBadge from "../dist/pfe-badge";
 
-storiesOf("Badge", module).add(
-  PfeBadge.tag,
-  () => `
-  <pfe-badge>
-    Badge
-  </pfe-badge>
-  `
-);
+const stories = storiesOf("Badge", module);
+
+// Add the readme
+import readme from "../README.md";
+stories.addParameters({
+  notes: {
+    markdown: readme
+  }
+});
+
+stories.addDecorator(storybookBridge.withKnobs);
+
+stories.add(PfeBadge.tag, () => {
+  const number = storybookBridge.number("number", 50);
+  const state = storybookBridge.select("pfe-state", ["moderate", "important", "critical", "success", "info"], "");
+  const threshold = storybookBridge.number("pfe-threshold", 100);
+
+  const staticTextExamples = [
+    {
+      text: "default",
+      state: ""
+    },
+    {
+      text: "info",
+      state: "info"
+    },
+    {
+      text: "important",
+      state: "important"
+    },
+    {
+      text: "success",
+      state: "success"
+    },
+    {
+      text: "critical",
+      state: "critical"
+    },
+    {
+      text: "moderate",
+      state: "moderate"
+    },
+  ];
+
+  const staticNumberExamples = [
+    {
+      number: 1,
+      state: "info",
+      threshold: null
+    },
+    {
+      number: 17,
+      state: "info",
+      threshold: 10
+    },
+    {
+      number: 900,
+      state: "info",
+      threshold: 100
+    },
+  ];
+
+  const customBadge = `<pfe-badge pfe-state="${state}" pfe-threshold="${threshold}" number="${number}">${number}</pfe-badge>`;
+
+  return `
+    <h1>Dynamic example</h1>
+    <h2>Use knobs to customize this badge!</h2>
+
+    ${customBadge}
+
+    <section>
+      <h2>Markup</h2>
+      <pre><code>${escapeHTML(customBadge)}</code>
+      </pre>
+    </section>
+    <h1>Static examples with numbers</h1>
+    ${staticNumberExamples
+      .map(
+        ex => `
+          <pfe-badge pfe-state="${ex.state}" number="${ex.number}" pfe-threshold="${ex.threshold}"></pfe-badge>
+        `
+      )
+      .join("\n")}
+
+    <h1>Static examples with text and color options</h1>
+    ${staticTextExamples
+      .map(
+        ex => `
+          <pfe-badge pfe-state="${ex.state}" text="${ex.text}"></pfe-badge>
+        `
+      )
+      .join("\n")}
+  `;
+});

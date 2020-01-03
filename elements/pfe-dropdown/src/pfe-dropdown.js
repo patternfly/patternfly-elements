bb9baefb784c4f65c10190cefcf4d23b68bdbf35
Ivana Rodriguez <ivrodrig@redhat.com>
2020-01-03 13:20:19 -0600

    Add basic toggle feature

--- elements/pfe-dropdown/src/pfe-dropdown.js
+++ elements/pfe-dropdown/src/pfe-dropdown.js
@@ -1,4 +1,4 @@
-import PFElement from "../pfelement/pfelement.js";
+import PFElement from "../../pfelement/dist/pfelement.js";
 
 class PfeDropdown extends PFElement {
   static get tag() {
@@ -19,15 +19,62 @@ class PfeDropdown extends PFElement {
 
   constructor() {
     super(PfeDropdown);
+
+    this.button = this.querySelector('button');
+    this.list = this.querySelector('ul');
+    // state
+    this.isOpen = false;
+
+    // events
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+    this.toggle = this.toggle.bind(this); 
   }
 
-  // connectedCallback() {
-  //   super.connectedCallback();
-  // }
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.button.addEventListener("click", this.toggle);
+  }
 
   // disconnectedCallback() {}
 
   // attributeChangedCallback(attr, oldValue, newValue) {}
+
+  open(event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.isOpen = true;
+    this.list.classList.add("open");
+    this.dispatchEvent(
+      new CustomEvent(`${this.tag}:open`, {
+        detail: {},
+        bubbles: true
+      })
+    );
+    return this;
+  }
+
+  close(event) {
+    if (event) {
+      event.preventDefault();
+    }
+    this.isOpen = false;
+    this.list.classList.remove("open");
+    this.dispatchEvent(
+      new CustomEvent(`${this.tag}:close`, {
+        detail: {},
+        bubbles: true
+      })
+    );
+    return this;
+  }
+
+  toggle(event) {
+    this.isOpen ? this.close(event) : this.open(event);
+    return this;
+  }
 }
 
 PFElement.create(PfeDropdown);

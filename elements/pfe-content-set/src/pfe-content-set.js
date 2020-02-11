2e70ddfbfd2cadb31dcda8ee27f2bfb4516ce5a5
Kyle Buchanan <kylebuch8@gmail.com>
2020-02-11 11:37:56 -0500

    Using context_update instead of manually adding the "on" attribute

--- elements/pfe-content-set/src/pfe-content-set.js
+++ elements/pfe-content-set/src/pfe-content-set.js
@@ -55,10 +55,6 @@ class PfeContentSet extends PFElement {
       this._observer.disconnect();
     }
 
-    if (this.hasAttribute("on")) {
-      this.on.value = this.getAttribute("on");
-    }
-
     if (this.isTab) {
       this._buildTabs();
     } else {
@@ -66,6 +62,7 @@ class PfeContentSet extends PFElement {
     }
 
     this.render();
+    this.context_update();
 
     if (window.ShadyCSS) {
       setTimeout(() => {
@@ -101,11 +98,6 @@ class PfeContentSet extends PFElement {
 
     if (!existingAccordion) {
       fragment.appendChild(accordion);
-    }
-
-    // Pass the theme property down to the accordion component
-    if (this.on) {
-      accordion.setAttribute("on", this.on.value);
     }
 
     if (!existingAccordion) {
@@ -154,11 +146,6 @@ class PfeContentSet extends PFElement {
     // Pass the variant attribute down to the tabs component
     if (this.variant.value !== this.variant.default) {
       tabs.setAttribute("pfe-variant", this.variant.value);
-    }
-
-    // Pass the theme property down to the tabs component
-    if (this.on) {
-      tabs.setAttribute("on", this.on.value);
     }
 
     if (this.align.value) {

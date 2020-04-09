6cb150f15b91b264ba0d108f027c60f341efd3ce
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2020-04-09 17:08:52 +0100

    initial commit wip

--- elements/pfe-codeblock/src/pfe-codeblock.js
+++ elements/pfe-codeblock/src/pfe-codeblock.js
@@ -1,4 +1,5 @@
 import PFElement from "../../pfelement/dist/pfelement.js";
+import Prism from "prismjs";
 
 class PfeCodeblock extends PFElement {
   static get tag() {
@@ -17,11 +18,25 @@ class PfeCodeblock extends PFElement {
     return "pfe-codeblock.scss";
   }
 
-  static get events() {
-    return {
-      change: `${this.tag}:change`,
-      click: `${this.tag}:click`
-    };
+  get codeLanguage() {
+    return this.getAttribute("pfe-language") || "base";
+  }
+
+  static get observedAttributes() {
+    return ["pfe-language"];
+  }
+
+  get codeblock() {
+    return this._codeblock;
+  }
+
+  set codeblock(text) {
+    if (!text) {
+      return;
+    }
+
+    this._codeblock = text;
+    this.renderCodeblock();
   }
 
   // Declare the type of this component
@@ -29,36 +44,87 @@ class PfeCodeblock extends PFElement {
     return PFElement.PfeTypes.Content;
   }
 
-  // static get observedAttributes() {
-  //   return [];
-  // }
+  static get observedAttributes() {
+    return [];
+  }
 
   constructor() {
+    console.log("constructor");
     super(PfeCodeblock, { type: PfeCodeblock.PfeType });
+
+    this._codeblock = null;
+    this._codeblockRender = null;
+    this._codeblockContainer = null;
+    this._observerConfig = { childList: true, subtree: true };
+    this._readyStateChangeHandler = this._readyStateChangeHandler.bind(this);
+
+    this.observer = new MutationObserver((mutationList, observer) => {
+      if (!this._codeblockContainer.textContent) {
+        this._codeblockRender.innerHTML = "";
+        return;
+      }
+
+      // TODO: when we stop supporting IE11, the need to disconnect and
+      // then reconnect will no longer be needed
+      observer.disconnect();
+      this.codeblock = this._codeblockContainer.textContent;
+      this._muationObserve();
+    });
   }
 
   connectedCallback() {
+    console.log("connectedCallback");
     super.connectedCallback();
-    // If you need to initialize any attributes, do that here
 
-    this.addEventListener(PfeCodeblock.events.change, this._changeHandler);
-    this.addEventListener(PfeCodeblock.events.click, this._clickHandler);
+    this._codeblockRender = document.createElement("div");
+    this._codeblockRender.setAttribute("pfe-codeblock-render", "");
+    this.appendChild(this._codeblockRender);
+
+    this.shadowRoot.querySelector("slot").addEventListener("slotchange", () => {
+      if (!this._codeblockContainer) {
+        this._codeblockContainer = this.querySelector(
+          "[pfe-codeblock-container]"
+        );
+        this._codeblockContainer.style.display = "none";
+
+        this._init();
+      }
+    });
   }
 
   disconnectedCallback() {
-    this.removeEventListener(PfeCodeblock.events.change, this._changeHandler);
-    this.removeEventListener(PfeCodeblock.events.click, this._clickHandler);
+    console.log("disconnectedCallback");
+    this.observer.disconnect();
   }
 
-  _changeHandler(event) {
-    this.emitEvent(PfeCodeblock.events.change, {
-      detail: {}
-    });
+  _readyStateChangeHandler(event) {
+    console.log("_readyStateChangeHandler");
+    if (event.target.readyState === "complete") {
+      document.removeEventListener(
+        "readystatechange",
+        this._readyStateChangeHandler
+      );
+      this._init();
+    }
   }
-  _clickHandler(event) {
-    this.emitEvent(PfeCodeblock.events.click, {
-      detail: {}
-    });
+
+  _init() {
+    console.log("_init");
+    if (this._codeblockContainer.textContent) {
+      this._codeblock = this._codeblockContainer.textContent;
+    }
+
+    this._muationObserve();
+  }
+
+  renderCodeblock() {
+    console.log("renderCodeblock");
+    this._codeblockRender.innerHTML = Prism.highlight(this.codeblock);
+  }
+
+  _muationObserve() {
+    console.log("_muationObserve");
+    this.observer.observe(this._codeblockContainer, this._observerConfig);
   }
 }
 

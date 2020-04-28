e1d1f804b6eec74aa2f42179f98e79be6ae0af28
Ivana Rodriguez <ivrodrig@redhat.com>
2020-04-28 11:57:35 -0500

    Add polyfill for closest

--- elements/pfe-dropdown/src/pfe-dropdown.js
+++ elements/pfe-dropdown/src/pfe-dropdown.js
@@ -1,6 +1,19 @@
 import PFElement from "../../pfelement/dist/pfelement.js";
 import PfeDropdownItem from "./pfe-dropdown-item.js";
 
+// @POLYFILL  Element.closest
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
 const KEYCODE = {
   DOWN: 40,
   END: 35,

5fbe8c013ae734a9403de1a2d98569dc3ee5a8cd
[ Cassondra ] <carobert@redhat.com>
2020-04-28 23:28:55 -0400

    feat Add a dummy schema and work through dynamic generation

--- elements/pfe-schema-editor/src/pfe-schema-editor.js
+++ elements/pfe-schema-editor/src/pfe-schema-editor.js
@@ -34,13 +34,41 @@ class PfeSchemaEditor extends PFElement {
   //   return [];
   // }
 
+  loadSchema(input) {
+    if (
+      typeof input === "object" &&
+      input.$schema.startsWith("http://json-schema.org")
+    ) {
+      this.schema = input;
+
+      this.emitEvent(PfeSchemaEditor.events.change, {
+        detail: {}
+      });
+    } else {
+      // @TODO :)
+      this.warn("Tis not a schema...");
+    }
+  }
+
   constructor() {
     super(PfeSchemaEditor, { type: PfeSchemaEditor.PfeType });
+
+    this.schema = {};
+
+    this._init = this._init.bind(this);
+    this._buildHeader = this._buildHeader.bind(this);
+    this._buildItem = this._buildItem.bind(this);
   }
 
   connectedCallback() {
     super.connectedCallback();
+
+    this._container = this.shadowRoot.querySelector(
+      ".pfe-schema-editor--container"
+    );
+
     // If you need to initialize any attributes, do that here
+    this._init();
 
     this.addEventListener(PfeSchemaEditor.events.change, this._changeHandler);
     this.addEventListener(PfeSchemaEditor.events.export, this._exportHandler);
@@ -57,11 +85,70 @@ class PfeSchemaEditor extends PFElement {
     );
   }
 
-  _changeHandler(event) {
-    this.emitEvent(PfeSchemaEditor.events.change, {
-      detail: {}
+  _buildHeader(template, object) {
+    // Set up the header
+    let field;
+    const header = template.content.cloneNode(true);
+    [header.querySelector(".toggle--header")].forEach(el => {
+      field = el.getAttribute("data-content");
+      el.textContent = object[field];
     });
+
+    return header;
   }
+
+  _buildItem(template, object) {
+    console.log(template);
+    console.log(object);
+
+    // Set up the header
+    let field;
+    const clone = template.content.cloneNode(true);
+
+    // [ header.querySelector(".toggle--header") ].forEach((el) => {
+    //   field = el.getAttribute("data-content");
+    //   el.textContent = object[field];
+    // });
+
+    return clone;
+  }
+
+  _init() {
+    // Parse schema
+    console.log(this.schema);
+
+    if (
+      typeof this.schema === "object" &&
+      Object.keys(this.schema).length > 0
+    ) {
+      this._container.innerHTML = "";
+
+      // Set up the header
+      this._container.appendChild(
+        this._buildHeader(this.shadowRoot.querySelector("#header"), this.schema)
+      );
+
+      // Loop over each item in the properties
+      Object.entries(this.schema.properties).forEach(item => {
+        let props = item[1];
+        let type = props.type;
+        let hidden = props.options && props.options.hidden ? true : false;
+        if (props.format) {
+          type += `-${props.format}`;
+        }
+
+        // Use type to get template
+        if (!hidden) {
+          this._buildItem(this.shadowRoot.querySelector(`#${type}`), props);
+        }
+      });
+    }
+  }
+
+  _changeHandler(event) {
+    this._init();
+  }
+
   _exportHandler(event) {
     this.emitEvent(PfeSchemaEditor.events.export, {
       detail: {}

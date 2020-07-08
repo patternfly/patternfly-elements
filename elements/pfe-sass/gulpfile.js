4179e8d6f4011cf58a866f72a62ca7facad2f14e
Wes Ruvalcaba <wesruv@gmail.com>
2020-07-08 13:24:47 -0400

    docs: #946: Adding sassdock to pfe-sass and adding Sass to editorconfig
    Cleaned up comments and rules for Sassdoc parser
    Made whitespace consistent in Scss files

--- elements/pfe-sass/gulpfile.js
+++ elements/pfe-sass/gulpfile.js
@@ -14,12 +14,12 @@ const path = require("path");
 const clean = require("gulp-clean");
 const mergeStream = require("merge-stream");
 const globSass = require("gulp-sass-globbing");
+const gulpClean = require("gulp-clean");
+const sassdoc = require("sassdoc");
 
 // Delete the temp directory
 task("clean", () => {
-  return src([
-    "__*.scss"
-  ], {
+  return src(["__*.scss"], {
     cwd: paths.compiled,
     read: false,
     allowEmpty: true
@@ -28,33 +28,49 @@ task("clean", () => {
 
 // Custom gulp for sass globbing
 task("sass:globbing", () => {
-    let stream = mergeStream();
-    ["extends", "functions", "maps", "mixins", "variables"].forEach((folder) => {
-        stream.add(src([
-          `${folder}/_*.scss`
-          // `!${folder}/_deprecated*.scss`,
-        ])
-          .pipe(globSass({
+  let stream = mergeStream();
+  ["extends", "functions", "maps", "mixins", "variables"].forEach(folder => {
+    stream.add(
+      src([
+        `${folder}/_*.scss`
+        // `!${folder}/_deprecated*.scss`,
+      ])
+        .pipe(
+          globSass(
+            {
               path: `__${folder}.scss`
-          }, {
-            signature: `// generated with sass globbing, v${version}`
-          }))
-          .pipe(dest(paths.compiled))
-        );
-    });
+            },
+            {
+              signature: `// generated with sass globbing, v${version}`
+            }
+          )
+        )
+        .pipe(dest(paths.compiled))
+    );
+  });
 
-    return stream;
+  return stream;
+});
+
+task("build:sassdoc", () => {
+  return src("**/*.scss").pipe(
+    sassdoc()
+    // {
+    //   'theme': 'neat'
+    // }
+  );
 });
 
 task("build", series("clean", "sass:globbing"));
 
 task("watch", () => {
-  return watch([
-    "{extends,functions,maps,mixins,variables}/_*.scss",
-    "pfe-sass.scss"
-  ], {
-    cwd: paths.compiled
-  }, series("build"));
+  return watch(
+    ["{extends,functions,maps,mixins,variables}/_*.scss", "pfe-sass.scss"],
+    {
+      cwd: paths.compiled
+    },
+    series("build")
+  );
 });
 
 task("dev", parallel("build", "watch"));

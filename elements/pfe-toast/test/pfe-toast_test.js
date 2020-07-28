0b1ddcad22de231e14e79523a96ff024b50959ec
Kyle Buchanan <kylebuch8@gmail.com>
2020-07-28 13:59:19 -0400

    framework tests for select, tabs, and toast

---
+++ elements/pfe-toast/test/pfe-toast_test.js
@@ -0,0 +1,65 @@
+suite('<pfe-toast>', () => {
+  test('it should upgrade', () => {
+    assert.instanceOf(document.querySelector('pfe-toast'), customElements.get("pfe-toast", 'pfe-toast should be an instance of pfeToast'));
+  });
+
+  test('it should be hidden by default', () => {
+    const pfeToast = document.getElementById('test');
+
+    assert.isTrue(pfeToast.hasAttribute('hidden'));
+  });
+
+  test('it should open programmatically', done => {
+    const pfeToast = document.getElementById('test');
+
+    pfeToast.open();
+
+    flush(() => {
+      setTimeout(() => {
+        assert.isFalse(pfeToast.hasAttribute('hidden'));
+        assert.isTrue(pfeToast.classList.contains('open'));
+        done();
+      }, 600);
+    });
+  });
+
+  test('it should close programmatically', done => {
+    const pfeToast = document.getElementById('test');
+  
+    pfeToast.close();
+    
+    flush(() => {
+      setTimeout(() => {
+        assert.isTrue(pfeToast.hasAttribute('hidden'));
+        assert.isFalse(pfeToast.classList.contains('open'));
+        done();
+      }, 600);
+    });
+  });
+
+  test('it should auto-dimiss after the provided time', () => { 
+    const pfeToast = document.getElementById('autoDismiss');
+    const autoDismissDelay = pfeToast.getAttribute('auto-dismiss');
+
+    pfeToast.open();
+
+    flush(() => {
+      setTimeout(() => {
+        assert.equal(autoDismissDelay, "500", "auto-dismiss should have provided value");
+        assert.isTrue(pfeToast.hasAttribute('hidden'));
+        assert.isFalse(pfeToast.classList.contains('open'));
+        done();
+      }, autoDismissDelay * 2);
+    });
+
+  });
+
+  test('it should have the right attributes when auto-dismiss enabled', () => { 
+    // test a11y attributes once finalized (waiting for feedback)
+  });
+
+  test('it should have the right attributes when auto-dismiss disabled', () => { 
+    // test a11y attributes once finalized (waiting for feedback)
+  });
+
+});

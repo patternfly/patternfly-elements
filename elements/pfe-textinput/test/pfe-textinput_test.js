fe4eba1f17f2e4dc18d247dd9b443ffd07585cef
Kyle Buchanan <kylebuch8@gmail.com>
2020-08-21 17:04:13 -0400

    updating pfe-textinput tests

--- elements/pfe-textinput/test/pfe-textinput_test.js
+++ elements/pfe-textinput/test/pfe-textinput_test.js
@@ -9,13 +9,6 @@ suite("<pfe-textinput>", () => {
     );
   });
 
-  test("it should set a tabindex of -1 on the light DOM input so it's not in the tab order", () => {
-    const pfeTextinput = document.querySelector("pfe-textinput");
-    const lightInput = pfeTextinput.querySelector("input");
-
-    assert.equal(lightInput.getAttribute("tabindex"), "-1");
-  });
-
   test("it should add a warning in the console if the pfe-textinput isn't provided any light DOM", () => {
     const spy = sinon.spy(console, 'warn');
     const pfeTextinput = document.createElement("pfe-textinput");
@@ -31,79 +24,7 @@ suite("<pfe-textinput>", () => {
     pfeTextinput.innerHTML = "<h1>Bad light DOM</h1>";
 
     document.body.appendChild(pfeTextinput);
-    sinon.assert.calledWith(spy, 'pfe-textinput: The only child in the light DOM must be a text input tag');
+    sinon.assert.calledWith(spy, 'pfe-textinput: The only child in the light DOM must be an input tag');
     spy.restore();
-  });
-
-  test("it should copy any of the attributes from the light DOM to the shadow DOM", () => {
-    const pfeTextinput = document.querySelector("#attribute-copy");
-    const shadowInput = pfeTextinput.shadowRoot.querySelector("span input");
-
-    assert.equal(shadowInput.getAttribute("type"), "text");
-    assert.equal(shadowInput.getAttribute("placeholder"), "Test");
-    assert.isTrue(shadowInput.hasAttribute("required"));
-    assert.equal(shadowInput.getAttribute("minlength"), "5");
-  });
-
-  test("it should NOT copy any of the attributes in the denyListAttributes array", done => {
-    const pfeTextinput = document.querySelector("#deny-list");
-    const lightInput = pfeTextinput.querySelector("input");
-    const shadowInput = pfeTextinput.shadowRoot.querySelector("span input");
-
-    assert.isFalse(shadowInput.hasAttribute("style"));
-    assert.isFalse(shadowInput.hasAttribute("tabindex"));
-
-    // try dynamically adding the style attribute
-    lightInput.style.display = "none";
-
-    flush(() => {
-      assert.isFalse(shadowInput.hasAttribute("style"));
-      done();
-    });
-  });
-
-  test("it should update the shadow DOM input when the light DOM input attributes change", done => {
-    const pfeTextinput = document.querySelector("#changes");
-    const lightInput = pfeTextinput.querySelector("input");
-    const shadowInput = pfeTextinput.shadowRoot.querySelector("span input");
-
-    lightInput.setAttribute("required", true);
-    lightInput.setAttribute("minlength", "5");
-    lightInput.setAttribute("maxlength", "40");
-    lightInput.setAttribute("disabled", true);
-    lightInput.removeAttribute("placeholder");
-    
-    flush(() => {
-      assert.isTrue(shadowInput.hasAttribute("required"));
-      assert.equal(shadowInput.getAttribute("minlength"), "5");
-      assert.equal(shadowInput.getAttribute("maxlength"), "40");
-      assert.isTrue(shadowInput.hasAttribute("disabled"));
-      assert.isFalse(shadowInput.hasAttribute("placeholder"));
-      done();
-    });
-  });
-
-  test("it should set focus on the shadow DOM input when the light DOM input receives focus", () => {
-    const pfeTextinput = document.querySelector("pfe-textinput");
-    const lightInput = pfeTextinput.querySelector("input");
-    const shadowInput = pfeTextinput.shadowRoot.querySelector("span input");
-
-    lightInput.focus();
-    assert.isTrue(pfeTextinput.shadowRoot.activeElement === shadowInput);
-  });
-
-  test("it should submit the nearest form on the press of an enter key", () => {
-    const form = document.querySelector("#form");
-    const formPfeTextinput = document.querySelector("#form-pfe-textinput");
-    const formInput = document.querySelector("#form-input");
-    const shadowInput = formPfeTextinput.shadowRoot.querySelector("span input");
-
-    form.addEventListener("submit", event => {
-      event.preventDefault();
-      assert.isTrue(true);
-    });
-
-    formInput.value = "some value";
-    pressEnter(shadowInput);
   });
 });

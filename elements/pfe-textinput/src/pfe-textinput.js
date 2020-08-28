886a66559a6f5538ba318bee78787401818024c6
Kyle Buchanan <kylebuch8@gmail.com>
2020-08-28 16:09:09 -0400

    adding ability to search for novalidate

--- elements/pfe-textinput/src/pfe-textinput.js
+++ elements/pfe-textinput/src/pfe-textinput.js
@@ -1,3 +1,5 @@
+// Import polyfills: closest
+import "./polyfills--pfe-textinput.js";
 import PFElement from "../../pfelement/dist/pfelement.js";
 
 const observerConfig = {
@@ -21,13 +23,30 @@ class PfeTextinput extends PFElement {
     return "pfe-textinput.scss";
   }
 
+  get novalidate() {
+    return this.hasAttribute("pfe-c-novalidate");
+  }
+
+  set novalidate(val) {
+    val = Boolean(val);
+
+    if (val) {
+      this.setAttribute("pfe-c-novalidate", "");
+    } else {
+      this.removeAttribute("pfe-c-novalidate");
+    }
+  }
+
   static get PfeType() {
     return PFElement.PfeTypes.Content;
   }
 
   constructor() {
     super(PfeTextinput, { type: PfeTextinput.PfeType });
+
+    this.novalidate = false;
     this._input = null;
+    this._listenersAdded = false;
     this._validHandler = this._validHandler.bind(this);
     this._invalidHandler = this._invalidHandler.bind(this);
     this._focusHandler = this._focusHandler.bind(this);
@@ -43,7 +62,7 @@ class PfeTextinput extends PFElement {
   }
 
   disconnectedCallback() {
-    if (this._input) {
+    if (this._listenersAdded) {
       this._input.removeEventListener("invalid", this._invalidHandler);
       this._input.removeEventListener("focus", this._focusHandler);
       this._input.removeEventListener("blur", this._checkValidity);
@@ -74,12 +93,38 @@ class PfeTextinput extends PFElement {
   _init() {
     if (this._isValidLightDOM()) {
       this._input = this.querySelector("input");
+      this._checkNoValidate();
+
+      if (this.novalidate) {
+        return;
+      }
+
       this._input.addEventListener("invalid", this._invalidHandler);
       this._input.addEventListener("focus", this._focusHandler);
       this._input.addEventListener("blur", this._checkValidity);
       this._input.addEventListener("change", this._checkValidity);
       this._input.addEventListener("keyup", this._checkValidity);
       this._input.addEventListener("paste", this._checkValidity);
+      this._listenersAdded = true;
+    }
+  }
+
+  // novalidate functionality is informed by
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#attr-novalidate
+  _checkNoValidate() {
+    const closestForm = this.closest("form");
+
+    if (closestForm.hasAttribute("novalidate")) {
+      this.novalidate = true;
+      return;
+    }
+
+    const formnovalidateElement = closestForm.querySelector(
+      `button[formnovalidate], input[type="submit"][formnovalidate], input[type="image"][formnovalidate]`
+    );
+
+    if (formnovalidateElement) {
+      this.novalidate = true;
     }
   }
 
---
+++ elements/pfe-textinput/src/polyfills--pfe-textinput.js
@@ -0,0 +1,12 @@
+// @POLYFILL  Element.closest
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}

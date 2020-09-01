a5bb3b3c2baa764e04a9a6e58225738d8d29e629
Kyle Buchanan <kylebuch8@gmail.com>
2020-08-31 20:12:39 -0400

    polyfill update

--- elements/pfe-textinput/src/polyfills--pfe-textinput.js
+++ elements/pfe-textinput/src/polyfills--pfe-textinput.js
@@ -10,3 +10,11 @@ if (!Element.prototype.closest) {
     return null;
   };
 }
+
+// @POLYFILL  Element.matches
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}

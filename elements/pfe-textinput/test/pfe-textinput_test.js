9f42cd4a6c4d5faf6ae897512be198350656fc53
Kyle Buchanan <kylebuch8@gmail.com>
2020-08-31 20:13:56 -0400

    pfe-textinput test updates

--- elements/pfe-textinput/test/pfe-textinput_test.js
+++ elements/pfe-textinput/test/pfe-textinput_test.js
@@ -1,4 +1,4 @@
-import { pressEnter } from '/components/@polymer/iron-test-helpers/mock-interactions.js';
+import { keyUpOn } from '/components/@polymer/iron-test-helpers/mock-interactions.js';
 
 suite("<pfe-textinput>", () => {
   test("it should upgrade", () => {
@@ -26,5 +26,87 @@ suite("<pfe-textinput>", () => {
     document.body.appendChild(pfeTextinput);
     sinon.assert.calledWith(spy, 'pfe-textinput: The only child in the light DOM must be an input tag');
     spy.restore();
+  });
+
+  test("it should add a pfe-c-novalidate attribute when the novalidate property is set to true", () => {
+    const textinput = document.querySelector("pfe-textinput");
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), false);
+
+    textinput.novalidate = true;
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), true);
+  });
+
+  test("it should remove a pfe-c-novalidate attribute when the novalidate property is set to false", () => {
+    const textinput = document.querySelector("pfe-textinput");
+    textinput.novalidate = true;
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), true);
+
+    textinput.novalidate = false;
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), false);
+  });
+
+  test("it should add event listeners to the input", () => {
+    const textinput = document.querySelector("pfe-textinput");
+    assert.equal(textinput._listenersAdded, true);
+  });
+
+  test("it should remove the event listeners if novalidate is true", () => {
+    const textinput = document.querySelector("pfe-textinput");
+    textinput.novalidate = true;
+
+    assert.equal(textinput._listenersAdded, false);
+    // reset
+    textinput.novalidate = false;
+  });
+
+  test("it should set novalidate to true if the parent form has a novalidate attribute", () => {
+    const textinput = document.querySelector("#novalidate pfe-textinput");
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), true);
+  });
+
+  test("it should set novalidate to true if the textinput has a formnovalidate attribute on a sibling button", () => {
+    const textinput = document.querySelector("#btn-novalidate pfe-textinput");
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), true);
+  });
+
+  test("it should set novalidate to true if the textinput has a formnovalidate attribute on a sibling input[type=\"submit\"]", () => {
+    const textinput = document.querySelector("#submit-novalidate pfe-textinput");
+    assert.equal(textinput.hasAttribute("pfe-c-novalidate"), true);
+  });
+
+  test("it should add a pfe-touched class when the textinput receives focus", () => {
+    const textinput = document.querySelector("pfe-textinput");
+    textinput._input.focus();
+    assert.equal(textinput.classList.contains("pfe-touched"), true);
+  });
+
+  test("it should add a pfe-invalid class when the textinput validity check does not pass", () => {
+    const form = document.querySelector("#add-invalid-class");
+    const textinput = form.querySelector("pfe-textinput");
+    const button = form.querySelector("button");
+
+    button.click();
+    assert.equal(textinput.classList.contains("pfe-invalid"), true);
+  });
+
+  test("it should remove a pfe-invalid class when the textinput validity check does pass", () => {
+    const form = document.querySelector("#add-invalid-class");
+    const textinput = form.querySelector("pfe-textinput");
+    textinput._input.focus();
+    textinput.querySelector("input").value = "something";
+    textinput._input.blur();
+    assert.equal(textinput.classList.contains("pfe-invalid"), false);
+  });
+
+  test("it should add a pfe-invalid class if the textinput emits an invalid event", () => {
+    const form = document.querySelector("#add-invalid-class-submit");
+    const textinput = form.querySelector("pfe-textinput");
+    const button = form.querySelector("button");
+
+    textinput._input.addEventListener("invalid", event => {
+      assert.equal(textinput.classList.contains("pfe-invalid"), true);
+    });
+
+    button.click();
   });
 });

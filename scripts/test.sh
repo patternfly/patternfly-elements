05926918022b7f863207dce59ef3541ed9686309
mwcz <mwc@clayto.com>
2020-10-02 15:20:30 -0400

    fix logic for running all tests, or just some tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,18 @@
 
 CMD="npm run build $@; ./node_modules/.bin/wct --configFile wct.conf.json"
 
+#######################################################
+#  If no elements were specified, run all the tests.  #
+#######################################################
+
+if [[ $# -eq 0 ]] ; then
+    CMD="$CMD \"elements/*/test/\" \"elements/*/test/old-test\""
+fi
+
+####################################################################################
+#  If one or more elements were requested, run the tests for only those elements.  #
+####################################################################################
+
 for el in "$@"; do
   if [[ $el == -* ]]; then
     CMD="$CMD $el"

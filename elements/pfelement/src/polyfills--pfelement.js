dfac90f6a3bb0f25ca63731292d7f44efe63d5fd
Kyle Buchanan <kylebuch8@gmail.com>
2020-11-09 17:38:30 -0500

    Object.entries polyfill for IE11

--- elements/pfelement/src/polyfills--pfelement.js
+++ elements/pfelement/src/polyfills--pfelement.js
@@ -49,3 +49,16 @@ if (!Array.prototype.includes) {
     }
   });
 }
+
+// @POLYFILL Object.entries
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+if (!Object.entries) {
+  Object.entries = function(obj) {
+    var ownProps = Object.keys(obj),
+      i = ownProps.length,
+      resArray = new Array(i); // preallocate the Array
+    while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
+}

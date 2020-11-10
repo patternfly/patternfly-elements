857baafc6e71be0ea0e5e4129d58ba44865f8a09
[ Cassondra ] <carobert@redhat.com>
2020-11-10 12:16:08 -0500

    Fix icon panel inconsistencies for IE11; pfe-styles baselines recommitted

--- elements/pfe-jump-links/test/pfe-jump-links_e2e.js
+++ elements/pfe-jump-links/test/pfe-jump-links_e2e.js
@@ -3,8 +3,8 @@ const element = require("../package.json").pfelement.elementName;
 describe(element, () => {
   before(() => {
     browser.url(`/elements/${element}/demo`);
+    $("#section1").scrollIntoView();
     browser.pause(3000);
-    // $("#section1").scrollIntoView();
   });
 
   // @TODO: Need a way to take full page shots of this component

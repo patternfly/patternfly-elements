b1902350cd2c4d1a2495258d79bc9693656c2769
[ Cassondra ] <carobert@redhat.com>
2020-12-03 13:44:53 -0500

    Update storybook

--- scripts/doc-listing-inject.js
+++ scripts/doc-listing-inject.js
@@ -14,52 +14,84 @@ let elementNames = fs.readdirSync(elementsDir).filter(file => fs.statSync(path.j
 elementNames = elementNames.filter(folder => fs.existsSync(`${elementsDir}/${folder}/demo/index.html`));
 
 let items = [];
+let polyfills = [];
 
 // -> todos contains the array of todos/fixme/polyfills, parsed
 glob(`${elementsDir}/*/src/*.js`, (er, files) => {
   files.forEach(file => {
-    items = items.concat(
+    let all = [];
+    all = all.concat(
       leasot.parse(fs.readFileSync(file, "utf8"), {
         extension: ".js",
         customTags: ["POLYFILL"],
         filename: file
       })
     );
+    all.forEach(entry => {
+      if (entry.tag === "TODO") items.push(entry);
+      else polyfills.push(entry);
+    });
   });
 
   let regex = new RegExp(/\[(.*?)\]\((.*?)\)/g);
 
   // -> markdown output of the todos
-  let output = "";
-  output += leasot.report(items, "markdown");
-
-  output = output.replace(regex, (full, label, link) => {
-    return (
-      "[" +
-      path
-        .basename(label, ".js")
-        .split("--")
-        .slice(-1)
-        .pop() +
-      "](" +
-      path.relative(__dirname, link) +
-      ")"
+  const cleanPaths = markdown =>
+    markdown.replace(
+      regex,
+      (full, label, link) =>
+        "[" +
+        path
+          .basename(label, ".js")
+          .split("--")
+          .slice(-1)
+          .pop() +
+        "](" +
+        path.relative(__dirname, link) +
+        ")"
     );
-  });
+
+  const removeHeadings = markdown => {
+    return markdown.replace(/###(.*?)$/g, "");
+  };
+
+  const tidyMarkdown = markdown => {
+    markdown = cleanPaths(markdown);
+    markdown = removeHeadings(markdown);
+    return markdown;
+  };
+
+  let output = tidyMarkdown(leasot.report(items, "markdown"));
+  let outputPolyfills = tidyMarkdown(leasot.report(polyfills, "markdown"));
 
   let markup = "";
-  let cardTemplate = (element, description) => `
-    <pfe-card color="lightest" border>
-      <h2>${element}</h2>
+  let cardTemplate = (element, title, description, image) => {
+    let label = title
+      ? title
+      : element
+          .split("-")
+          .splice(1)
+          .join(" ");
+    return `
+    <pfe-card color="lightest" pfe-border>
+      <img overflow="top right left" src="/brand/components@2x.png" alt="">
+      <h2>${label.charAt(0).toUpperCase()}${label.slice(1)}</h2>
       ${description ? `<p>${description}</p>` : ""}
-      <pfe-cta slot="pfe-card--footer"><a
-          href="../elements/${element}/demo">View demo page</a></pfe-cta>
+      <div class="pfe-l-grid pfe-m-gutters pfe-m-all-6-col" slot="pfe-card--footer">
+        <pfe-cta priority="primary"><a
+          href="../elements/${element}/demo">Demo</a></pfe-cta>
+        <pfe-cta priority="secondary" variant="wind"><a
+              href="http://localhost:9001/?path=/story/${label.toLowerCase()}--${element}">Storybook</a></pfe-cta>
+      </div>
     </pfe-card>
   `;
+  };
 
   elementNames.forEach(element => {
-    let description = require(`${elementsDir}/${element}/package.json`).description;
-    markup += `\n\t\t\t\t${cardTemplate(element, description)}`;
+    const title = require(`${elementsDir}/${element}/package.json`).pfelement.title;
+    const description = require(`${elementsDir}/${element}/package.json`).description;
+    const image = require(`${elementsDir}/${element}/package.json`).pfelement.preview;
+    markup += `\n\t\t\t\t${cardTemplate(element, title, description, image)}`;
   });
 
   markup += "\n\t\t\t";
@@ -69,7 +101,8 @@ glob(`${elementsDir}/*/src/*.js`, (er, files) => {
   const compiledTpl = template(tmpl);
   const newContents = compiledTpl({
     components: markup,
-    todo: items.length > 0 ? output : ""
+    todo: items.length > 0 ? output : "",
+    polyfill: polyfills.length > 0 ? outputPolyfills : ""
   });
 
   // // Read in the wrapper template

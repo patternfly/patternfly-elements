034e96c287f28e96cb0d9f82327376102e0f75b6
Wes Ruvalcaba <wesruv@gmail.com>
2020-12-14 14:54:14 -0500

    CPFED-3940: Making updates based on feedback

--- elements/pfe-primary-detail/src/pfe-primary-detail.js
+++ elements/pfe-primary-detail/src/pfe-primary-detail.js
@@ -1,5 +1,11 @@
 import PFElement from "../../pfelement/dist/pfelement.js";
 
+// list of attributes that we DO NOT want to pass from
+// the _externalBtn to our shadow DOM button. For example,
+// the style attribute could ruin our encapsulated styles
+// in the shadow DOM
+const denyListAttributes = ["style"];
+
 class PfePrimaryDetail extends PFElement {
   static get tag() {
     return "pfe-primary-detail";
@@ -38,6 +44,8 @@ class PfePrimaryDetail extends PFElement {
     super(PfePrimaryDetail, { type: PfePrimaryDetail.PfeType });
 
     this._handleHideShow = this._handleHideShow.bind(this);
+    this._initDetailsNav = this._initDetailsNav.bind(this);
+    this._initDetail = this._initDetail.bind(this);
 
     this._detailsNav = this.shadowRoot.getElementById("details-nav");
   }
@@ -47,10 +55,10 @@ class PfePrimaryDetail extends PFElement {
     // If you need to initialize any attributes, do that here
 
     this._slots = {
-      detailsNav: this.shadowRoot.querySelector('slot[name="details-nav"]').assignedElements(),
-      details: this.shadowRoot.querySelector('slot[name="details"]').assignedElements(),
-      detailsNavHeader: this.shadowRoot.querySelector('slot[name="details-nav--header"]').assignedElements(),
-      detailsNavFooter: this.shadowRoot.querySelector('slot[name="details-nav--footer"]').assignedElements()
+      detailsNav: this.getSlot("details-nav"),
+      details: this.getSlot("details"),
+      detailsNavHeader: this.getSlot("details-nav--header"),
+      detailsNavFooter: this.getSlot("details-nav--footer")
     };
 
     this._detailsWrapper = this.shadowRoot.getElementById("details-wrapper");
@@ -59,13 +67,74 @@ class PfePrimaryDetail extends PFElement {
     this._scanLightDom();
     // Set first item as active for initial load
     this._handleHideShow({ target: this._slots.detailsNav[0] });
+
+    // @todo Add mutation observer in case any content changes, rerun scanLightDom?
   }
 
-  // @todo Add mutation observer in case any content changes, rerun scanLightDom?
-
   disconnectedCallback() {
-    for (let index = 0; index < this._slots.detailsNav.length; index++) {
-      this._slots.detailsNav[index].removeEventListener("click", this._handleHideShow);
+    if (this._slots.detailsNav) {
+      for (let index = 0; index < this._slots.detailsNav.length; index++) {
+        this._slots.detailsNav[index].removeEventListener("click", this._handleHideShow);
+      }
+    }
+  }
+
+  /**
+   * Updates markup of details-nav elements to be toggle buttons
+   * @param {object} toggle Slotted element (probably a headline) to be made into a button toggle
+   * @param {integer} index The index of the item in the details-nav slot
+   */
+  _initDetailsNav(detailNavElement, index) {
+    let attr = detailNavElement.attributes;
+    detailNavElement.dataset.index = index;
+
+    const toggle = document.createElement("button");
+
+    toggle.innerHTML = detailNavElement.innerHTML;
+
+    // Copy over attributes from original element that aren't in denyList
+    [...attr].forEach(detailNavElement => {
+      if (!denyListAttributes.includes(detailNavElement.name)) {
+        toggle.setAttribute(detailNavElement.name, detailNavElement.value);
+      }
+    });
+
+    // If the detailNavElement does not have a ID, set a unique ID
+    if (!detailNavElement.id) {
+      toggle.setAttribute(
+        "id",
+        `pfe-detail-toggle-${Math.random()
+          .toString(36)
+          .substr(2, 9)}`
+      );
+    }
+
+    toggle.addEventListener("click", this._handleHideShow);
+    this._slots.detailsNav[index] = toggle;
+    detailNavElement.replaceWith(toggle);
+  }
+
+  _initDetail(detail, index) {
+    // If the toggle does not have a ID, set a unique ID
+    if (!detail.hasAttribute("id")) {
+      detail.setAttribute(
+        "id",
+        `pfe-detail-${Math.random()
+          .toString(36)
+          .substr(2, 9)}`
+      );
+    }
+
+    const toggleId = this._slots.detailsNav[index].getAttribute("id");
+    if (!detail.hasAttribute("aria-labelledby") && toggleId) {
+      detail.setAttribute("aria-labelledby", toggleId);
+    }
+
+    detail.dataset.index = index;
+
+    // Swing back to detailsNav to add aria-controls, now that details have an Id
+    if (!this._slots.detailsNav[index].hasAttribute("aria-controls") && detail.id) {
+      this._slots.detailsNav[index].setAttribute("aria-controls", detail.id);
     }
   }
 
@@ -74,66 +143,20 @@ class PfePrimaryDetail extends PFElement {
    */
   _scanLightDom() {
     if (this._slots.detailsNav.length !== this._slots.details.length) {
-      console.error(
-        `${this.tag}: The number of item headings does not match the number of item details. Found ${this._slots.detailsNav.length} item headings & ${this._slots.details.length} item details.`
+      this.error(
+        `The number of item headings does not match the number of item details. Found ${this._slots.detailsNav.length} item headings & ${this._slots.details.length} item details.`
       );
       return;
     }
 
     // Setup left sidebar navigation
     this._slots.detailsNav.forEach((toggle, index) => {
-      let attr = toggle.attributes;
-      toggle.dataset.index = index;
-
-      const button = document.createElement("button");
-
-      button.innerText = toggle.textContent;
-      // Copy over attributes from original element
-      [...attr].forEach(toggle => {
-        button.setAttribute(toggle.name, toggle.value);
-      });
-
-      // If the toggle does not have a ID, set a unique ID
-      if (!toggle.hasAttribute("id")) {
-        button.setAttribute(
-          "id",
-          `pfe-detail-toggle-${Math.random()
-            .toString(36)
-            .substr(2, 9)}`
-        );
-      }
-
-      button.addEventListener("click", this._handleHideShow);
-      this._slots.detailsNav[index] = button;
-      toggle.replaceWith(button);
+      this._initDetailsNav(toggle, index);
     });
 
     // Setup item detail elements
     this._slots.details.forEach((detail, index) => {
-      // If the toggle does not have a ID, set a unique ID
-      if (!detail.hasAttribute("id")) {
-        detail.setAttribute(
-          "id",
-          `pfe-detail-${Math.random()
-            .toString(36)
-            .substr(2, 9)}`
-        );
-      }
-
-      const toggleId = this._slots.detailsNav[index].getAttribute("id");
-      if (!detail.hasAttribute("aria-labelledby") && toggleId) {
-        detail.setAttribute("aria-labelledby", toggleId);
-      }
-
-      detail.dataset.index = index;
-    });
-
-    // Swing back to detailsNav to add aria-controls, now that details have an Id
-    this._slots.detailsNav.forEach((toggle, index) => {
-      const detailId = this._slots.details[index].getAttribute("id");
-      if (!toggle.hasAttribute("aria-controls") && detailId) {
-        toggle.setAttribute("aria-controls", detailId);
-      }
+      this._initDetail(detail, index);
     });
   }
 
@@ -163,7 +186,7 @@ class PfePrimaryDetail extends PFElement {
 
       // Add active attributes to Next Item
       nextItem.classList.add("pfe-primary-detail__toggle--active");
-      nextItem.setAttribute("aria-expanded", "false");
+      nextItem.setAttribute("aria-expanded", "true");
 
       // Add active attributes to Next Details
       nextDetails.classList.add("pfe-primary-detail__details--active");

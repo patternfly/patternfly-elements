cd71949ea04afc93ac2efaac71852eae7970097d
heyMP <mgp140@gmail.com>
2021-01-05 16:08:03 -0500

    add custom storybook defaults
    https://github.com/patternfly/patternfly-elements/pull/1235/files/c0753744d52158f78279774374d579b2e8bb4497#r548155043

--- elements/pfe-clipboard/demo/pfe-clipboard.story.js
+++ elements/pfe-clipboard/demo/pfe-clipboard.story.js
@@ -1,4 +1,5 @@
 import { storiesOf } from "@storybook/polymer";
+import { withActions } from "@storybook/addon-actions";
 import * as bridge from "@storybook/addon-knobs/polymer";
 import * as tools from "../../../.storybook/utils.js";
 
@@ -6,16 +7,23 @@ import PfeClipboard from "../dist/pfe-clipboard";
 
 const stories = storiesOf("Clipboard", module);
 
+// Add the readme
+import readme from "../README.md";
+stories.addParameters({
+  notes: {
+    markdown: readme
+  }
+});
+
 // Define the template to be used
 const template = (data = {}) => {
   return tools.component(PfeClipboard.tag, data.prop, data.slots);
 };
 
-// Use these to get dynamically generated content
-// const defaultHeading = tools.autoHeading(true);
-const defaultContent = tools.autoContent(1, 2);
-
 stories.addDecorator(bridge.withKnobs);
+
+// Log events
+stories.addDecorator(withActions("pfe-clipboard:copied"));
 
 stories.add(PfeClipboard.tag, () => {
   let config = {};
@@ -29,20 +37,42 @@ stories.add(PfeClipboard.tag, () => {
   const slots = PfeClipboard.slots;
 
   //-- Set any custom content for the slots here
+  slots.default.default = "Copy URL";
+  slots.textSuccess.default = "";
+  slots.icon.default = "";
 
   // Trigger the auto generation of the knobs for slots
   config.has = tools.autoContentKnobs(slots, bridge);
 
   //-- Build your slots here using config.has["icon"] to get user content
   // prettier-ignore
-  config.slots = [{
-    content: defaultContent
-  }];
 
-  //-- Reset default values show they don't render in the markup
-  // if (config.prop[""] === "default") {
-  //   config.prop[""] = "";
-  // }
+  config.slots = [];
+
+  if (config.has.default.length > 0) {
+    config.slots.push({
+      content: config.has.default
+    });
+  }
+
+  if (config.has.textSuccess.length > 0) {
+    config.slots.push({
+      slot: "pfe-clipboard--text--success",
+      content: config.has.textSuccess
+    });
+  }
+
+  if (config.has.icon.length > 0) {
+    config.slots.push({
+      slot: "pfe-clipboard--icon",
+      content: config.has.icon
+    });
+  }
+
+  // -- Reset default values show they don't render in the markup
+  if (config.prop[""] === "default") {
+    config.prop[""] = "";
+  }
 
   const rendered = template(config);
   return tools.preview(rendered);

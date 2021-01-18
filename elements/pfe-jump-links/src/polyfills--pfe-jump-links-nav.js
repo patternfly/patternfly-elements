e1891423fc6b9d3cdfc80016f1a5322a0cc6700d
heyMP <mgp140@gmail.com>
2021-01-18 17:07:07 -0500

    fix: Polyfill Number.isInteger for IE11

--- elements/pfe-jump-links/src/polyfills--pfe-jump-links-nav.js
+++ elements/pfe-jump-links/src/polyfills--pfe-jump-links-nav.js
@@ -3,3 +3,12 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
+// @POLYFILL Number.prototype.isInteger
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger#polyfill
+if (window.Number && !Number.prototype.isInteger) {
+  Number.isInteger =
+    Number.isInteger ||
+    function(value) {
+      return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
+    };
+}

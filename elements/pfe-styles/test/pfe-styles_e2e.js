76e9302fc7e321ac43765d5e5fcdef1cfbce67d6
Kendall Totten <ktotten@redhat.com>
2021-01-18 13:33:06 -0500

    Wrap default block elements for type in a .pfe-c-content class

--- elements/pfe-styles/test/pfe-styles_e2e.js
+++ elements/pfe-styles/test/pfe-styles_e2e.js
@@ -14,17 +14,5 @@ describe(element, () => {
   it("should compare to the baseline", () => {
     expect(browser.checkFullPageScreen(element)).toBeLessThan(1.25);
   });
-
-  before(() => {
-    browser.url(`/elements/${element}/demo/typography`);
-    browser.pause(1000);
-  });
-
-  it("should take a screenshot", () => {
-    browser.saveFullPageScreen(element);
-  });
-
-  it("should compare to the baseline", () => {
-    expect(browser.checkFullPageScreen(element)).toBeLessThan(1.25);
-  });
+  
 });

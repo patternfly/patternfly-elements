c223ba53b3dc7dbac99eae024ee3e6fcf7f0b4d2
heyMP <mgp140@gmail.com>
2021-01-21 12:31:27 -0500

    chore: improve description of public copyURLToClipboard function

--- elements/pfe-clipboard/src/pfe-clipboard.js
+++ elements/pfe-clipboard/src/pfe-clipboard.js
@@ -198,8 +198,13 @@ class PfeClipboard extends PFElement {
   }
 
   /**
-   * Copy url to the user's system clipboard clipboard
-   * https://caniuse.com/mdn-api_navigator_clipboard
+   * Copy url to the user's system clipboard
+   *
+   * If available, it will use the new Navigator API to access the system clipboard
+   * https://developer.mozilla.org/en-US/docs/Web/API/Navigator/clipboard
+   *
+   * If unavailable, it will use the legacy execCommand("copy")
+   * https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
    * @async
    * @return {Promise<string>} url
    */

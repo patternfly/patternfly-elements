df43bdb00e076a3505bda692ec63ac0c92fbf8e3
[ Cassondra ] <carobert@redhat.com>
2021-01-26 12:28:54 -0500

    First pass at storybook

--- elements/pfe-primary-detail/demo/pfe-primary-detail.story.js
+++ elements/pfe-primary-detail/demo/pfe-primary-detail.story.js
@@ -8,7 +8,7 @@
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 import { storiesOf } from "@storybook/polymer";
-import * as storybookBridge from "@storybook/addon-knobs";
+import * as bridge from "@storybook/addon-knobs";
 import * as tools from "../../../.storybook/utils.js";
 
 import PfePrimaryDetail from "../dist/pfe-primary-detail";
@@ -21,10 +21,15 @@ const template = (data = {}) => {
 };
 
 // Use these to get dynamically generated content
-// const defaultHeading = tools.autoHeading(true);
+const defaultHeading = tools.autoHeading(true);
 // const defaultContent = tools.autoContent(1, 2);
 
-stories.addDecorator(storybookBridge.withKnobs);
+let list = [];
+for (let i = 1; i <= 6; i++) {
+  list.push(tools.autoHeading(true));
+}
+
+stories.addDecorator(bridge.withKnobs);
 
 stories.add(PfePrimaryDetail.tag, () => {
   let config = {};
@@ -33,12 +38,103 @@ stories.add(PfePrimaryDetail.tag, () => {
   //-- Set any custom defaults just for storybook here
 
   // Trigger the auto generation of the knobs for attributes
-  config.prop = tools.autoPropKnobs(PfePrimaryDetail);
+  // config.prop = tools.autoPropKnobs(PfePrimaryDetail);
 
-  const slots = PfePrimaryDetail.slots;
+  // config.slots = PfePrimaryDetail.slots;
+
+  config.slots = [
+    {
+      slot: "details-nav",
+      tag: "h3",
+      content: defaultHeading
+    },
+    {
+      slot: "details",
+      tag: "ul",
+      content: list
+        .map(content =>
+          tools.customTag({
+            tag: "li",
+            attributes: {
+              href: "#nowhere"
+            },
+            content: content
+          })
+        )
+        .join("")
+    },
+    {
+      slot: "details-nav",
+      tag: "h3",
+      content: defaultHeading
+    },
+    {
+      slot: "details",
+      tag: "ul",
+      content: list
+        .map(content =>
+          tools.customTag({
+            tag: "li",
+            attributes: {
+              href: "#nowhere"
+            },
+            content: content
+          })
+        )
+        .join("")
+    },
+    {
+      slot: "details-nav",
+      tag: "h3",
+      content: defaultHeading
+    },
+    {
+      slot: "details",
+      tag: "ul",
+      content: list
+        .map(content =>
+          tools.customTag({
+            tag: "li",
+            attributes: {
+              href: "#nowhere"
+            },
+            content: content
+          })
+        )
+        .join("")
+    },
+    {
+      slot: "details-nav",
+      tag: "h3",
+      content: defaultHeading
+    },
+    {
+      slot: "details",
+      tag: "ul",
+      content: list
+        .map(content =>
+          tools.customTag({
+            tag: "li",
+            attributes: {
+              href: "#nowhere"
+            },
+            content: content
+          })
+        )
+        .join("")
+    },
+    {
+      slot: "details-nav--footer",
+      content: tools.component("pfe-cta", { priority: "primary" }, [
+        {
+          content: "<a href='#'>All products</a>"
+        }
+      ])
+    }
+  ];
 
   // Trigger the auto generation of the knobs for slots
-  config.has = tools.autoContentKnobs(slots, storybookBridge);
+  // config.has = tools.autoContentKnobs(slots, bridge);
 
   //-- Build your slots here using config.has[""] to get user content
   // prettier-ignore
@@ -49,54 +145,54 @@ stories.add(PfePrimaryDetail.tag, () => {
   // }
 
   const rendered = template(config);
-  // return tools.preview(rendered);
+  return tools.preview(rendered);
 
-  return `
-      <pfe-primary-detail>
-        <h3 slot="details-nav">Infrastructure and Management</h3>
-        <ul slot="details">
-          <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
-          <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-        </ul>
+  // return `
+  //     <pfe-primary-detail>
+  //       <h3 slot="details-nav">Infrastructure and Management</h3>
+  //       <ul slot="details">
+  //         <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
+  //         <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //       </ul>
 
-        <h3 slot="details-nav">Cloud Computing</h3>
-        <ul slot="details">
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-          <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
-          <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-          <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
-          <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-        </ul>
+  //       <h3 slot="details-nav">Cloud Computing</h3>
+  //       <ul slot="details">
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //         <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
+  //         <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //         <li><a href="#nowhere">Lorum ipsum dolor sit amet</a></li>
+  //         <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //       </ul>
 
-        <h3 slot="details-nav">Storage</h3>
-        <ul slot="details">
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-        </ul>
+  //       <h3 slot="details-nav">Storage</h3>
+  //       <ul slot="details">
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //       </ul>
 
-        <h3 slot="details-nav">Runtimes</h3>
-        <ul slot="details">
-          <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
-          <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
-          <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
-          <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
-        </ul>
+  //       <h3 slot="details-nav">Runtimes</h3>
+  //       <ul slot="details">
+  //         <li><a href="#nowhere">Pellentesque fermentum dolor</a></li>
+  //         <li><a href="#nowhere">Morbi in sem quis dui placerat ornare</a></li>
+  //         <li><a href="#nowhere">Aliquam tincidunt mauris eu risus</a></li>
+  //         <li><a href="#nowhere">Praesent dapibus, neque id cursus faucibus</a></li>
+  //       </ul>
 
-        <div slot="details-nav--footer" style="padding: 1em 0.75em 2em;">
-          <pfe-cta priority="primary"><a href="#">All Products</a></pfe-cta>
-        </div>
-      </pfe-primary-detail>
-    `;
+  //       <div slot="details-nav--footer" style="padding: 1em 0.75em 2em;">
+  //         <pfe-cta priority="primary"><a href="#">All Products</a></pfe-cta>
+  //       </div>
+  //     </pfe-primary-detail>
+  //   `;
 });

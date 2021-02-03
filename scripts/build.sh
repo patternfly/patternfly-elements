8d0ba62be6d4f769841216cb3fc777571b5f178c
[ Cassondra ] <carobert@redhat.com>
2021-02-03 15:58:03 -0500

    Remove pfe-sass from compile list if other components provided

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,16 @@
 CMD="npm run lerna -- run build --parallel --no-bail --include-dependencies"
 
 for el in "$@"; do
-  CMD="$CMD --scope \"*/$el\""
+  [[ "$el" != "pfe-sass" ]] && CMD="$CMD --scope \"*/$el\""
 done
+
+# If pfe-sass was the only provided input, compile it
+[[ "$@" == "pfe-sass" ]] && CMD="$CMD --scope \"*/$el\""
 
 CMD="$CMD && npm run build-storybook"
 
-eval $CMD
+# eval $CMD
+echo $CMD
 
 rm -rf examples/node_modules
 mkdir examples/node_modules

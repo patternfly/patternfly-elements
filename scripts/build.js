bda6836a1b80775753d053ab67d2964cbf8d9281
[ Cassondra ] <carobert@redhat.com>
2021-02-03 20:50:37 -0500

    chore: Update test and build to use node scripts

---
+++ scripts/build.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+process.env.FORCE_COLOR = true;
+
+const shell = require("shelljs");
+const argv = require("yargs")
+  // Set up --help documentation.
+  // You can view these by running `npm run build -- --help`.
+  .usage("Usage: npm build -- [options]")
+  .example([
+    ["npm run build", "(compile all components)"],
+    ["npm run build -- pfe-card", "(compile one component)"],
+    ["npm run build -- pfe-card pfe-band", "(compile multiple components)"],
+    ["npm run build -- --storybook", "(build storybook instance)"]
+  ])
+  .options({
+    storybook: {
+      default: false,
+      alias: "s",
+      describe: "build the storybook instance",
+      type: "boolean"
+    }
+  }).argv;
+
+// Arguments with no prefix are added to the `argv._` array.
+let components = argv._;
+let scope = [];
+
+// If pfe-sass is the only component provided, add it to the scope
+if (components.length === 1 && components.includes("pfe-sass")) {
+  scope.push(components[0]);
+} else if (components.length > 0) {
+  // Remove pfe-sass from the array but maintain the others
+  scope = components.filter(item => item !== "pfe-sass");
+}
+
+// Run the build task for each component in parallel, include dependencies
+shell.exec(
+  `npm run lerna -- run build --stream --no-bail --include-dependencies --scope "*/pfe-sass" && \
+   npm run lerna -- run build --parallel --no-bail --include-dependencies ${
+     scope.length > 0 ? scope.map(item => `--scope "*/${item}"`).join(" ") : ""
+   } ${argv.storybook ? "npm run build-storybook" : ""}`,
+  code => process.exit(code)
+);
--- scripts/test.js
+++ scripts/test.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
+process.env.FORCE_COLOR = true;
 
 const shell = require("shelljs");
-const { exit } = require("yargs");
 const argv = require("yargs")
   // Set up --help documentation.
   // You can view these by running `npm test -- --help`.
   .usage("Usage: npm test -- [options]")
   .example([
     ["npm test", "(run all tests, builds by default)"],
-    ["npm test -- card", "(run one test)"],
-    ["npm test -- card band", "(run multiple tests)"],
-    ["npm test -- --build=false", "(do not build the component(s) prior to running tests)"]
+    ["npm test -- pfe-card", "(run one test)"],
+    ["npm test -- pfe-card pfe-band", "(run multiple tests)"],
+    ["npm test -- --nobuild", "(do not build the component(s) prior to running tests)"]
   ])
   .options({
-    build: {
-      default: true,
-      alias: "b",
-      describe: "build the component(s) prior to running tests",
+    nobuild: {
+      default: false,
+      alias: "nb",
+      describe: "do not build the component(s) prior to running tests",
       type: "boolean"
     }
   }).argv;
@@ -28,7 +28,7 @@ let components = argv._;
 
 // Access all arguments using `argv`.
 // Add commands depending on which options are provided.
-const build = argv.build ? `npm run build ${components.join(" ")}; ` : "";
+const build = !argv.nobuild ? `npm run build ${components.join(" ")}; ` : "";
 
 shell.exec(
   `${build}./node_modules/.bin/wct --config-file wct.conf.json --npm ${

3f0b30b0aee87fab9802756c1090dcf3fcf76936
[ Cassondra ] <carobert@redhat.com>
2021-02-09 17:36:03 -0500

    chore: Revert changes from build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ export FORCE_COLOR=true
 CMD="npm run lerna -- run build --parallel --no-bail --include-dependencies"
 
 for el in "$@"; do
-  CMD="$CMD --scope \"*/$el\""
+  [[ "$el" != "pfe-sass" ]] || [[ "$*" == "pfe-sass" ]] && CMD="$CMD --scope \"*/$el\""
 done
 
 # If all components are being built (thus $* is empty), ignore pfe-sass (it gets built by components as a dependency)
@@ -14,3 +14,7 @@ if [[ -z "$*" ]]; then
 fi
 
 eval $CMD
+
+rm -rf examples/node_modules
+mkdir examples/node_modules
+cp -r node_modules/@webcomponents node_modules/requirejs examples/node_modules/

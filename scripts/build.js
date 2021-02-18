efe8e679624743d6669869bc69e969c4753b7e84
[ Cassondra ] <carobert@redhat.com>
2021-02-18 16:39:54 -0500

    chore: Tidy up unused build script code

--- scripts/build.js
+++ scripts/build.js
@@ -23,17 +23,6 @@ const argv = require("yargs")
       alias: "s",
       describe: "build the storybook instance",
       type: "boolean"
-    },
-    // quiet: {
-    //   describe: "reduce noise in console output",
-    //   type: "boolean",
-    //   default: true
-    // },
-    verbose: {
-      alias: "v",
-      describe: "increase noise in console output",
-      type: "boolean",
-      default: false
     }
   }).argv;
 
@@ -55,65 +44,20 @@ if (invalid.length > 0) {
 }
 
 if (invalid.length > 0) {
-  shell.echo(chalk`{red No component directory found for: ${invalid.join(", ")}}`);
+  shell.echo(chalk`{bold No component directory found for: {red ${invalid.join(", ")}}}\n`);
   // Remove invalid items from the array
-  components = components.filter(item => allComponents.includes(item));
+  components = components.filter(item => !invalid.includes(item));
+  // If the array is now empty, exit the script
+  if (components.length === 0) shell.exit(1);
 }
 
 let currentComponent;
 let colorIdx = 0;
 
 // Build the command out to be run
-let cmd = `lerna -- run build --no-bail --include-dependencies ${components.map(el => `--scope '*/${el}'`).join(" ")}`;
+let cmd = `lerna -- run build --no-bail --stream --include-dependencies ${components.map(el => `--scope '*/${el}'`).join(" ")}`;
+
 // Run the command
-const build = shell.exec(`npm run ${cmd}`, { silent: true, async: true }); //, (code, stdout, stderr) => {
-
-const processStream = (line, stack) => {
-  if (!line.match(/\n/)) return [];
-
-  let lines = line.split("\n");
-
-  // If the end of the line is not a newline, capture that part and add it back to the empty line
-  if (!line.match(/\n$/)) {
-    // Pop the last item off the array
-    lines.pop();
-
-    // Reset the stack equal to the substring
-    let pos = line.lastIndexOf(/\n/);
-    stack = stack.substr(pos + 1);
-  }
-
-  // Split lines out by newline breaks for parsing
-  return lines;
-};
-
-let out = "";
-build.stdout.on("data", data => {
-  if (!data) return;
-
-  processStream((out += data), out).forEach(line => {
-    let color = currentColor(colorIdx);
-    if (currentComponent) shell.echo("-n", chalk[color].bold(`@patternfly/${currentComponent}: `));
-    shell.echo(chalk.reset(line));
-  });
-});
-
-let err = "";
-build.stderr.on("data", data => {
-  if (!data) return;
-
-  processStream((err += data), err).forEach((line, idx, lines) => {
-    // Capture component name being built
-    let match = line.trim().match(/'build' in '@patternfly\/([\w-]+)' in (.*?):$/);
-    // Update the name of the component currently being built
-    if (match && match[1] && match[1] !== currentComponent) {
-      currentComponent = match[1];
-      shell.echo("");
-      if (++colorIdx > colors.length - 1) colorIdx = 0;
-    }
-    // if (match[2]) @TODO do something with the build time
-    shell.echo(chalk.gray(line));
-  });
-});
+const build = shell.exec(`npm run ${cmd}`);
 
 if (argv.storybook) shell.exec(`npm run build-storybook`);

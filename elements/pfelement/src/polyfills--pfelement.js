a3f6b407a57a7ca76679587feddeb807141bff16
[ Cassondra ] <carobert@redhat.com>
2021-03-30 16:32:45 -0400

    fix: Update baselines

--- elements/pfelement/src/polyfills--pfelement.js
+++ elements/pfelement/src/polyfills--pfelement.js
@@ -66,10 +66,10 @@ if (!Object.entries) {
 // @POLYFILL String.startsWith
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#polyfill
 if (!String.prototype.startsWith) {
-  Object.defineProperty(String.prototype, 'startsWith', {
-      value: function(search, rawPos) {
-          var pos = rawPos > 0 ? rawPos|0 : 0;
-          return this.substring(pos, pos + search.length) === search;
-      }
+  Object.defineProperty(String.prototype, "startsWith", {
+    value: function(search, rawPos) {
+      var pos = rawPos > 0 ? rawPos | 0 : 0;
+      return this.substring(pos, pos + search.length) === search;
+    }
   });
 }

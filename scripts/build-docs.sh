71ff8230806c3bbc0c9bcd73630a39bfc31f82af
[ Cassondra ] <carobert@redhat.com>
2021-04-01 11:11:20 -0400

    Update build to pull in assets from components

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -8,20 +8,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR > /dev/null
 pushd ../docs/ > /dev/null
 
-echo "Cleaning public/ dir."
-rm -rf _site
+echo "Copy documentation from components"
+for f in ../elements/*/docs; do
+  COMPONENT="${f/..\/elements\/pfe-}";
+  COMPONENT="${COMPONENT/\/docs}"
+  DOCS_DIR="components/${COMPONENT}"
+  echo "Creating ${DOCS_DIR}"
+  cp -rf $f $DOCS_DIR
+done
 
 echo "Building 11ty docs."
-npm run build
+npm install && npm run build
 
 echo "Copying elements directory"
 mkdir _site/elements;
-
-# for f in ../elements/*/docs; do
-#   DOCS_DIR="_site${f/..}";
-#   echo "Creating ${DOCS_DIR}" 
-#   mkdir -p $DOCS_DIR
-# done
 
 for f in ../elements/*/demo; do
   DEMO_DIR="_site${f/..}";
@@ -51,12 +51,6 @@ done
 
 echo "Copying examples directory"
 cp -r ../examples ./_site/
-
-echo "Building storybook."
-cd ../
-npm run build-storybook
-mkdir docs/_site/storybook
-cp -r ./.storybook_out/* ./docs/_site/storybook
 
 popd > /dev/null
 popd > /dev/null

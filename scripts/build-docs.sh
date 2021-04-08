847de975fc9577d765593fddf92979af8fe84442
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-04-08 13:32:33 -0400

    fork: 11ty updates; scripting assist (#1508)
    * Work on creating a "source of truth" for the docs
    
    * Add docs folder
    
    * Update build to pull in assets from components
    
    * Remove component assets from git log
    
    * Update readme instructions; add script for installing 11ty
    
    * Update scripts
    
    * Minor CSS tweak to push footer to bottom no matter how much content exists
    
    Co-authored-by: castastrophe <casnroberts+github@gmail.com>

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -8,11 +8,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR > /dev/null
 pushd ../docs/ > /dev/null
 
-echo "Cleaning public/ dir."
-rm -rf _site
+echo "Copy documentation from components"
+for f in ../elements/*/docs; do
+  COMPONENT="${f/..\/elements\/pfe-}";
+  COMPONENT="${COMPONENT/\/docs}"
+  DOCS_DIR="components/${COMPONENT}"
+  echo "Creating ${DOCS_DIR}"
+  cp -rf $f $DOCS_DIR
+done
 
 echo "Building 11ty docs."
-npm run build
+npm install && npm run build
 
 echo "Copying elements directory"
 mkdir _site/elements;
@@ -45,12 +51,6 @@ done
 
 echo "Copying examples directory"
 cp -r ../examples ./_site/
-
-echo "Building storybook."
-cd ../
-npm run build-storybook
-mkdir docs/_site/storybook
-cp -r ./.storybook_out/* ./docs/_site/storybook
 
 popd > /dev/null
 popd > /dev/null

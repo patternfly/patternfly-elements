1d5cf046295b1c1e039b92ba19177c3ac707a931
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-04-13 14:14:19 -0400

    feat: Updating styles

--- elements/pfe-accordion/src/pfe-accordion--fallback.js
+++ elements/pfe-accordion/src/pfe-accordion--fallback.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        document.querySelectorAll("pfe-accordion-header:not([PFElement])").forEach(header => {
-            header.setAttribute("tabindex", "0");
-            header.addEventListener("click", event => header.toggleAttribute("active"));
-            header.addEventListener("keydown", event => {
-                if (event.key === "Enter") header.toggleAttribute("active");
-            });
-        });
-    }, 3000);
+  setTimeout(() => {
+    document.querySelectorAll("pfe-accordion-header:not([PFElement])").forEach(header => {
+      header.setAttribute("tabindex", "0");
+      header.addEventListener("click", event => header.toggleAttribute("active"));
+      header.addEventListener("keydown", event => {
+        if (event.key === "Enter") header.toggleAttribute("active");
+      });
+    });
+  }, 3000);
 });

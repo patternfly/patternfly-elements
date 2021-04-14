58d7399eb06ba6f3c6663abf1cd6150a5ab870d2
heyMP <mgp140@gmail.com>
2021-04-13 22:29:35 -0400

    fix: added setup element references to readtime element

--- elements/pfe-readtime/test/pfe-readtime_test.js
+++ elements/pfe-readtime/test/pfe-readtime_test.js
@@ -1,14 +1,21 @@
 const elements = [...document.querySelectorAll("pfe-readtime")];
 
 suite("<pfe-readtime>", () => {
+  let pfeReadtime;
+  let langCode;
 
-    test("it should upgrade", () => {
-        assert.instanceOf(
-            document.querySelector("pfe-readtime"),
-            customElements.get("pfe-readtime"),
-            "pfe-readtime should be an instance of pfeReadtime"
-        );
-    });
+  suiteSetup(() => {
+    pfeReadtime = document.querySelector("pfe-readtime");
+    langCode = document.querySelector("html").getAttribute("lang");
+  })
+
+  test("it should upgrade", () => {
+    assert.instanceOf(
+        document.querySelector("pfe-readtime"),
+        customElements.get("pfe-readtime"),
+        "pfe-readtime should be an instance of pfeReadtime"
+    );
+  });
 
 
         // Write tests for each attribute
@@ -18,34 +25,27 @@ suite("<pfe-readtime>", () => {
         //if pfe-readtime has word-count attribute make sure it is getting that values
         //wpm is set to X expect readtime value to be Y
         test("it calculates readtime based on wordcount given", done => {
-          const pfeReadtime = document.querySelector("#pfeReadtime");
-
           pfeReadtime.setAttribute("wpm", "100");
 
-          assert.equal(pfeReadtime.readtime, "5");
+          assert.equal(pfeReadtime.getAttribute("readtime"), "5");
           done();
         });
 
         test("that readtime is hidden if readtime is < 1", done => {
-          const pfeReadtime = document.querySelector("#pfeReadtime");
-
           pfeReadtime.setAttribute("word-count", "0");
 
           assert.equal(pfeReadtime.shadowRoot.textContent, "");
-          assert.isTrue(pfeReadtime.hidden);
+          assert.isTrue(pfeReadtime.hasAttriute("hidden"));
           done();
         });
 
 
         test("if lang=zh that wpm and readtime update accordingly", done => {
-          const langCode = document.querySelector("html").getAttribute("lang");
-          const pfeReadtime = document.querySelector("#pfeReadtime");
-
           pfeReadtime.setAttribute("word-count", "158");
 
           langCode.setAttribute("lang", "zh");
 
-          assert.equal(pfeReadtime.wpm, "158");
+          assert.equal(pfeReadtime.getAttribute("wpm"), "158");
           assert.equal(pfeReadtime.readtime, "1");
           done();
         });

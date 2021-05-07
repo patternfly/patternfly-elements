c95e33314a2af2ce1a9596da687a77e138dc312d
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-05-07 15:28:18 -0400

    Remove failing test and fix commented out code for testing

--- elements/pfe-accordion/test/pfe-accordion_test.js
+++ elements/pfe-accordion/test/pfe-accordion_test.js
@@ -1,6 +1,5 @@
 suite('<pfe-accordion>', () => {
   test('it should upgrade pfe-accordion', () => {
-    assert.equal("foo", "bar");
     assert.instanceOf(document.querySelector('pfe-accordion'), customElements.get("pfe-accordion"), 'pfe-accordion should be an instance of PfeAccordion');
   });
 

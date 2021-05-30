ea149411e742f862b184f320484f1e95f68400c2
Wes Ruvalcaba <wesruv@gmail.com>
2021-05-30 18:22:07 -0500

    Progress on tests for navigation

--- elements/pfe-navigation/test/pfe-navigation_test.js
+++ elements/pfe-navigation/test/pfe-navigation_test.js
@@ -1,39 +1,152 @@
 let pfeNavigation = null;
+let mainMenuDropdowns = null;
+let secondaryLinksDropdowns = null;
+// Gets populated in early tests with checkToggleAndDropdownBasics
+const allToggles = {};
 
-function checkMenuLinkActiveAttributes(toggleElement) {
-  const toggleName = toggleElement.dataset.machineName;
+/**
+ * Checks the required attributes on a dropdown and wrapper
+ * @param {Element} toggle Reference to toggle element
+ * @param {Element} dropdownWrapper Reference to corresponding .pfe-navigation__dropdown-wrapper element
+ * @param {String} machineName Identifier for error messages
+ */
+function checkToggleAndDropdownBasics(toggle, dropdownWrapper, machineName) {
+  // Check Toggle
   assert.isTrue(
-    toggleElement.getAttribute('aria-expanded') === 'true',
-    `The ${toggleName} toggle did not have aria-expanded after it was clicked`
+    toggle && toggle.tagName === 'BUTTON',
+    `${machineName} Toggle button is missing for a dropdown`
   );
   assert.isTrue(
-    toggleElement.parentElement.classList.contains('pfe-navigation__menu-item--open'),
-    `The parent element for the ${toggleName} toggle is missing the pfe-navigation__menu-item--open class`
+    toggle.hasAttribute('aria-expanded'),
+    `${machineName} Toggle does not have aria-expanded`
   );
-  const dropdown = pfeNavigation.shadowRoot.getElementById(
-    pfeNavigation._getDropdownId(toggleElement.id)
+  assert.isTrue(
+    toggle.hasAttribute('aria-controls'),
+    `${machineName} Toggle does not have aria-controls`
   );
+  assert.isString(
+    toggle.id,
+    `${machineName} Toggle does not have an id`
+  );
+
+  if (toggle.id && !allToggles[toggle.id]) {
+    allToggles[toggle.id] = toggle;
+  }
+
+  // Check Dropdown Wrapper
+  assert.isTrue(
+    dropdownWrapper.classList.contains('pfe-navigation__dropdown-wrapper'),
+    `${machineName} Dropdown is missing it's dropdown wrapper`
+  );
+
+  assert.isString(
+    dropdownWrapper.id,
+    `${machineName} Dropdown is missing an id`
+  );
+  assert.isTrue(
+    dropdownWrapper.hasAttribute('aria-hidden'),
+    `${machineName} Dropdown is missing aria-hidden`
+  );
+
+  // Check connection between the two
+  assert.isTrue(
+    toggle.getAttribute('aria-controls') === dropdownWrapper.id,
+    `${machineName} toggle doesn't have aria-controls attribute that matches dropdown`
+  );
+}
+
+/**
+ * Tests toggle and dropdown attributes
+ * @param {Element} toggle Reference to toggle element
+ * @param {Element} dropdownWrapper Reference to corresponding .pfe-navigation__dropdown-wrapper element
+ * @param {String} machineName Identifier for error messages
+ * @param {Boolean} active If the toggle & dropdown should be tested for active or inactive state
+ */
+function checkToggleAndDropdownState(toggle, dropdownWrapper, machineName, active = true) {
+  const ariaExpanded = active ? 'true' : 'false';
+  assert.isTrue(
+    toggle.getAttribute('aria-expanded') === ariaExpanded,
+    `The ${machineName} toggle should have aria-expanded set to ${ariaExpanded}`
+  );
+
+  if (toggle.id.startsWith('main-menu')) {
+    if (active) {
+      assert.isTrue(
+        toggle.parentElement.classList.contains('pfe-navigation__menu-item--open'),
+        `The parent element for the ${machineName} toggle is missing the pfe-navigation__menu-item--open class`
+      );
+    }
+    else {
+      assert.isFalse(
+        toggle.parentElement.classList.contains('pfe-navigation__menu-item--open'),
+        `The parent element for the ${machineName} toggle has the pfe-navigation__menu-item--open class, and should not`
+      );
+    }
+  }
+
+  // Get the dropdown wrapper if we don't have it
+  if (!dropdownWrapper) {
+    const dropdownId = pfeNavigation._getDropdownId(toggle.id);
+    if (dropdownId) {
+      // It could be in the lightDOM or the shadow DOM
+      dropdownWrapper =
+        pfeNavigation.shadowRoot.getElementById(dropdownId) ||
+        document.getElementById(dropdownId);
+    }
+  }
   assert.isNotNull(
-    dropdown,
-    `Was not able to find dropdown element that is associated to the ${toggleName} toggle`
+    dropdownWrapper,
+    `Was not able to find dropdown element that is associated to the ${machineName} toggle`
   );
+
+  const ariaHidden = active ? 'false' : 'true';
   assert.isTrue(
-    dropdown.getAttribute('aria-hidden') === 'false',
-    `The ${toggleName} toggle's dropdown is active and does not have aria-hidden set to false`
+    dropdownWrapper.getAttribute('aria-hidden') === ariaHidden,
+    `The ${machineName} toggle's dropdown should have aria-hidden set to ${ariaHidden}`
   );
-  assert.isFalse(
-    dropdown.hasAttribute('tabindex'),
-    `The ${toggleName} toggle's dropdown is active and has the tabindex attribute`
-  );
-  assert.isFalse(
-    dropdown.classList.contains('pfe-navigation__dropdown-wrapper--invisible'),
-    `The ${toggleName} toggle's dropdown is active and has the class pfe-navigation__dropdown-wrapper--invisible`
-  );
+  if (active) {
+    assert.isFalse(
+      dropdownWrapper.hasAttribute('tabindex'),
+      `The ${machineName} toggle's dropdown is active and has the tabindex attribute`
+    );
+    assert.isFalse(
+      dropdownWrapper.classList.contains('pfe-navigation__dropdown-wrapper--invisible'),
+      `The ${machineName} toggle's dropdown is active and has the class pfe-navigation__dropdown-wrapper--invisible`
+    );
+  }
+  else {
+    assert.isTrue(
+      dropdownWrapper.getAttribute('tabindex') === "-1",
+      `The ${machineName} toggle's dropdown is inactive and tabindex should be set to -1`
+    );
+    assert.isTrue(
+      dropdownWrapper.classList.contains('pfe-navigation__dropdown-wrapper--invisible'),
+      `The ${machineName} toggle's dropdown is inactive and is missing pfe-navigation__dropdown-wrapper--invisible`
+    );
+  }
+}
+
+/**
+ * Checks to make sure all toggles besides one give are inactive
+ * @param {String} activeToggleId ID of active toggle
+ */
+function checkInactiveToggleAndDropdownState(activeToggleId) {
+  // Check to make sure all other toggles are inactive
+  const allTogglesKeys = Object.keys(allToggles);
+  console.log(allToggles, allTogglesKeys);
+  for (let index = 0; index < allTogglesKeys.length; index++) {
+    const toggle = allToggles[allTogglesKeys[index]];
+    if (activeToggleId !== toggle.id) {
+      checkToggleAndDropdownState(toggle, null, toggle.id, false);
+    }
+  }
 }
 
 suite('<pfe-navigation>', () => {
   setup(() => {
     pfeNavigation = document.querySelector('pfe-navigation');
+    mainMenuDropdowns = pfeNavigation.shadowRoot.querySelectorAll('#pfe-navigation__menu .pfe-navigation__dropdown');
+    secondaryLinksDropdowns = pfeNavigation.querySelectorAll('[slot="secondary-links"] pfe-navigation-dropdown');
   });
 
   test('Component should upgrade', () => {
@@ -50,11 +163,35 @@ suite('<pfe-navigation>', () => {
     );
   });
 
-  test('Should have same number of main menu top level items', () => {
+  test('Main menu items should be processed with proper HTML in the shadow DOM', () => {
+    const menuListItems = pfeNavigation.shadowRoot.querySelectorAll('#pfe-navigation__menu > li');
     assert.isTrue(
-      pfeNavigation.querySelectorAll('#pfe-navigation__menu > li').length === pfeNavigation.shadowRoot.querySelectorAll('#pfe-navigation__menu > li').length,
+      pfeNavigation.querySelectorAll('#pfe-navigation__menu > li').length === menuListItems.length,
       "The number of menu items in the light DOM does not match the shadow DOM"
     );
+
+    assert.isTrue(
+      pfeNavigation.querySelectorAll('#pfe-navigation__menu .pfe-navigation__dropdown').length === mainMenuDropdowns.length,
+      "The number of dropdowns in the lightDOM doesn't match the shadow DOM"
+      );
+
+    for (let index = 0; index < mainMenuDropdowns.length; index++) {
+      const dropdown = mainMenuDropdowns[index];
+      const dropdownMenuItem = dropdown.closest('.pfe-navigation__menu-item');
+      const toggle = dropdownMenuItem.querySelector('.pfe-navigation__menu-link');
+      const dropdownWrapper = dropdown.parentElement
+
+      // Check toggle
+      assert.isString(
+        toggle.dataset.machineName,
+        `Toggle does not have Machine name`
+      );
+      const machineName = toggle.dataset.machineName;
+
+      checkToggleAndDropdownBasics(toggle, dropdownWrapper, machineName);
+      checkToggleAndDropdownState(toggle, dropdownWrapper, machineName, false);
+    }
+
   });
 
   test('Search toggle visibility', () => {
@@ -66,7 +203,8 @@ suite('<pfe-navigation>', () => {
          searchButton !== null && window.getComputedStyle(searchButton).display !== 'none',
         "Search slot is present, but search button's display is none"
       );
-    } else {
+    }
+    else {
       assert.isTrue(
         searchButton !== null && window.getComputedStyle(searchButton).display === 'none',
         "Search toggle should be display none, but isn't"
@@ -74,10 +212,34 @@ suite('<pfe-navigation>', () => {
    }
   });
 
+  test('Dropdowns in secondary link areas should get upgraded', () => {
+    for (let index = 0; index < secondaryLinksDropdowns.length; index++) {
+      const dropdown = secondaryLinksDropdowns[index];
+      const dropdownOuterWrapper = dropdown.closest('[slot="secondary-links"]');
+      const toggle = dropdownOuterWrapper.querySelector('.pfe-navigation__secondary-link');
+      const dropdownWrapper = dropdown.parentElement;
+
+      assert.isString(
+        dropdown.dataset.idSuffix,
+        "Secondary links dropdown is missing the data-id-suffix attribute"
+      );
+      const machineName = dropdown.dataset.idSuffix;
+
+      checkToggleAndDropdownBasics(toggle, dropdownWrapper, machineName);
+      checkToggleAndDropdownState(toggle, dropdownWrapper, machineName, false);
+    }
+  });
+
   test('A toggle and dropdown should get certain attributes when it is active', () => {
     const firstMenuLink = pfeNavigation.shadowRoot.querySelector('.pfe-navigation__menu-link');
     firstMenuLink.click();
-    checkMenuLinkActiveAttributes(firstMenuLink);
+    checkToggleAndDropdownState(firstMenuLink, null, firstMenuLink.dataset.machineName, true);
+    checkInactiveToggleAndDropdownState(firstMenuLink.id);
+
+    const firstSecondaryLinkDropdown = pfeNavigation.querySelector('button.pfe-navigation__secondary-link');
+    firstSecondaryLinkDropdown.click();
+    checkToggleAndDropdownState(firstSecondaryLinkDropdown, null, firstSecondaryLinkDropdown.id, true);
+    checkInactiveToggleAndDropdownState(firstSecondaryLinkDropdown.id);
   });
 
   // test('', () => {

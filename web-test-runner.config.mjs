c9c3b5654ac05875eabfce0727c54af183e4ac09
Zack Hawkins <zack.hawkins@outlook.com>
2021-06-24 08:51:09 -0400

    chore: Minor testing updates (#1690)
    * Minor testing updates
    
    * chore: update changelog
    
    Co-authored-by: Zack Hawkins <zhawkins@redhat.com>
    Co-authored-by: heyMP <mgp140@gmail.com>

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -5,12 +5,22 @@ export default {
     }
   },
   files: "elements/*/test/*.spec.js",
+  testRunnerHtml: testFramework =>
+    `<html>
+      <head>
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
   groups: [
     {
       name: 'with-vue',
       testRunnerHtml: testFramework =>
         `<html>
           <head>
+            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
             <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" crossorigin></script>
           </head>
           <body>
@@ -24,6 +34,7 @@ export default {
       testRunnerHtml: testFramework =>
         `<html>
           <head>
+            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
             <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
             <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
             <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>

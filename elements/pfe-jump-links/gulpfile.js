bf6c037952925854977d1f1a66c43a8067a8f767
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-07-06 16:05:06 -0400

    fix: Revert gulp updates

--- elements/pfe-jump-links/gulpfile.js
+++ elements/pfe-jump-links/gulpfile.js
@@ -1,48 +1,4 @@
 const gulpFactory = require("../../scripts/gulpfile.factory.js");
 const pfelementPackage = require("./package.json");
 
-const { task, src, series } = require("gulp");
-const jsdoc = require("gulp-jsdoc3");
-const del = require("del");
-
-task("clean:jsdoc", () =>
-  del(["demo/**", "!demo/example.html"], {
-    read: false,
-    allowEmpty: true,
-  })
-);
-
-task("build:jsdoc", (cb) => {
-  src(["README.md", "dist/pfe-jump-links.js"], {
-    read: false,
-    allowEmpty: true,
-  }).pipe(
-    jsdoc(
-      {
-        opts: {
-          destination: "jsdoc/",
-          template: "../../node_modules/foodoc/template",
-        },
-        // https://github.com/steveush/foodoc#configuring-the-template
-        templates: {
-          systemName: "PatternFly Elements",
-          systemSummary: "A set of community-created web components based on PatternFly design.",
-          systemLogo: "../../brand/logo/svg/pfe-icon-white-shaded.svg",
-          favicon: "../../brand/logo/svg/pfe-icon-blue-shaded.svg",
-          systemColor: "rgb(0, 64, 128)",
-          copyright: "©2021 Red Hat, Inc.",
-          includeDate: true,
-          dateFormat: "YYYY MMM DD",
-          showAccessFilter: false,
-          collapseSymbols: false,
-          stylesheets: ["../pfe-styles/dist/pfe-base.min.css"],
-        },
-      },
-      cb
-    )
-  );
-});
-
-task("jsdoc", series("clean:jsdoc", "build:jsdoc"));
-
-gulpFactory(Object.assign(pfelementPackage, { postbundle: ["jsdoc"] }));
+gulpFactory(pfelementPackage);

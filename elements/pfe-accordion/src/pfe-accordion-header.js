2830fd12704c2deffa6ac4b34bb8db84668443a8
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-07-29 17:06:20 -0400

    fix: Migrate tests to new testing framework

--- elements/pfe-accordion/src/pfe-accordion-header.js
+++ elements/pfe-accordion/src/pfe-accordion-header.js
@@ -49,7 +49,6 @@ class PfeAccordionHeader extends PFElement {
       expanded: {
         title: "Expanded",
         type: Boolean,
-        cascade: "#pfe-accordion-header--button",
         observer: "_expandedChanged",
       },
     };

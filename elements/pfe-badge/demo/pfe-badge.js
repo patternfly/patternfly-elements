818b5a9a4d67c96dd29dbeacf3de7a319a61439d
Benny Powers <bepowers@redhat.com>
2021-10-25 16:14:16 +0300

    feat(badge)!: migrate pfe-badge to lit
    **Fixes**
    - add {{version}} to element classes
    
    **Tests**
    - e2e tests with page object model
    - build demo with optional shadowroot
    
    **Docs**
    - rearrange docs urls
    - inline docs
    - refactor docs page
    - add changeset

---
+++ elements/pfe-badge/demo/pfe-badge.js
@@ -0,0 +1,17 @@
+import '@patternfly/pfe-badge';
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-codeblock';
+import '@patternfly/pfe-select';
+
+const root = document.querySelector('[data-demo="pfe-badge"]')?.shadowRoot ?? document;
+
+for (const el of root.querySelectorAll('pfe-badge')) {
+  const code = el.closest('pfe-band')?.querySelector?.('code');
+  if (code) {
+    code.textContent = (el.outerHTML
+      .replace(' pfelement=""', '')
+      .replace(' class="PFElement"', '')
+      .replace(/ on="(null|light)"/g, '')
+    );
+  }
+}

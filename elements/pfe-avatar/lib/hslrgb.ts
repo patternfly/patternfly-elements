f09531f618b70c99d34f381ba2179063355f2d55
Benny Powers <bepowers@redhat.com>
2021-10-25 16:14:07 +0300

    feat(avatar)!: migrate pfe-avatar to lit

--- elements/pfe-avatar/lib/hslrgb.ts
+++ elements/pfe-avatar/lib/hslrgb.ts
@@ -1,11 +1,26 @@
-function h2rgb(v1, v2, vH) {
-  if (vH < 0) vH += 1;
-  if (vH > 1) vH -= 1;
-  if (6 * vH < 1) return v1 + (v2 - v1) * 6 * vH;
-  if (2 * vH < 1) return v2;
-  if (3 * vH < 2) return v1 + (v2 - v1) * (2 / 3 - vH) * 6;
+/* eslint-disable prefer-const */
+
+function h2rgb(v1: number, v2: number, vH: number): number {
+  if (vH < 0) {
+    vH += 1;
+  }
+  if (vH > 1) {
+    vH -= 1;
+  }
+  if (6 * vH < 1) {
+    return v1 + (v2 - v1) * 6 * vH;
+  }
+  if (2 * vH < 1) {
+    return v2;
+  }
+  if (3 * vH < 2) {
+    return v1 + (v2 - v1) * (2 / 3 - vH) * 6;
+  }
   return v1;
 }
+
+export type RGBTriple = [R: number, G: number, B: number];
+export type HSLTriple = [H: number, S: number, L: number];
 
 /**
  * Convert an HSL color to RGB.
@@ -17,25 +32,26 @@ function h2rgb(v1, v2, vH) {
  *
  * @see https://www.easyrgb.com/en/math.php
  */
-export function hsl2rgb(_H, _S, _L) {
-  let R, G, B;
+export function hsl2rgb(_H: number, _S: number, _L: number): RGBTriple {
+  let R; let G; let B;
 
   const H = Math.max(0, Math.min(1, _H));
   const S = Math.max(0, Math.min(1, _S));
   const L = Math.max(0, Math.min(1, _L));
 
-  if (S == 0) {
+  if (S === 0) {
     R = L * 255;
     G = L * 255;
     B = L * 255;
   } else {
-    let a, b;
+    let a; let b;
 
     if (L < 0.5) {
       b = L * (1 + S);
     } else {
       b = L + S - S * L;
     }
+
 
     a = 2 * L - b;
 
@@ -57,8 +73,8 @@ export function hsl2rgb(_H, _S, _L) {
  *
  * @see https://www.easyrgb.com/en/math.php
  */
-export function rgb2hsl(_R, _G, _B) {
-  let H, S, L;
+export function rgb2hsl(_R: number, _G: number, _B: number): HSLTriple {
+  let H; let S; let L;
 
   const R = Math.max(0, Math.min(255, _R));
   const G = Math.max(0, Math.min(255, _G));
@@ -68,33 +84,36 @@ export function rgb2hsl(_R, _G, _B) {
   const g = G / 255;
   const b = B / 255;
 
-  const var_min = Math.min(Math.min(r, g), b);
-  const var_max = Math.max(Math.max(r, g), b);
-  const del_max = var_max - var_min;
+  const varMin = Math.min(Math.min(r, g), b);
+  const varMax = Math.max(Math.max(r, g), b);
+  const delMax = varMax - varMin;
 
-  L = (var_max + var_min) / 2;
+  L = (varMax + varMin) / 2;
 
-  if (del_max === 0) {
+  if (delMax === 0) {
     H = 0;
     S = 0;
   } else {
     if (L < 0.5) {
-      S = del_max / (var_max + var_min);
+      S = delMax / (varMax + varMin);
     } else {
-      S = del_max / (2 - var_max - var_min);
+      S = delMax / (2 - varMax - varMin);
     }
 
-    const del_r = ((var_max - r) / 6 + del_max / 2) / del_max;
-    const del_g = ((var_max - g) / 6 + del_max / 2) / del_max;
-    const del_b = ((var_max - b) / 6 + del_max / 2) / del_max;
 
-    if (r == var_max) {
-      H = del_b - del_g;
-    } else if (g == var_max) {
-      H = 1 / 3 + del_r - del_b;
-    } else if (b == var_max) {
-      H = 2 / 3 + del_g - del_r;
+    const delR = ((varMax - r) / 6 + delMax / 2) / delMax;
+    const delG = ((varMax - g) / 6 + delMax / 2) / delMax;
+    const delB = ((varMax - b) / 6 + delMax / 2) / delMax;
+
+    if (r === varMax) {
+      H = delB - delG;
+    } else if (g === varMax) {
+      H = 1 / 3 + delR - delB;
+    } else if (b === varMax) {
+      H = 2 / 3 + delG - delR;
     }
+
+    H ??= 0;
 
     if (H < 0) {
       H += 1;

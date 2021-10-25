8549870ef83e491982b93b27f1fc89d4396cd187
Benny Powers <bepowers@redhat.com>
2021-10-25 16:14:23 +0300

    feat(button)!: migrate pfe-button to lit
    **Features**
    
    - synchronize `type` attr
    
    **Breaking Changes**
    - use ::slotted button instead of copying button into shadow DOM
    
    **Fixes**
    
    - slotted button styles
    - add {{version}} to element classes
    - reflect disabled attribute
    
    **Tests**
    
    test(button): e2e tests
    test(button): e2e tests with page object model
    
    **Docs**
    
    - todo for context support
    - fix demo
    - inline docs
    - add changeset
    
    fix(button): rename `pfeEvent` to `deprecatedCustomEvent`

---
+++ elements/pfe-button/test/pfe-button.spec.ts
@@ -0,0 +1,78 @@
+import { expect, html, oneEvent } from '@open-wc/testing';
+import { spy } from 'sinon';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { PfeButton } from '@patternfly/pfe-button';
+
+// reference the imported value in runtime code so that typescript doesn't strip the import
+PfeButton;
+
+const element = html`
+  <pfe-button>
+    <button>Button</button>
+  </pfe-button>
+`;
+
+const badElement = html`
+  <pfe-button>
+    <div>Bad button</div>
+  </pfe-button>
+`;
+
+describe('<pfe-button>', function() {
+  it('should upgrade', async function() {
+    const el = await createFixture(element);
+    expect(el, 'pfe-button should be an instance of PfeButton')
+      .to.be.an.instanceOf(customElements.get('pfe-button'))
+      .and
+      .to.be.an.instanceOf(PfeButton);
+  });
+
+  it('should log a console warning if the light dom inside pfe-button is not a button', async function() {
+    const spyConsole = spy(console, 'warn');
+    const el = await createFixture(badElement);
+
+    expect(el).to.exist;
+    expect(spyConsole, 'The only child in the light DOM must be a button tag')
+      .to.have.been.calledWith('[pfe-button]');
+    spyConsole.restore();
+  });
+
+  it('should synchronize disabled attribute to the light DOM button', async function() {
+    const el = await createFixture<PfeButton>(element);
+    const lightDomBtn = el.querySelector('button')!;
+
+    el.disabled = true;
+    await el.updateComplete;
+
+    expect(lightDomBtn).dom.to.equal('<button disabled>Button</button>');
+
+    el.disabled = false;
+    await el.updateComplete;
+
+    expect(lightDomBtn).dom.to.equal('<button>Button</button>');
+  });
+
+  it('should synchronize type attribute to the light DOM button', async function() {
+    const el = await createFixture<PfeButton>(element);
+    const lightDomBtn = el.querySelector('button')!;
+
+    el.type = 'reset';
+    await el.updateComplete;
+
+    expect(el.type).to.equal('reset');
+    expect(lightDomBtn).dom.to.equal('<button type="reset">Button</button>');
+
+    el.type = undefined;
+    await el.updateComplete;
+
+    expect(lightDomBtn).dom.to.equal('<button>Button</button>');
+  });
+
+  it('should send a pfe-button:click event on click', async function() {
+    const el = await createFixture<PfeButton>(element);
+    setTimeout(() => el.querySelector('button')!.click());
+    const event = await oneEvent(document, 'pfe-button:click');
+    expect(event).to.be.ok;
+    expect(event.type).to.equal('pfe-button:click');
+  });
+});

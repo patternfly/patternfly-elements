66744b3b4756a38738d5539c6b6ea501bf87b50e
Benny Powers <bepowers@redhat.com>
2021-10-27 20:31:58 +0300

    feat(card)!: migrate pfe-card to lit
    test(card): adjust test for changes in slot controller
    
    test(card): e2e tests

---
+++ elements/pfe-card/test/pfe-card.e2e.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from '@playwright/test';
+import { test } from '@patternfly/pfe-tools/test/playwright/fixtures.js';
+
+test.describe('pfe-card', () => {
+  test('snapshot', async ({ page }) => {
+    await page.goto('http://localhost:8080/demo/pfe-card/');
+    await page.waitForLoadState('networkidle');
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot('pfe-card.png');
+  });
+});

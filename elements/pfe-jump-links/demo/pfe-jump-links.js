7a34c1d3844e6061a66c2e31efc1c9208415c56c
Benny Powers <bepowers@redhat.com>
2021-10-31 09:14:31 +0200

    feat(jump-links)!: migrate pfe-jump-links to lit
    fix(jump-links): doc updates and fixes (#1829)
    
    * chore: remove old demo sticky nav
    
    * fix: enable offset to be set to zero
    
    * chore: remove old demo panel offset
    
    * fix: accent-bar mixin wasn't setting proper width
    
    * feat: support multiple nested scroll containers
    
    * chore: update docs
    
    * remove duplicate background-color property.
    
    feat(jump-links): migrate events to ComposedEvent
    
    fix(jump-links): type of ActiveItemEvent param
    
    feat: changed event names/types for change, stuck & content-change. Updated tests
    
    style(jump-links): lint
    
    test(jump-links): update demo file
    
    test(jump-links): e2e tests with page object model
    
    test(jump-links): build demo with optional shadowroot
    
    docs(jump-links): rearrange docs urls
    
    Co-Authored-By:    Michael Potter <mgp140@gmail.com>
    
    fix(jump-links): prevent uncaught error
    
    if there are no links and autobuild is unset
    
    docs(jump-links): inline docs
    
    docs(jump-links): refactor docs page
    
    docs(jump-links): add changeset

---
+++ elements/pfe-jump-links/demo/pfe-jump-links.js
@@ -0,0 +1,22 @@
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-jump-links';
+
+const root = document.querySelector('[data-demo="pfe-jump-links"]')?.shadowRoot ?? document;
+
+import { installRouter } from 'pwa-helpers/router.js';
+
+/**
+ * scroll to element with id in the URL hash.
+ * @this {HTMLElement}
+ */
+async function route(location = window.location, event) {
+  event.preventDefault();
+  event.stopPropagating();
+  if (location.hash) {
+    root?.querySelector(location.hash)?.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+installRouter(route);
+
+route();

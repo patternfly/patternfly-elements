2bd6fdb8a8fdc6e8a44e16c213982f718b568d4f
Benny Powers <bepowers@redhat.com>
2021-10-31 09:14:22 +0200

    feat(icon-panel)!: migrate pfe-icon-panel to lit

---
+++ elements/pfe-icon-panel/demo/pfe-icon-panel.js
@@ -0,0 +1,11 @@
+const PfeIcon = customElements.get('pfe-icon');
+
+PfeIcon.addIconSet(
+  'local',
+  '/elements/pfe-icon/demo/',
+  function (name, iconSetName, iconSetPath) {
+    const regex = new RegExp(`^${iconSetName}-(.*)`);
+    const [, match] = regex.exec(name);
+    return `${iconSetPath}${match}.svg`;
+  }
+);

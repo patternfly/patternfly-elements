358971aabc35e1663cd6e9cd547c32298be7887d
Benny Powers <bepowers@redhat.com>
2021-10-31 09:15:29 +0200

    feat(styles)!: migrate pfe-styles to dart-sass
    Moves pfe-styles to the 'core' section of monorepo

---
+++ core/pfe-styles/test/pfe-styles.e2e.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from '@playwright/test';
+import { test } from '@patternfly/pfe-tools/test/playwright/fixtures.js';
+
+test.describe('pfe-styles', () => {
+  test('snapshot', async ({ page }) => {
+    await page.goto('https://localhost:8080/core/pfe-styles/');
+    await page.waitForLoadState('networkidle');
+    expect(await page.screenshot()).toMatchSnapshot('pfe-styles.png');
+  });
+});

66e43de4038611a1a4813097b46333c24accc124
Benny Powers <bepowers@redhat.com>
2021-10-31 09:24:54 +0200

    feat(core)!: add pfe-core and remove pfelement
    Removes PFElement base class, renames pfelement package to pfe-core, and
    moves it to the 'core' section of monorepo
    
    feat(core): improve slot controller performance
    
    feat(core): ComposedEvent
    
    A composed, bubbling event for UI interactions.
    PFE components that fire events should subclass ComposedEvent.
    
    test(core): e2e tests
    
    fix(core): remove has_ attrs
    
    feat(core): implement autoreveal
    
    adds `window.PfeConfig.autoReveal = false` to disable auto reveal
    
    fix(core): better autoreveal
    
    add optional `no-auto-reveal` attr on body
    
    style(core): lint
    
    perf(core): optimize `@observed` decorator
    
    now caches values until first update, then removes the caching controller
    
    docs(core): add README
    
    docs(core): add changeset

---
+++ core/pfe-core/core.ts
@@ -0,0 +1,94 @@
+import type { ComplexAttributeConverter } from 'lit';
+
+export interface PfeConfig {
+  /** Set to false to disable client-side page load performance tracking */
+  trackPerformance?: boolean;
+  /** Set to false to disable various debug logs */
+  log?: boolean;
+  /** Set to false to disable automatically removing `unresolved` attr from body */
+  autoReveal?: boolean;
+}
+
+export interface Breakpoints {
+  /** $pf-global--breakpoint--xs: 0 !default; */
+  'xs': '0px',
+  /** $pf-global--breakpoint--sm: 576px !default; */
+  'sm': '576px',
+  /** $pf-global--breakpoint--md: 768px !default; */
+  'md': '768px',
+  /** $pf-global--breakpoint--lg: 992px !default; */
+  'lg': '992px',
+  /** $pf-global--breakpoint--xl: 1200px !default; */
+  'xl': '1200px',
+  /** $pf-global--breakpoint--2xl: 1450px !default; */
+  '2xl': '1450px',
+}
+
+export type ContextTheme = (
+  | 'dark'
+  | 'light'
+  | 'saturated'
+);
+
+/**
+ * A boolean value that indicates if the performance should be tracked.
+ * For use in a JS file or script tag; can also be added in the constructor of a component during development.
+ * @example trackPerformance(true);
+ */
+export function trackPerformance(preference = null) {
+  if (preference !== null) {
+    window.PfeConfig.trackPerformance = false;
+  }
+  return window.PfeConfig.trackPerformance;
+}
+
+export const NumberListConverter: ComplexAttributeConverter<null|number[]> = {
+  fromAttribute(value: string) {
+    if (typeof value !== 'string') {
+      return null;
+    } else {
+      return value.split(',').map(x => x.trim()).map(x => parseInt(x, 10));
+    }
+  },
+  toAttribute(value: number[]) {
+    return value.join(',');
+  },
+};
+
+/**
+ * A composed, bubbling event for UI interactions
+ * e.g. when an accordion panel opens.
+ */
+export class ComposedEvent extends Event {
+  constructor(type: string, init?: EventInit) {
+    super(type, {
+      bubbles: true,
+      composed: true,
+      ...init
+    });
+  }
+}
+
+declare global {
+  interface Window {
+    /** Global configuration settings for PatternFly Elements */
+    PfeConfig: PfeConfig;
+  }
+}
+
+// 👇 SIDE EFFECTS 👇
+
+/** Global patternfly elements config */
+window.PfeConfig = Object.assign(window.PfeConfig ?? {}, {
+  autoReveal: window.PfeConfig?.autoReveal ?? !document.body.hasAttribute('no-auto-reveal'),
+  get log() {
+    return !!localStorage.pfeLog;
+  },
+  set log(v: boolean) {
+    if (v) {
+      localStorage.setItem('pfeLog', `${true}`);
+    } else {
+      localStorage.removeItem('pfeLog');
+    }
+  },
+});

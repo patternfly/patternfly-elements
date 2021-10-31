942a2eb83e210b112aeccde663c89b6878013e31
Benny Powers <bepowers@redhat.com>
2021-10-31 09:24:54 +0200

    feat(core)!: add pfe-core and remove pfelement
    Removes PFElement base class, renames pfelement package to pfe-core, and moves it to the 'core' section of monorepo
    
    **Features**
    
    - add `SlotController`
    - add `LightDOMController`
    - add `ColorContextController`
    - add `CSSVariableController`
    - add `Logger` controller
    - add `PropertyObserverController`
    - add `ComposedEvent`
      - A composed, bubbling event for UI interactions.
      - PFE components that fire events should subclass ComposedEvent.
    - add `@trace` decorator
    - add `@time` performance-tracking method decorator
    - add `@observed` decorator
      - now caches values until first update, then removes the caching controller
    - add `deprecatedCustomEvent` function
    - implement autoreveal
      - adds `window.PfeConfig.autoReveal = false` to disable auto reveal
      - adds optional `no-auto-reveal` attr on body
    - allow document meta tag configuration
    - export a color context type
    - add StyleController to compose CSS into reactive controllers
    - deprecate the pfe-contexts mixin
    
    **Fixes**
    
    - allow perf controller when trackPerformance is true
    - remove context variable if context is `null`
    
    **Breaking Changes**
    
    - remove `has_` attrs
    - remove Breakpoints interface from core
      - to be moved to jump-links, it's only consumer
    - moves core styles (i.e. pfe.min.css) to `pfe-styles`
    
    Co-Authored-By: Michael Potter <heyMP@users.noreply.github.com>
    Co-Authored-By: Steven Spriggs <steven.spriggs@gmail.com>

---
+++ core/pfe-core/controllers/property-observer-controller.ts
@@ -0,0 +1,52 @@
+import type { ReactiveController, ReactiveElement } from 'lit';
+
+export const observedController = Symbol('observed properties controller');
+
+export type ChangeCallback<T = ReactiveElement> = (
+  this: T,
+  old?: T[keyof T],
+  newV?: T[keyof T],
+) => void;
+
+export type ChangeCallbackName = `_${string}Changed`;
+
+export type PropertyObserverHost<T> = T & Record<ChangeCallbackName, ChangeCallback<T>> & {
+  [observedController]: PropertyObserverController;
+}
+
+/** This controller holds a cache of observed property values which were set before the element updated */
+export class PropertyObserverController implements ReactiveController {
+  private static hosts: WeakMap<HTMLElement, PropertyObserverController> = new WeakMap();
+
+  private values = new Map<string, [methodName: string, values: [unknown, unknown]]>();
+
+  private delete(key: string) {
+    this.values.delete(key);
+  }
+
+  constructor(private host: ReactiveElement) {
+    if (PropertyObserverController.hosts.get(host)) {
+      return PropertyObserverController.hosts.get(host) as PropertyObserverController;
+    }
+    host.addController(this);
+    (host as PropertyObserverHost<ReactiveElement>)[observedController] = this;
+  }
+
+  /** Set any cached valued accumulated between constructor and connectedCallback */
+  hostUpdate() {
+    for (const [key, [methodName, [oldVal, newVal]]] of this.values) {
+      // @ts-expect-error: be cool, typescript
+      this.host[methodName as keyof ReactiveElement]?.(oldVal, newVal);
+      this.delete(key);
+    }
+  }
+
+  /** Once the element has updated, we no longer need this controller, so we remove it */
+  hostUpdated() {
+    this.host.removeController(this);
+  }
+
+  cache(key: string, methodName: string, ...vals: [unknown, unknown]) {
+    this.values.set(key, [methodName, vals]);
+  }
+}

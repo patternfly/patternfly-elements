a2ffba67d489fa107a13e8954b8e768964020f38
Benny Powers <bepowers@redhat.com>
2021-10-31 09:14:38 +0200

    feat(modal)!: migrate pfe-modal to lit
    fix(modal): compose UI events
    
    test(pfe-modal): e2e tests
    
    test(pfe-modal): fix types in e2e tests

---
+++ elements/pfe-modal/test/pfe-modal.e2e.spec.ts
@@ -0,0 +1,26 @@
+import type { LitElement } from 'lit';
+
+import { expect } from '@playwright/test';
+import { test } from '@patternfly/pfe-tools/test/playwright/fixtures.js';
+
+const tagName = 'pfe-modal';
+
+test.describe(tagName, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`http://localhost:8080/demo/${tagName}/`);
+    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(100);
+    await page.evaluate(async x => customElements.whenDefined(x), tagName);
+    await page.$eval(tagName, async (el: LitElement) => el.updateComplete);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await page.$eval(`${tagName} button`, (el: HTMLElement) => el.click());
+    await page.$eval(tagName, async (el: LitElement) => el.updateComplete);
+    await page.waitForTimeout(100);
+  });
+
+  test('snapshot', async ({ page }) => {
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`${tagName}.png`);
+  });
+});

22109bd7ccdb47d803059501c6d21de7dfbf2310
Benny Powers <bepowers@redhat.com>
2021-10-31 09:14:35 +0200

    feat(markdown)!: migrate pfe-markdown to lit

---
+++ elements/pfe-markdown/demo/pfe-markdown.js
@@ -0,0 +1,38 @@
+const { shadowRoot: root } = document.querySelector('[data-demo="pfe-markdown"]');
+
+const markdownSample = `
+Sometimes you want numbered lists:
+
+1. One
+2. Two
+3. Three
+
+Sometimes you want bullet points:
+
+* Start a line with a star
+* Profit!
+
+Alternatively,
+
+- Dashes work just as well
+- And if you have sub points, put two spaces before the dash or star:
+\t- Like this
+\t- And this`.trimStart();
+
+const firstMarkdownSample = "# First Markdown Sample";
+const secondMarkdownSample = "## Second Markdown Sample";
+
+root.getElementById('update-dynamic').addEventListener('click', function () {
+  root.querySelector("#markdownTag").innerHTML = markdownSample;
+});
+
+root.querySelector("#markdownTagSecondTime").innerHTML = firstMarkdownSample;
+
+root.getElementById('update-dynamic-twice').addEventListener('click', function () {
+  root.querySelector("#markdownTagSecondTime").innerHTML = secondMarkdownSample;
+});
+
+root.getElementById('update-light-dom').addEventListener('click', function () {
+  const element = root.querySelector("#markdownTagUpdatedLightDom");
+  element.innerHTML += "\n### This is a heading level 3";
+});

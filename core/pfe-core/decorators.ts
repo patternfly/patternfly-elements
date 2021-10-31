85396ab305312a896b63a25534c3887378fbc7d0
Benny Powers <bepowers@redhat.com>
2021-10-31 09:24:54 +0200

    feat(core)!: add pfe-core and remove pfelement
    Removes PFElement base class, renames pfelement package to pfe-core, and
    moves it to the 'core' section of monorepo
    
    feat(core): improve slot controller performance
    
    feat(core): ComposedEvent
    
    A composed, bubbling event for UI interactions.
    PFE components that fire events should subclass ComposedEvent.

---
+++ core/pfe-core/decorators.ts
@@ -0,0 +1,5 @@
+export * from './decorators/bound.js';
+export * from './decorators/cascades.js';
+export * from './decorators/initializer.js';
+export * from './decorators/observed.js';
+export * from './decorators/pfelement.js';

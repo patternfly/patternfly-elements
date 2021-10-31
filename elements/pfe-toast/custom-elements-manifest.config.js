8469bdecbad549c528f3fbbb3bbacbe1c6630665
Benny Powers <bepowers@redhat.com>
2021-10-31 09:15:37 +0200

    feat(toast)!: migrate pfe-toast to lit
    **Features**
    
    - add CSS parts
    - add static `toast(text)` method
      - allows users to create an ephemeral toast via JS
    
    **Fixes**
    
    - compose UI events
      - rename `pfeEvent` to `deprecatedCustomEvent`
    - add {{version}} to element classes
    
    **Docs**
    - 2.0 [pfe-toast] update docs (#1826)
      - pfe-toast update docs
      - add .no-header-styles opt-out class for headers
    - rearrange docs urls
    - inline docs
    - refactor docs page
    - add changeset
    - use pfe-styles
    
    **Tests**
    
    - e2e tests with page object model
    - build demo with optional shadowroot
    
    Co-Authored-By: Michael Potter <mgp140@gmail.com>
    Co-Authored-By: Steven Spriggs <steven.spriggs@gmail.com>

---
+++ elements/pfe-toast/custom-elements-manifest.config.js
@@ -0,0 +1,5 @@
+import { pfeCustomElementsManifestConfig } from '@patternfly/pfe-tools/custom-elements-manifest.js';
+
+export default pfeCustomElementsManifestConfig({
+  globs: ['pfe-*.ts'],
+});
---
+++ elements/pfe-toast/test/pfe-toast.e2e.spec.ts
@@ -0,0 +1,14 @@
+import { test } from '@playwright/test';
+import { PfeDemoPage } from '@patternfly/pfe-tools/test/playwright/PfeDemoPage.js';
+
+const tagName = 'pfe-toast';
+
+test.describe(tagName, () => {
+  test('snapshot', async ({ page }) => {
+    const componentPage = new PfeDemoPage(page, tagName);
+    await componentPage.navigate();
+    await page.click('[data-toast="toast2"]');
+    await page.waitForTimeout(1000);
+    await componentPage.snapshot();
+  });
+});

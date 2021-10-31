6aaba9c4e571d030404391a2453d92b0a8cd8891
Benny Powers <bepowers@redhat.com>
2021-10-31 09:15:37 +0200

    feat(toast)!: migrate pfe-toast to lit
    **Features**
    
    - add CSS parts
    - add static `toast(text)` method
      - allows users to create an ephemeral toast via JS
    
    **Fixes**
    
    - compose UI events
      - rename `pfeEvent` to `deprecatedCustomEvent`
    - add {{version}} to element classes
    
    **Docs**
    - 2.0 [pfe-toast] update docs (#1826)
      - pfe-toast update docs
      - add .no-header-styles opt-out class for headers
    - rearrange docs urls
    - inline docs
    - refactor docs page
    - add changeset
    - use pfe-styles
    
    **Tests**
    
    - e2e tests with page object model
    - build demo with optional shadowroot
    
    Co-Authored-By: Michael Potter <mgp140@gmail.com>
    Co-Authored-By: Steven Spriggs <steven.spriggs@gmail.com>

---
+++ elements/pfe-toast/demo/pfe-toast.js
@@ -0,0 +1,17 @@
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-cta';
+import { PfeToast } from '@patternfly/pfe-toast';
+
+const root = document.querySelector('[data-demo="pfe-toast"]')?.shadowRoot ?? document;
+const jsToastButton = root.getElementById('js-toast');
+const contentInput = root.getElementById('content');
+
+for (const button of root.querySelectorAll('[data-toast]')) {
+  button.addEventListener('click', function(event) {
+    root.getElementById(event.target.dataset.toast).toggle();
+  });
+}
+
+jsToastButton.addEventListener('click', () => {
+  PfeToast.toast(contentInput.value);
+});

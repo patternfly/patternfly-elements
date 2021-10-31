8e5642f5a253b6a5785fea8128ce8c7213ec43a0
Benny Powers <bepowers@redhat.com>
2021-10-31 09:13:52 +0200

    feat(clipboard)!: migrate pfe-clipboard to lit
    fix(clipboard): hide deprecated slot when not in use
    
    fix(clipboard): add CopiedEvent
    
    feat(clipboard): Updated size of copy icon for Safari display (#1824)
    
    style(clipboard): lint
    
    style(clipboard): lint
    
    test(clipboard): e2e tests
    
    test(clipboard): e2e tests with page object model
    
    test(clipboard): build demo with optional shadowroot
    
    docs(clipboard): rearrange docs urls
    
    Co-authored-by: Rob Lahoda <rob@roblahoda.com>
    
    docs(clipboard): inline docs
    
    docs(clipboard): refactor docs page
    
    docs(clipboard): add changeset

---
+++ elements/pfe-clipboard/demo/pfe-clipboard.js
@@ -0,0 +1,14 @@
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-clipboard';
+import '@patternfly/pfe-icon';
+
+const root = document.querySelector('[data-demo="pfe-clipboard"]')?.shadowRoot ?? document;
+
+root.getElementById('propertyCopy').contentToCopy = `
+  <h2>Clipboard: with custom text & copying text from element</h2>
+  <pfe-clipboard role="button" tabindex="0" copy-from="#textToCopy">
+    <span slot="text">This will copy the text in the text field below!</span>
+    <span slot="success">Making some copies!</span>
+  </pfe-clipboard>
+  <input type="text" id="textToCopy" value="This text will be copied!!11"></input>
+`;

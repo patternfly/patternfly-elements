3ffb30ed81afb92f47703b69b014fb0e96c9ca2d
Benny Powers <bepowers@redhat.com>
2021-10-31 09:14:17 +0200

    feat!: migrate pfe-health-index

--- elements/pfe-health-index/docs/index.md
+++ elements/pfe-health-index/docs/index.md
@@ -1,5 +1,5 @@
 ---
-layout: layout-basic.html
+layout: layout-basic.njk
 title: Health index
 description: Shows a health grade
 package: pfe-health-index
@@ -9,142 +9,68 @@ tags:
   - component
 ---
 
-::: section header
-# {{ title }}
-:::
-
-::: section
-## Overview
-Health index shows a health grade in either a default, mini, or large variation.
-
-<div class="pfe-l-grid pfe-m-gutters pfe-m-all-4-col">
-  <section>
-    <h3>Default</h3>
-    <div>
+{% renderOverview for=package, title=title %}
+  <div class="pfe-l-grid pfe-m-gutters pfe-m-all-4-col">
+    <section style="display: grid">
+      <h3>Default</h3>
       <pfe-health-index health-index="A">A</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="B">B</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="C">C</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="D">D</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="E">E</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="F">F</pfe-health-index>
-    </div>
-  </section>
-
-  <section>
-    <h3>Mini</h3>
-    <div>
+    </section>
+    <section>
+      <h3>Mini</h3>
       <pfe-health-index health-index="A" size="mini">A</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="B" size="mini">B</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="C" size="mini">C</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="D" size="mini">D</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="E" size="mini">E</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="F" size="mini">F</pfe-health-index>
-    </div>
-  </section>
-
-  <section>
-    <h3>Large</h3>
-    <div>
+    </section>
+    <section>
+      <h3>Large</h3>
       <pfe-health-index health-index="A" size="lg">A</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="B" size="lg">B</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="C" size="lg">C</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="D" size="lg">D</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="E" size="lg">E</pfe-health-index>
-    </div>
-    <div>
       <pfe-health-index health-index="F" size="lg">F</pfe-health-index>
-    </div>
-  </section>
-</div>
-:::
+    </section>
+  </div>
+{% endrenderOverview %}
 
-::: section
-## Installation
+{% band header="Usage" %}
+  ### Default
+  <pfe-health-index health-index="A">A</pfe-health-index>
+  ```html
+  <pfe-health-index health-index="A">A</pfe-health-index>
+  ```
 
-```shell
-npm install @patternfly/{{ package }}
-```
-:::
+  ### Mini
+  <pfe-health-index health-index="A" size="mini">A</pfe-health-index>
+  ```html
+  <pfe-health-index health-index="A" size="mini">A</pfe-health-index>
+  ```
 
-::: section
-## Usage
+  ### Large
+  <pfe-health-index health-index="A" size="lg">A</pfe-health-index>
+  ```html
+  <pfe-health-index health-index="A" size="lg">A</pfe-health-index>
+  ```
+{% endband %}
 
-### Default
-<pfe-health-index health-index="A">A</pfe-health-index>
-```html
-<pfe-health-index health-index="A">A</pfe-health-index>
-```
+{% renderSlots for=package %}{% endrenderSlots %}
 
-### Mini
-<pfe-health-index health-index="A" size="mini">A</pfe-health-index>
-```html
-<pfe-health-index health-index="A" size="mini">A</pfe-health-index>
-```
+{% renderAttributes for=package %}{% endrenderAttributes %}
 
-### Large
-<pfe-health-index health-index="A" size="lg">A</pfe-health-index>
-```html
-<pfe-health-index health-index="A" size="lg">A</pfe-health-index>
-```
-:::
+{% renderProperties for=package %}{% endrenderProperties %}
 
-::: section
-## Slots
-None
-:::
+{% renderMethods for=package %}{% endrenderMethods %}
 
-::: section
-## Attributes
-### health-index (observed)
+{% renderEvents for=package %}{% endrenderEvents %}
 
-Sets the value for the health index in the UI.
+{% renderCssCustomProperties for=package %}{% endrenderCssCustomProperties %}
 
-### size (observed)
-
-Changes the size.
-
-Possible values: `mini`, `lg`
-:::
-
-::: section
-## Methods
-None
-:::
-
-::: section
-## Events
-None
-:::
-
-::: section
-## Styling hooks
-None
-:::
+{% renderCssParts for=package %}{% endrenderCssParts %}
---
+++ elements/pfe-health-index/test/pfe-health-index.spec.ts
@@ -0,0 +1,197 @@
+import { expect, html, nextFrame, aTimeout } from '@open-wc/testing';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+
+import { PfeHealthIndex } from '@patternfly/pfe-health-index';
+
+import { spy, SinonSpy } from 'sinon';
+
+describe('<pfe-health-index>', function() {
+  let consoleSpy: SinonSpy;
+
+  const TEMPLATES = {
+    simple: html`<pfe-health-index></pfe-health-index>`,
+    original: html`<pfe-health-index id="original" health-index="A">A</pfe-health-index>`,
+    lowercase: html`<pfe-health-index id="lowercase" health-index="a">a</pfe-health-index>`,
+    badIndex: html`<pfe-health-index id="badIndex">fdsa</pfe-health-index>`,
+    changingIndex: html`<pfe-health-index id="changingIndex" health-index="A">A</pfe-health-index>`,
+    A: html`<pfe-health-index id="A" health-index="A">A</pfe-health-index>`,
+    B: html`<pfe-health-index id="B" health-index="B">B</pfe-health-index>`,
+    C: html`<pfe-health-index id="C" health-index="C">C</pfe-health-index>`,
+    D: html`<pfe-health-index id="D" health-index="D">D</pfe-health-index>`,
+    E: html`<pfe-health-index id="E" health-index="E">E</pfe-health-index>`,
+    F: html`<pfe-health-index id="F" health-index="F">F</pfe-health-index>`,
+    largeSize: html`<pfe-health-index id="largeSize" health-index="A">A</pfe-health-index>`,
+    miniSize: html`<pfe-health-index id="miniSize" health-index="A" size="mini">A</pfe-health-index>`,
+  };
+
+  beforeEach(function() {
+    consoleSpy = spy(console, 'warn');
+  });
+
+  afterEach(function() {
+    consoleSpy.restore();
+  });
+
+  it('should upgrade', async function() {
+    const element = await createFixture<PfeHealthIndex>(TEMPLATES.simple);
+    expect(element, 'the <pfe-health-index> should be an instance of PfeHealthIndex')
+      .to.be.an.instanceof(customElements.get('pfe-health-index'))
+      .and
+      .to.be.an.instanceof(PfeHealthIndex);
+  });
+
+  it('should show a health index', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.original);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('A');
+    expect(activeBox?.classList.contains('a')).to.be.true;
+    expect(shadowRootTextContent).to.equal('A');
+  });
+
+  it('should show a health index with uppercase text when given lowercase text', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.lowercase);
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    await healthIndex.updateComplete;
+    expect(activeBox?.classList.contains('a')).to.be.true;
+    expect(shadowRootTextContent).to.equal('A');
+  });
+
+  it('should show a console warning if an invalid health index is provided', async function() {
+    await createFixture<PfeHealthIndex>(TEMPLATES.badIndex);
+
+    await nextFrame();
+
+    expect(consoleSpy)
+      .to.have.been.calledOnceWith(
+        '[pfe-health-index#badIndex]',
+        'a valid health-index was not provided. Please use A, B, C, D, E, or F'
+      );
+  });
+
+  it('should change the index if the health-index attribute is updated', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.changingIndex);
+    await nextFrame();
+
+    const getTextContent = () => healthIndex.textContent;
+    const getActiveBox = () => healthIndex.shadowRoot!.querySelector('.box.active');
+    const getShadowRootTextContent = () =>
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(getTextContent(), 'initial').to.equal('A');
+    expect(getActiveBox()!.classList.contains('a'), 'initial class').to.be.true;
+    expect(getShadowRootTextContent(), 'initial shadow').to.equal('A');
+
+    healthIndex.setAttribute('health-index', 'B');
+
+    await nextFrame();
+
+    expect(healthIndex.textContent).to.equal('B');
+    expect(getActiveBox()!.classList.contains('b')).to.be.true;
+    expect(getShadowRootTextContent()).to.equal('B');
+  });
+
+  it('should show a health index of A', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.A);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('A');
+    expect(activeBox.classList.contains('a')).to.be.true;
+    expect(shadowRootTextContent).to.equal('A');
+  });
+
+  it('should show a health index of B', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.B);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('B');
+    expect(activeBox.classList.contains('b')).to.be.true;
+    expect(shadowRootTextContent).to.equal('B');
+  });
+
+  it('should show a health index of C', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.C);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('C');
+    expect(activeBox.classList.contains('c')).to.be.true;
+    expect(shadowRootTextContent).to.equal('C');
+  });
+
+  it('should show a health index of D', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.D);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('D');
+    expect(activeBox.classList.contains('d')).to.be.true;
+    expect(shadowRootTextContent).to.equal('D');
+  });
+
+  it('should show a health index of E', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.E);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('E');
+    expect(activeBox.classList.contains('e')).to.be.true;
+    expect(shadowRootTextContent).to.equal('E');
+  });
+
+  it('should show a health index of F', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.F);
+    const healthIndexTextContent = healthIndex.textContent;
+    const activeBox = healthIndex.shadowRoot!.querySelector('.box.active')!;
+    const shadowRootTextContent =
+      healthIndex.shadowRoot!.querySelector('#healthIndex')!.textContent;
+
+    expect(healthIndexTextContent).to.equal('F');
+    expect(activeBox.classList.contains('f')).to.be.true;
+    expect(shadowRootTextContent).to.equal('F');
+  });
+
+  it('should increase in size if size attribute is set to lg', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.largeSize);
+    const {
+      offsetWidth: defaultWidth,
+      offsetHeight: defaultHeight,
+    } = healthIndex.shadowRoot!.querySelector<HTMLElement>('.box.active')!;
+
+    healthIndex.setAttribute('size', 'lg');
+
+    await healthIndex.updateComplete;
+
+    const {
+      offsetWidth,
+      offsetHeight,
+    } = healthIndex.shadowRoot!.querySelector<HTMLElement>('.box.active')!;
+
+    expect(defaultWidth < offsetWidth).to.be.true;
+    expect(defaultHeight < offsetHeight).to.be.true;
+  });
+
+  it('should only show a single box if the size attribute is set to mini', async function() {
+    const healthIndex = await createFixture<PfeHealthIndex>(TEMPLATES.miniSize);
+    const boxes = healthIndex.shadowRoot!.querySelectorAll('.box');
+    expect(boxes.length).to.equal(1);
+  });
+});

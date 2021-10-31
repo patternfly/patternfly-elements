94a1a592b14069e2e819341e99dc1f11886e7cf3
Benny Powers <bepowers@redhat.com>
2021-10-31 09:15:37 +0200

    feat!: migrate pfe-toast

--- elements/pfe-toast/docs/index.md
+++ elements/pfe-toast/docs/index.md
@@ -1,5 +1,5 @@
 ---
-layout: layout-basic.html
+layout: layout-basic.njk
 title: Toast
 description: An alert hidden on page load and slides in/out of the view
 package: pfe-toast
@@ -8,7 +8,7 @@ packages:
   - pfe-button
 tags:
   - component
---- 
+---
 
 <style>
   pfe-toast#overview-toast {
@@ -16,115 +16,53 @@ tags:
   }
 </style>
 
-::: section header
-# {{ title }}
-:::
+{% renderOverview for=package, title=title %}
+  <pfe-button>
+    <button>Trigger toast</button>
+  </pfe-button>
 
-::: section
-## Overview
+  <pfe-toast id="overview-toast">
+    <h2>Do you feel toasted?</h2>
+    <p>Biodiesel wolf franzen, jean shorts pabst lomo cloud bread gentrify cronut af migas vinyl four dollar toast scenester twee. Twee synth hammock hella activated charcoal keffiyeh, farm-to-table cray try-hard tofu fixie truffaut leggings actually. Tote bag poutine kale chips intelligentsia health goth, thundercats affogato tofu literally vegan umami slow-carb VHS chillwave.</p>
+  </pfe-toast>
 
-Toast is a self-contained alert that is hidden on page load and slides in/out of the view when programmatically opened/closed.
+  <script>
+    const button = document.querySelector("pfe-button button");
+    const toast = document.querySelector("pfe-toast#overview-toast");
+    button.addEventListener("click", (e) => { toast.toggle()});
+  </script>
+{% endrenderOverview %}
 
-<pfe-button>
-  <button>Trigger toast</button>
-</pfe-button>
+{% band header="Usage" %}
+  ```html
+  <pfe-toast>
+    <p>You've been successfully toasted!</p>
+  </pfe-toast>
+  ```
 
-:::
+  ```javascript
+  const toast = document.querySelector("pfe-toast");
+  toast.open(); // open pfe-toast
+  toast.close(); // close pfe-toast
+  toast.toggle(); // toggle pfe-toast
+  ```
+{% endband %}
 
-::: section
-## Installation
+{% renderSlots for=package %}{% endrenderSlots %}
 
-```shell
-npm install @patternfly/{{ package }}
-```
-:::
+{% renderAttributes for=package %}{% endrenderAttributes %}
 
-::: section
-## Usage
+{% renderProperties for=package %}{% endrenderProperties %}
 
-```html
-<pfe-toast>
-  <p>You've been successfully toasted!</p>
-</pfe-toast>
-```
+{% renderMethods for=package %}{% endrenderMethods %}
 
-```javascript
-const toast = document.querySelector("pfe-toast");
-toast.open(); // open pfe-toast
-toast.close(); // close pfe-toast
-toast.toggle(); // toggle pfe-toast
-```
-:::
+{% renderEvents for=package %}{% endrenderEvents %}
 
-::: section
-## Slots
-### default
-The default slot can contain any type of content.
-:::
+{% renderCssCustomProperties for=package %}
+  - Max width: Allows you to specify the maximum width of the component. **Variable name:** `--pfe-toast--MaxWidth`.
+  - Min width: Allows you to specify the minimum width of the component. **Variable name:** `--pfe-toast--MinWidth`.
+  - Top: Allows you to customize the distance between the component and the top of its container. **Variable name:** `--pfe-toast--Top`.
+  - Right: Allows you to customize the distance between the component and the right of its container. **Variable name:** ` --pfe-toast--Right`.
+{% endrenderCssCustomProperties %}
 
-::: section
-## Attributes
-### auto-dismiss
-This is an optional attribute string that you can provide to automatically dismiss the alert. The auto-dismiss delay value can be provided in seconds or in milliseconds. For example, `auto-dismiss="3s"` and `auto-dismiss="3000ms"` will dismiss the toast alert after three seconds. If no delay value is provided, it will default to eight seconds.
-
-### close-label
-This is an optional attribute string that you can provide that sets the aria-label on the close button in the shadow DOM. The aria-label attribute will default to "Close".
-:::
-
-::: section
-## Methods
-### open
-
-Manually opens a toast. Return the toast that has been opened.
-
-```javascript
-document.querySelector("pfe-toast").open();
-```
-
-### close
-
-Manually closes a toast. Returns the toast that has been closed.
-
-```javascript
-document.querySelector("pfe-toast").close();
-```
-
-### toggle
-
-Manually toggles a toast. Returns the toast that has been toggled.
-
-```javascript
-document.querySelector("pfe-toast").toggle();
-```
-:::
-
-::: section
-## Events
-### pfe-toast:open
-Fires when a toast is manually openned.
-
-### pfe-toast:close
-Fires when a toast is manually closed.
-
-:::
-
-::: section 
-## Styling hooks
-- Max width: Allows you to specify the maximum width of the component. **Variable name:** `--pfe-toast--MaxWidth`.
-- Min width: Allows you to specify the minimum width of the component. **Variable name:** `--pfe-toast--MinWidth`.
-- Top: Allows you to customize the distance between the component and the top of its container. **Variable name:** `--pfe-toast--Top`.
-- Right: Allows you to customize the distance between the component and the right of its container. **Variable name:** ` --pfe-toast--Right`.
-:::
-
-<pfe-toast id="overview-toast">
-  <h2>Do you feel toasted?</h2>
-  <p>Biodiesel wolf franzen, jean shorts pabst lomo cloud bread gentrify cronut af migas vinyl four dollar toast scenester twee. Twee synth hammock hella activated charcoal keffiyeh, farm-to-table cray try-hard tofu fixie truffaut leggings actually. Tote bag poutine kale chips intelligentsia health goth, thundercats affogato tofu literally vegan umami slow-carb VHS chillwave.</p>
-</pfe-toast>
-
-<script>
-  const button = document.querySelector("pfe-button button");
-  const toast = document.querySelector("pfe-toast#overview-toast");
-  button.addEventListener("click", () => {
-    toast.toggle();
-  });
-</script>
+{% renderCssParts for=package %}{% endrenderCssParts %}
---
+++ elements/pfe-toast/pfe-toast.ts
@@ -0,0 +1,198 @@
+import { LitElement, html } from 'lit';
+import { customElement, property, state, query } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+import { bound, observed, pfelement } from '@patternfly/pfe-core/decorators.js';
+import { pfeEvent } from '@patternfly/pfe-core/functions/pfeEvent.js';
+
+import style from './pfe-toast.scss';
+
+export class ToastOpenEvent extends Event {
+  constructor() {
+    super('open', { bubbles: true });
+  }
+}
+
+export class ToastCloseEvent extends Event {
+  constructor() {
+    super('close', { bubbles: true });
+  }
+}
+
+/**
+ * Toast is a self-contained alert that is hidden on page load and slides in/out of the view when programmatically opened/closed.
+ *
+ * @slot - The default slot can contain any type of content.
+ *
+ * @fires open - Fires when a toast is manually openned.
+ * @fires close - Fires when a toast is manually closed.
+ * @fires pfe-toast:open - {@deprecated Use `open`} Fires when a toast is manually openned.
+ * @fires pfe-toast:close - {@deprecated Use `close`} Fires when a toast is manually closed.
+ */
+@customElement('pfe-toast') @pfelement()
+export class PfeToast extends LitElement {
+  static readonly styles = [style];
+
+  /**
+   * This is an optional attribute string that you can provide to automatically dismiss the alert.
+   * The auto-dismiss delay value can be provided in seconds or in milliseconds.
+   * For example, `auto-dismiss="3s"` and `auto-dismiss="3000ms"` will dismiss the toast alert
+   * after three seconds.
+   *
+   * If no delay value is provided, it will default to eight seconds.
+   */
+  @observed('_setAccessibility')
+  @property({ attribute: 'auto-dismiss', reflect: true }) autoDismiss?: string;
+
+  /**
+   * This is an optional attribute string that you can provide that sets the aria-label on the close button in the shadow DOM.
+   * The aria-label attribute will default to "Close".
+   */
+  @property({ attribute: 'close-label', reflect: true }) closeLabel = 'Close';
+
+  @state() private doesAutoDismiss = !!this.autoDismiss;
+
+  @state() private isOpen = false;
+
+  @query('.pfe-toast__close') private _toastCloseButton?: HTMLButtonElement|null;
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.hidden = true;
+    this._setAccessibility();
+    // attach listeners
+    this.addEventListener('keydown', this._keydownHandler);
+  }
+
+  render() {
+    return html`
+      <div class="pfe-toast__container">
+        <!-- Toast content (default slot) -->
+        <div class="pfe-toast__content">
+          <slot></slot>
+        </div>
+        <!-- Close button -->
+        <button class="pfe-toast__close" aria-label="${ifDefined(this.closeLabel)}" @click="${this.close}">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="-11 11 22 23">
+            <path d="M30 16.669v-1.331c0-0.363-0.131-0.675-0.394-0.938s-0.575-0.394-0.938-0.394h-10.669v-10.65c0-0.362-0.131-0.675-0.394-0.938s-0.575-0.394-0.938-0.394h-1.331c-0.363 0-0.675 0.131-0.938 0.394s-0.394 0.575-0.394 0.938v10.644h-10.675c-0.362 0-0.675 0.131-0.938 0.394s-0.394 0.575-0.394 0.938v1.331c0 0.363 0.131 0.675 0.394 0.938s0.575 0.394 0.938 0.394h10.669v10.644c0 0.363 0.131 0.675 0.394 0.938 0.262 0.262 0.575 0.394 0.938 0.394h1.331c0.363 0 0.675-0.131 0.938-0.394s0.394-0.575 0.394-0.938v-10.637h10.669c0.363 0 0.675-0.131 0.938-0.394 0.269-0.262 0.4-0.575 0.4-0.938z"
+                transform="rotate(45)" />
+          </svg>
+        </button>
+      </div>
+    `;
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener('keydown', this._keydownHandler);
+  }
+
+  private _setAccessibility() {
+    this.doesAutoDismiss = !!this.autoDismiss;
+    if (!this.doesAutoDismiss) {
+      this.removeAttribute('aria-live');
+      this.removeAttribute('aria-atomic');
+
+      this.setAttribute('role', 'alertdialog');
+      // default if none provided
+      if (!this.hasAttribute('aria-label'))
+        this.setAttribute('aria-label', 'Alert dialog');
+
+      this.setAttribute('aria-describedby', 'pfe-toast__content');
+    } else {
+      this.removeAttribute('aria-label');
+      this.removeAttribute('aria-describedby');
+
+      this.setAttribute('role', 'alert');
+      this.setAttribute('aria-live', 'polite');
+      this.setAttribute('aria-atomic', 'true');
+    }
+  }
+
+  private _toMilliseconds(value = ''): number {
+    // set default delay if none provided
+    const digits = value.match(/\d+/) || [8000];
+    const unit = value.match(/\D+/) || '';
+    return unit[0] === 's' ? Number(digits[0]) * 1000 : Number(digits[0]);
+  }
+
+  @bound private _keydownHandler(event: KeyboardEvent) {
+    const { target } = event;
+    const { key } = event;
+    switch (key) {
+      case 'Escape':
+      case 'Esc':
+        event.preventDefault();
+        this.close();
+        break;
+      case 'Enter':
+        if (target === this._toastCloseButton)
+          event.preventDefault();
+        this.close();
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Manually opens a toast. Return the toast that has been opened.
+   *
+   * ```javascript
+   * document.querySelector("pfe-toast").open();
+   * ```
+   */
+  @bound open() {
+    this.isOpen = true;
+    this.hidden = false;
+    setTimeout(() => this.classList.add('open'), 500);
+
+    this.dispatchEvent(new ToastOpenEvent());
+    this.dispatchEvent(pfeEvent('pfe-toast:open'));
+
+    if (this.doesAutoDismiss)
+      setTimeout(this.close, this._toMilliseconds(this.autoDismiss));
+
+
+    return this;
+  }
+
+  /**
+   * Manually closes a toast. Returns the toast that has been closed.
+   *
+   * ```javascript
+   * document.querySelector("pfe-toast").close();
+   * ```
+   */
+  @bound close() {
+    this.isOpen = false;
+
+    setTimeout(() => {
+      this.classList.remove('open');
+      this.hidden = true;
+    }, 500);
+
+    this.dispatchEvent(new ToastCloseEvent());
+    this.dispatchEvent(pfeEvent('pfe-toast:close'));
+
+    return this;
+  }
+
+  /**
+   * Manually toggles a toast. Returns the toast that has been toggled.
+   *
+   * ```javascript
+   * document.querySelector("pfe-toast").toggle();
+   * ```
+   */
+  @bound toggle() {
+    this.isOpen ? this.close() : this.open();
+    return this;
+  }
+}
+
+ declare global {
+   interface HTMLElementTagNameMap {
+     'pfe-toast': PfeToast;
+   }
+ }

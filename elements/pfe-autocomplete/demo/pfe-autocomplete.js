c444678536faba10c0eab284f9ba2e7a17272aa2
Benny Powers <bepowers@redhat.com>
2021-11-01 10:48:03 +0200

    feat(autocomplete)!: migrate pfe-autocomplete to lit
    fix(autocomplete): compose UI events
    
    test(autocomplete): visual regression tests
    
    test(pfe-autocomplete): e2e tests with page object model
    
    test(pfe-autocomplete): build demo with optional shadowroot
    
    docs(pfe-autocomplete): rearrange docs urls
    
    test(autocomplete): open the dropdown

---
+++ elements/pfe-autocomplete/demo/pfe-autocomplete.js
@@ -0,0 +1,57 @@
+import '@patternfly/pfe-autocomplete';
+import '@patternfly/pfe-band';
+import '@patternfly/pfe-select';
+
+const root = document.querySelector('[data-demo="pfe-autocomplete"]')?.shadowRoot ?? document;
+
+const searchAutocomplete = root.querySelector('#search-lazy-loading');
+
+const items = [
+  'Item 1',
+  'Item 2',
+  'United States',
+  'Chicago Cubs',
+  'Red Hat',
+  'Purple',
+  'Curious George',
+  'United Kingdom',
+  'Elephant',
+  'Baseball',
+  'Bingo',
+  'Book',
+  'Android',
+  'iOS',
+  'Linux',
+  'Red Hat Enterprise Linux',
+];
+
+function staticCallback(params, callback) {
+  const regx = new RegExp(`^${params.query}`, 'i');
+  callback(items.filter(x => regx.test(x)));
+}
+
+function ajaxCallback(params, callback) {
+  searchAutocomplete.loading = true;
+  const url = new URL('http://openlibrary.org/search.json');
+  url.searchParams.append('title', params.query);
+  fetch(url.toString())
+    .then(x => x.json())
+    .then(({ docs }) => docs.map(x => x.title))
+    .then(callback)
+    .then(() => searchAutocomplete.loading = false);
+}
+
+function onSearch(event) {
+  const output = event.target.closest('pfe-band').querySelector('output');
+  if (output)
+    output.textContent = event.value;
+}
+
+for (const element of root.querySelectorAll('pfe-autocomplete'))
+  element.addEventListener('search', onSearch);
+
+for (const element of root.querySelectorAll('.static'))
+  element.autocompleteRequest = staticCallback;
+
+// autocomplete call
+searchAutocomplete.autocompleteRequest = ajaxCallback;

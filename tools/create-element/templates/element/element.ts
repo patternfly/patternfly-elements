108e7454bf333fef6b4f6185caa3d277f92a7101
Benny Powers <web@bennypowers.com>
2021-11-30 22:16:13 +0200

    feat(create)!: add create-element generator
    - e2e test file for elements
    - add {{version}} to element templates

---
+++ tools/create-element/templates/element/element.ts
@@ -0,0 +1,29 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import { pfelement } from '@patternfly/pfe-core/decorators.js';
+
+import styles from './<%= tagName %>.scss';
+
+/**
+ * <%= readmeName %>
+ * @slot - Place element content here
+ */
+@customElement('<%= tagName %>') @pfelement()
+export class <%= className %> extends LitElement {
+  static readonly version = '{{version}}';
+
+  static readonly styles = [styles];
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    '<%= tagName %>': <%= className %>;
+  }
+}

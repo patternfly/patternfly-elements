0916ceaac93a80346855ecbe98aa2155b47c60f2
Benny Powers <web@bennypowers.com>
2021-11-30 22:16:13 +0200

    feat(create)!: add create-element generator
    style(create): lint
    
    feat(create): e2e test file for elements

--- generators/element/templates/gulpfile.js
+++
@@ -1,4 +0,0 @@
-const gulpFactory = require("<%= gulpFactoryLocation %>");
-const pfelementPackage = require("./package.json");
-
-gulpFactory(pfelementPackage);
--- generators/element/templates/rollup.config.js
+++
@@ -1,5 +0,0 @@
-// rollup.config.js
-import configFactory from "<%= rollupConfigLocation %>";
-import pfelementPackage from "./package.json";
-
-export default configFactory(pfelementPackage.pfelement);

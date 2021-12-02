02117e716ff3da8abe1fbd63fd11c2d1386efb68
Benny Powers <web@bennypowers.com>
2021-12-02 15:55:47 +0200

    feat!: scripts that use pfe-tools

--- scripts/license-update.js
+++ scripts/license-update.js
@@ -1,18 +1,22 @@
-const fs = require("fs");
-const path = require("path");
+/* eslint-env node */
+/* eslint-disable no-console */
+const fs = require('fs');
+const path = require('path');
 
-const elementsDir = path.join(__dirname, "../elements");
+const elementsDir = path.join(__dirname, '../elements');
 
 let elementNames = fs.readdirSync(elementsDir).filter(file => fs.statSync(path.join(elementsDir, file)).isDirectory());
 
 // Remove sass helpers from the listings
-elementNames = elementNames.filter(folder => !folder.includes("sass"));
+elementNames = elementNames.filter(folder => !folder.includes('sass'));
 
 // Copy the LICENSE.txt file from root to the root for each element
-if (fs.existsSync("LICENSE.txt")) {
+if (fs.existsSync('LICENSE.txt')) {
   elementNames.forEach(element => {
-    fs.copyFile("LICENSE.txt", path.join("elements", element, "LICENSE.txt"), err => {
-      if (err) throw err;
+    fs.copyFile('LICENSE.txt', path.join('elements', element, 'LICENSE.txt'), err => {
+      if (err) {
+        throw err;
+      }
       console.log(`LICENSE.txt was copied to ${element}`);
     });
   });

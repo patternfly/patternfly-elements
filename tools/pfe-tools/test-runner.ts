06d171f53097255ad2a5b04b61291d39af7f2f31
Benny Powers <web@bennypowers.com>
2021-12-02 15:55:37 +0200

    feat!: pfe-tools

---
+++ tools/pfe-tools/test-runner.ts
@@ -0,0 +1,71 @@
+import type { PfeDevServerConfigOptions } from './dev-server';
+import type { TestRunnerConfig } from '@web/test-runner';
+
+import { playwrightLauncher } from '@web/test-runner-playwright';
+import { pfeDevServerConfig } from './dev-server.js';
+
+export type PfeTestRunnerConfigOptions = PfeDevServerConfigOptions
+
+export function pfeTestRunnerConfig(opts: PfeTestRunnerConfigOptions): TestRunnerConfig {
+  const { open, ...devServerConfig } = pfeDevServerConfig(opts);
+  return {
+    ...devServerConfig,
+    browsers: [
+      playwrightLauncher({
+        createBrowserContext: async ({ browser }) => {
+          const context = await browser.newContext();
+          // grant permissions to access the users clipboard
+          await context.grantPermissions(['clipboard-read', 'clipboard-write']);
+          return context;
+        },
+      }),
+    ],
+    testFramework: {
+      config: {
+        ui: 'bdd',
+      },
+    },
+    files: 'elements/*/test/*.spec.ts',
+    testRunnerHtml: testFramework =>
+      `<html>
+      <head>
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
+    groups: [
+      {
+        name: 'with-vue',
+        testRunnerHtml: testFramework =>
+          `<html>
+          <head>
+            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+            <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" crossorigin></script>
+          </head>
+          <body>
+            <div id="root"></div>
+            <script type="module" src="${testFramework}"></script>
+          </body>
+        </html>`,
+      },
+      {
+        name: 'with-react',
+        testRunnerHtml: testFramework =>
+          `<html>
+          <head>
+            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+            <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+            <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+            <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>
+          </head>
+          <body>
+            <div id="root"></div>
+            <script type="module" src="${testFramework}"></script>
+          </body>
+        </html>`,
+      },
+    ],
+  };
+}

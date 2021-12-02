667db9669cc995aadbbc16838aa1d0beb8735db9
Benny Powers <web@bennypowers.com>
2021-12-02 15:55:47 +0200

    feat(scripts)!: scripts that use pfe-tools
    style(scripts): lint scripts
    
    feat(scripts): rearrange docs urls
    
    feat(scripts): build for production in CI
    
    chore(scripts): minify bundle in prod
    
    chore(scripts): simplify build scripts

---
+++ test/pfe-styles.e2e.spec.cjs
@@ -0,0 +1,27 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __reExport = (target, module2, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && key !== "default")
+        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+  }
+  return target;
+};
+var __toModule = (module2) => {
+  return __reExport(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", module2 && module2.__esModule && "default" in module2 ? { get: () => module2.default, enumerable: true } : { value: module2, enumerable: true })), module2);
+};
+var import_test = __toModule(require("@playwright/test"));
+var import_fixtures = __toModule(require("@patternfly/pfe-tools/test/playwright/fixtures.js"));
+import_fixtures.test.describe("pfe-styles", () => {
+  (0, import_fixtures.test)("snapshot", async ({ page }) => {
+    await page.goto("https://localhost:8080/core/pfe-styles/");
+    await page.waitForLoadState("networkidle");
+    (0, import_test.expect)(await page.screenshot()).toMatchSnapshot("pfe-styles.png");
+  });
+});

7ec6eddbe2435f81f2486a8f57114bc23e58b339
heyMP <mgp140@gmail.com>
2021-12-13 18:58:04 -0500

    pfe-tools: added build ts files to js to build script

--- tools/pfe-tools/_scripts/build.js
+++ tools/pfe-tools/_scripts/build.js
@@ -2,13 +2,15 @@ import glob from 'glob';
 import esbuild from 'esbuild';
 import { fileURLToPath } from 'url';
 
+const WATCH = !!process.argv?.splice(2)?.find(arg => arg === 'watch');
+
 const cwd = fileURLToPath(new URL('../', import.meta.url));
 
 const entryPoints = glob.sync('**/*.ts', { cwd, ignore: '**/*.d.ts' });
 
 esbuild.build({
   target: 'es2020',
-  watch: true,
+  watch: WATCH,
   color: true,
   outdir: cwd,
   entryPoints,

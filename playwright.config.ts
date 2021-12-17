5ec8cbef46ba536edb2801595f14d62faaac4f62
Benny Powers <web@bennypowers.com>
2021-12-17 11:26:49 +0200

    chore(config): config files
    fix(config): run analyze in parallel with demo
    
    fixes #1821
    
    chore(config): ignore test artifacts
    
    chore(config): eslintignore
    
    chore(config): package.json scripts
    
    chore(config): package.json scripts
    
    chore(config): e2e test config files
    
    chore(config): e2e test scripts
    
    chore(config): ignore test results
    
    chore(config): e2e test script
    
    chore(config): testing files

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: 'elements',
+  testMatch: '*.e2e.spec.js',
+  timeout: 120 * 1000,
+
+  webServer: {
+    command: 'npx @web/dev-server --config ./docs/demo/web-dev-server.demo.config.js',
+    port: 8080,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+
+  use: {
+    viewport: { width: 1920, height: 1080 },
+  },
+
+  expect: {
+    toMatchSnapshot: { threshold: 0.2 },
+  },
+};
+
+export default config;

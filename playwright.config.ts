41af3401f7a617b4fdfe9552b51cc00bf2b348e5
Benny Powers <web@bennypowers.com>
2021-12-22 17:22:32 +0200

    chore(config): e2e test config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,67 +1,24 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-import { devices } from '@playwright/test';
-
 const config: PlaywrightTestConfig = {
-  testDir: 'test',
+  testDir: 'elements',
   testMatch: '*.e2e.spec.js',
-
-  globalSetup: require.resolve('@patternfly/pfe-tools/test/playwright/global-setup'),
-  globalTeardown: require.resolve('@patternfly/pfe-tools/test/playwright/global-teardown'),
+  timeout: 6000,
 
   webServer: {
-    command: 'npm run build && npx http-server _site',
+    command: 'cd _site && npx http-server -p 8080',
     port: 8080,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 
   use: {
-    headless: false,
     viewport: { width: 1920, height: 1080 },
   },
 
-  timeout: 6000,
-
-  projects: [
-    // -- BrowserStack Projects --
-    // name should be of the format browser@browser_version:os os_version@browserstack
-
-    // Windows 10 Chrome
-    {
-      name: 'chrome@latest:Windows 10@browserstack',
-      use: {
-        browserName: 'chromium',
-        channel: 'chrome',
-      },
-    },
-
-    // macOS Firefox
-    {
-      name: 'playwright-firefox@latest:OSX Catalina@browserstack',
-      use: {
-        browserName: 'firefox',
-        ignoreHTTPSErrors: true,
-      },
-    },
-
-    // macOS Safari
-    {
-      name: 'playwright-webkit@latest:OSX Big Sur@browserstack',
-      use: {
-        browserName: 'webkit',
-      },
-    },
-
-    // iPhone
-    // {
-    //   name: 'playwright-webkit@latest:OSX Big Sur@browserstack',
-    //   use: {
-    //     browserName: 'webkit',
-    //     ...devices['iPhone 12']
-    //   },
-    // },
-
-  ],
+  expect: {
+    toMatchSnapshot: { threshold: 0.2 },
+  },
 };
+
 export default config;

2f808140880d6dd9adf5df41409f952e796fd25e
Benny Powers <web@bennypowers.com>
2021-12-28 17:25:04 +0200

    chore(config): browserstack test config

--- playwright.browserstack.config.ts
+++ playwright.browserstack.config.ts
@@ -1,13 +1,31 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
-import baseConfig from './playwright.config';
 import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  ...baseConfig,
   globalSetup: require.resolve('@patternfly/pfe-tools/test/playwright/global-setup'),
   globalTeardown: require.resolve('@patternfly/pfe-tools/test/playwright/global-teardown'),
-  webServer: undefined,
+
+  testDir: 'elements',
+  testMatch: '*.e2e.spec.js',
+
+  timeout: 120 * 1000,
+
+  webServer: {
+    command: 'npx @web/dev-server --config ./docs/demo/web-dev-server.demo.config.js',
+    port: 8080,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+
+  use: {
+    viewport: { width: 1920, height: 1080 },
+  },
+
+  expect: {
+    toMatchSnapshot: { threshold: 0.2 },
+  },
+
   projects: [
     {
       name: 'chrome@latest:Windows 10@browserstack',

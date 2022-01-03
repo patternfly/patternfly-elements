eea63ae0cd87327f046718d3dc386b61d32dd21f
Benny Powers <bennyp@redhat.com>
2022-01-03 11:09:53 +0200

    chore(config): config files
    - add tools to commitlintrc
    - run analyze in parallel with demo
      - fixes #1821
    
    - ignore test artifacts
    - ignore legal build txt
    - ignore node_modules in playwright tests
    - package.json scripts
    - testing files
    - e2e test config files
    - e2e npm scripts
    - use percy for e2e tests via webhook
    - test parallel
    - don't make demos an spa
    - lighthouse config
    - re-arrange docs urls
    - track changes to docs
    - rename netlify plugin
    - site build scripts
    - update docs scripts
    - add changesets config
    - update 11ty config
    - install husky hooks and lint-staged
    - rename and refactor commitlintrc
    - allow `scripts` scope in commits
    - allow long commit body

---
+++ .eleventy.cjs
@@ -0,0 +1,83 @@
+const compress = require('compression');
+const anchorsPlugin = require('@orchidjs/eleventy-plugin-ids');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const directoryOutputPlugin = require('@11ty/eleventy-plugin-directory-output');
+
+const pfeAssetsPlugin = require('./docs/_plugins/pfe-assets.cjs');
+const customElementsManifestPlugin = require('./docs/_plugins/custom-elements-manifest.cjs');
+const orderTagsPlugin = require('./docs/_plugins/order-tags.cjs');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setQuietMode(process.env.npm_config_quiet);
+  eleventyConfig.setWatchThrottleWaitTime(500);
+
+  /** Copy and manage site assets from the monorepo */
+  eleventyConfig.addPlugin(pfeAssetsPlugin);
+
+  /** Generate and consume custom elements manifests */
+  eleventyConfig.addPlugin(customElementsManifestPlugin);
+
+  /** Collections to organize alphabetically instead of by date */
+  eleventyConfig.addPlugin(orderTagsPlugin, { tags: ['component'], order: 'alphabetically' });
+
+  /** Collections to organize by order instead of date */
+  eleventyConfig.addPlugin(orderTagsPlugin, { tags: ['develop'] });
+
+  /** Add IDs to heading elements */
+  eleventyConfig.addPlugin(anchorsPlugin, {
+    formatter(element, existingids) {
+      if (
+        !existingids.includes(element.getAttribute('id')) &&
+        element.hasAttribute('slot') &&
+        element.closest('pfe-card')
+      ) {
+        return null;
+      } else {
+        return eleventyConfig.javascriptFunctions
+          .slug(element.textContent)
+          .replace(/[&,+()$~%.'":*?!<>{}]/g, '');
+      }
+    },
+  });
+
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    alwaysWrapLineHighlights: true,
+    lineSeparator: '\n',
+  });
+
+  eleventyConfig.setQuietMode(true);
+  eleventyConfig.addPlugin(directoryOutputPlugin, {
+    // Customize columns
+    columns: {
+      filesize: true, // Use `false` to disable
+      benchmark: true, // Use `false` to disable
+    },
+
+    // Will show in yellow if greater than this number of bytes
+    warningFileSize: 400 * 1000,
+  });
+
+  eleventyConfig.setBrowserSyncConfig({
+    open: 'local',
+    server: {
+      baseDir: '_site',
+      middleware: [compress()],
+    },
+  });
+
+  return {
+    dir: {
+      input: './docs',
+    },
+    markdownTemplateEngine: 'njk',
+    templateFormats: [
+      'html',
+      'njk',
+      'md',
+      'css',
+      'js',
+      'svg',
+      'png',
+    ],
+  };
+};

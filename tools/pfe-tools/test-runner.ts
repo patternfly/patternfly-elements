2c1fab35f66d98eab5d5302ba6cd5e493c9c5a1a
Benny Powers <bennyp@redhat.com>
2022-01-04 20:13:36 +0200

    feat(tools): summary reporter

--- tools/pfe-tools/test-runner.ts
+++ tools/pfe-tools/test-runner.ts
@@ -6,6 +6,7 @@ import { playwrightLauncher } from '@web/test-runner-playwright';
 import { junitReporter } from '@web/test-runner-junit-reporter';
 
 import { pfeDevServerConfig } from './dev-server.js';
+import { summaryReporter } from './test/summary-reporter.js';
 
 export interface PfeTestRunnerConfigOptions extends PfeDevServerConfigOptions {
   files?: string[];
@@ -44,37 +45,39 @@ export function pfeTestRunnerConfig(opts: PfeTestRunnerConfigOptions): TestRunne
     groups: [
       {
         name: 'with-vue',
-        testRunnerHtml: testFramework =>
-          `<html>
-          <head>
-            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
-            <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" crossorigin></script>
-          </head>
-          <body>
-            <div id="root"></div>
-            <script type="module" src="${testFramework}"></script>
-          </body>
-        </html>`,
+        testRunnerHtml: testFramework => `
+          <html>
+            <head>
+              <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+              <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" crossorigin></script>
+            </head>
+            <body>
+              <div id="root"></div>
+              <script type="module" src="${testFramework}"></script>
+            </body>
+          </html>
+        `,
       },
       {
         name: 'with-react',
-        testRunnerHtml: testFramework =>
-          `<html>
-          <head>
-            <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
-            <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-            <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
-            <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>
-          </head>
-          <body>
-            <div id="root"></div>
-            <script type="module" src="${testFramework}"></script>
-          </body>
-        </html>`,
+        testRunnerHtml: testFramework => `
+          <html>
+            <head>
+              <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
+              <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+              <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+              <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>
+            </head>
+            <body>
+              <div id="root"></div>
+              <script type="module" src="${testFramework}"></script>
+            </body>
+          </html>
+        `,
       },
     ],
     reporters: [
-      defaultReporter({ reportTestProgress: true }),
+      process.argv.some(x => x.match(/-w|--watch/)) ? defaultReporter({ reportTestProgress: true }) : summaryReporter({ flatten: false }),
       junitReporter({
         outputPath: './test-results/test-results.xml',
         reportLogs: true,

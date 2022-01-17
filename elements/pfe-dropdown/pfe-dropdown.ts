4392c4c0e2ba6a38d6bed7697124de9144686944
heyMP <mgp140@gmail.com>
2022-01-17 13:49:09 -0500

    chore: document todo

--- elements/pfe-dropdown/pfe-dropdown.ts
+++ elements/pfe-dropdown/pfe-dropdown.ts
@@ -266,6 +266,7 @@ export class PfeDropdown extends LitElement {
 
   /** modify DOM if custom options are passed in an array */
   private async _modifyDOM(options: PfeDropdownOption[], clean: Boolean = true) {
+    // @todo evaluate how expensive it is to use innerHTML in this manor
     // remove all dropdown items and separators
     if (clean) {
       this.innerHTML = '';

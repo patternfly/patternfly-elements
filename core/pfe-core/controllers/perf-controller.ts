3b6d0cdb71657c1d789aa7eb31b1dad727f238db
Benny Powers <bennyp@redhat.com>
2022-01-25 13:56:27 +0200

    fix(core): allow perf controller when trackPerformance is true

--- core/pfe-core/controllers/perf-controller.ts
+++ core/pfe-core/controllers/perf-controller.ts
@@ -3,12 +3,14 @@ import type { ReactiveController, ReactiveElement } from 'lit';
 import { getRandomId } from '../functions/random.js';
 
 export class PerfController implements ReactiveController {
-  markCount = 0;
+  hasMeasured = false;
+
   markId: string;
 
-  constructor(host: ReactiveElement) {
+  constructor(private host: ReactiveElement) {
     host.addController(this);
 
+    // Set up the mark ID based on existing ID on component if it exists
     if (!host.id) {
       this.markId = getRandomId(host.localName);
     } else if (host.id.startsWith('pfe-') && !host.id.startsWith(host.localName)) {
@@ -21,19 +23,28 @@ export class PerfController implements ReactiveController {
   }
 
   hostUpdate() {
-    // Set up the mark ID based on existing ID on component if it exists
-    if (this.markCount < 1) {
-      this.markCount = this.markCount + 1;
-
-      // Navigation start, i.e., the browser first sees that the user has navigated to the page
-      performance.measure(`${this.markId}-from-navigation-to-first-render`, undefined, `${this.markId}-rendered`);
-
-      // Render is run before connection unless delayRender is used
-      performance.measure(
-        `${this.markId}-from-defined-to-first-render`,
-        `${this.markId}-defined`,
-        `${this.markId}-rendered`
-      );
+    if (!this.hasMeasured) {
+      this.measure();
     }
+  }
+
+  measure() {
+    this.hasMeasured = true;
+
+    performance.mark(`${this.markId}-rendered`);
+
+    // Navigation start, i.e., the browser first sees that the user has navigated to the page
+    performance.measure(`${this.markId}-from-navigation-to-first-render`, undefined, `${this.markId}-rendered`);
+
+    // Render is run before connection unless delayRender is used
+    performance.measure(
+      `${this.markId}-from-defined-to-first-render`,
+      `${this.markId}-defined`,
+      `${this.markId}-rendered`
+    );
+
+    // Once we've measured time to render, we no longer need the controller,
+    // so we allow it to be garbage-collected
+    this.host.removeController(this);
   }
 }

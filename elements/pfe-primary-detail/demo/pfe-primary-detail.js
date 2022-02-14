16cb20c3612f33c2a6859651fc48462bb9c622fc
Steven Spriggs <steven.spriggs@gmail.com>
2022-02-14 14:35:54 -0500

    fix(primary-detail): fix animations, move breakpoint prop to private
    fix(primary-detail): remove scss causing extra border
    
    fix(primary-detail): refactor primary-detail

--- elements/pfe-primary-detail/demo/pfe-primary-detail.js
+++ elements/pfe-primary-detail/demo/pfe-primary-detail.js
@@ -1,4 +1,5 @@
 import '@patternfly/pfe-band';
+import '@patternfly/pfe-cta';
 import { PrimaryDetailChangeEvent } from '@patternfly/pfe-primary-detail';
 
 const root = document.querySelector('[data-demo="pfe-primary-detail"]')?.shadowRoot ?? document;
@@ -8,6 +9,15 @@ await Promise.all(Array.from(root.querySelectorAll('pfe-primary-detail'), x => x
 root.addEventListener('change', event => {
   if (event instanceof PrimaryDetailChangeEvent) {
     // eslint-disable-next-line no-console
-    console.log(`${event.tab.textContent} opened. ${event.previousTab?.textContent ?? 'nothing'} closed.`);
+    console.log(`New event: ${event.tab.textContent} opened. ${event.previousTab?.textContent ?? 'nothing'} closed.`);
   }
+});
+
+root.addEventListener('pfe-primary-detail:hidden-tab', event => {
+  // eslint-disable-next-line no-console
+  console.log(`Depreciated event: ${event.detail.tab.textContent} tab is closing. ID: ${event.detail.tab.id}`);
+});
+root.addEventListener('pfe-primary-detail:shown-tab', event => {
+  // eslint-disable-next-line no-console
+  console.log(`Depreciated event: ${event.detail.tab.textContent} tab is opening. ID: ${event.detail.tab.id}`);
 });

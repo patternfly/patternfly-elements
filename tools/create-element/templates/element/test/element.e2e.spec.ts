c37a5436d29a07a737b82d209b95496087f5e0e5
Benny Powers <web@bennypowers.com>
2022-03-03 13:41:21 -0500

    fix(create): update tsconfig paths and e2e tests

--- tools/create-element/templates/element/test/element.e2e.spec.ts
+++ tools/create-element/templates/element/test/element.e2e.spec.ts
@@ -1,20 +1,12 @@
-import type { LitElement } from 'lit';
-
-import { expect } from '@playwright/test';
-import { test } from '@patternfly/pfe-tools/test/playwright/fixtures.js';
+import { test } from '@playwright/test';
+import { PfeDemoPage } from '@patternfly/pfe-tools/test/playwright/PfeDemoPage.js';
 
 const tagName = '<%= tagName %>';
 
 test.describe(tagName, () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(`http://localhost:8080/demo/${tagName}/`);
-    await page.waitForLoadState('networkidle');
-    await page.waitForTimeout(100);
-    await page.evaluate(async x => customElements.whenDefined(x), tagName);
-    await page.$eval(tagName, async (el: LitElement) => el.updateComplete);
-  });
-
   test('snapshot', async ({ page }) => {
-    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`${tagName}.png`);
+    const componentPage = new PfeDemoPage(page, tagName);
+    await componentPage.navigate();
+    await componentPage.snapshot();
   });
 });

755c6c0949ccb587d0a138ad9f273a4134fafb0a
Benny Powers <web@bennypowers.com>
2022-03-09 10:08:00 -0500

    fix(ci): bundle on release create

--- scripts/bundle-release.cjs
+++ scripts/bundle-release.cjs
@@ -20,15 +20,24 @@ module.exports = async function({ github, glob, workspace, context }) {
 
   const params = { owner, release_id: release.id, repo };
 
-  // Create or fetch artifacts
-  await singleFileBuild({ outfile });
-  await copyFile(`${cwd}/core/pfe-styles/pfe.min.css`, `${cwd}/pfe.min.css`);
+  if (!release.assets.some(x => x.name === 'pfe.min.tgz')) {
+    // Create or fetch artifacts
+    await singleFileBuild({ outfile });
+    await copyFile(`${cwd}/core/pfe-styles/pfe.min.css`, `${cwd}/pfe.min.css`);
 
-  const globber = await glob.create('pfe.min.*');
-  const files = await globber.glob() ?? [];
+    const globber = await glob.create('pfe.min.*');
+    const files = await globber.glob() ?? [];
 
-  console.log('Creating tarball for', files.join(', ')); // eslint-disable-line
-  await tar.c({ gzip: true, file: 'pfe.min.tgz' }, files);
+    console.log('Creating tarball for', files.join(', ')); // eslint-disable-line
+    await tar.c({ gzip: true, file: 'pfe.min.tgz' }, files);
+
+    // Upload the all-repo bundle to the release
+    await github.rest.repos.uploadReleaseAsset({
+      ...params,
+      name: 'pfe.min.tgz',
+      data: await readFile(`${cwd}/pfe.min.tgz`),
+    });
+  }
 
   // download the tarball that was published to NPM
   const { stdout } = await execaCommand(`npm pack ${tag}`);
@@ -38,17 +47,12 @@ module.exports = async function({ github, glob, workspace, context }) {
   // Upload the NPM tarball to the release
   if (match) {
     const [name] = match;
-    await github.rest.repos.uploadReleaseAsset({
-      ...params,
-      name,
-      data: await readFile(`${cwd}/${name}`),
-    });
+    if (!release.assets.some(x => x.name === name)) {
+      await github.rest.repos.uploadReleaseAsset({
+        ...params,
+        name,
+        data: await readFile(`${cwd}/${name}`),
+      });
+    }
   }
-
-  // Upload the all-repo bundle to the release
-  await github.rest.repos.uploadReleaseAsset({
-    ...params,
-    name: 'pfe.min.tgz',
-    data: await readFile(`${cwd}/pfe.min.tgz`),
-  });
 };

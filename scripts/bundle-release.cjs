c305aadd39ca0997e69069a68ff23fe13b462465
Benny Powers <web@bennypowers.com>
2022-03-09 09:25:58 -0500

    chore: debug bundle release workflow

--- scripts/bundle-release.cjs
+++ scripts/bundle-release.cjs
@@ -1,29 +1,18 @@
-const sleep = ms => new Promise(res => setTimeout(res, ms));
-
-/** exponential back-off function to await results while avoiding rate limits */
-async function backoff(fn, depth = 0) {
-  try {
-    return await fn();
-  } catch (e) {
-    if (depth > 7) {
-      throw e;
-    } else {
-      await sleep(2 ** depth * 10);
-      return backoff(fn, depth + 1);
-    }
-  }
-}
-
-module.exports = async function({ github, glob, workspace, publishedPackages }) {
+module.exports = async function({ github, glob, workspace, context }) {
   const tar = require('tar');
   const { readFile, copyFile } = require('fs').promises;
 
   const { singleFileBuild } = await import('../tools/pfe-tools/esbuild.js');
   const { execaCommand } = await import('execa');
 
+  const { payload } = context;
+  const { release } = payload;
+
   // https://github.com/patternfly/patternfly-elements
   const owner = 'patternfly';
   const repo = 'patternfly-elements';
+
+  const tag = release.tag_name;
 
   // repo root
   const cwd = `${workspace}`;
@@ -31,56 +20,49 @@ module.exports = async function({ github, glob, workspace, publishedPackages }) 
 
   await singleFileBuild({ outfile });
 
-  async function getRelease({ owner, repo, tag }) {
-    const release = await github.request(`GET /repos/{owner}/{repo}/releases/tags/{tag}`, { owner, repo, tag });
-    if (!release.id) {
-      throw new Error(`Could not find release for tag: ${tag}`);
-    }
+  const params = {
+    owner,
+    release_id: release.id,
+    repo
+  };
+
+  await copyFile(`${cwd}/core/pfe-styles/pfe.min.css`, `${cwd}/pfe.min.css`);
+
+  const globber = await glob.create('pfe.min.*');
+  const files = await globber.glob();
+
+  // eslint-disable-next-line
+  console.log('creating tarball for', files);
+
+  await tar.c({ gzip: true, file: 'pfe.min.tgz' }, files);
+
+  // upload the bundle to each release
+  await github.rest.repos.uploadReleaseAsset({
+    ...params,
+    name: 'pfe.min.tgz',
+    data: await readFile(`${cwd}/pfe.min.tgz`),
+  });
+
+  const { packageName } = tag.match(/^(?<packageName>@[-\w]+[/]{1}[-\w]+)@(.*)$/)?.groups ?? {};
+
+  if (!packageName) {
+    // eslint-disable-next-line
+    console.log(release);
+    throw new Error('No Package found');
   }
 
-  for (const { name: packageName, version } of publishedPackages) {
-    // get the tag for the release for this package
-    const tag = `${packageName}@${version}`;
+  // make a tarball for the package
+  // this was already published to npm in the changesets action
+  const { stdout } = await execaCommand(`npm run pack -w ${packageName}`);
+  const match = stdout.match(/^[\w-.]+\.tgz$/g);
 
-    // wait until the release is created
-    const release = await backoff(() => getRelease({ owner, repo, tag }));
-
-    const params = {
-      owner,
-      release_id: release.id,
-      repo
-    };
-
-    await copyFile(`${cwd}/core/pfe-styles/pfe.min.css`, `${cwd}/pfe.min.css`);
-
-    const globber = await glob.create('pfe.min.*');
-    const files = await globber.glob();
-
-    // eslint-disable-next-line
-    console.log('creating tarball for', files);
-
-    await tar.c({ gzip: true, file: 'pfe.min.tgz' }, files);
-
-    // upload the bundle to each release
+  // upload the package tarball to the release
+  if (match) {
+    const [name] = match;
     await github.rest.repos.uploadReleaseAsset({
       ...params,
-      name: 'pfe.min.tgz',
-      data: await readFile(`${cwd}/pfe.min.tgz`),
+      name,
+      data: await readFile(`${cwd}/${name}`),
     });
-
-    // make a tarball for the package
-    // this was already published to npm in the changesets action
-    const { stdout } = await execaCommand(`npm run pack -w ${packageName}`);
-    const match = stdout.match(/^[\w-.]+\.tgz$/g);
-
-    // upload the package tarball to the release
-    if (match) {
-      const [name] = match;
-      await github.rest.repos.uploadReleaseAsset({
-        ...params,
-        name,
-        data: await readFile(`${cwd}/${name}`),
-      });
-    }
   }
 };

10b187fbc5c58e6ac520a1287dda8f750e63ee15
Benny Powers <web@bennypowers.com>
2022-03-13 11:20:01 -0400

    fix(ci): typecheck bundle script
    also allows individual releases to fail without stopping the rest

--- scripts/bundle-release.cjs
+++ scripts/bundle-release.cjs
@@ -1,7 +1,22 @@
+// @ts-check
+
+/**
+ * @typedef {object} API
+ * @property {import('@actions/core')} core
+ * @property {import('@actions/exec')} exec
+ * @property {import('@actions/glob')} glob
+ * @property {import('@octokit/rest').Octokit} github
+ */
+
+/** @param {number} ms */
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 const NPM_OUTPUT_FILENAME_RE = /^(?:[\s]+)?(?:npm )?(?<name>[-\w.]+\.tgz)$/mg;
 
+/**
+ * @param {import('@actions/exec')} exec
+ * @param {string} command
+ */
 async function execCommand(exec, command) {
   const [cmd, ...args] = command.split(' ');
 
@@ -9,12 +24,14 @@ async function execCommand(exec, command) {
   let stderr = '';
 
   const code = await exec.exec(cmd, args, {
-    stdout: data => {
-      stdout += data.toString();
-    },
-    stderr: data => {
-      stderr += data.toString();
-    },
+    listeners: {
+      stdout(data) {
+        stdout += data.toString();
+      },
+      stderr(data) {
+        stderr += data.toString();
+      },
+    }
   });
 
   if (code !== 0) {
@@ -23,7 +40,15 @@ async function execCommand(exec, command) {
     return stdout;
   }
 }
-/** Wait exponentially longer, by seconds, each time we fail to fetch the release */
+
+/**
+ * Wait exponentially longer, by seconds, each time we fail to fetch the release
+ * @template T
+ * @param {(...args: any[]) => T} fn async function to be backed off
+ * @param {number} retries current count of retries
+ * @param {number} max Max number of retries
+ * @return {Promise<T>}
+ */
 async function backoff(fn, retries = 0, max = 10) {
   try {
     return await fn();
@@ -38,7 +63,14 @@ async function backoff(fn, retries = 0, max = 10) {
   }
 }
 
-async function getBundle({ core, exec, glob, workspace }) {
+
+/**
+ * @param {object} params
+ * @param {API} params.api
+ * @param {string} params.workspace
+ * @return string filename
+ */
+async function getBundle({ api, workspace }) {
   const { basename } = require('path');
   const { copyFile } = require('fs').promises;
   const { singleFileBuild } = await import('../tools/pfe-tools/esbuild.js');
@@ -47,100 +79,132 @@ async function getBundle({ core, exec, glob, workspace }) {
   await singleFileBuild({ outfile: `${workspace}/pfe.min.js` });
   await copyFile(`${workspace}/core/pfe-styles/pfe.min.css`, `${workspace}/pfe.min.css`);
 
-  const globber = await glob.create('pfe.min.*');
+  const globber = await api.glob.create('pfe.min.*');
   const files = (await globber.glob() ?? []).map(path => basename(path));
-
-  core.info(files);
 
   const file = 'pfe.min.tgz';
 
-  core.info(`Creating ${file} with ${files.join(', ')}`);
+  api.core.info(`Creating ${file} with ${files.join(', ')}`);
 
-  console.log(await execCommand(exec, 'pwd'));
-  console.log(await execCommand(exec, 'ls -1'));
-
-  core.info(await execCommand(exec, `tar -czf ${file} ${files.join(' ')}`));
+  api.core.info(await execCommand(api.exec, `tar -czf ${file} ${files.join(' ')}`));
 
   try {
-    core.info('Tarball contents:');
-    core.info(await execCommand(exec, `tar -tf pfe.min.tgz`));
+    api.core.info('Tarball contents:');
+    api.core.info(await execCommand(api.exec, `tar -tf pfe.min.tgz`));
+    return file;
   } catch {
     null;
   }
-
-  return file;
 }
 
-module.exports = async function bundle({ core, exec, github, glob, tags = '', workspace }) {
-  await execCommand(exec, 'git config advice.detachedHead false');
+/** @typedef {Awaited<ReturnType<import('@octokit/rest').Octokit['repos']['getRelease']>>['data']} Release */
 
+/**
+ * @param {object} params
+ * @param {API} params.api
+ * @param {string} params.owner
+ * @param {string} params.repo
+ * @param {string} params.tag
+ * @param {string} params.workspace
+ * @param {string} params.name
+ * @param {Release} params.release
+ */
+async function uploadAssetOverwriting({ api, owner, repo, tag, workspace, release, name }) {
   const { readFile } = require('fs').promises;
 
-  tags = tags.split(',').map(x => x.trim());
+  // Delete any existing asset with that name
+  for (const { id, name: existing } of release.assets ?? []) {
+    if (name === existing) {
+      api.core.info(`${name} exists for ${tag}, deleting`);
+      await api.github.rest.repos.deleteReleaseAsset({ owner, repo, asset_id: id });
+    }
+  }
+
+  // Upload the all-repo bundle to the release
+  const data = await readFile(`${workspace}/${name}`);
+  api.core.info(`Uploading ${name} to ${tag}`);
+  await api.github.rest.repos.uploadReleaseAsset({
+    tag,
+    owner,
+    repo,
+    release_id: release.id,
+    name,
+    // @ts-expect-error: seems to work tho
+    data,
+  });
+}
+
+/**
+ * @param {object} params
+ * @param {import('@actions/core')} params.core
+ * @param {import('@actions/exec')} params.exec
+ * @param {import('@actions/glob')} params.glob
+ * @param {import('@octokit/rest').Octokit} params.github
+ * @param {string} params.owner
+ * @param {string} params.repo
+ * @param {string} params.tags
+ * @param {string} params.workspace
+ * @param {string} params.name
+ * @param {Release} params.release
+ */
+module.exports = async function bundle({ tags = '', workspace, ...api }) {
+  await execCommand(api.exec, 'git config advice.detachedHead false');
+
+  /** @type {API} */
+  api;
+
+  const tagsList = tags.split(',').map(x => x.trim());
 
   // https://github.com/patternfly/patternfly-elements
   const owner = 'patternfly';
   const repo = 'patternfly-elements';
 
-  for (const tag of tags) {
-    core.info(`Bundling tag ${tag}`);
+  const errors = [];
 
-    core.info('Fetching release');
-    const response = await backoff(async () =>
-      await github.rest.repos.getReleaseByTag({ owner, repo, tag }));
+  for (const tag of tagsList) {
+    try {
+      api.core.info(`Bundling tag ${tag}`);
 
-    const release = response.data;
+      api.core.info('Fetching release');
+      const response = await backoff(async () =>
+        await api.github.rest.repos.getReleaseByTag({ owner, repo, tag }));
 
-    core.info(await release);
+      const release = response.data;
 
-    const params = { owner, release_id: release.id, repo };
+      api.core.debug(JSON.stringify(release ?? null, null, 2));
 
-    core.info(`Checking out ${tag}`);
-    await execCommand(exec, `git checkout ${tag}`);
+      const params = { api, owner, repo, release, tag, workspace };
 
-    core.info(`Installing dependencies for ${tag}`);
-    await execCommand(exec, `npm ci --prefer-offline`);
-    await execCommand(exec, 'ls -1');
-    core.info(`Bundling Packages for ${tag}`);
-    const bundleFileName = await getBundle({ core, exec, github, glob, workspace });
+      // Check out the requested tag, install it's dependencies, and build
+      api.core.info(`Checking out ${tag}`);
+      await execCommand(api.exec, `git checkout ${tag}`);
+      api.core.info(`Installing dependencies for ${tag}`);
+      await execCommand(api.exec, `npm ci --prefer-offline`);
+      api.core.info(`Bundling Packages for ${tag}`);
 
-    // Delete any existing asset with that name
-    for (const { id, name } of release.assets ?? []) {
-      if (name === bundleFileName) {
-        core.info(`${name} exists for ${tag}, deleting`);
-        await github.rest.repos.deleteReleaseAsset({ owner, repo, asset_id: id });
-      }
-    }
+      // Upload single-file bundle to release
+      await uploadAssetOverwriting({ ...params, name: await getBundle({ api, workspace }) });
 
-    // Upload the all-repo bundle to the release
-    const data = await readFile(`${workspace}/${bundleFileName}`);
-    core.info(`Uploading ${bundleFileName} to ${tag}`);
-    await github.rest.repos.uploadReleaseAsset({ ...params, name: bundleFileName, data });
+      // Download the package tarball from NPM
+      const stdout = await execCommand(api.exec, `npm pack ${tag}`);
 
-    // Download the package tarball from NPM
-    const stdout = await execCommand(exec, `npm pack ${tag}`);
-    // multiple fallbacks for parsing that output
-    const {
-      name = stdout.split('\n').pop().replace(/^npm /, '') ||
-        `${tag.replace(/[@/]/g, '-')}.tgz`.replace(/^-/, '')
-    } =
-      NPM_OUTPUT_FILENAME_RE.exec(stdout)?.groups ?? {};
+      // Multiple fallbacks for parsing `npm pack` output
+      let name = NPM_OUTPUT_FILENAME_RE.exec(stdout)?.groups?.name;
+      name ||= stdout.split('\n').pop().replace(/^npm /, '');
+      name ||= `${tag.replace(/[@/]/g, '-')}.tgz`.replace(/^-/, '');
 
-    if (name) {
-      for (const { id, name: existing } of release.assets ?? []) {
-        if (existing === name) {
-          core.info(`${name} exists for ${tag}, deleting`);
-          await github.rest.repos.deleteReleaseAsset({ owner, repo, asset_id: id });
-        }
-      }
       // Upload the NPM tarball to the release
-      const data = await readFile(`${workspace}/${name}`);
-      core.info(`Uploading ${name} to ${tag}`);
-      await github.rest.repos.uploadReleaseAsset({ ...params, name, data });
-    } else {
-      core.error(stdout);
-      core.setFailed(`Could not get NPM tarball for ${tag}`);
+      if (name) {
+        await uploadAssetOverwriting({ ...params, name });
+      } else {
+        throw new Error(`Could not get NPM tarball for ${tag}`);
+      }
+    } catch (error) {
+      api.core.error(error);
+      errors.push(error);
     }
+  } if (errors.length) {
+    api.core.setFailed(errors.join('\n\n'));
   }
 };
 

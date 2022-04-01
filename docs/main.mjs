c96189eecd2210f0b6f4b3a50dca5d697b53fdb4
Benny Powers <web@bennypowers.com>
2022-04-01 13:11:37 -0400

    docs: update use of pfe-select in docs pages

--- docs/main.mjs
+++ docs/main.mjs
@@ -11,12 +11,12 @@ PfeIcon.addIconSet(
 
 const themeableSection = document.querySelector('#themeable-section');
 const themeSelect = themeableSection.querySelector('pfe-select');
-themeSelect.addEventListener('change', event => {
-  themeableSection.className = event.detail.value;
+themeSelect.addEventListener('select', event => {
+  themeableSection.className = event.value;
 });
 
 const contextBand = document.getElementById('context-band');
 const contextSelect = contextBand.querySelector('pfe-select');
-contextSelect.addEventListener('change', event => {
-  contextBand.setAttribute('color-palette', event.detail.value);
+contextSelect.addEventListener('select', event => {
+  contextBand.setAttribute('color-palette', event.value || 'base');
 });

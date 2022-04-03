813a5f7290923fb607f3e325a6d60a68677b00a3
Benny Powers <web@bennypowers.com>
2022-04-03 17:26:28 -0400

    feat(core): add `@deprecation` decorator

--- core/pfe-core/decorators.ts
+++ core/pfe-core/decorators.ts
@@ -1,6 +1,7 @@
 export * from './decorators/bound.js';
 export * from './decorators/cascades.js';
 export * from './decorators/color-context.js';
+export * from './decorators/deprecation.js';
 export * from './decorators/initializer.js';
 export * from './decorators/observed.js';
 export * from './decorators/pfelement.js';

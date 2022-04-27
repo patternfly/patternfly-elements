c49c691a3330676f96a777b7f8335387c62d1acc
Benny Powers <web@bennypowers.com>
2022-04-27 13:05:38 +0300

    feat(tools): color testing utils

---
+++ .changeset/gentle-dingos-count.md
@@ -0,0 +1,9 @@
+---
+"@patternfly/pfe-tools": minor
+---
+
+Adds `colored(colorString)` assertion to chai when using `createFixture`
+
+```js
+expect("rgba(0,0,0,0)").to.be.colored("transparent");
+```
---
+++ tools/pfe-tools/test/utils.ts
@@ -0,0 +1,10 @@
+export type Position = [x: number, y: number];
+
+export function getElementPosition(element: Element): Position {
+  const { x, y, width, height } = element.getBoundingClientRect();
+
+  return [
+    Math.floor(x + window.pageXOffset + width / 2),
+    Math.floor(y + window.pageYOffset + height / 2),
+  ];
+}

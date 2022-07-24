c619f99f40bb79db4ad20ace19f7da336d21b3b2
Steven Spriggs <steven.spriggs@gmail.com>
2022-07-24 11:54:41 -0400

    feat(label)!: PF1:1 (#2058)
    * feat(label)!: add pfv4 styles api and layout
    
    * refactor: remove is- prefixed boolean attribute
    
    * chore: adds BaseLabel to custom manifest config
    
    * fix: alignment issue with icon
    
    * fix: remove unnecessary style
    
    * docs: update docs with new api and examples
    
    * fix: outline border color and purple icon
    
    * fix: cyan and orange text color
    
    * chore: add changeset
    
    * chore(label): update .changeset/green-berries-collect.md
    
    * feat(label): removable labels
    
    * docs(label): inline docs
    
    * style(label): lint package json
    
    * test(label): suit the test cases
    
    Co-authored-by: ronniemcbride <73180910+ronniemcbride@users.noreply.github.com>
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>
    Co-authored-by: Steven Spriggs <steven.spriggs@gmail.com>

--- elements/pfe-label/demo/pfe-label.js
+++ elements/pfe-label/demo/pfe-label.js
@@ -1,7 +1,12 @@
 import '@patternfly/pfe-label';
 import '@patternfly/pfe-codeblock';
 import '@patternfly/pfe-band';
+import '@patternfly/pfe-icon/font-awesome.js';
 
 const root = document.querySelector('[data-demo="pfe-label"]')?.shadowRoot ?? document;
 
 root.querySelector('pfe-label');
+
+root.addEventListener('close', function(event) {
+  event.target.remove();
+});
--- elements/pfe-label/test/pfe-label.spec.ts
+++ elements/pfe-label/test/pfe-label.spec.ts
@@ -5,27 +5,31 @@ import { getColor, hexToRgb } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
 import { PfeIcon } from '@patternfly/pfe-icon';
 
 const example = html`
-  <pfe-label></pfe-label>
+  <pfe-label>Default</pfe-label>
 `;
 
 const exampleWithColorAttribute = html`
-  <pfe-label color="red"></pfe-label>
+  <pfe-label color="red">Red</pfe-label>
 `;
 
 const exampleWithColorAttributeEmpty = html`
-  <pfe-label color=""></pfe-label>
+  <pfe-label color="">Default</pfe-label>
 `;
 
 const exampleWithOutlineAttribute = html`
-  <pfe-label outline></pfe-label>
+  <pfe-label variant="outline">Default Outline</pfe-label>
 `;
 
 const exampleWithIconAttribute = html`
-  <pfe-label icon="rh-icon-virtual-storage-stack"></pfe-label>
+  <pfe-label icon="rh-icon-virtual-storage-stack">Default Icon</pfe-label>
 `;
 
 const exampleWithIconAttributeEmpty = html`
-  <pfe-label icon=""></pfe-label>
+  <pfe-label icon="">Default</pfe-label>
+`;
+
+const exampleWithCompactAttribute = html`
+  <pfe-label compact>Default Compact</pfe-label>
 `;
 
 
@@ -51,50 +55,45 @@ describe('<pfe-label>', function() {
   it('should display default variant', async function() {
     const el = await createFixture<PfeLabel>(example);
     await el.updateComplete;
-    const shadowDom = el.shadowRoot!.querySelector('.pfe-label')!;
-    const style = getComputedStyle(shadowDom);
+    const container = el.shadowRoot!.querySelector('#container')!;
+    const beforeStyles = getComputedStyle(container, '::before');
 
-    expect(getColor(shadowDom, 'color')).to.deep.equal(hexToRgb('#151515'));
-    expect(getColor(shadowDom, 'background-color')).to.deep.equal(hexToRgb('#f0f0f0'));
-
-    // border should be transparent
-    expect(style.getPropertyValue('border-color')).to.equal('rgba(0, 0, 0, 0)');
+    expect(getColor(container, 'color')).to.deep.equal(hexToRgb('#151515'));
+    expect(getColor(el, 'background-color')).to.deep.equal(hexToRgb('#f5f5f5'));
+    expect(beforeStyles.getPropertyValue('border-color')).to.equal('rgb(210, 210, 210)');
   });
 
   it('should display color variant if color attribute is present', async function() {
     const el = await createFixture<PfeLabel>(exampleWithColorAttribute);
     await el.updateComplete;
-    const shadowDom = el.shadowRoot!.querySelector('.pfe-label')!;
-    const style = getComputedStyle(shadowDom);
+    const container = el.shadowRoot!.querySelector('#container')!;
+    const beforeStyles = getComputedStyle(container, '::before');
 
-    expect(getColor(shadowDom, 'color')).to.deep.equal(hexToRgb('#7d1007'));
-    expect(getColor(shadowDom, 'background-color')).to.deep.equal(hexToRgb('#faeae8'));
-
-    // border should be transparent
-    expect(style.getPropertyValue('border-color')).to.equal('rgba(0, 0, 0, 0)');
+    expect(getColor(container, 'color')).to.deep.equal(hexToRgb('#7d1007'));
+    expect(getColor(el, 'background-color')).to.deep.equal(hexToRgb('#faeae8'));
+    expect(beforeStyles.getPropertyValue('border-color')).to.equal('rgb(201, 25, 11)');
   });
 
   it('should display default variant if color attribute is present but empty', async function() {
     const el = await createFixture<PfeLabel>( exampleWithColorAttributeEmpty);
     await el.updateComplete;
-    const shadowDom = el.shadowRoot!.querySelector('.pfe-label')!;
-    const style = getComputedStyle(shadowDom);
+    const container = el.shadowRoot!.querySelector('#container')!;
+    const beforeStyles = getComputedStyle(container, '::before');
 
-    expect(getColor(shadowDom, 'color')).to.deep.equal(hexToRgb('#151515'));
-    expect(getColor(shadowDom, 'background-color')).to.deep.equal(hexToRgb('#f0f0f0'));
-
-    // border should be transparent
-    expect(style.getPropertyValue('border-color')).to.equal('rgba(0, 0, 0, 0)');
+    expect(getColor(container, 'color')).to.deep.equal(hexToRgb('#151515'));
+    expect(getColor(el, 'background-color')).to.deep.equal(hexToRgb('#f5f5f5'));
+    expect(beforeStyles.getPropertyValue('border-color')).to.equal('rgb(210, 210, 210)');
   });
 
   it('should display outline variant if the attribute outline is present', async function() {
     const el = await createFixture<PfeLabel>(exampleWithOutlineAttribute);
     await el.updateComplete;
-    const shadowDom = el.shadowRoot!.querySelector('.pfe-label')!;
+    const container = el.shadowRoot!.querySelector('#container')!;
+    const beforeStyles = getComputedStyle(container, '::before');
 
-    expect(getColor(shadowDom, 'color')).to.deep.equal(hexToRgb('#151515'));
-    expect(getColor(shadowDom, 'background-color')).to.deep.equal(hexToRgb('#ffffff'));
-    expect(getColor(shadowDom, 'border-color')).to.deep.equal(hexToRgb('#d2d2d2'));
+    expect(getColor(el, 'color')).to.deep.equal(hexToRgb('#151515'));
+    expect(getColor(el, 'background-color')).to.deep.equal(hexToRgb('#ffffff'));
+    expect(beforeStyles.getPropertyValue('border-color')).to.equal('rgb(210, 210, 210)');
   });
 
   it('should render a pfe-icon if the icon attribute is present and valid', async function() {
@@ -111,5 +110,21 @@ describe('<pfe-label>', function() {
     await el.updateComplete;
     const icon = el.shadowRoot!.querySelector('pfe-icon')!;
     expect(icon).to.be.equal(null);
+  });
+
+  it('should display compact version if the attribute is-compact is present', async function() {
+    const el = await createFixture<PfeLabel>(exampleWithCompactAttribute);
+    await el.updateComplete;
+    const container = el.shadowRoot!.querySelector('#container')!;
+    const beforeStyles = getComputedStyle(container, '::before');
+    const elStyles = getComputedStyle(el);
+
+    expect(getColor(container, 'color')).to.deep.equal(hexToRgb('#151515'));
+    expect(getColor(el, 'background-color')).to.deep.equal(hexToRgb('#f5f5f5'));
+    expect(beforeStyles.getPropertyValue('border-color')).to.equal('rgb(210, 210, 210)');
+    expect(elStyles.getPropertyValue('padding-top')).to.equal('0px');
+    expect(elStyles.getPropertyValue('padding-right')).to.equal('8px'); // 0.5rem = 8px @ 16px browser default
+    expect(elStyles.getPropertyValue('padding-bottom')).to.equal('0px');
+    expect(elStyles.getPropertyValue('padding-left')).to.equal('8px'); // 0.5rem = 8px @ 16px browser default
   });
 });

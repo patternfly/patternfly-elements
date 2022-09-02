19f4cda4c35f6a28a7d4025c7bffb37b1d152e8e
Steven Spriggs <steven.spriggs@gmail.com>
2022-09-02 12:31:45 -0400

    fix(tabs): update accessibility after slot change

--- elements/pfe-tabs/pfe-tabs.ts
+++ elements/pfe-tabs/pfe-tabs.ts
@@ -298,6 +298,7 @@ export class PfeTabs extends LitElement {
         tab.classList.add('pfe-tab-last');
       }
     });
+    this.#updateAccessibility();
   }
 
   async #updateAccessibility(): Promise<void> {

1a4b25c397fe9c8fba22b4a7cd7bea69e8801dc6
Brian Ferry <bferry@redhat.com>
2022-09-21 09:33:02 -0400

    fix: removing pfe-band from the demo docs

--- elements/pfe-accordion/demo/pfe-accordion.js
+++ elements/pfe-accordion/demo/pfe-accordion.js
@@ -1,11 +1,10 @@
 import '@patternfly/pfe-accordion';
-import '@patternfly/pfe-band';
 import '@patternfly/pfe-button';
 import '@patternfly/pfe-card';
 import '@patternfly/pfe-jump-links';
 import '@patternfly/pfe-select';
 
-const pfeAccordion = document.querySelector('#context-band-3 pfe-accordion') ?? document;
+const pfeAccordion = document.querySelector('#bordered-example') ?? document;
 const isLarge = document.getElementById('toggle-size');
 
 isLarge.addEventListener('change', function(event) {

5676199be2ff6945f4a342f60bc417b81ab1a8d3
Benny Powers <web@bennypowers.com>
2022-09-23 00:39:10 +0300

    fix(icon): build icons without dependencies

--- scripts/icons/build.ts
+++ scripts/icons/build.ts
@@ -3,9 +3,11 @@ import type { IconSpec } from './icons.js';
 import { mkdir, writeFile } from 'node:fs/promises';
 import * as SETS from './icons.js';
 
+// const getContent = (name: string, { height, width, path, xOffset = 0, yOffset = 0 }: IconSpec) =>
+//   `import{svg}from'lit';export default svg\`<svg xmlns="http://www.w3.org/2000/svg" data-icon-name="${name}" height="${height}" width="${width}" viewBox="${[xOffset, yOffset, width, height].join(' ')}"><path d="${path}" /></svg>\`;`;
+
 const getContent = (name: string, { height, width, path, xOffset = 0, yOffset = 0 }: IconSpec) =>
-  `import { svg } from 'lit';
-export default svg\`<svg xmlns="http://www.w3.org/2000/svg" data-icon-name="${name}" height="${height}" width="${width}" viewBox="${[xOffset, yOffset, width, height].join(' ')}"><path d="${path}" /></svg>\`;`;
+  `const t = document.createElement('template');t.innerHTML=\`<svg xmlns="http://www.w3.org/2000/svg" data-icon-name="${name}" height="${height}" width="${width}" viewBox="${[xOffset, yOffset, width, height].join(' ')}"><path d="${path}" /></svg>\`;export default t.content.cloneNode(true);`;
 
 for (const [setName, icons] of Object.entries(SETS)) {
   const dir = new URL(`../../elements/pfe-icon/icons/${setName}/`, import.meta.url);

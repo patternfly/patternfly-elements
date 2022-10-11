41d9bbfbdf6281c59b2890a54c8c549490bbb544
Benny Powers <web@bennypowers.com>
2022-10-11 17:25:53 +0300

    docs(switch): use importShim

--- elements/pfe-switch/demo/pfe-switch.js
+++ elements/pfe-switch/demo/pfe-switch.js
@@ -1,8 +1,18 @@
+// https://github.com/guybedford/es-module-shims#polyfill-edge-case-dynamic-import
+
 if (!('ElementInternals' in window && 'setFormValue' in window.ElementInternals.prototype)) {
-  await import('element-internals-polyfill');
+  if (window.importShim) {
+    await window.importShim('element-internals-polyfill');
+  } else {
+    await import('element-internals-polyfill');
+  }
 }
 
-import('@patternfly/pfe-switch');
+if (window.importShim) {
+  await window.importShim('@patternfly/pfe-switch');
+} else {
+  await import('@patternfly/pfe-switch');
+}
 
 document.getElementById('form-disabled').addEventListener('change', /** @this{HTMLFieldsetElement}*/function(event) {
   const controls = document.getElementById(event.target.getAttribute('aria-controls'));

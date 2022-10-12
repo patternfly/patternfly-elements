4bb5cb1636f3fad9edcaa6fa957299338ed8fa53
Steven Spriggs <steven.spriggs@gmail.com>
2022-10-12 11:58:50 -0400

    fix(tabs): remove unused imports from demo js

--- elements/pfe-tabs/demo/pfe-tabs.js
+++ elements/pfe-tabs/demo/pfe-tabs.js
@@ -1,6 +1,4 @@
-import '@patternfly/pfe-band';
 import '@patternfly/pfe-tabs';
-import '@patternfly/pfe-tooltip';
 
 const root = document.querySelector('[data-demo="pfe-tabs"]')?.shadowRoot ?? document;
 

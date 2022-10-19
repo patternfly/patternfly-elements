25e9d679d2e36c07385699d44a5918f6626396c0
Benny Powers <web@bennypowers.com>
2022-10-19 10:42:11 +0300

    refactor(tabs): use pfe-icon, make icons protected

--- elements/pfe-tabs/BaseTabs.ts
+++ elements/pfe-tabs/BaseTabs.ts
@@ -22,6 +22,9 @@ export abstract class BaseTabs extends LitElement {
 
   @queryAssignedElements({ slot: 'tab' }) protected _tabs!: BaseTab[];
   protected static readonly scrollTimeoutDelay = 0;
+  protected static readonly scrollIconLeft = 'angle-left';
+  protected static readonly scrollIconRight = 'angle-right';
+  protected static readonly scrollIconSet = 'fas';
 
   @queryAssignedElements() protected _panels!: BaseTabPanel[];
 
@@ -122,22 +125,23 @@ export abstract class BaseTabs extends LitElement {
   }
 
   override render() {
+    const { scrollIconSet, scrollIconLeft, scrollIconRight } = this.constructor as typeof BaseTabs;
     return html`
       <div part="container">
         <div part="tabs-container">
           ${this._showScrollButtons ? html`
             <button id="previousTab" aria-label="Scroll left" ?disabled="${!this._overflowOnLeft}" @click="${this.#scrollLeft}">
-              <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 256 512" aria-hidden="true" role="img" style="vertical-align: -0.125em;"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
             </button>`
           : html``}
+            <pfe-icon icon="${scrollIconLeft}" set="${scrollIconSet}" loading="eager"></pfe-icon>
           <div part="tabs" role="tablist">
             <slot name="tab" @slotchange="${this.onSlotchange}"></slot>
           </div>
           ${this._showScrollButtons ? html`
             <button id="nextTab" aria-label="Scroll right" ?disabled="${!this._overflowOnRight}" @click="${this.#scrollRight}">
-              <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 256 512" aria-hidden="true" role="img" style="vertical-align: -0.125em;"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
             </button>`
           : html``}
+            <pfe-icon icon="${scrollIconRight}" set="${scrollIconSet}" loading="eager"></pfe-icon>
         </div>
         <div part="panels">
           <slot @slotchange="${this.onSlotchange}"></slot>

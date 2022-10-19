9dd0136d72ba0f0009e4d75d2f8195cd91518265
Benny Powers <web@bennypowers.com>
2022-10-19 10:35:43 +0300

    refactor(tools): use typescript syntax

--- core/pfe-core/functions/isElementInView.ts
+++ core/pfe-core/functions/isElementInView.ts
@@ -1,17 +1,18 @@
-/** This function returns whether or not an element is within the viewable area of a container. If partial is true,
+/**
+ * This function returns whether or not an element is within the viewable area of a container. If partial is true,
  * then this function will return true even if only part of the element is in view.
  *
- * @param {HTMLElement} container  The container to check if the element is in view of.
- * @param {HTMLElement} element    The element to check if it is view
- * @param {boolean} partial   true if partial view is allowed
- * @param {boolean} strict    true if strict mode is set, never consider the container width and element width
+ * @param container  The container to check if the element is in view of.
+ * @param element    The element to check if it is view
+ * @param partial   true if partial view is allowed
+ * @param strict    true if strict mode is set, never consider the container width and element width
  *
- * @returns { boolean } True if the component is in View.
+ * @returns True if the component is in View.
  */
 export function isElementInView(
   container: HTMLElement,
   element: HTMLElement,
-  partial: boolean,
+  partial = false,
   strict = false
 ): boolean {
   if (!container || !element) {
@@ -25,7 +26,9 @@ export function isElementInView(
   const elementBoundsRight = Math.floor(elementBounds.right);
 
   // Check if in view
-  const isTotallyInView = elementBoundsLeft >= containerBoundsLeft && elementBoundsRight <= containerBoundsRight;
+  const isTotallyInView =
+    elementBoundsLeft >= containerBoundsLeft &&
+    elementBoundsRight <= containerBoundsRight;
   const isPartiallyInView =
     (partial || (!strict && containerBounds.width < elementBounds.width)) &&
     ((elementBoundsLeft < containerBoundsLeft && elementBoundsRight > containerBoundsLeft) ||

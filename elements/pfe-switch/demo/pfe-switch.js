4fca9b1c3ed0c341b85deca462fcf3b9ad61ae08
Benny Powers <web@bennypowers.com>
2022-10-27 15:14:37 -0400

    docs(switch): nested in label case

--- elements/pfe-switch/demo/pfe-switch.js
+++ elements/pfe-switch/demo/pfe-switch.js
@@ -1,5 +1,6 @@
 import 'element-internals-polyfill';
 import '@patternfly/pfe-switch';
+import '@patternfly/pfe-button';
 
 document.getElementById('form-disabled').addEventListener('change', /** @this{HTMLFieldsetElement}*/function(event) {
   const controls = document.getElementById(event.target.getAttribute('aria-controls'));

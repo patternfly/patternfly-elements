4400866a3e0fe0b29d2ef9a20fe57257638acf18
Kyle Buchanan <kylebuch8@gmail.com>
2022-10-27 15:28:24 -0400

    feat(switch): add pfe-switch (#2162)
    * feat(switch): adding pfe-switch for the 1:1 project
    
    * chore(switch): updating package-lock.json
    
    * chore(switch): updating casing in loading of BaseSwitch.scss file
    
    * feat(switch): finishing dev work on switch
    
    * test(switch): adding tests to switch
    
    * chore(switch): changing order of listing in tsconfig
    
    * docs(switch): adding css properties and slots to docs
    
    * fix(switch): making sure super.connectedCallback is called first
    
    * docs(switch): updating the README and adding a changeset
    
    * docs(switch): adding a preview image for switch
    
    * feat(switch): implement face
    
    * docs: update es-module-shims
    
    * docs(switch): use importShim
    
    * fix(switch): a11y
    
    * fix(switch): use AOM instead of shadow controls
    
    * refactor(switch): move css to pfe-switch
    
    * fix(switch): update labels on connect
    
    * fix(switch): keyboard a11y
    
    * feat(tools): add a11y tree testing
    
    * test(switch): a11y testing
    
    * docs(switch): update switch docs
    
    * chore: update package-lock
    
    * chore: update package-lock
    
    * fix(switch): fixing issue where the hidden check was occupying height for some reason
    
    * docs(switch): updating docs for switch
    
    * fix(switch): handle nested in label
    
    * chore: remove cem patch
    
    * chore(tools): update dependencies
    
    Co-authored-by: Benny Powers <web@bennypowers.com>
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

---
+++ .changeset/silly-cows-change.md
@@ -0,0 +1,5 @@
+---
+"@patternfly/pfe-tools": patch
+---
+
+update dependencies
--- tools/pfe-tools/test/vue-wrapper.ts
+++ tools/pfe-tools/test/vue-wrapper.ts
@@ -54,6 +54,7 @@ export async function fixture<T extends Element = HTMLElement>(
     'pfe-readtime',
     'pfe-select',
     'pfe-spinner',
+    'pfe-switch',
     'pfe-tabs',
     'pfe-timestamp',
   ];

a87ac711493d19ec1087921b4ff08c04ab7c4e7a
Brian Ferry <bferry@redhat.com>
2022-11-03 10:36:54 -0400

    chore: updating the pfe-card styles and the styledocs

--- elements/pfe-card/BaseCard.ts
+++ elements/pfe-card/BaseCard.ts
@@ -6,56 +6,6 @@ import { classMap } from 'lit/directives/class-map.js';
 
 import style from './BaseCard.scss';
 
-/**
- * This element creates a header, body, and footer region in which to place
- * content or other components.
- *
- * @summary Gives a preview of information in a small layout
- *
- * @slot header
- *       If this slot is used, we expect a heading level tag (h1, h2, h3, h4, h5, h6).
- *       An icon, svg, or use of the icon component are also valid in this region.
- * @slot - Any content that is not designated for the header or footer slot, will go to this slot.
- * @slot footer
- *       Use this slot for anything that you want to be stuck to the base of the card.
- *       This region is bottom-aligned.
- *
- * @slot pfe-card--header - {@deprecated use header}
- * @slot pfe-card--footer - {@deprecated use footer}
- *
- * @csspart header - The container for *header* content
- * @csspart body - The container for *body* content
- * @csspart footer - The container for *footer* content
- *
- *
- * @cssproperty {<color>} --pfe-theme--color--surface--lightest   {@default `#ffffff`}
- * @cssproperty {<color>} --pfe-theme--color--surface--lighter    {@default `#ececec`}
- * @cssproperty {<color>} --pfe-theme--color--surface--base       {@default `#f0f0f0`}
- * @cssproperty {<color>} --pfe-theme--color--surface--darker     {@default `#3c3f42`}
- * @cssproperty {<color>} --pfe-theme--color--surface--darkest    {@default `#151515`}
- * @cssproperty {<color>} --pfe-theme--color--surface--accent     {@default `#004080`}
- * @cssproperty {<color>} --pfe-theme--color--surface--complement {@default `#002952`}
- *
- * @cssproperty --pfe-band--BackgroundColor
- *              Though using the `color` attribute is strongly recommended when setting the background color for the band, you can also use completely custom colors by updating the `--pfe-band--BackgroundColor` variable.  If you update this value manually, you should also update the `--theme` context variable to invoke the right theme on it and it's child elements.  Supported themes include: `light`, `dark`, and `saturated`.
- * @cssproperty --pfe-card--BackgroundPosition
- *              This is designed for use with the `img-src` attribute to allow you to align your background image.  Default value is `center center`.
- *              {@default `center center`}
- * @cssproperty --pfe-card--Border
- *              This allows the customization of a border around the entire container.
- *              {@default `center center`}
- * @cssproperty --pfe-card--Border
- *              This allows the customization of a border around the entire container.
- * @cssproperty --pfe-card--BorderRadius
- *              This allows the customization of a border radius around the entire container.
- * @cssproperty --pfe-card--BorderWeight
- *              This allows the customization of a border weight around the entire container.
- * @cssproperty --pfe-card--BorderStyle
- *              This allows the customization of a border style around the entire container.
- * @cssproperty --pfe-card--BorderColor
- *              This allows the customization of a border color around the entire container.
- */
-
 export abstract class BaseCard extends LitElement {
   static readonly version = '{{version}}';
 
@@ -75,22 +25,20 @@ export abstract class BaseCard extends LitElement {
 
    render() {
      const classes = {
-       'has-header': this.slots.hasSlotted('header', 'pfe-card--header'),
-       'has-footer': this.slots.hasSlotted('footer', 'pfe-card--footer'),
+       'has-header': this.slots.hasSlotted('header'),
+       'has-footer': this.slots.hasSlotted('footer'),
        'has-body': this.slots.hasSlotted(),
      };
 
      return html`
       <div class="pfe-card__header ${classMap(classes)}" part="header">
         <slot name="header"></slot>
-        <slot name="pfe-card--header"></slot>
       </div>
       <div class="pfe-card__body ${classMap(classes)}" part="body">
         <slot></slot>
       </div>
       <div class="pfe-card__footer ${classMap(classes)}" part="footer">
         <slot name="footer"></slot>
-        <slot name="pfe-card--footer"></slot>
       </div>
     `;
    }
--- elements/pfe-card/pfe-card.ts
+++ elements/pfe-card/pfe-card.ts
@@ -2,8 +2,6 @@ import { customElement, property } from 'lit/decorators.js';
 
 import style from './pfe-card.scss';
 import { BaseCard } from './BaseCard';
-import { bound } from '@patternfly/pfe-core/decorators.js';
-// import { bound } from '@patternfly/pfe-core/decorators/bound.js';
 
 /**
  * This element creates a header, body, and footer region in which to place
@@ -17,42 +15,35 @@ import { bound } from '@patternfly/pfe-core/decorators.js';
  * @slot - Any content that is not designated for the header or footer slot, will go to this slot.
  * @slot footer
  *       Use this slot for anything that you want to be stuck to the base of the card.
- *       This region is bottom-aligned.
- *
- * @slot pfe-card--header - {@deprecated use header}
- * @slot pfe-card--footer - {@deprecated use footer}
  *
  * @csspart header - The container for *header* content
  * @csspart body - The container for *body* content
  * @csspart footer - The container for *footer* content
  *
  *
- * @cssproperty {<color>} --pfe-theme--color--surface--lightest   {@default `#ffffff`}
- * @cssproperty {<color>} --pfe-theme--color--surface--lighter    {@default `#ececec`}
- * @cssproperty {<color>} --pfe-theme--color--surface--base       {@default `#f0f0f0`}
- * @cssproperty {<color>} --pfe-theme--color--surface--darker     {@default `#3c3f42`}
- * @cssproperty {<color>} --pfe-theme--color--surface--darkest    {@default `#151515`}
- * @cssproperty {<color>} --pfe-theme--color--surface--accent     {@default `#004080`}
- * @cssproperty {<color>} --pfe-theme--color--surface--complement {@default `#002952`}
- *
- * @cssproperty --pfe-band--BackgroundColor
- *              Though using the `color` attribute is strongly recommended when setting the background color for the band, you can also use completely custom colors by updating the `--pfe-band--BackgroundColor` variable.  If you update this value manually, you should also update the `--theme` context variable to invoke the right theme on it and it's child elements.  Supported themes include: `light`, `dark`, and `saturated`.
- * @cssproperty --pfe-card--BackgroundPosition
- *              This is designed for use with the `img-src` attribute to allow you to align your background image.  Default value is `center center`.
- *              {@default `center center`}
- * @cssproperty --pfe-card--Border
- *              This allows the customization of a border around the entire container.
- *              {@default `center center`}
- * @cssproperty --pfe-card--Border
- *              This allows the customization of a border around the entire container.
- * @cssproperty --pfe-card--BorderRadius
- *              This allows the customization of a border radius around the entire container.
- * @cssproperty --pfe-card--BorderWeight
- *              This allows the customization of a border weight around the entire container.
- * @cssproperty --pfe-card--BorderStyle
- *              This allows the customization of a border style around the entire container.
- * @cssproperty --pfe-card--BorderColor
- *              This allows the customization of a border color around the entire container.
+ * @cssproperty {<color>} --pf-c-card--BackgroundColor {@default `#ffffff`}
+ * @cssproperty {<color>} --pf-c-card--BoxShadow {@default `0 0.0625rem 0.125rem 0 rgba(3, 3, 3, 0.12), 0 0 0.125rem 0 rgba(3, 3, 3, 0.06)`}
+ * @cssproperty {<color>} --pf-c-card--size-compact__body--FontSize {@default `.875rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact__footer--FontSize {@default `1rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact--first-child--PaddingTop {@default `1.5rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact--child--PaddingRight {@default `1rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact--child--PaddingBottom {@default `1rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact--child--PaddingLeft {@default `1rem`}
+ * @cssproperty {<color>} --pf-c-card--size-compact__title--not--last-child--PaddingBottom {@default `.5rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large__title--FontSize {@default `1.25rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large--first-child--PaddingTop {@default `2rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large--child--PaddingRight {@default `2rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large--child--PaddingBottom {@default `2rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large--child--PaddingLeft {@default `2rem`}
+ * @cssproperty {<color>} --pf-c-card--size-large__title--not--last-child--PaddingBottom {@default `1.5rem`}
+ * @cssproperty {<color>} --pf-c-card--m-flat--BorderWidth {@default `1px solid #d2d2d2`}
+ * @cssproperty {<color>} --pf-c-card--m-plain--BoxShadow {@default `none`}
+ * @cssproperty {<color>} --pf-c-card--m-plain--BackgroundColor {@default `transparent`}
+ * @cssproperty {<color>} --pf-c-card--m-rounded--BorderRadius {@default `3px`}
+ * @cssproperty {<color>} --pf-c-card--m-full-height--Height {@default `100%`}
+ * @cssproperty {<color>} --pf-c-card__title--FontFamily {@default `"RedHatDisplayUpdated", helvetica, arial, sans-serif`}
+ * @cssproperty {<color>} --_pf-c-card__title--FontSize {@default `1rem`}
+ * @cssproperty {<color>} --pf-c-card__title--FontWeight {@default `700`}
  */
 @customElement('pfe-card')
 export class PfeCard extends BaseCard {

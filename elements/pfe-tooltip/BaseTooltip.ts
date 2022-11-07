2f8314f3e0d88626fb56075edc846c2e217767e7
Brian Ferry <brian.z.ferry@gmail.com>
2022-11-07 18:22:01 -0500

    feat: adding floating-ui/dom and removing pooperjs/core

--- elements/pfe-tooltip/BaseTooltip.ts
+++ elements/pfe-tooltip/BaseTooltip.ts
@@ -1,12 +1,11 @@
-import type { Placement } from '@patternfly/pfe-core/controllers/floating-dom-controller.js';
-
 import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
-import { classMap } from 'lit/directives/class-map.js';
-
-import { FloatingDOMController } from '@patternfly/pfe-core/controllers/floating-dom-controller.js';
+import { computePosition, Placement, offset, shift, autoPlacement, arrow } from '@floating-ui/dom';
 
 import style from './BaseTooltip.scss';
+
+const enterEvents = ['focus', 'tap', 'click', 'mouseenter'];
+const exitEvents = ['blur', 'mouseleave'];
 
 export abstract class BaseTooltip extends LitElement {
   static readonly styles = [style];
@@ -14,30 +13,32 @@ export abstract class BaseTooltip extends LitElement {
   /** The placement of the tooltip, relative to the invoking content */
   @property({ reflect: true }) position: Placement = 'top';
 
+  @property({ reflect: true, type: Boolean }) showing = false;
+
   /**
    * Optional tooltip offset: a comma separated coordinate pair of numbers
    *     ```html
    *     <pfe-tooltip offset="5,6">...</pfe-tooltip>
    *     ```
    */
-  @property({ converter: {
-    fromAttribute(numbers: string) {
-      return numbers.split(',').map(x => parseInt(x.trim()));
+  @property({
+    converter: {
+      fromAttribute(numbers: string) {
+        return numbers.split(',').map(x => parseInt(x.trim()));
+      }
     }
-  } })
+  })
     offset = [0, 15];
 
-  #domController: FloatingDOMController = new FloatingDOMController(this);
-
-  get #isOpen(): boolean {
-    return this.#domController.open;
+  get #arrow(): HTMLElement | null {
+    return this.shadowRoot?.querySelector<HTMLElement>('#arrow') ?? null;
   }
 
-  get #invoker(): HTMLElement|null {
+  get #invoker(): HTMLElement | null {
     return this.shadowRoot?.querySelector<HTMLElement>('#invoker') ?? null;
   }
 
-  get #tooltip(): HTMLElement|null {
+  get #tooltip(): HTMLElement | null {
     return this.shadowRoot?.querySelector<HTMLElement>(`#tooltip`) ?? null;
   }
 
@@ -46,39 +47,66 @@ export abstract class BaseTooltip extends LitElement {
     if (!['top', 'bottom'].includes(this.position)) {
       this.offset = [-4, 15];
     }
-    this.#addListeners();
+    // this.#addListeners();
+    enterEvents.forEach(evt => this.addEventListener(evt, this.show));
+    exitEvents.forEach(evt => this.addEventListener(evt, this.hide));
   }
 
-  /** Show the tooltip */
   show() {
     if (this.#invoker && this.#tooltip) {
-      this.#domController.create(this.#invoker, this.#tooltip, this.position, this.offset);
-      this.#domController.show();
+      computePosition(this.#invoker!, this.#tooltip!, {
+        strategy: 'absolute',
+        placement: this.position,
+        middleware: [
+          offset(this.offset[1]),
+          shift(),
+          autoPlacement({ allowedPlacements: ['top', 'left', 'right', 'bottom'] }),
+          arrow({
+            element: this.#arrow!
+          }),
+        ]
+      }).then(({ x, y, middlewareData }) => {
+        Object.assign(this.#tooltip!.style, {
+          top: `${y}px`,
+          left: `${x}px`,
+        });
+
+        if (middlewareData.arrow) {
+          const { y: arrowY, x: arrowX } = middlewareData.arrow;
+
+          const staticSide = {
+            top: 'bottom',
+            right: 'left',
+            bottom: 'top',
+            left: 'right',
+          }[this.position.split('-')[0]];
+
+          Object.assign(this.#arrow!.style, {
+            left: arrowX != null ? `${arrowX}px` : '',
+            top: arrowY != null ? `${arrowY}px` : '',
+            right: '',
+            bottom: '',
+            [staticSide]: '-4px',
+          });
+        }
+      });
     }
+    this.showing = true;
   }
 
-  /** Hide the tooltip */
+
   hide() {
-    this.#domController.hide();
-  }
-
-  #addListeners() {
-    this.addEventListener('focus', this.show);
-    this.addEventListener('blur', this.hide);
-    this.addEventListener('tap', this.show);
-    this.addEventListener('click', this.show);
-    this.addEventListener('mouseenter', this.show);
-    this.addEventListener('mouseleave', this.hide);
+    this.showing = false;
   }
 
   override render() {
-    const { initialized } = this.#domController;
+    // const { initialized } = this.#domController;
     return html`
       <div id="invoker" role="tooltip" tabindex="0" aria-labelledby="tooltip">
         <slot></slot>
       </div>
-      <div id="tooltip" aria-hidden=${!this.#isOpen} class=${classMap({ initialized })}>
-        <div class="arrow"></div>
+      <div id="tooltip">
+        <div id="arrow"></div>
         <div id="content" class="content">
           <slot name="content"></slot>
         </div>

a83ca750315cadad2d8b7a7258f948250d28abc3
Benny Powers <web@bennypowers.com>
2022-12-08 11:41:06 +0200

    fix(avatar): various fixes

--- elements/pfe-avatar/BaseAvatar.ts
+++ elements/pfe-avatar/BaseAvatar.ts
@@ -1,30 +1,27 @@
 import { LitElement, html } from 'lit';
 import { property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 import style from './BaseAvatar.scss';
+
+const DEFAULT = new URL('./lib/img_avatar-light.svg', import.meta.url).pathname;
 
 /**
  * Avatar is an element for displaying a user's avatar image.
  *
  *
  * @summary For displaying a user's avatar image
- *
- * @csspart {HTMLImageElement} img - The image element for when an image URL is provided
  */
 
-export abstract class BaseAvatar extends LitElement {
+export class BaseAvatar extends LitElement {
   static readonly version = '{{version}}';
 
   static readonly styles = [style];
 
-  /**
-  * The URL to the user's custom avatar image.
-  */
-  @property({ reflect: true }) src?: string = '/elements/pfe-avatar/lib/img_avatar-light.svg';
+  /** The URL to the user's custom avatar image. */
+  @property() src?: string = DEFAULT;
 
-  /**
-  * The alt text for the avatar image.
-  */
+  /** The alt text for the avatar image. */
   @property({ reflect: true }) alt?: string = 'Avatar image';
 
   /** Size of the Avatar */
@@ -33,11 +30,9 @@ export abstract class BaseAvatar extends LitElement {
   render() {
     return html`
       <img
-        class="pf-c-avatar"
         size=${this.size}
-        alt=${this.alt}
-        src=${this.src}
-      />
+        alt=${this.alt ?? ''}
+        src=${ifDefined(this.src)}>
     `;
   }
 }
--- elements/pfe-avatar/pfe-avatar.ts
+++ elements/pfe-avatar/pfe-avatar.ts
@@ -1,7 +1,4 @@
-import { html } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
-
-import { pfelement } from '@patternfly/pfe-core/decorators.js';
 
 import { BaseAvatar } from './BaseAvatar.js';
 
@@ -12,11 +9,8 @@ import style from './pfe-avatar.scss';
  *
  *
  * @summary For displaying a user's avatar image
- *
- * @csspart {HTMLImageElement} img - The image element for when an image URL is provided
  */
-
-@customElement('pfe-avatar') @pfelement()
+@customElement('pfe-avatar')
 export class PfeAvatar extends BaseAvatar {
   static readonly version = '{{version}}';
 
@@ -25,20 +19,12 @@ export class PfeAvatar extends BaseAvatar {
   /** Size of the Avatar */
   @property({ reflect: true }) size: 'sm'|'md'|'lg'|'xl' = 'sm';
 
-  render() {
-    return html`
-      <img
-        class="pf-c-avatar"
-        size=${this.size}
-        alt=${this.alt}
-        src=${this.src}
-      />
-    `;
-  }
+  /** Whether to display a border around the avatar */
+  @property({ reflect: true }) border?: 'light'|'dark';
 }
 
-  declare global {
-    interface HTMLElementTagNameMap {
-      'pfe-avatar': PfeAvatar;
-    }
+declare global {
+  interface HTMLElementTagNameMap {
+    'pfe-avatar': PfeAvatar;
   }
+}

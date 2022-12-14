c6613113bdca4e2b15b50a0c0492f8807deb750d
Benny Powers <web@bennypowers.com>
2022-12-14 18:22:49 +0200

    docs(accordion): fixes in demo

--- elements/pfe-accordion/demo/pfe-accordion.js
+++ elements/pfe-accordion/demo/pfe-accordion.js
@@ -2,7 +2,7 @@ import '@patternfly/pfe-accordion';
 import '@patternfly/pfe-button';
 import '@patternfly/pfe-switch';
 
-const bordered = document.getElementById('#bordered-example');
+const bordered = document.getElementById('bordered-example');
 const isLarge = document.getElementById('toggle-size');
 
 isLarge.addEventListener('change', function() {

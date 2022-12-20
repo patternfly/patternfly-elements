0298953033b0c02d14f7187900adf75c7706e6ca
Brian Ferry <brian.z.ferry@gmail.com>
2022-12-20 09:12:53 -0500

    feat(card): pfe-card 1:1  (#2222)
    * fix: initial commit for pfv4 card
    
    * fix: adding BaseCard.ts / scss and reformatting stylings
    
    * fix: updating the --pf-c-card css
    
    * fix: updating the --pf-c-card css
    
    * fix: export basecard.js
    
    * fix: updating the base styles to match new format
    
    * fix: removing attributes from basecard
    
    * chore: updating the pfe-card styles and the styledocs
    
    * fix: removing old styles for demos, adding newlines back for css
    
    * fix: adding stylings back to the pfe-card demo and adding spacing for the buttons
    
    * fix: updating demo to show fullHeight and border more easily.  adding changeset
    
    * test(card): :white_check_mark: updating tests for pfe-card 1:1
    
    * fix: removing pfe specific class names from basecard
    
    * docs: updating docs to use pfe-switch instead of checkbox inputs
    
    * fix(card): updating fullHeight to full-height attribute
    
    * fix(card): fixing broken unit test, adding accessibility tests, article wrapper
    
    * feat(card): adding a11ysnapshot plugin, test to card spec
    
    * fix(card): clean up in demo and tests
    
    * fix(card): hide empty regions
    
    remove unused css
    
    * docs(card): update demo
    
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>
    Co-authored-by: Em Nouveau <enouveau@redhat.com>
    Co-authored-by: Benny Powers <web@bennypowers.com>

--- elements/pfe-card/demo/pfe-card.js
+++ elements/pfe-card/demo/pfe-card.js
@@ -1,9 +1,22 @@
-import '@patternfly/pfe-band';
 import '@patternfly/pfe-card';
-import '@patternfly/pfe-select';
+import '@patternfly/pfe-button';
+import '@patternfly/pfe-switch';
+const form = document.getElementById('card-settings');
+const card = document.querySelector('pfe-card');
 
-const root = document.querySelector('[data-demo="pfe-card"]')?.shadowRoot ?? document;
-
-root.querySelector('#context-selector').addEventListener('select', event => {
-  event.target.closest('pfe-card').setAttribute('color', event.value);
+form.addEventListener('change', function(event) {
+  const { checked } = event.target;
+  const { attribute } = event.target.dataset;
+  switch (attribute) {
+    case 'flat':
+    case 'rounded':
+    case 'plain':
+    case 'full-height':
+      card.toggleAttribute(attribute, checked);
+      break;
+    case 'size':
+      card.setAttribute('size', form.querySelector('label[for="size"]:not([hidden])').textContent.toLowerCase());
+      break;
+  }
 });
+
--- elements/pfe-card/test/pfe-card.spec.ts
+++ elements/pfe-card/test/pfe-card.spec.ts
@@ -1,60 +1,159 @@
 import { expect, html, aTimeout } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
-import { hexToRgb, getColor } from '@patternfly/pfe-tools/test/hex-to-rgb.js';
-
+import { a11ySnapshot, findAccessibilityNode } from '@web/test-runner-commands';
 import '@patternfly/pfe-tools/test/stub-logger.js';
 
 import { PfeCard } from '@patternfly/pfe-card';
 
-/** Returns the luminance value from RGB */
-const luminance = (r: number, g: number, b: number): number => {
-  return (0.2126 * r / 255 + 0.7152 * g / 255 + 0.0722 * b / 255);
-};
-
-const TEMPLATES = {
-
-  card1: html`
-    <pfe-card id="card1">
-      <h2 slot="header">Card 1</h2>
-      <p>This is pfe-card.</p>
-      <div slot="footer">Text in footer</div>
-    </pfe-card>`,
-
-  card2: html`
-    <pfe-card id="card2">
-      <img src="https://placekitten.com/200/150" alt=""/>
-    </pfe-card>`,
-
-  card3: html`
-    <pfe-card id="card3">
-      <h2 slot="header">Card 3</h2>
-      <img src="https://placekitten.com/1000/500" alt="" overflow="top right bottom left"/>
-    </pfe-card>`,
-
-  card4: html`
-    <pfe-card id="card4">
-      <h2 slot="header">Card 4</h2>
-      <p>This is pfe-card.</p>
-      <div slot="footer">Text in footer</div>
-    </pfe-card>`,
-
-};
-
-const dynamicHeaderFooter = html`
-  <pfe-card id="dynamicHeaderFooter">
-    This is the card
-  </pfe-card>`;
-
 describe('<pfe-card>', function() {
   it('should upgrade', async function() {
-    expect(await createFixture<PfeCard>(TEMPLATES.card1))
+    expect(await createFixture<PfeCard>(html`<pfe-card></pfe-card>`))
       .to.be.an.instanceof(customElements.get('pfe-card'))
       .and
       .to.be.an.instanceof(PfeCard);
   });
 
+  describe('with header and footer content', function() {
+    let element: PfeCard;
+    let origHeight: number;
+
+    beforeEach(async function() {
+      element = await createFixture<PfeCard>(html`
+        <pfe-card id="card1">
+          <h2 slot="header">Card 1</h2>
+          <p>This is pfe-card.</p>
+          <span slot="footer">Text in footer</span>
+        </pfe-card>`);
+    });
+
+    beforeEach(function() {
+      const { height } = element.getBoundingClientRect();
+      origHeight = height;
+    });
+
+    describe('size', function() {
+      describe('unset', function() {
+        it('should have default sizing for card parts', function() {
+          expect(getComputedStyle(element.querySelector('p')!, 'body font-size').getPropertyValue('font-size')).to.equal('16px');
+          expect(getComputedStyle(element.querySelector('[slot=footer]')!, 'footer font-size').getPropertyValue('font-size')).to.equal('16px');
+        });
+      });
+
+      describe('compact', function() {
+        beforeEach(async function() {
+          element.setAttribute('size', 'compact');
+        });
+        it('should be smaller', function() {
+          const { height } = element.getBoundingClientRect();
+          expect(origHeight, 'height').to.be.greaterThan(height);
+        });
+      });
+
+      describe('large', function() {
+        beforeEach(async function() {
+          element.setAttribute('size', 'large');
+        });
+        it('should have larger font sizes for body, footer, more padding for header', function() {
+          const { height } = element.getBoundingClientRect();
+          expect(origHeight, 'height').to.be.lessThan(height);
+          expect(getComputedStyle(element.querySelector('p')!, 'body font-size').getPropertyValue('font-size'))
+            .to.equal('16px');
+          expect(getComputedStyle(element.querySelector('[slot=footer]')!, 'footer font-size').getPropertyValue('font-size'))
+            .to.equal('16px');
+        });
+      });
+    });
+
+    describe('rounded', function() {
+      describe('unset', function() {
+        it('should have default border radius', function() {
+          expect(getComputedStyle(element).getPropertyValue('border-radius')).to.equal('0px');
+        });
+      });
+
+      describe('is set', function() {
+        it('should have 3px border radius', async function() {
+          element.setAttribute('rounded', '');
+          await element.updateComplete;
+          expect(getComputedStyle(element).getPropertyValue('border-radius')).to.equal('3px');
+        });
+      });
+    });
+
+    describe('flat', function() {
+      describe('unset', function() {
+        it('should have default box shadow and border (none)', function() {
+          expect(getComputedStyle(element).getPropertyValue('box-shadow')).not.to.equal('none');
+          expect(getComputedStyle(element).getPropertyValue('border')).to.equal('0px none rgb(0, 0, 0)');
+        });
+      });
+
+      describe('is set', function() {
+        it('should not have a box shadow and should have an added border', async function() {
+          element.setAttribute('flat', '');
+          await element.updateComplete;
+
+          expect(getComputedStyle(element).getPropertyValue('box-shadow')).to.equal('none');
+          expect(getComputedStyle(element).getPropertyValue('border')).not.to.equal('none');
+        });
+      });
+    });
+
+    describe('full-height', function() {
+      describe('is not set', function() {
+        it('should not be taller', async function() {
+          const { height } = element.getBoundingClientRect();
+          expect(height).to.equal(origHeight);
+        });
+      });
+
+      describe('is set', function() {
+        beforeEach(async function() {
+          element.toggleAttribute('full-height', true);
+          await element.updateComplete;
+        });
+
+        it('should be taller', async function() {
+          const { height } = element.getBoundingClientRect();
+          expect(height).to.be.greaterThan(origHeight);
+        });
+      });
+    });
+
+    describe('plain', function() {
+      describe('unset', function() {
+        it('should have the default box shadow and background-color', function() {
+          expect(getComputedStyle(element).getPropertyValue('box-shadow')).not.to.be.undefined;
+          expect(getComputedStyle(element).getPropertyValue('background-color')).to.equal('rgb(255, 255, 255)');
+        });
+      });
+      describe('is set', function() {
+        it('should have transparent background color and no box shadow', async function() {
+          element.setAttribute('plain', '');
+          expect(getComputedStyle(element).getPropertyValue('box-shadow')).to.equal('none');
+          expect(getComputedStyle(element).getPropertyValue('background-color')).to.equal('rgba(0, 0, 0, 0)');
+        });
+      });
+    });
+
+    describe('accessible', function() {
+      it('should be accessible in both the light and shadow dom', async function() {
+        expect(element).to.be.accessible();
+      });
+
+      it.skip('should have an article element wrapper in the shadow dom', async function() {
+        const snapshot = await a11ySnapshot();
+        const foundNode = findAccessibilityNode(snapshot, node => node.role === 'WebArea');
+        expect(foundNode, 'A node with the supplied name has been found.').to.not.be.null;
+      });
+    });
+  });
+
   it(`should render a header and footer when content for those slots are added dynamically`, async function() {
-    const element = await createFixture<PfeCard>(dynamicHeaderFooter);
+    const element = await createFixture<PfeCard>(html`
+      <pfe-card id="dynamicHeaderFooter">
+        This is the card
+      </pfe-card>`);
 
     const header = document.createElement('h2');
     header.setAttribute('slot', 'header');
@@ -78,305 +177,5 @@ describe('<pfe-card>', function() {
 
     expect(cardHeaderSlot.assignedNodes().length).to.equal(1);
     expect(cardFooterSlot.assignedNodes().length).to.equal(1);
-  });
-
-  it(`should render a header and footer when content for those deprecated slots are added dynamically`, async function() {
-    const element = await createFixture<PfeCard>(dynamicHeaderFooter);
-
-    const header = document.createElement('h2');
-    header.setAttribute('slot', 'pfe-card--header');
-    header.textContent = 'Card Header';
-
-    const footer = document.createElement('div');
-    footer.setAttribute('slot', 'pfe-card--footer');
-    footer.textContent = 'This is the footer';
-
-    element.prepend(header);
-    element.appendChild(footer);
-
-    await element.updateComplete;
-
-    const cardHeaderSlot =
-      element.shadowRoot!.querySelector<HTMLSlotElement>('slot[name="pfe-card--header"]')!;
-    const cardFooterSlot =
-      element.shadowRoot!.querySelector<HTMLSlotElement>('slot[name="pfe-card--footer"]')!;
-
-    await element.updateComplete;
-
-    expect(cardHeaderSlot.assignedNodes().length).to.equal(1);
-    expect(cardFooterSlot.assignedNodes().length).to.equal(1);
-  });
-
-  // Iterate over the colors object to test expected background color results
-  describe('color palettes', function() {
-    // Themes and their expected hex values
-    Object.entries({
-      default: '#f0f0f0',
-      darker: '#3c3f42',
-      darkest: '#151515',
-      accent: '#004080',
-      complement: '#002952',
-      lightest: '#ffffff',
-    }).forEach(([colorName, colorValue]) => {
-      it(`it should have a background color of ${colorValue} when color-palette is ${colorName}`, async function() {
-        const element = await createFixture<PfeCard>(TEMPLATES.card1);
-        // If this is not the default color, update the color attribute
-        if (colorName !== 'default') {
-          element.setAttribute('color-palette', colorName);
-        }
-
-        // Get the background color value
-        const [r, g, b] = getColor(element, 'background-color');
-        // Test that the color is rendering as expected
-        expect([r, g, b]).to.deep.equal(hexToRgb(colorValue));
-        // Test that the color is working
-        if (['dark', 'darker', 'darkest', 'complement', 'accent'].includes(colorName)) {
-          expect(luminance(r, g, b)).to.be.lessThan(0.5);
-        } else {
-          expect(luminance(r, g, b)).to.be.greaterThan(0.5);
-        }
-      });
-
-      it(`it should have a background color of ${colorValue} when deprecated color attribute is ${colorName}`, async function() {
-        const element = await createFixture<PfeCard>(TEMPLATES.card1);
-        // If this is not the default color, update the color attribute
-        if (colorName !== 'default') {
-          element.setAttribute('color', colorName);
-        }
-
-        await element.updateComplete;
-
-        // Get the background color value
-        const [r, g, b] = getColor(element, 'background-color');
-        // Test that the color is rendering as expected
-        expect([r, g, b]).to.deep.equal(hexToRgb(colorValue));
-        // Test that the color is working
-        if (['dark', 'darker', 'darkest', 'complement', 'accent'].includes(colorName)) {
-          expect(luminance(r, g, b)).to.be.lessThan(0.5);
-        } else {
-          expect(luminance(r, g, b)).to.be.greaterThan(0.5);
-        }
-      });
-    });
-  });
-
-  it('should have standard padding when size is not set', async function() {
-    const element = await createFixture<PfeCard>(TEMPLATES.card1);
-    expect(getComputedStyle(element).getPropertyValue('padding')).to.equal('32px');
-  });
-
-  it('should have reduced padding when size is small', async function() {
-    const element = await createFixture<PfeCard>(TEMPLATES.card1);
-    element.setAttribute('size', 'small');
-    expect(getComputedStyle(element).getPropertyValue('padding')).to.equal('16px');
-    element.removeAttribute('size');
-  });
-
-  it('should have a standard border when border is set', async function() {
-    const element = await createFixture<PfeCard>(TEMPLATES.card1);
-    element.setAttribute('border', '');
-
-    expect(getColor(element, 'border-left-color')).to.deep.equal(hexToRgb('#d2d2d2'));
-    expect(Math.round(parseFloat(getComputedStyle(element).getPropertyValue('border-left-width'))))
-      .to.equal(1);
-  });
-
-  // Iterate over the slots object to test expected results
-  describe('slots', function() {
-    Object.entries({
-      header: {
-        name: 'header',
-        class: 'pfe-card__header',
-        content: 'Card 1',
-      },
-      body: {
-        name: undefined,
-        class: 'pfe-card__body',
-        content: 'This is pfe-card.',
-      },
-      footer: {
-        name: 'footer',
-        class: 'pfe-card__footer',
-        content: 'Text in footer',
-      },
-    }).forEach(([title, config]) => {
-      it(`${title} content is placed into correct slot`, async function() {
-        const element = await createFixture<PfeCard>(TEMPLATES.card1);
-        const selector = title !== 'body' ? `[slot=${config.name}]` : 'p';
-        expect(element.querySelector(selector)!.assignedSlot)
-          .to.equal(element.shadowRoot!.querySelector(`.${config.class} > *`) );
-
-        const content = element.shadowRoot!.querySelector<HTMLSlotElement>(`.${config.class} > *`)!
-          .assignedNodes()
-          .map(n => n.textContent)
-          .join('')
-          .trim();
-        expect(content).to.equal(config.content);
-      });
-    });
-  });
-
-  describe('deprecated slots', function() {
-    let element: PfeCard|undefined;
-
-    beforeEach(async function() {
-      element = await createFixture<PfeCard>(html`
-        <pfe-card id="deprecated-slots">
-          <h2 slot="pfe-card--header">Deprecated header</h2>
-          <p>This is pfe-card.</p>
-          <div slot="pfe-card--footer">Deprecated footer</div>
-        </pfe-card>`);
-    });
-
-    afterEach(function() {
-      element = undefined;
-    });
-
-    Object.entries({
-      header: {
-        name: 'pfe-card--header',
-        class: 'pfe-card__header',
-        content: 'Deprecated header',
-      },
-      body: {
-        name: undefined,
-        class: 'pfe-card__body',
-        content: 'This is pfe-card.',
-      },
-      footer: {
-        name: 'pfe-card--footer',
-        class: 'pfe-card__footer',
-        content: 'Deprecated footer',
-      },
-    }).forEach(([title, config]) => {
-      it(`${title} content is placed into correct slot`, async function() {
-        const selector = title !== 'body' ? `[slot="${config.name}"]` : 'p';
-
-        const slotSelector = config.name ? `.${config.class} > [name="${config.name}"]` : `.${config.class} > slot`;
-
-        expect(element!.querySelector(selector)!.assignedSlot)
-          .to.equal(element!.shadowRoot!.querySelector(slotSelector));
-
-        const content = element!.shadowRoot!
-          .querySelector<HTMLSlotElement>(slotSelector)!
-          .assignedNodes()
-          .map(n => n.textContent)
-          .join('')
-          .trim();
-
-        expect(content).to.equal(config.content);
-      });
-    });
-  });
-
-  // Iterate over possibilities for images
-  describe('overflow', function() {
-    ['top', 'right', 'bottom', 'left'].forEach(direction => {
-      it(`image should overflow to the ${direction}`, async function() {
-        const element = await createFixture<PfeCard>(TEMPLATES.card2);
-        const image = element.querySelector('img')!;
-        image.setAttribute('overflow', direction);
-        await aTimeout(50);
-        const margin = direction !== 'bottom' ? '-32px' : '-35px';
-        expect(getComputedStyle(image).getPropertyValue(`margin-${direction}`)).to.equal(margin);
-      });
-    });
-
-    it('image should overflow all padding', async function() {
-      const element = await createFixture<PfeCard>(TEMPLATES.card3);
-      const image = element.querySelector('img')!;
-      expect(image.getAttribute('overflow')).to.equal('top right bottom left');
-      const style = getComputedStyle(image);
-      await aTimeout(50);
-      expect(style.getPropertyValue('margin-top'), 'margin-top').to.equal('-32px');
-      expect(style.getPropertyValue('margin-right'), 'margin-right').to.equal('-32px');
-      expect(style.getPropertyValue('margin-bottom'), 'margin-bottom').to.equal('-35px');
-      expect(style.getPropertyValue('margin-left'), 'margin-left').to.equal('-32px');
-    });
-  });
-
-  // Iterate over the custom properties to test overrides work
-  describe('custom properties', function() {
-    Object.values({
-      paddingTop: {
-        variable: '--pfe-card--PaddingTop',
-        css: 'padding-top',
-        default: '32px',
-        custom: '28px',
-      },
-      paddingRight: {
-        variable: '--pfe-card--PaddingRight',
-        css: 'padding-right',
-        default: '32px',
-        custom: '28px',
-      },
-      paddingBottom: {
-        variable: '--pfe-card--PaddingBottom',
-        css: 'padding-bottom',
-        default: '32px',
-        custom: '28px',
-      },
-      paddingLeft: {
-        variable: '--pfe-card--PaddingLeft',
-        css: 'padding-left',
-        default: '32px',
-        custom: '28px',
-      },
-      padding: {
-        variable: '--pfe-card--Padding',
-        css: 'padding',
-        default: '32px',
-        custom: '20px 10px',
-      },
-      borderRadius: {
-        variable: '--pfe-card--BorderRadius',
-        css: 'border-radius',
-        default: '3px',
-        custom: '50%',
-      },
-      border: {
-        variable: '--pfe-card--Border',
-        css: 'border',
-        default: '0px solid rgb(210, 210, 210)',
-        custom: '1px solid #eee',
-        customExpected: '1px solid rgb(238, 238, 238)',
-      },
-      backgroundColor: {
-        variable: '--pfe-card--BackgroundColor',
-        css: 'background-color',
-        default: '#dfdfdf',
-        defaultExpected: 'rgb(240, 240, 240)',
-        custom: 'hotpink',
-        customExpected: 'rgb(255, 105, 180)',
-      },
-    }).forEach(async property => {
-      describe(`${property.variable}`, function() {
-        let element: PfeCard;
-        let style: CSSStyleDeclaration;
-        beforeEach(async function() {
-          element = await createFixture<PfeCard>(TEMPLATES.card1);
-          style = getComputedStyle(element);
-          await aTimeout(50);
-        });
-
-        it('Uses expected default', async function() {
-          const actual = style.getPropertyValue(property.css);
-          const expected = property.defaultExpected ?? property.default;
-          expect(actual).to.equal(expected);
-        });
-
-        it('Allows user customization', async function() {
-          // Update the variable
-          element.style.setProperty(property.variable, property.custom);
-
-          // Test the update worked
-          await aTimeout(50);
-
-          const actual = style.getPropertyValue(property.css);
-          const expected = property.customExpected ?? property.custom;
-          expect(actual).to.equal(expected);
-        });
-      });
-    });
   });
 });

4ae5e9dcd8a8b4e0340edfe58f24c3533d8c814c
Benny Powers <web@bennypowers.com>
2022-12-20 14:44:15 +0200

    docs(card): update demo

--- elements/pfe-card/demo/pfe-card.js
+++ elements/pfe-card/demo/pfe-card.js
@@ -1,47 +1,22 @@
 import '@patternfly/pfe-card';
 import '@patternfly/pfe-button';
 import '@patternfly/pfe-switch';
-const pfeSwitchAll = document.querySelectorAll('pfe-switch');
-const pfeCard = document.querySelector('pfe-card');
-const cardContainer = document.querySelector('#card-container');
+const form = document.getElementById('card-settings');
+const card = document.querySelector('pfe-card');
 
-function setAttr(name, val = '') {
-  pfeCard.setAttribute(name, val);
-}
-
-function rmAttr(name, val = '') {
-  if (pfeCard?.attributes?.getNamedItem(name) && val !== '' && pfeCard?.attributes?.getNamedItem(name)?.value !== val) {
-    return;
+form.addEventListener('change', function(event) {
+  const { checked } = event.target;
+  const { attribute } = event.target.dataset;
+  switch (attribute) {
+    case 'flat':
+    case 'rounded':
+    case 'plain':
+    case 'full-height':
+      card.toggleAttribute(attribute, checked);
+      break;
+    case 'size':
+      card.setAttribute('size', form.querySelector('label[for="size"]:not([hidden])').textContent.toLowerCase());
+      break;
   }
-  pfeCard.removeAttribute(name);
-}
-
-pfeSwitchAll.forEach(pfeSwitch => {
-  pfeSwitch.addEventListener('click', event => {
-    const enabled = event.target.checked;
-    switch (event.target.id) {
-      case 'compact':
-        enabled ? setAttr('size', 'compact') : rmAttr('size', 'compact');
-        break;
-      case 'flat':
-        enabled ? setAttr('flat') : rmAttr('flat');
-        break;
-      case 'rounded':
-        enabled ? setAttr('rounded') : rmAttr('rounded');
-        break;
-      case 'large':
-        enabled ? setAttr('size', 'large') : rmAttr('size', 'large');
-        break;
-      case 'fullHeight':
-        enabled ? setAttr('full-height') : rmAttr('full-height');
-        cardContainer.classList.add('resize');
-        break;
-      case 'plain':
-        enabled ? setAttr('plain') : rmAttr('plain');
-        break;
-      default:
-        break;
-    }
-  });
 });
 

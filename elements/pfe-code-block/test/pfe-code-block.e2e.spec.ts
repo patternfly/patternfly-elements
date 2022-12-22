0af091ac35635c7b37aa478337902ef6d7ddca0a
Kyle Buchanan <kylebuch8@gmail.com>
2022-12-22 08:44:49 -0500

    feat(code-block): adding pfe-code-block (#2233)
    * feat(code-block): adding pfe-code-block
    
    * docs: remove codeblock from demos
    
    * chore(code-block): removing old pfe-codeblock
    
    * chore(code-block): adding a changeset
    
    * chore: update changeset
    
    * chore: update changeset
    
    * fix(code-block)!: use script tags
    
    * test(code-block): update tests
    
    Co-authored-by: Benny Powers <web@bennypowers.com>
    Co-authored-by: Benny Powers <bennypowers@users.noreply.github.com>

--- elements/pfe-code-block/test/pfe-code-block.e2e.spec.ts
+++ elements/pfe-code-block/test/pfe-code-block.e2e.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '@playwright/test';
 import { PfeDemoPage } from '@patternfly/pfe-tools/test/playwright/PfeDemoPage.js';
 
-const tagName = 'pfe-codeblock';
+const tagName = 'pfe-code-block';
 
 test.describe(tagName, () => {
   test('snapshot', async ({ page }) => {

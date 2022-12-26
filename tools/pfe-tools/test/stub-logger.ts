093c8beb55f0563c35a8463cd189ce4b8f05f07c
Benny Powers <web@bennypowers.com>
2022-12-26 19:17:47 +0200

    chore: reluctantly ignore a typescript error

--- tools/pfe-tools/test/stub-logger.ts
+++ tools/pfe-tools/test/stub-logger.ts
@@ -1,6 +1,11 @@
 import type { SinonStub } from 'sinon';
 
-import { Logger } from '@patternfly/pfe-core/controllers/logger.js';
+import {
+  Logger
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: it's either this or break the whole monorepo
+} from '@patternfly/pfe-core/controllers/logger.js';
+
 import { stub } from 'sinon';
 
 for (const method of ['log', 'warn', 'error'] as const) {

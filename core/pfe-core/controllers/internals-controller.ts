fbbfa7742c8e0d400b25e363e227a3fd97af7817
Benny Powers <web@bennypowers.com>
2022-12-26 17:42:27 +0200

    feat(core): add ARIAMixin props to InternalsController

--- core/pfe-core/controllers/internals-controller.ts
+++ core/pfe-core/controllers/internals-controller.ts
@@ -1,9 +1,50 @@
 import type { ReactiveController, ReactiveControllerHost } from 'lit';
 
-export class InternalsController implements ReactiveController {
+export class InternalsController implements ReactiveController, ARIAMixin {
   #internals: ElementInternals;
 
   disabled = false;
+
+  declare role: ARIAMixin['role'];
+  declare ariaAtomic: ARIAMixin['ariaAtomic'];
+  declare ariaAutoComplete: ARIAMixin['ariaAutoComplete'];
+  declare ariaBusy: ARIAMixin['ariaBusy'];
+  declare ariaChecked: ARIAMixin['ariaChecked'];
+  declare ariaColCount: ARIAMixin['ariaColCount'];
+  declare ariaColIndex: ARIAMixin['ariaColIndex'];
+  declare ariaColIndexText: ARIAMixin['ariaColIndexText'];
+  declare ariaColSpan: ARIAMixin['ariaColSpan'];
+  declare ariaCurrent: ARIAMixin['ariaCurrent'];
+  declare ariaDisabled: ARIAMixin['ariaDisabled'];
+  declare ariaExpanded: ARIAMixin['ariaExpanded'];
+  declare ariaHasPopup: ARIAMixin['ariaHasPopup'];
+  declare ariaHidden: ARIAMixin['ariaHidden'];
+  declare ariaInvalid: ARIAMixin['ariaInvalid'];
+  declare ariaKeyShortcuts: ARIAMixin['ariaKeyShortcuts'];
+  declare ariaLabel: ARIAMixin['ariaLabel'];
+  declare ariaLevel: ARIAMixin['ariaLevel'];
+  declare ariaLive: ARIAMixin['ariaLive'];
+  declare ariaModal: ARIAMixin['ariaModal'];
+  declare ariaMultiLine: ARIAMixin['ariaMultiLine'];
+  declare ariaMultiSelectable: ARIAMixin['ariaMultiSelectable'];
+  declare ariaOrientation: ARIAMixin['ariaOrientation'];
+  declare ariaPlaceholder: ARIAMixin['ariaPlaceholder'];
+  declare ariaPosInSet: ARIAMixin['ariaPosInSet'];
+  declare ariaPressed: ARIAMixin['ariaPressed'];
+  declare ariaReadOnly: ARIAMixin['ariaReadOnly'];
+  declare ariaRequired: ARIAMixin['ariaRequired'];
+  declare ariaRoleDescription: ARIAMixin['ariaRoleDescription'];
+  declare ariaRowCount: ARIAMixin['ariaRowCount'];
+  declare ariaRowIndex: ARIAMixin['ariaRowIndex'];
+  declare ariaRowIndexText: ARIAMixin['ariaRowIndexText'];
+  declare ariaRowSpan: ARIAMixin['ariaRowSpan'];
+  declare ariaSelected: ARIAMixin['ariaSelected'];
+  declare ariaSetSize: ARIAMixin['ariaSetSize'];
+  declare ariaSort: ARIAMixin['ariaSort'];
+  declare ariaValueMax: ARIAMixin['ariaValueMax'];
+  declare ariaValueMin: ARIAMixin['ariaValueMin'];
+  declare ariaValueNow: ARIAMixin['ariaValueNow'];
+  declare ariaValueText: ARIAMixin['ariaValueText'];
 
   constructor(
     public host: ReactiveControllerHost & HTMLElement,
@@ -22,6 +63,16 @@ export class InternalsController implements ReactiveController {
   formDisabledCallback(disabled: boolean) {
     this.disabled = disabled;
     this.host.requestUpdate();
+    for (const key of Object.keys(Object.getPrototypeOf(this.#internals))) {
+      Object.defineProperty(this, key, {
+        get() {
+          return this.#internals[key];
+        },
+        set(value) {
+          this.#internals[key] = value;
+        }
+      });
+    }
   }
 
   hostConnected?(): void

8633d3e69c0dde56b7b3ca17cbcedfb53c7d7112
Benny Powers <web@bennypowers.com>
2022-12-26 10:00:03 +0200

    test: update tests

--- elements/pfe-clipboard-copy/test/pfe-clipboard-copy.spec.ts
+++ elements/pfe-clipboard-copy/test/pfe-clipboard-copy.spec.ts
@@ -1,51 +1,111 @@
-import { setViewport } from '@web/test-runner-commands';
+import type { SinonStub } from 'sinon';
+
+import { sendKeys } from '@web/test-runner-commands';
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { PfeClipboardCopy } from '@patternfly/pfe-clipboard-copy';
 
-const element = html`
-  <pfe-clipboard-copy value="copy me!"></pfe-clipboard-copy>
-`;
+import { stub } from 'sinon';
 
 describe('<pfe-clipboard-copy>', function() {
+  let element: PfeClipboardCopy;
+  beforeEach(function stubCopy() {
+    stub(navigator.clipboard, 'writeText');
+  });
+
+  afterEach(function restoreCopy() {
+    (navigator.clipboard.writeText as SinonStub).restore();
+  });
+
+  beforeEach(async function() {
+    element = await createFixture<PfeClipboardCopy>(html`
+      <pfe-clipboard-copy></pfe-clipboard-copy>
+    `);
+  });
+
   it('should upgrade', async function() {
-    const el = await createFixture <PfeClipboardCopy>(element);
     const klass = customElements.get('pfe-clipboard-copy');
-    expect(el)
+    expect(element)
       .to.be.an.instanceOf(klass)
       .and
       .to.be.an.instanceOf(PfeClipboardCopy);
   });
 
-  it('it should have a value property.', async function() {
-    const el = await createFixture <PfeClipboardCopy>(element);
-    el.setAttribute('value', 'another copy item!');
-    await el.updateComplete;
-    expect(el.shadowRoot?.querySelector('input')?.value).to.equal('another copy item!');
+  describe('setting value attribute', function() {
+    const VALUE = 'another copy item!';
+    beforeEach(async function() {
+      element.setAttribute('value', VALUE);
+      await element.updateComplete;
+    });
+    describe('then calling copy()', function() {
+      beforeEach(async function() {
+        await element.copy();
+      });
+      it('should copy the new value.', function() {
+        expect(navigator.clipboard.writeText).to.have.been.calledWith(VALUE);
+      });
+    });
   });
 
-  it('it should contain a readonly property', async function() {
-    const el = await createFixture <PfeClipboardCopy>(element);
-    el.setAttribute('readonly', '');
-    await el.updateComplete;
-    const input = el.shadowRoot?.querySelector('input') as HTMLElement;
-    expect(input?.hasAttribute('disabled')).to.be.true;
-    // it should be grayed out
-    expect(getComputedStyle(input)?.getPropertyValue('background-color')).to.equal('rgb(240, 240, 240)');
+  describe('setting readonly attribute', function() {
+    const VALUE = 'HI';
+    beforeEach(async function() {
+      element.toggleAttribute('readonly', true);
+      await element.updateComplete;
+    });
+    describe('then focusing the element and typing', function() {
+      beforeEach(async function() {
+        element.focus();
+        await sendKeys({ type: VALUE });
+      });
+      describe('then calling copy', function() {
+        beforeEach(async function() {
+          await element.copy();
+        });
+        it('should not include the new value', async function() {
+          expect(navigator.clipboard.writeText).to.not.have.been.calledWith(VALUE);
+        });
+      });
+    });
   });
 
-  it('it should be expandable', async function() {
-    const el = await createFixture <PfeClipboardCopy>(element);
-    el.setAttribute('variant', 'expansion');
-    await el.updateComplete;
-    // ensure the dropdown action button is present
-    expect(el.shadowRoot?.querySelector('[part~="dropdown-button"]')).to.not.be.null;
-    // ensure the dropdown is not present
-    expect(el.shadowRoot?.querySelector('[part~="dropdown"]')).to.be.null;
-
-    // check if the dropdown appears if you expand it.
-    el.expanded = true;
-    await el.updateComplete;
-    expect(el.shadowRoot?.querySelector('[part~="dropdown"]')).to.not.be.null;
+  describe('setting expandable value', function() {
+    let initialHeight: number;
+    let initialFocus: Element;
+    const VALUE = 'Hello!';
+    beforeEach(async function() {
+      element.toggleAttribute('expandable', true);
+      await element.updateComplete;
+      initialHeight = element.getBoundingClientRect().height;
+    });
+    describe('then focusing the element and pressing SPACE', function() {
+      beforeEach(async function() {
+        element.focus();
+        initialFocus = element.shadowRoot!.activeElement!;
+        await sendKeys({ press: ' ' });
+      });
+      it('it should be expanded', async function() {
+        expect(element.getBoundingClientRect().height).to.be.greaterThan(initialHeight);
+      });
+      describe('then focusing the textarea', function() {
+        beforeEach(async function() {
+          await sendKeys({ press: 'Tab' });
+          // NB: pfe-tooltip forces double tabindex
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: 'Tab' });
+        });
+        it('focuses a different element', function() {
+          expect(element.shadowRoot?.activeElement).to.not.equal(initialFocus);
+        });
+        describe('then typing', function() {
+          beforeEach(async function() {
+            await sendKeys({ type: VALUE });
+          });
+          it('updates the value property', function() {
+            expect(element.value).to.equal(VALUE);
+          });
+        });
+      });
+    });
   });
 });

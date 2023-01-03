2404ee6d2617292fb97663cd0a1e22508f96a4fd
Benny Powers <web@bennypowers.com>
2023-01-03 14:58:46 +0200

    feat(core): hook into formDisabledCallback in internalscontroller

--- core/pfe-core/controllers/internals-controller.ts
+++ core/pfe-core/controllers/internals-controller.ts
@@ -3,10 +3,25 @@ import type { ReactiveController, ReactiveControllerHost } from 'lit';
 export class InternalsController implements ReactiveController {
   #internals: ElementInternals;
 
+  disabled = false;
+
   constructor(
     public host: ReactiveControllerHost & HTMLElement,
   ) {
     this.#internals = host.attachInternals();
+    const { formDisabledCallback } = host;
+    Object.defineProperty(Object.getPrototypeOf(host), 'formDisabledCallback', {
+      configurable: true,
+      value: (disabled: boolean) => {
+        formDisabledCallback?.call(host, disabled);
+        this.formDisabledCallback(disabled);
+      }
+    });
+  }
+
+  formDisabledCallback(disabled: boolean) {
+    this.disabled = disabled;
+    this.host.requestUpdate();
   }
 
   hostConnected?(): void

82da44c1167fc5d94c2462caa4769b0e71fdafe1
Benny Powers <bennypowers@users.noreply.github.com>
2023-01-16 17:16:52 +0200

    feat(jump-links)!: reimplement in line with PFv4 (#2283)
    * feat(jump-links)!: initial commit 1:1
    
    * feat(core): add ARIAMixin props to InternalsController
    
    * fix: add some PFv4 styles
    
    * test: fix test typings
    
    * chore: fix ts build
    
    * fix: styles mostly complete
    
    * feat: scroll spy
    
    * fix: refactor, fix, improve
    
    * refactor: controllers, obvs
    
    * feat: added root node escape hatch
    
    * docs: add changesets

---
+++ core/pfe-core/controllers/scroll-spy-controller.ts
@@ -0,0 +1,170 @@
+import type { ReactiveController, ReactiveControllerHost } from 'lit';
+
+export interface ScrollSpyControllerOptions extends IntersectionObserverInit {
+  /**
+   * Tag names of legal link children.
+   * Legal children must have an `href` property/attribute pair, like `<a>`.
+   */
+  tagNames: string[];
+
+  /**
+   * Attribute to set on the active link element.
+   * @default 'active'
+   */
+  activeAttribute?: string;
+
+  /**
+   * The root node to query content for
+   * @default the host's root node
+   */
+  rootNode?: Node;
+  /**
+   * function to call on link children to get their URL hash (i.e. id to scroll to)
+   * @default el => el.getAttribute('href');
+   */
+  getHash?: (el: Element) => string|null;
+}
+
+export class ScrollSpyController implements ReactiveController {
+  #tagNames: string[];
+  #activeAttribute: string;
+
+  #io?: IntersectionObserver;
+
+  /** Which link's targets have already scrolled past? */
+  #passedLinks = new Set<Element>();
+
+  /** Ignore intersections? */
+  #force = false;
+
+  /** Has the intersection observer found an element? */
+  #intersected = false;
+
+  #root: ScrollSpyControllerOptions['root'];
+  #rootMargin?: string;
+  #threshold: number|number[];
+
+  #rootNode: Node;
+  #getHash: (el: Element) => string|null;
+
+  get #linkChildren(): Element[] {
+    return Array.from(this.host.querySelectorAll(this.#tagNames.join(',')))
+      .filter(this.#getHash);
+  }
+
+  get root() {
+    return this.#root;
+  }
+
+  set root(v) {
+    this.#root = v;
+    this.#io?.disconnect();
+    this.#initIo();
+  }
+
+  get rootMargin() {
+    return this.#rootMargin;
+  }
+
+  set rootMargin(v) {
+    this.#rootMargin = v;
+    this.#io?.disconnect();
+    this.#initIo();
+  }
+
+  get threshold() {
+    return this.#threshold;
+  }
+
+  set threshold(v) {
+    this.#threshold = v;
+    this.#io?.disconnect();
+    this.#initIo();
+  }
+
+  constructor(
+    private host: ReactiveControllerHost & HTMLElement,
+    options: ScrollSpyControllerOptions,
+  ) {
+    host.addController(this);
+    this.#tagNames = options.tagNames;
+    this.#root = options.root;
+    this.#rootMargin = options.rootMargin;
+    this.#activeAttribute = options.activeAttribute ?? 'active';
+    this.#threshold = options.threshold ?? 0.85;
+    this.#rootNode = options.rootNode ?? host.getRootNode();
+    this.#getHash = options?.getHash ?? ((el: Element) => el.getAttribute('href'));
+  }
+
+  hostConnected() {
+    this.#initIo();
+  }
+
+  #initIo() {
+    const rootNode = this.#rootNode;
+    if (rootNode instanceof Document || rootNode instanceof ShadowRoot) {
+      const { rootMargin, threshold, root } = this;
+      this.#io = new IntersectionObserver(r => this.#onIo(r), { root, rootMargin, threshold });
+      this.#linkChildren
+        .map(x => this.#getHash(x))
+        .filter((x): x is string => !!x)
+        .map(x => rootNode.getElementById(x.replace('#', '')))
+        .filter((x): x is HTMLElement => !!x)
+        .forEach(target => this.#io?.observe(target));
+    }
+  }
+
+  #markPassed(link: Element, force: boolean) {
+    if (force) {
+      this.#passedLinks.add(link);
+    } else {
+      this.#passedLinks.delete(link);
+    }
+  }
+
+  #setActive(link?: EventTarget|null) {
+    for (const child of this.#linkChildren) {
+      child.toggleAttribute(this.#activeAttribute, child === link);
+    }
+  }
+
+  async #nextIntersection() {
+    this.#intersected = false;
+    // safeguard the loop
+    setTimeout(() => this.#intersected = false, 3000);
+    while (!this.#intersected) {
+      await new Promise(requestAnimationFrame);
+    }
+  }
+
+  async #onIo(entries: IntersectionObserverEntry[]) {
+    if (!this.#force) {
+      for (const { target, boundingClientRect, intersectionRect } of entries) {
+        const selector = `:is(${this.#tagNames.join(',')})[href="#${target.id}"]`;
+        const link = this.host.querySelector(selector);
+        if (link) {
+          this.#markPassed(link, boundingClientRect.top < intersectionRect.top);
+        }
+      }
+      const link = [...this.#passedLinks];
+      const last = link.at(-1);
+      this.#setActive(last ?? this.#linkChildren.at(0));
+    }
+    this.#intersected = true;
+  }
+
+  /** Explicitly set the active item */
+  public async setActive(link: EventTarget|null) {
+    this.#force = true;
+    this.#setActive(link);
+    let sawActive = false;
+    for (const child of this.#linkChildren) {
+      this.#markPassed(child, !sawActive);
+      if (child === link) {
+        sawActive = true;
+      }
+    }
+    await this.#nextIntersection();
+    this.#force = false;
+  }
+}
--- elements/pfe-jump-links/pfe-jump-links.ts
+++ elements/pfe-jump-links/pfe-jump-links.ts
@@ -1,17 +1,142 @@
-import { PfeJumpLinksNav } from './pfe-jump-links-nav.js';
-export { PfeJumpLinksPanel } from './pfe-jump-links-panel.js';
-export { PfeJumpLinksNav };
+import { html, LitElement } from 'lit';
+import { customElement, property, queryAssignedElements } from 'lit/decorators.js';
 
-import { pfelement } from '@patternfly/pfe-core/decorators.js';
-import { customElement } from 'lit/decorators.js';
+import { ScrollSpyController } from '@patternfly/pfe-core/controllers/scroll-spy-controller.js';
+import { RovingTabindexController } from '@patternfly/pfe-core/controllers/roving-tabindex-controller.js';
+
+import './pfe-jump-links-item.js';
+import '@patternfly/pfe-icon';
+
+import style from './pfe-jump-links.scss';
+import { PfeJumpLinksItem } from './pfe-jump-links-item.js';
 
 /**
- * Jump links act as persistent navigation that consists of a list of anchor links.
- * Selecting a link moves a user to content that corresponds with the link selected.
- * A link is displayed as active when the content it links to is visible in the browser window.
+ * **Jump links** allow users to navigate to sections within a page.
+ * @fires toggle - when the `expanded` disclosure widget is toggled
+ * @slot - Place pfe-jump-links-items here
  *
- * @summary Moves a user to content when a link is selected
- * @cssprop --pfe-jump-links-panel--offset You can control offset in your styling layer as well. This value can be set directly on the component inside a style attribute, e.g. style="--pfe-jump-links-panel--offset: 100;" or using the appropriate selector in another file. Please note that adding an attribute will take precedence over a css value. At the moment only integer values passed to this key are valid. No other values are supported. This means that passing "300px", "2rem","calc(100% - 12px)" will all result in JavaScript errors. You should pass a number that correlates to pixels. To read about the offset attribute, see above.
+ * @cssprop --pf-c-jump-links__list--Display
+ * @cssprop --pf-c-jump-links__list--FlexDirection
+ * @cssprop --pf-c-jump-links__list--PaddingTop -- padding around the list of links
+ * @cssprop --pf-c-jump-links__list--PaddingRight
+ * @cssprop --pf-c-jump-links__list--PaddingBottom
+ * @cssprop --pf-c-jump-links__list--PaddingLeft
+ * @cssprop --pf-c-jump-links__list--Visibility
+ * @cssprop --pf-c-jump-links__list--before--BorderColor
+ * @cssprop --pf-c-jump-links__list--before--BorderTopWidth
+ * @cssprop --pf-c-jump-links__list--before--BorderRightWidth
+ * @cssprop --pf-c-jump-links__list--before--BorderBottomWidth
+ * @cssprop --pf-c-jump-links__list--before--BorderLeftWidth
+ * @cssprop --pf-c-jump-links__toggle--MarginBottom--base
+ *
+ * @cssprop --pf-c-jump-links__toggle--MarginTop -- padding around the expandable jump links disclosure widget.
+ * @cssprop --pf-c-jump-links__toggle--MarginBottom
+ * @cssprop --pf-c-jump-links__toggle--MarginBottom--base
+ * @cssprop --pf-c-jump-links__toggle--MarginLeft
+ * @cssprop --pf-c-jump-links__toggle-text--Color
+ * @cssprop --pf-c-button--PaddingTop -- padding around the expandable jump links disclosure widget.
+ * @cssprop --pf-c-button--PaddingRight
+ * @cssprop --pf-c-button--PaddingBottom
+ * @cssprop --pf-c-button--PaddingLeft
+ *
+ * @cssprop --pf-c-jump-links__toggle-icon--Rotate
+ * @cssprop --pf-c-jump-links__toggle-icon--Transition
+ * @cssprop --pf-c-jump-links__toggle-text--MarginLeft
+ *
+ * @cssprop --pf-c-jump-links--m-expanded__toggle--MarginBottom
+ * @cssprop --pf-c-jump-links--m-expanded__toggle-icon--Rotate
+ * @cssprop --pf-c-jump-links--m-expanded__toggle-icon--Color
+ *
+ * @cssprop --pf-c-jump-links--m-vertical__list--PaddingTop
+ * @cssprop --pf-c-jump-links--m-vertical__list--PaddingRight
+ * @cssprop --pf-c-jump-links--m-vertical__list--PaddingBottom
+ * @cssprop --pf-c-jump-links--m-vertical__list--PaddingLeft
+ * @cssprop --pf-c-jump-links--m-vertical__list--before--BorderTopWidth
+ * @cssprop --pf-c-jump-links--m-vertical__list--before--BorderLeftWidth
+ * @cssprop --pf-c-jump-links--m-vertical__item--m-current__link--before--BorderTopWidth
+ * @cssprop --pf-c-jump-links--m-vertical__item--m-current__link--before--BorderLeftWidth
+ * @cssprop --pf-c-jump-links--m-vertical__list--FlexDirection
  */
-@customElement('pfe-jump-links') @pfelement()
-export class PfeJumpLinks extends PfeJumpLinksNav { }
+@customElement('pfe-jump-links')
+export class PfeJumpLinks extends LitElement {
+  static readonly styles = [style];
+
+  @queryAssignedElements() _items!:PfeJumpLinksItem[];
+
+  @property({ reflect: true, type: Boolean }) expandable = false;
+
+  @property({ reflect: true, type: Boolean }) expanded = false;
+
+  @property({ reflect: true, type: Boolean }) vertical = false;
+
+  @property({ reflect: true, type: Boolean }) centered = false;
+
+  @property({ type: Number }) offset = 0;
+
+  @property() label?: string;
+
+  #items!:PfeJumpLinksItem[];
+  #init = false;
+  #rovingTabindexController = new RovingTabindexController(this);
+
+  #spy = new ScrollSpyController(this, {
+    rootMargin: `${this.offset}px 0px 0px 0px`,
+    tagNames: ['pfe-jump-links-item'],
+  });
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener('select', this.#onSelect);
+  }
+
+  override updated(changed: Map<string, unknown>) {
+    if (changed.has('offset')) {
+      this.#spy.rootMargin = `${this.offset ?? 0}px 0px 0px 0px`;
+    }
+  }
+
+  render() {
+    return html`
+      <nav id="container">${this.expandable ? html`
+        <details ?open="${this.expanded}" @toggle="${this.#onToggle}">
+          <summary>
+            <pfe-icon icon="chevron-right"></pfe-icon>
+            <span id="label">${this.label}</span>
+          </summary>
+          <slot role="listbox" @slotchange="${this.#onSlotchange}"></slot>
+        </details>` : html`
+        <span id="label">${this.label}</span>
+        <slot role="listbox" @slotchange="${this.#onSlotchange}"></slot>`}
+      </nav>
+    `;
+  }
+
+  #onSlotchange() {
+    this.#items = this._items;
+    const items = this.#items?.map(item=>item.link);
+    if (this.#init) {
+      this.#rovingTabindexController.updateItems(items);
+    } else {
+      this.#rovingTabindexController.initItems(items);
+      this.#init = true;
+    }
+  }
+
+  async #onSelect(event: Event) {
+    this.#spy.setActive(event.target);
+  }
+
+  #onToggle(event: Event) {
+    if (event.target instanceof HTMLDetailsElement) {
+      this.expanded = event.target.open;
+    }
+    this.dispatchEvent(new Event('toggle'));
+  }
+}
+
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'pfe-jump-links': PfeJumpLinks;
+  }
+}

28a7dae168ff294a24ae4871adc9a0aafef90ca0
Benny Powers <web@bennypowers.com>
2023-01-19 08:27:22 +0200

    fix(tools): lit css specifier when inlining

--- tools/pfe-tools/typescript/transformers/css-imports.cjs
+++ tools/pfe-tools/typescript/transformers/css-imports.cjs
@@ -1,57 +1,87 @@
 // @ts-check
 const ts = require('typescript');
 const fs = require('node:fs');
-const path = require('node:path');
 const { pathToFileURL } = require('node:url');
 
 /**
- * Replace .css import specifiers with .css.js import specifiers
-* @param {import('typescript').Program} program
-* @return {import('typescript').TransformerFactory<import('typescript').Node>}
+ * @param {import('typescript').CoreTransformationContext} ctx
+ * @param {import('typescript').SourceFile} sourceFile
  */
-module.exports = function(program, { inline = false } = {}) {
+function createLitCssImportStatement(ctx, sourceFile) {
+  for (const statement of sourceFile.statements) {
+    if (
+      ts.isImportDeclaration(statement) &&
+      statement.moduleSpecifier.getText() === 'lit') {
+      for (const binding of statement.importClause?.namedBindings?.getChildren() ?? []) {
+        if (binding.getText() === 'css') {
+          console.log(statement)
+          return;
+        }
+      }
+    }
+  }
+  return ctx.factory.createImportDeclaration(
+    undefined,
+    ctx.factory.createImportClause(
+      false,
+      undefined,
+      ctx.factory.createNamedImports([
+        ctx.factory.createImportSpecifier(
+          false,
+          undefined,
+          ctx.factory.createIdentifier('css')
+        )
+      ]),
+    ),
+    ctx.factory.createStringLiteral('lit'),
+  );
+}
+
+/**
+ * @param {import('typescript').CoreTransformationContext} ctx
+ * @param {string} content
+ * @param {string} [name]
+ */
+function createLitCssTaggedTemplateLiteral(ctx, content, name) {
+  return ctx.factory.createVariableStatement(
+    undefined,
+    ctx.factory.createVariableDeclarationList([
+      ctx.factory.createVariableDeclaration(
+        name ?? 'style',
+        undefined,
+        undefined,
+        ctx.factory.createTaggedTemplateExpression(
+          ctx.factory.createIdentifier('css'),
+          undefined,
+          ctx.factory.createNoSubstitutionTemplateLiteral(content),
+        )
+      )
+    ], ts.NodeFlags.Const)
+  );
+}
+
+/**
+ * Replace .css import specifiers with .css.js import specifiers
+ * @param {import('typescript').Program} _program
+ * @return {import('typescript').TransformerFactory<import('typescript').Node>}
+ */
+module.exports = function(_program, { inline = false } = {}) {
   return ctx => {
+    /**
+     * @param {import('typescript').Node} node
+     */
     function visitor(node) {
       if (ts.isImportDeclaration(node) && !node.importClause?.isTypeOnly) {
         const specifier = node.moduleSpecifier.getText().replace(/^'(.*)'$/, '$1');
         if (specifier.endsWith('.css')) {
           if (inline) {
-            const cw = program.getCurrentDirectory();
             const { fileName } = node.getSourceFile();
             const dir = pathToFileURL(fileName);
             const url = new URL(specifier, dir);
             const content = fs.readFileSync(url, 'utf-8');
             return [
-              ctx.factory.createImportDeclaration(
-                undefined,
-                ctx.factory.createImportClause(
-                  false,
-                  undefined,
-                  ctx.factory.createNamedImports([
-                    ctx.factory.createImportSpecifier(
-                      false,
-                      undefined,
-                      ctx.factory.createIdentifier('css')
-                    )
-                  ]),
-                ),
-                ctx.factory.createIdentifier('lit'),
-              ),
-              ctx.factory.createVariableStatement(
-                undefined,
-                ctx.factory.createVariableDeclarationList([
-                  ctx.factory.createVariableDeclaration(
-                    node.importClause?.name ?? 'style',
-                    undefined,
-                    undefined,
-                    ctx.factory.createTaggedTemplateExpression(
-                      ctx.factory.createIdentifier('css'),
-                      undefined,
-                      ctx.factory.createNoSubstitutionTemplateLiteral(content),
-                    )
-                  )
-                ], ts.NodeFlags.Const)
-              ),
+              createLitCssImportStatement(ctx, node.getSourceFile()),
+              createLitCssTaggedTemplateLiteral(ctx, content, node.importClause?.name?.getText()),
             ];
           } else {
             return ctx.factory.createImportDeclaration(

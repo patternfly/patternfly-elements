075ad25ca33e54d36f951262eee33e3f97b907a7
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-01-27 15:13:28 -0500

    fix(core): made roving-tabindex-controller's updateActiveItem public

---
+++ .changeset/tasty-ties-try.md
@@ -0,0 +1,6 @@
+---
+"@patternfly/pfe-core": patch
+---
+
+
+Fixed `roving-tabindex-controller.js` so active item can be set without setting focus
--- core/pfe-core/controllers/roving-tabindex-controller.ts
+++ core/pfe-core/controllers/roving-tabindex-controller.ts
@@ -158,7 +158,7 @@ export class RovingTabindexController implements ReactiveController {
   /**
    * sets tabindex of item based on whether or not it is active
    */
-  #updateActiveItem(item?: HTMLElement):void {
+  updateActiveItem(item?: HTMLElement):void {
     if (item) {
       if (!!this.#activeItem && item !== this.#activeItem) {
         this.#activeItem.tabIndex = -1;
@@ -172,7 +172,7 @@ export class RovingTabindexController implements ReactiveController {
    * focuses on an item and sets it as active
    */
   focusOnItem(item?: HTMLElement):void {
-    this.#updateActiveItem(item || this.firstItem);
+    this.updateActiveItem(item || this.firstItem);
     this.#activeItem?.focus();
   }
 

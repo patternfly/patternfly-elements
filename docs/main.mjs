f6295e5ae02ab1c180997e677bfde183efc35764
Benny Powers <web@bennypowers.com>
2023-01-30 10:45:33 +0200

    docs: icon import path

--- docs/main.mjs
+++ docs/main.mjs
@@ -4,4 +4,4 @@ import 'element-internals-polyfill';
 
 // Workaround for bundled pf-icon: make icon imports absolute, instead of relative to the bundle
 PfIcon.getIconUrl = (set, icon) =>
-  new URL(`/components/pf-icon/icons/${set}/${icon}.js`, import.meta.url);
+  new URL(`/components/icon/icons/${set}/${icon}.js`, import.meta.url);

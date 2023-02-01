2944d143b1017acafd86cb97b0cd3331e52952b2
Benny Powers <web@bennypowers.com>
2023-02-01 06:52:50 +0200

    fix(accordion)!: remove history atr

--- elements/pf-accordion/pf-accordion.ts
+++ elements/pf-accordion/pf-accordion.ts
@@ -32,17 +32,6 @@ import style from './pf-accordion.css';
 export class PfAccordion extends BaseAccordion {
   static readonly styles = [...BaseAccordion.styles, style];
 
-  protected static instances = new Set<PfAccordion>();
-
-  /**
-   * Initializes the mini-SPA which modifies the location with accordion state
-   */
-  static initRouter() {
-    window.addEventListener('popstate', () =>
-      PfAccordion.instances.forEach(el =>
-        el.#updateStateFromURL()));
-  }
-
   /** When true, only one accordion panel may be expanded at a time */
   @property({ reflect: true, type: Boolean }) single = false;
 
@@ -55,55 +44,7 @@ export class PfAccordion extends BaseAccordion {
   })
   @property({ type: Boolean, reflect: true }) large = false;
 
-  /**
-   * Updates `window.history` and the URL to create sharable links.
-   * With the `history` attribute, the accordion *must* have an `id`.
-   *
-   * The URL pattern will be `?{id-of-tabs}={index-of-expanded-items}`.
-   * In the example below, selecting "Accordion 2" will update the URL as follows:
-   * `?lorem-ipsum=2`. The index value for the expanded items starts at 1.
-   *
-   * ```html
-   * <pf-accordion history id="lorem-ipsum">
-   *   <pf-accordion-header>
-   *     <h3>Accordion 1</h3>
-   *   </pf-accordion-header>
-   *   <pf-accordion-panel>
-   *     <p>Accordion 1 panel content.</p>
-   *   </pf-accordion-panel>
-   *   <pf-accordion-header>
-   *     <h3>Accordion 2</h3>
-   *   </pf-accordion-header>
-   *   <pf-accordion-panel>
-   *     <p>Accordion 2 panel content.</p>
-   *   </pf-accordion-panel>
-   * </pf-accordion>
-   * ```
-   *
-   * To expand multiple sets, you can dash separate indexes: ?lorem-ipsum=1-2.
-   */
-  @property({ type: Boolean }) history = false;
-
   @property({ type: Boolean, reflect: true }) fixed = false;
-
-  #updateHistory = true;
-
-  #logger = new Logger(this);
-
-  #mo = new MutationObserver(() => this.#updateStateFromURL());
-
-  override connectedCallback(): void {
-    super.connectedCallback();
-    PfAccordion.instances.add(this);
-    this.addEventListener('change', this.#updateURLHistory);
-    this.#mo.observe(this, { childList: true });
-    this.#updateStateFromURL();
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    PfAccordion.instances.delete(this);
-  }
 
   override async expand(index: number, parentAccordion?: BaseAccordion) {
     if (index === -1) {
@@ -120,68 +61,6 @@ export class PfAccordion extends BaseAccordion {
     }
     await super.expand(index, parentAccordion);
   }
-
-  #getIndexesFromURL() {
-    // Capture the URL parameters
-    const urlParams = new URLSearchParams(window.location.search);
-
-    // If parameters exist and they contain the ID for this accordion
-    if (urlParams.has(this.id)) {
-      const params = urlParams.get(this.id);
-      // Split the parameters by underscore to see if more than 1 item is expanded
-      const indexes = (params ?? '').split('-');
-      if (indexes.length < 0) {
-        return [];
-      }
-
-      // Clean up the results by converting to array count
-      return indexes.map(item => parseInt(item.trim(), 10) - 1);
-    }
-  }
-
-  /**
-   * This handles updating the URL parameters based on the current state
-   * of the global this.expanded array
-   * @requires this.expandedSets {Array}
-   */
-  #updateURLHistory() {
-    if (!this.history || !this.#updateHistory) {
-      return;
-    }
-
-    if (!this.id) {
-      this.#logger.error(`The history feature cannot update the URL without an ID added to the accordion tag.`);
-      return;
-    }
-
-    // Capture the URL and rebuild it using the new state
-    const url = new URL(window.location.href);
-
-    // If values exist in the array, add them to the parameter string
-    // Otherwise delete the set entirely
-    if (this.expandedSets.size > 0) {
-      // Iterate the expanded array by 1 to convert to human-readable vs. array notation;
-      // sort values numerically and connect them using a dash
-      url.searchParams.set(this.id, Array.from(this.expandedSets)
-        .sort((a, b) => a - b)
-        .join('-'));
-    } else {
-      url.searchParams.delete(this.id);
-    }
-
-    // Note: Using replace state protects the user's back navigation
-    history.replaceState({}, '', url.toString());
-  }
-
-  /**
-   * This captures the URL parameters and expands each item in the array
-   */
-  #updateStateFromURL() {
-    const indexesFromURL = this.#getIndexesFromURL() ?? [];
-    this.#updateHistory = false;
-    indexesFromURL.forEach(idx => this.expand(idx));
-    this.#updateHistory = true;
-  }
 }
 
 declare global {
@@ -189,5 +68,3 @@ declare global {
     'pf-accordion': PfAccordion;
   }
 }
-
-PfAccordion.initRouter();

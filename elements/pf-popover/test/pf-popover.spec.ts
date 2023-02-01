b0471baea387f9ec8aa6abf357d9554066496b3f
Ivana Rodriguez <ivrodrig@redhat.com>
2023-02-01 15:26:22 -0500

    test: add basic unit test coverage

--- elements/pf-popover/test/pf-popover.spec.ts
+++ elements/pf-popover/test/pf-popover.spec.ts
@@ -1,18 +1,86 @@
-import { expect, html } from '@open-wc/testing';
-import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { expect, html, fixture } from '@open-wc/testing';
+import type { A11yTreeSnapshot } from '@patternfly/pfe-tools/test/a11y-snapshot.js';
+import { a11ySnapshot } from '@patternfly/pfe-tools/test/a11y-snapshot.js';
+import { sendKeys } from '@web/test-runner-commands';
 import { PfPopover } from '@patternfly/elements/pf-popover/pf-popover.js';
-
-const element = html`
-  <pf-popover></pf-popover>
-`;
+import { PfButton } from '@patternfly/elements/pf-button/pf-button.js';
 
 describe('<pf-popover>', function() {
+  let element: PfPopover;
+  let snapshot: A11yTreeSnapshot;
+
+  beforeEach(async function() {
+    element = await fixture<PfPopover>(html`
+      <pf-popover
+        heading="Popover heading"
+        body="Popovers are triggered by click rather than hover."
+        footer="Popover footer"
+      >
+        <pf-button>Toggle popover</pf-button>
+      </pf-popover>
+    `);
+    snapshot = await a11ySnapshot();
+  });
+
   it('should upgrade', async function() {
-    const el = await createFixture <PfPopover>(element);
     const klass = customElements.get('pf-popover');
-    expect(el)
-      .to.be.an.instanceOf(klass)
-      .and
-      .to.be.an.instanceOf(PfPopover);
+    expect(element).to.be.an.instanceOf(klass).and.to.be.an.instanceOf(PfPopover);
+  });
+
+  it('should be accessible', async function() {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+
+  it('should hide popover content from assistive technology', async function() {
+    expect(snapshot.children).to.deep.equal([{ name: 'Toggle popover', role: 'button' }]);
+  });
+
+  it('should show popover content from assistive technology', async function() {
+    await sendKeys({ press: 'Tab' });
+    expect(document.activeElement).to.be.an.instanceof(PfButton);
+    await sendKeys({ press: 'Enter' });
+    snapshot = await a11ySnapshot();
+    expect(snapshot.children).to.deep.equal([
+      {
+        focused: true,
+        name: 'Toggle popover',
+        role: 'button',
+      },
+      {
+        name: 'Close popover',
+        role: 'button',
+      },
+      {
+        level: 6,
+        name: 'Popover heading',
+        role: 'heading',
+      },
+      {
+        name: 'Popovers are triggered by click rather than hover.',
+        role: 'text',
+      },
+      {
+        name: 'Popover footer',
+        role: 'text',
+      },
+    ]);
+  });
+
+  it('should be closeable on close button select', async function() {
+    await sendKeys({ press: 'Tab' });
+    await sendKeys({ press: 'Enter' });
+    // tab to close button
+    await sendKeys({ press: 'Tab' });
+    await sendKeys({ press: 'Enter' });
+    snapshot = await a11ySnapshot();
+    expect(snapshot.children).to.deep.equal([{ name: 'Toggle popover', role: 'button' }]);
+  });
+
+  it('should be closeable on escape', async function() {
+    await sendKeys({ press: 'Tab' });
+    await sendKeys({ press: 'Enter' });
+    await sendKeys({ press: 'Escape' });
+    snapshot = await a11ySnapshot();
+    expect(snapshot.children).to.deep.equal([{ focused: true, name: 'Toggle popover', role: 'button' }]);
   });
 });

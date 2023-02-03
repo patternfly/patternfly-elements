d906f64049d96769b8eacf3511b72bbf8e5c630e
Steven Spriggs <steven.spriggs@gmail.com>
2023-02-03 14:33:10 -0500

    test(tabs): remove check for role on shadowdowm button

--- elements/pf-tabs/test/pf-tabs.spec.ts
+++ elements/pf-tabs/test/pf-tabs.spec.ts
@@ -56,8 +56,6 @@ describe('<pf-tabs>', function() {
     tabs.forEach(function(tab: Element, index: number) {
       const tabId = tab.getAttribute('id');
       const tabControls = tab.getAttribute('aria-controls');
-      const tabButton = tab.shadowRoot?.querySelector('button');
-      expect(tabButton?.getAttribute('role')).to.equal('tab');
       tabPanels.forEach(function(panel: Element, pindex: number) {
         if (index === pindex) {
           expect(panel.getAttribute('aria-labelledby')).to.equal(tabId);

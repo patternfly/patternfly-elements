09785286013d9e88e3ca4f5ab1137bbfe645172e
Ivana Rodriguez <ivrodrig@redhat.com>
2023-03-10 10:41:18 -0500

    chore: clean up

--- elements/pf-popover/pf-popover.ts
+++ elements/pf-popover/pf-popover.ts
@@ -322,7 +322,7 @@ export class PfPopover extends LitElement {
     const hasHeading = this.#slots.hasSlotted('heading') || !!this.heading;
     const headingLevel = headingLevels.find(level => level === this.headingLevel) ?? 6;
     const heading = html`<slot id="heading" name="heading" part="heading" ?hidden=${!hasHeading}
-      >${unsafeStatic(`<h${headingLevel}>${this.heading}</h${headingLevel}>`)}</slot
+      >${unsafeStatic(`<h${headingLevel}>${this.heading ?? ''}</h${headingLevel}>`)}</slot
     >`;
     const asHeader = hasHeading && hasIcon;
     const header = asHeader ?
@@ -355,14 +355,13 @@ export class PfPopover extends LitElement {
             </footer>
           </div>
           <pf-button
-            id="close-button"
-            label=${this.closeButtonLabel ?? 'Close popover'}
-            part="close-button"
-            plain
-            @click=${this.hide}
-            @keydown=${this._onKeydown}
-            ?hidden=${this.hideClose}
-          >
+              id="close-button"
+              label=${this.closeButtonLabel ?? 'Close popover'}
+              part="close-button"
+              plain
+              @click=${this.hide}
+              @keydown=${this._onKeydown}
+              ?hidden=${this.hideClose}>
             <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 352 512">
               <path
                 d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"

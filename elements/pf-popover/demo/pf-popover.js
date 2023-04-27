998bbc1632459295ff4b138cf5ed467d86fe0153
Benny Powers <web@bennypowers.com>
2023-04-27 15:11:13 +0300

    docs: flex layout, markup

--- elements/pf-popover/demo/pf-popover.js
+++ elements/pf-popover/demo/pf-popover.js
@@ -3,20 +3,24 @@ import '@patternfly/elements/pf-button/pf-button.js';
 import '@patternfly/elements/pf-icon/pf-icon.js';
 
 // Positions
-const section = document.querySelector('#basic');
-const select = document.querySelector('#position-select');
-const element = section.querySelector('pf-popover');
-select.addEventListener('change', event => element.setAttribute('position', event.target.value));
+const select = document.getElementById('position-select');
+select.addEventListener('change', event =>
+  event.target
+    .closest('fieldset')
+    .querySelector('pf-popover')
+    .setAttribute('position', event.target.value));
 
 // Close popover from content
-const section1 = document.querySelector('#close-from-content');
-const element1 = section1.querySelector('pf-popover');
-const closeButton = element1.querySelector('#close-button');
-closeButton.addEventListener('click', () => element1.hide());
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', event => event.target.closest('pf-popover').hide());
 
 // Alert variants
-const section2 = document.querySelector('#alert');
-const select2 = document.querySelector('#alert-select');
-const element2 = section2.querySelector('pf-popover');
-select2.addEventListener('change', event => element2.setAttribute('alert-severity', event.target.value));
+const alert = document.getElementById('alert');
+alert.addEventListener('change', event =>
+  alert
+    .querySelector('pf-popover')
+    .setAttribute('alert-severity', event.target.closest('form').elements.severity.value));
 
+document.addEventListener('submit', function(event) {
+  event.preventDefault();
+});

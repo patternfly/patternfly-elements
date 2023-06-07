b03d2b7496f6cfd99111fbae24cdde5303f3538f
Brian Ferry <bferry@redhat.com>
2023-06-07 15:08:04 -0400

    test: adding test cases for pf-progress

--- elements/pf-progress/test/pf-progress.spec.ts
+++ elements/pf-progress/test/pf-progress.spec.ts
@@ -1,17 +1,24 @@
-import { expect, html } from '@open-wc/testing';
-import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { expect, html, fixture } from '@open-wc/testing';
 import { PfProgress } from '@patternfly/elements/pf-progress/pf-progress.js';
 
 describe('<pf-progress>', function() {
-  describe('simply instantiating', function() {
-    let element: PfProgress;
-    it('should upgrade', async function() {
-      element = await createFixture<PfProgress>(html`<pf-progress></pf-progress>`);
-      const klass = customElements.get('pf-progress');
-      expect(element)
-        .to.be.an.instanceOf(klass)
-        .and
-        .to.be.an.instanceOf(PfProgress);
-    });
+  let element: PfProgress;
+
+  beforeEach(async function() {
+    element = await fixture<PfProgress>(html`
+      <pf-progress value="33"
+                  title="Progress title"
+                  label="Progress label">
+      </pf-progress>
+    `);
+  });
+
+  it('should upgrade', async function() {
+    const klass = customElements.get('pf-progress');
+    expect(element).to.be.an.instanceOf(klass).and.to.be.an.instanceOf(PfProgress);
+  });
+
+  it('should be accessible', async function() {
+    await expect(element).shadowDom.to.be.accessible();
   });
 });

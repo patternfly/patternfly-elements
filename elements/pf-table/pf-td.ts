235e84d2edc1755b8f3d273a320777244f77fe91
Ivana Rodriguez <ivrodrig@redhat.com>
2023-07-24 13:14:40 -0400

    feat: add expandable row button

--- elements/pf-table/pf-td.ts
+++ elements/pf-table/pf-td.ts
@@ -1,8 +1,19 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
+import { ComposedEvent } from '@patternfly/pfe-core';
+import '@patternfly/elements/pf-button/pf-button.js';
+import '@patternfly/elements/pf-icon/pf-icon.js';
 
 import styles from './pf-td.css';
+
+export class TdChangeEvent extends ComposedEvent {
+  constructor(
+    public expanded: boolean
+  ) {
+    super('change');
+  }
+}
 
 /**
  * Table data cell
@@ -14,10 +25,20 @@ export class PfTd extends LitElement {
 
   @property({ reflect: true }) role = 'cell';
 
+  @property({ type: Boolean, reflect: true }) expanded = false;
+
+  @property({ type: Boolean, reflect: true, attribute: 'expand-button' }) expandButton = false;
+
   render() {
-    return html`
-      <slot></slot>
-    `;
+    return !this.expandButton ?
+      html`<slot></slot>`
+      : html`<pf-button id="button" aria-expanded=${this.expanded} plain @click=${this.#onClick}>
+               <pf-icon id="icon" icon="angle-right" size="md"></pf-icon>
+             </pf-button>`;
+  }
+
+  #onClick() {
+    this.dispatchEvent(new TdChangeEvent(!this.expanded));
   }
 }
 

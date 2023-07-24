7a63196aa734d5393084fdcb907b83c9ecb2eb4d
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-07-24 15:14:16 -0400

    fix(listbox): updated demo js

--- elements/pf-listbox/demo/pf-listbox.js
+++ elements/pf-listbox/demo/pf-listbox.js
@@ -1,29 +1,27 @@
 import '@patternfly/elements/pf-listbox/pf-listbox.js';
 
-window.addEventListener('load', () => {
-  /**
-   * pf-listbox in a combox box
-   * @see {@link https://www.w3.org/WAI/ARIA/apg/patterns/combobox/examples/combobox-autocomplete-both/|WAI ARIA Examples: Combobox with ARIAutocomplete}
-   */
-  const input = document.querySelector('#text');
-  const listbox = document.querySelector('#listbox');
-  if (listbox && input) {
-    listbox.filter = input.value;
-    input.addEventListener('input', () => {
-      if (input.value !== listbox.value) {
-        listbox.filter = input.value;
-      }
-    });
-    listbox.addEventListener('change', () => {
-      const val = (listbox.value || '');
-      if (val.length > 0) {
-        input.value = val;
-        input.focus();
-        input.setSelectionRange(
-          listbox.filter.length,
-          val.length
-        );
-      }
-    });
-  }
-});
+/**
+ * pf-listbox in a combox box
+ * @see {@link https://www.w3.org/WAI/ARIA/apg/patterns/combobox/examples/combobox-autocomplete-both/|WAI ARIA Examples: Combobox with ARIAutocomplete}
+ */
+const input = document.querySelector('#text');
+const listbox = document.querySelector('#listbox');
+if (listbox && input) {
+  listbox.filter = input.value;
+  input.addEventListener('input', () => {
+    if (input.value !== listbox.value) {
+      listbox.filter = input.value;
+    }
+  });
+  listbox.addEventListener('change', () => {
+    const val = (listbox.value || '');
+    if (val.length > 0) {
+      input.value = val;
+      input.focus();
+      input.setSelectionRange(
+        listbox.filter.length,
+        val.length
+      );
+    }
+  });
+}

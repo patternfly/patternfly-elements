23e666bec69ac050bdaa5482c08d5983cefef7b5
Benny Powers <web@bennypowers.com>
2023-08-08 16:15:58 +0300

    feat(progress): truncated title

--- elements/pf-progress/pf-progress.ts
+++ elements/pf-progress/pf-progress.ts
@@ -103,32 +103,27 @@ export class PfProgress extends LitElement {
   #internals = this.attachInternals();
 
   /** Represents the value of the progress bar */
-  @property({ reflect: true, type: Number })
-    value = 0;
+  @property({ reflect: true, type: Number }) value = 0;
 
   /** Title above the progress bar */
-  @property()
-    title = '';
+  @property() description?: string;
+
+  @property({ type: Boolean, reflect: true, attribute: 'description-truncated' }) descriptionTruncated = false;
 
   /** Maximum value for the progress bar */
-  @property({ type: Number, reflect: true })
-    max = 100;
+  @property({ type: Number, reflect: true }) max = 100;
 
   /** Minimum value for the progress bar */
-  @property({ type: Number, reflect: true })
-    min = 0;
+  @property({ type: Number, reflect: true }) min = 0;
 
   /** Size of the progress bar (height) */
-  @property()
-    size: '' | 'sm' | 'lg' = '';
+  @property() size?: 'sm' | 'lg';
 
   /** Where the percentage will be displayed with the progress element */
-  @property({ attribute: 'measure-location' })
-    measureLocation: '' | 'outside' | 'inside' | 'none' = '';
+  @property({ attribute: 'measure-location' }) measureLocation?: 'outside' | 'inside' | 'none';
 
   /** Variant of the progress bar */
-  @property()
-    variant: '' | 'success' | 'danger' | 'warning' = '';
+  @property() variant?: 'success' | 'danger' | 'warning';
 
   get #calculatedPercentage(): number {
     const { value, min, max } = this;
@@ -139,52 +134,72 @@ export class PfProgress extends LitElement {
     return Math.min(percentage, 100);
   }
 
-  willUpdate(changed: PropertyValues<this>) {
+  get #icon() {
+    return ICONS.get(this.variant ?? '')?.icon;
+  }
+
+  override willUpdate(changed: PropertyValues<this>) {
     if (changed.has('value') || changed.has('min') || changed.has('max')) {
       this.#internals.ariaValueNow = this.#calculatedPercentage.toString();
+    }
+    if (this.#icon) {
+      import('@patternfly/elements/pf-icon/pf-icon.js');
+    }
+    if (this.descriptionTruncated) {
+      import('@patternfly/elements/pf-tooltip/pf-tooltip.js');
     }
   }
 
   render() {
-    const { size, measureLocation, variant, title } = this;
-    const icon = variant && ICONS.get(variant)?.icon;
-    const singleLine = title.length === 0;
+    const { size, measureLocation, variant, description, descriptionTruncated } = this;
+    const icon = this.#icon;
+    const singleLine = description?.length === 0;
+    const pct = this.#calculatedPercentage;
+    const width = `${pct}%`;
+
+    const descriptionTemplate = (description ?? '') && html`
+      <div id="description" aria-hidden="true">${description}</div>
+    `;
 
     return html`
-      <div
-        class="container ${classMap({ [size]: !!size, [measureLocation]: !!measureLocation, [variant]: !!variant, singleLine })}">
+      <div id="container" class="${classMap({
+          [size ?? '']: !!size,
+          [measureLocation ?? '']: !!measureLocation,
+          [variant ?? '']: !!variant,
+          singleLine,
+          descriptionTruncated,
+        })}">
 
-        ${title ?
-          html`
-          <div
-            id="title"
-            class="title"
-            aria-hidden="true">
-              ${title}
-          </div>` : ''}
+        <div id="description" aria-hidden="true">${description ?? ''}</div>
+        ${!descriptionTruncated ? '' : html`
+        <pf-tooltip content="${this.description ?? ''}"
+                    trigger="description"></pf-tooltip>
+        `}
 
-          ${measureLocation !== 'none' ? html`<div class="status" aria-hidden="true">
-            ${html`${measureLocation === 'inside' ? `${this.#calculatedPercentage}%` : ''}`}
-            <pf-icon set="fas"
-                  size="md"
-                  ?hidden="${!icon}"
-                  icon="${ifDefined(icon)}"></pf-icon>
-          </div>` : ''}
+        ${measureLocation === 'none' ? '' : html`
+        <div id="status" aria-hidden="true">
+          ${measureLocation !== 'inside' ? '' : width}
+          <pf-icon set="fas"
+                   icon="${ifDefined(icon)}"
+                   size="md"
+                   ?hidden="${!icon}"
+          ></pf-icon>
+        </div>
+        `}
 
-          <progress
-            max="100"
-            value="${this.#calculatedPercentage}"
-            aria-valuemin="0"
-            aria-valuenow="${this.#calculatedPercentage}"
-            aria-valuemax="100">
-          </progress>
+          <progress id="progress"
+                    max="100"
+                    value="${pct}"
+                    aria-valuemin="0"
+                    aria-valuenow="${pct}"
+                    aria-valuemax="100"
+          ></progress>
 
-          ${measureLocation === 'inside' ? html`
-            <span
-              class="progress-span"
-              style="${styleMap({ 'width': `${this.#calculatedPercentage}%` })}"
-              data-value="${this.#calculatedPercentage}%"></span>`
-            : ''}
+          ${measureLocation !== 'inside' ? '' : html`
+          <span id="progress-span"
+                style="${styleMap({ width })}"
+                data-value="${width}"></span>
+          `}
       </div>`;
   }
 }

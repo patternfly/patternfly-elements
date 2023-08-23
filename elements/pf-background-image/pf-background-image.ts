745ae4130d41674ca6fff316bc153b2fe29850f5
Steven Spriggs <steven.spriggs@gmail.com>
2023-08-23 15:11:06 -0400

    fix(background-image): styleMap and remove content wrapper

--- elements/pf-background-image/pf-background-image.ts
+++ elements/pf-background-image/pf-background-image.ts
@@ -1,7 +1,9 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { queryAssignedElements } from 'lit/decorators/query-assigned-elements.js';
 import { property } from 'lit/decorators/property.js';
+
+import { styleMap } from 'lit/directives/style-map.js';
 
 import styles from './pf-background-image.css';
 
@@ -62,29 +64,34 @@ export class PfBackgroundImage extends LitElement {
   #updated = false;
 
   render() {
+    const cssProps = {
+      '--_background-image': this.src ? `url(${this.src})` : ``,
+      '--_background-image-2x': this.src2x ? `url(${this.src2x})` : ``,
+      '--_background-image-sm': this.srcSm ? `url(${this.srcSm})` : ``,
+      '--_background-image-sm-2x': this.srcSm2x ? `url(${this.srcSm2x})` : ``,
+      '--_background-image-lg': this.srcLg ? `url(${this.srcLg})` : ``
+    };
+
     return html`
-      <div id="container" part="container" style="${this.#cssVars()}">
+      <div id="container" part="container" style="${styleMap(cssProps)}">
         ${!this.filter ? html`` : html`
           <slot name="filter" @slotchange=${this.#onSlotChange}>
-
-              ${(this.#svg && this.#updated) ? this.#svg : html`
-                <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0">
-                  <filter id="filter">
-                    <feColorMatrix type="matrix" values="1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 1 0"></feColorMatrix>
-                    <feComponentTransfer color-interpolation-filters="sRGB" result="duotone">
-                      <feFuncR type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncR>
-                      <feFuncG type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncG>
-                      <feFuncB type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncB>
-                      <feFuncA type="table" tableValues="0 1"></feFuncA>
-                    </feComponentTransfer>
-                  </filter>
-                </svg>
-              `}
+            ${(this.#svg && this.#updated) ? this.#svg : html`
+              <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0">
+                <filter id="filter">
+                  <feColorMatrix type="matrix" values="1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 1 0"></feColorMatrix>
+                  <feComponentTransfer color-interpolation-filters="sRGB" result="duotone">
+                    <feFuncR type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncR>
+                    <feFuncG type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncG>
+                    <feFuncB type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncB>
+                    <feFuncA type="table" tableValues="0 1"></feFuncA>
+                  </feComponentTransfer>
+                </filter>
+              </svg>
+            `}
           </slot>
         `}
-        <div class="content" part="content">
-          <slot></slot>
-        </div>
+        <slot part="content"></slot>
       </div>
     `;
   }
@@ -98,20 +105,6 @@ export class PfBackgroundImage extends LitElement {
     } else {
       this.#updated = false;
     }
-  }
-
-  #cssVars() {
-    const collection = new Map();
-    collection.set('--pf-c-background-image--BackgroundImage', this.src);
-    collection.set('--pf-c-background-image--BackgroundImage-2x', this.src2x);
-    collection.set('--pf-c-background-image--BackgroundImage--sm', this.srcSm);
-    collection.set('--pf-c-background-image--BackgroundImage--sm-2x', this.srcSm2x);
-    collection.set('--pf-c-background-image--BackgroundImage--lg', this.srcLg);
-    return Array.from(collection.entries()).map(([key, value]) => {
-      if (value) {
-        return `${key}: url(${value}); `;
-      }
-    }).join('');
   }
 }
 

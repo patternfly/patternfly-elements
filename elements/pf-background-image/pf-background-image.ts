0668059f5e4df3813d64c1aa78d40254876d46e7
Steven Spriggs <steven.spriggs@gmail.com>
2023-08-24 13:25:00 -0400

    fix(background-image): align cssprop value with PFv4 missed one

--- elements/pf-background-image/pf-background-image.ts
+++ elements/pf-background-image/pf-background-image.ts
@@ -77,7 +77,7 @@ export class PfBackgroundImage extends LitElement {
           <slot name="filter" @slotchange=${this.#onSlotChange}>
             ${(this.#svg && this.#updated) ? this.#svg : html`
               <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0">
-                <filter id="filter">
+                <filter id="image_overlay">
                   <feColorMatrix type="matrix" values="1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 1 0"></feColorMatrix>
                   <feComponentTransfer color-interpolation-filters="sRGB" result="duotone">
                     <feFuncR type="table" tableValues="0.086274509803922 0.43921568627451"></feFuncR>

f7cc5aa4335b460121135c3e4972fdece4b2f909
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-08-24 09:42:13 -0400

    feat(select): updated icon slot styles and select demos

--- elements/pf-select/demo/pf-select.js
+++ elements/pf-select/demo/pf-select.js
@@ -1,1 +1,3 @@
 import '@patternfly/elements/pf-select/pf-select.js';
+import { PfIcon } from '../../pf-icon/pf-icon'; '@patternfly/elements/pf-icon/pf-icon.js';
+PfIcon.addIconSet('pf', (set, icon) => new URL(`./icons/${set}/${icon}.js`, import.meta.url));

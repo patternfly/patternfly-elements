a8a9899482f575dc4e7f6f44254035840f04b85e
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-08-28 15:56:10 -0400

    feat(chip): added tests and removed button from hidden chips

--- elements/pf-chip/pf-chip.ts
+++ elements/pf-chip/pf-chip.ts
@@ -45,17 +45,17 @@ export class PfChip extends LitElement {
   render() {
     return html`
       <div id="outer">
-        ${this.overflowChip ? html`
-          <button id="overflow-button" class="chip-content" ?disabled="${this.overflowHidden}">
+        ${this.overflowChip && !this.readOnly && !this.overflowHidden ? html`
+          <button id="overflow-button" class="chip-content">
             <span id="chip-text" part="text"><slot></slot></span>
           </button>
         ` : html`
           <span class="chip-content">
             <span id="chip-text" part="text"><slot></slot></span>
           </span>
-          ${this.readOnly ? '' : html`
-            <button id="close-button" @click=${this.#onClick} ?disabled="${this.overflowHidden}">
-              <svg aria-label="${this.closeLabel}" fill="currentColor" viewBox="0 0 352 512">
+          ${this.readOnly || this.overflowHidden ? '' : html`
+            <button id="close-button" @click=${this.#onClick} aria-describedby="chip-text" aria-label="${this.closeLabel}">
+              <svg aria-hidden="true" fill="currentColor" viewBox="0 0 352 512">
                 <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
               </svg>
             </button>
--- elements/pf-chip/test/pf-chip.spec.ts
+++ elements/pf-chip/test/pf-chip.spec.ts
@@ -1,8 +1,28 @@
 import { expect, html } from '@open-wc/testing';
 import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
 import { PfChip } from '@patternfly/elements/pf-chip/pf-chip.js';
+import { sendKeys, sendMouse } from '@web/test-runner-commands';
 
-describe('<pf-chip>', function() {
+
+async function tab() {
+  await sendKeys({ press: 'Tab' });
+}
+
+async function enter() {
+  await sendKeys({ press: 'Enter' });
+}
+
+async function click(element: HTMLElement) {
+  const { x, y, width, height } = element.getBoundingClientRect();
+  const position = [x + width / 2, y + height / 2].map(Math.round) as [number, number];
+  await sendMouse({ type: 'click', position });
+}
+
+function activeElement(element: HTMLElement) {
+  return element.shadowRoot?.activeElement;
+}
+
+describe('<pf-chip>', async function() {
   describe('simply instantiating', function() {
     let element: PfChip;
     it('imperatively instantiates', function() {
@@ -16,6 +36,115 @@ describe('<pf-chip>', function() {
         .to.be.an.instanceOf(klass)
         .and
         .to.be.an.instanceOf(PfChip);
+    });
+  });
+
+  it('should be accessible', async function() {
+    const element = await createFixture<PfChip>(html`<pf-chip></pf-chip>`);
+    await expect(element).to.be.accessible();
+  });
+
+  describe('default behavior', function() {
+    let element: PfChip; let parent: HTMLElement; let button: HTMLButtonElement;
+
+    beforeEach(async function() {
+      element = await createFixture<PfChip>(html`<pf-chip></pf-chip>`);
+      button = await element.shadowRoot?.querySelector('button');
+    });
+
+    it('should focus programatically', async function() {
+      element.focus();
+      await expect(activeElement(element)?.getAttribute('aria-label')).to.equal(element.closeLabel);
+    });
+
+    it('should close using mouse', async function() {
+      await click(button);
+      expect(document.querySelector('pf-chip')).to.be.null;
+    });
+
+    describe('should work with a keyboard', function() {
+      it('focuses using `Tab` key', async function() {
+        await tab();
+        await expect(activeElement(element)?.getAttribute('aria-label')).to.equal(element.closeLabel);
+
+        describe('should close using `Enter` key', async function() {
+          it('closes', async function() {
+            await element.focus();
+            await enter();
+            expect(document.querySelector('pf-chip')).to.be.null;
+          });
+        });
+      });
+    });
+  });
+
+  describe('overflow-chip', function() {
+    let element: PfChip; let parent: HTMLElement; let button: HTMLButtonElement;
+
+    beforeEach(async function() {
+      element = await createFixture<PfChip>(html`<pf-chip overflow-chip>Overflow</pf-chip>`);
+      button = await element.shadowRoot?.querySelector('button');
+    });
+
+    it('should be accessible', async function() {
+      await expect(element).to.be.accessible();
+    });
+
+    it('should not have a close button', async function() {
+      await expect(button?.ariaLabel).to.not.equal(element.closeLabel);
+    });
+
+    it('should focus programatically', async function() {
+      element.focus();
+      await expect(activeElement(element)).to.exist;
+    });
+
+    it('should focus using tab key', async function() {
+      await tab();
+      await expect(activeElement(element)).to.exist;
+    });
+
+    it('should NOT close using `Enter` key', async function() {
+      element.focus();
+      await enter();
+      expect(document.querySelector('pf-chip')).to.not.be.null;
+    });
+
+    it('should NOT close using mouse', async function() {
+      await click(element);
+      expect(document.querySelector('pf-chip')).to.not.be.null;
+    });
+  });
+
+  describe('read-only', async function() {
+    let element: PfChip; let parent: HTMLElement; let button: HTMLButtonElement;
+
+    beforeEach(async function() {
+      element = await createFixture<PfChip>(html`<pf-chip read-only></pf-chip>`);
+      button = await element.shadowRoot?.querySelector('button');
+    });
+
+    it('should be accessible', async function() {
+      await expect(element).to.be.accessible();
+    });
+    it('should not have a button', async function() {
+      await expect(button).to.be.null;
+    });
+  });
+
+  describe('overflow-hidden', async function() {
+    let element: PfChip; let parent: HTMLElement; let button: HTMLButtonElement;
+
+    beforeEach(async function() {
+      element = await createFixture<PfChip>(html`<pf-chip overflow-hidden></pf-chip>`);
+      button = await element.shadowRoot?.querySelector('button');
+    });
+
+    it('should be accessible', async function() {
+      await expect(element).to.be.accessible();
+    });
+    it('should not have a button', async function() {
+      await expect(button).to.be.null;
     });
   });
 });

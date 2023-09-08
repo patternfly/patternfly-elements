90abad6b1a98f691fe4db518eb189bf936bec31d
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-09-08 13:06:06 -0400

    feat(select): increased target size for a11y

--- elements/pf-select/pf-select-option.ts
+++ elements/pf-select/pf-select-option.ts
@@ -99,7 +99,7 @@ export class PfSelectOption extends LitElement {
 
   render() {
     return html`
-      <div>
+      <div id="outer">
         <input 
           type="checkbox" 
           aria-hidden="true" 
@@ -114,8 +114,8 @@ export class PfSelectOption extends LitElement {
           aria-hidden="true">
           <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
         </svg>
+        <div id="description"><slot name="description"></slot></div>
       </div>
-      <slot name="description"></slot>
     `;
   }
 

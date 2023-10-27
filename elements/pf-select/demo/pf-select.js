c8267f5d7eb140c0a7fcac1cb024859eef122e1b
Nikki Massaro Kauffman <nmassaro@redhat.com>
2023-10-27 16:25:55 -0400

    docs(select): split demos and inline scripts

--- elements/pf-select/demo/pf-select.js
+++
@@ -1,3 +0,0 @@
-import '@patternfly/elements/pf-select/pf-select.js';
-import { PfIcon } from '@patternfly/elements/pf-icon/pf-icon.js';
-PfIcon.addIconSet('pf', (set, icon) => new URL(`./icons/${set}/${icon}.js`, import.meta.url));

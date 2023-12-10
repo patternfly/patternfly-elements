1027109a3910bcbe12becfe83bfb06bb3378dc61
Benny Powers <web@bennypowers.com>
2023-12-10 11:18:15 +0200

    fix: popover render

--- elements/pf-popover/pf-popover.ts
+++ elements/pf-popover/pf-popover.ts
@@ -340,19 +340,7 @@ export class PfPopover extends LitElement {
       <slot id="heading" name="heading" part="heading" ?hidden=${!hasHeading}>${headingContent}</slot>
     `;
 
-    const header = !(hasHeading && hasIcon) ? headingSlotWithFallback : html`
-      <header part="header">
-        <span part="icon">
-          <slot name="icon">
-            <pf-icon icon="${this.icon ?? PfPopover.alertIcons.get(this.alertSeverity as AlertSeverity) ?? ''}"
-                     set="${ifDefined(this.iconSet)}"
-                     size="md"></pf-icon>
-          </slot>
-        </span>${!this.alertSeverity ? nothing : html`
-        <span class="visually-hidden">${this.alertSeverityText ?? `${this.alertSeverity} alert:`}</span>`}
-        ${headingSlotWithFallback}
-      </header>
-    `;
+    const headerIcon = this.icon ?? PfPopover.alertIcons.get(this.alertSeverity as AlertSeverity) ?? '';
 
     return html`
       <div id="container"
@@ -360,9 +348,9 @@ export class PfPopover extends LitElement {
            class="${classMap({ [anchor]: !!anchor, [alignment]: !!alignment })}">
         <slot id="trigger"
               @slotchange="${this.#triggerChanged}"
-              @keydown=${this.onKeydown}
-              @click=${this.toggle}></slot>
-        <dialog id="popover" aria-labelledby="heading" aria-describedby="body" aria-label=${ifDefined(this.label)}>
+              @keydown="${this.onKeydown}"
+              @click="${this.toggle}"></slot>
+        <dialog id="popover">
           <div id="arrow"></div>
           <div id="content" part="content">
             <pf-button id="close-button"
@@ -376,7 +364,18 @@ export class PfPopover extends LitElement {
                 <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/>
               </svg>
             </pf-button>
-            ${header}
+            ${!(hasHeading && hasIcon) ? headingSlotWithFallback : html`
+            <header part="header">
+              <span part="icon">
+                <slot name="icon">
+                  <pf-icon icon="${headerIcon}"
+                           set="${ifDefined(this.iconSet)}"
+                           size="md"></pf-icon>
+                </slot>
+              </span>${!this.alertSeverity ? nothing : html`
+              <span class="visually-hidden">${this.alertSeverityText ?? `${this.alertSeverity} alert:`}</span>`}
+              ${headingSlotWithFallback}
+            </header>`}
             <slot id="body" part="body" name="body">${this.body ?? ''}</slot>
             <footer part="footer" ?hidden=${!hasFooter}>
               <slot name="footer">${this.footer}</slot>

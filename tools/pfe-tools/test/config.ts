1697347d6bd953cab9dff82dc641c2a9388733e4
Benny Powers <web@bennypowers.com>
2023-12-21 10:29:30 +0200

    fix(tools)!: remove framework test wrappers
    We will retool this and develop something better. In the mean time, use the react wrapper components

--- tools/pfe-tools/test/config.ts
+++ tools/pfe-tools/test/config.ts
@@ -71,45 +71,11 @@ export function pfeTestRunnerConfig(opts: PfeTestRunnerConfigOptions): TestRunne
       },
     },
     testRunnerHtml,
-    groups: [
-      {
-        name: 'with-vue',
-        testRunnerHtml: testFramework => /* html */`
-          <html>
-            <head>
-              <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
-              <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js" crossorigin></script>
-            </head>
-            <body>
-              <div id="root"></div>
-              <script type="module" src="${testFramework}"></script>
-            </body>
-          </html>
-        `,
-      },
-      {
-        name: 'with-react',
-        testRunnerHtml: testFramework => /* html */`
-          <html>
-            <head>
-              <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes">
-              <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
-              <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
-              <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>
-            </head>
-            <body>
-              <div id="root"></div>
-              <script type="module" src="${testFramework}"></script>
-            </body>
-          </html>
-        `,
-      },
-    ],
     reporters,
     plugins: [
       ...devServerConfig.plugins ?? [],
       ...opts.plugins ?? [],
       a11ySnapshotPlugin(),
-    ]
+    ],
   };
 }

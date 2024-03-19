33330abf0f54e40d2f1fb418dcb047ed5702454c
Benny Powers <web@bennypowers.com>
2024-03-19 15:04:55 +0200

    fix(chip): use the new pf-button

--- elements/pf-chip/pf-chip.ts
+++ elements/pf-chip/pf-chip.ts
@@ -5,11 +5,7 @@ import { property } from 'lit/decorators/property.js';
 import '@patternfly/elements/pf-button/pf-button.js';
 
 import styles from './pf-chip.css';
-
-import buttonBaseButtonStyles from '../pf-button/BaseButton.css';
-import buttonBaseStyles from '../pf-button/pf-button-base.css';
-import buttonTokenStyles from '../pf-button/pf-button-tokens.css';
-import buttonPlainStyles from '../pf-button/pf-button-plain.css';
+import shared from './pf-chip-shared.css';
 
 export class PfChipRemoveEvent extends Event {
   constructor(public chip: PfChip) {
@@ -30,14 +26,7 @@ export class PfChipRemoveEvent extends Event {
  */
 @customElement('pf-chip')
 export class PfChip extends LitElement {
-  static readonly styles = [
-    styles,
-    // TODO(bennypowers): reexamine using `<pf-button>` directly once cross-root aria is solved in browsers
-    buttonBaseButtonStyles,
-    buttonBaseStyles,
-    buttonTokenStyles,
-    buttonPlainStyles,
-  ];
+  static readonly styles = [shared, styles];
 
   static override readonly shadowRootOptions: ShadowRootInit = { ...LitElement.shadowRootOptions, delegatesFocus: true };
 
@@ -68,15 +57,13 @@ export class PfChip extends LitElement {
         <span id="chip-text" part="text">
           <slot></slot>
         </span>
-        <button id="close-button"
-                class="pf-button plain hasIcon"
-                aria-label="${this.accessibleCloseLabel}"
+        <pf-button id="close-button"
+                plain
+                icon="close" icon-set="patternfly"
+                label="${this.accessibleCloseLabel}"
                 aria-describedby="chip-text"
                 ?hidden="${this.readonly || this.overflowChip}"
-                @click="${this.#onClick}">
-          <pf-icon icon="close"
-                   set="patternfly"></pf-icon>
-        </button>
+                @click="${this.#onClick}"></pf-button>
       </div>
     `;
   }

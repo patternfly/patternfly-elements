37bf1f545cfcd864cdf4a03f0301f4c255cb387e
Steven Spriggs <steven.spriggs@gmail.com>
2024-03-21 15:01:36 -0400

    fix(switch): add aria-hidden to svg

--- elements/pf-switch/BaseSwitch.ts
+++ elements/pf-switch/BaseSwitch.ts
@@ -49,7 +49,7 @@ export abstract class BaseSwitch extends LitElement {
   override render() {
     return html`
       <div id="container" tabindex="0">
-        <svg id="toggle" fill="currentColor" height="1em" width="1em" viewBox="0 0 512 512">
+        <svg id="toggle" fill="currentColor" height="1em" width="1em" viewBox="0 0 512 512" aria-hidden="true">
           <path ?hidden=${!this.showCheckIcon} d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
         </svg>
       </div>

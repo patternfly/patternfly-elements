85f8b628b5d3806c9d534aa12c0abf29ff976622
Benny Powers <web@bennypowers.com>
2024-07-09 12:35:22 +0300

    feat(core): add `@listen` decorator

---
+++ core/pfe-core/decorators/listen.ts
@@ -0,0 +1,26 @@
+import type { LitElement } from 'lit';
+
+/**
+ * Listens for a given event on the custom element.
+ * equivalent to calling `this.addEventListener` in the constructor
+ * @param type event type e.g. `click`
+ * @param options event listener options object e.g. `{ passive: true }`
+ */
+export function listen(type: string, options?: EventListenerOptions) {
+  return function(
+    proto: LitElement,
+    methodName: string,
+  ) {
+    const origConnected = proto.connectedCallback;
+    const origDisconnected = proto.disconnectedCallback;
+    const listener = (proto as any)[methodName] as EventListener;
+    proto.connectedCallback = function() {
+      origConnected();
+      this.addEventListener(type, listener, options);
+    };
+    proto.disconnectedCallback = function() {
+      origDisconnected();
+      this.removeEventListener(type, listener, options);
+    };
+  };
+}

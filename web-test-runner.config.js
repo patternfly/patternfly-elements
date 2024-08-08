77c550159bcb4a58713ffc9a70fbcfd72697a52c
Benny Powers <web@bennypowers.com>
2024-08-08 16:44:30 +0300

    chore: import maps in tests

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,11 +1,27 @@
 import { pfeTestRunnerConfig } from '@patternfly/pfe-tools/test/config.js';
 import { a11ySnapshotPlugin } from '@web/test-runner-commands/plugins';
+import { getPatternflyIconNodemodulesImports } from '@patternfly/pfe-tools/dev-server/config.js';
 
 export default pfeTestRunnerConfig({
   // workaround for https://github.com/evanw/esbuild/issues/3019
   tsconfig: 'tsconfig.esbuild.json',
   files: ['!tools/create-element/templates/**/*'],
   // uncomment to get default wtr reporter
+  importMapOptions: {
+    providers: {
+      'zero-md': 'nodemodules',
+      '@patternfly/icons': 'nodemodules',
+      '@patternfly/elements': 'monorepotypescript',
+      '@patternfly/pfe-tools': 'monorepotypescript',
+      '@patternfly/pfe-core': 'monorepotypescript',
+    },
+    inputMap: {
+      imports: {
+        '@patternfly/pfe-tools/environment.js': './_site/tools/environment.js',
+        ...await getPatternflyIconNodemodulesImports(import.meta.url),
+      },
+    },
+  },
   reporter: 'default',
   plugins: [
     a11ySnapshotPlugin(),

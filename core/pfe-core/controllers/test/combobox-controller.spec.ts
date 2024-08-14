3c42af5858509e38add3f4956c93424600502954
Steven Spriggs <steven.spriggs@gmail.com>
2024-08-14 16:14:48 -0400

    test(select): add lightdom options slotted test

--- core/pfe-core/controllers/test/combobox-controller.spec.ts
+++ core/pfe-core/controllers/test/combobox-controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable lit-a11y/accessible-name */
 import { expect, fixture } from '@open-wc/testing';
 import { sendKeys } from '@web/test-runner-commands';
 import { a11ySnapshot, querySnapshot } from '@patternfly/pfe-tools/test/a11y-snapshot.js';
@@ -8,12 +9,14 @@ import { state } from 'lit/decorators/state.js';
 import { LitElement, html } from 'lit';
 
 import { ComboboxController } from '../combobox-controller.js';
+import { SlotController } from '../slot-controller.js';
 
 function press(key: string) {
   return async function() {
     await sendKeys({ press: key });
   };
 }
+
 
 describe('ComboboxController', function() {
   describe('With toggle and combobox in shadow root but items in light DOM', function() {
@@ -62,7 +65,7 @@ describe('ComboboxController', function() {
             <div id="listbox" ?hidden="${!this.expanded}">
               <option id="placeholder"
                       aria-disabled="true"
-                      aria-hidden="${String(!!(this.#combobox.selected.length))}">
+                      aria-hidden="${String(!!(this.#combobox.selected.length)) as 'true' | 'false'}">
                 Select an Option
               </option>
               ${this.#combobox.renderItemsToShadowRoot()}
@@ -93,7 +96,118 @@ describe('ComboboxController', function() {
         beforeEach(press('Tab'));
         it('does not focus the toggle button', async function() {
           const snapshot = await a11ySnapshot();
-          expect(querySnapshot(snapshot, { focused: true })).to.not.be.ok;
+          expect(querySnapshot(snapshot, { focused: true })).to.equal(null);
+        });
+      });
+      describe('pressing ArrowDown', function() {
+        beforeEach(press('ArrowDown'));
+
+        it('expands the listbox', async function() {
+          const snapshot = await a11ySnapshot();
+          expect(querySnapshot(snapshot, { role: 'listbox' })?.children, 'listbox children').to.have.length(11);
+          expect(querySnapshot(snapshot, { role: 'combobox' })?.expanded, 'expanded').to.equal(true);
+        });
+
+        it('maintains DOM focus on the combobox', async function() {
+          const snapshot = await a11ySnapshot();
+          expect(querySnapshot(snapshot, { focused: true })?.role).to.equal('combobox');
+        });
+
+        it.skip('sets active state on the first option', async function() {
+          // maybe this is a problem with the demo element
+          await element.updateComplete;
+          expect(element.querySelector('option')?.classList.contains('active')).to.equal(true);
+        });
+
+        describe('pressing Escape', function() {
+          beforeEach(press('Escape'));
+          it('collapses the listbox', async function() {
+            const snapshot = await a11ySnapshot();
+            expect(querySnapshot(snapshot, { role: 'combobox' })?.expanded, 'expanded').to.equal(undefined);
+          });
+          it('maintains DOM focus on the combobox', async function() {
+            const snapshot = await a11ySnapshot();
+            expect(querySnapshot(snapshot, { focused: true })?.role).to.equal('combobox');
+          });
+        });
+      });
+    });
+  });
+
+  describe('With input, listbox, and options in the lightdom', function() {
+    let element: XComboboxLight;
+
+    @customElement('x-combobox-light')
+    class XComboboxLight extends LitElement {
+      #slots = new SlotController(this, null, 'input', 'button');
+
+      /** List of options */
+      get options(): HTMLOptionElement[] {
+        return [...Array.from(this.querySelectorAll('option'))].filter(x => !!x);
+      }
+
+      #canControlLightDom = ComboboxController.canControlLightDom;
+
+      #combobox = ComboboxController.of(this, {
+        multi: false,
+        isExpanded: () => this.expanded,
+        requestShowListbox: () => this.expanded ||= true,
+        requestHideListbox: () => ((this.expanded &&= false), true),
+        getListboxElement: () => this.#canControlLightDom ? this._listbox as HTMLElement : [...this.#slots.getSlotted()].shift() as HTMLElement,
+        getToggleButton: () => [...this.#slots.getSlotted('button')].shift() as HTMLElement,
+        getComboboxInput: () => [...this.#slots.getSlotted('input')].shift() as HTMLElement,
+        getFallbackLabel: () => 'options',
+        getItems: () => this.options,
+      });
+
+      @query('#listbox') private _listbox?: HTMLElement;
+
+      @state() expanded = false;
+
+      override render() {
+        return html`
+            <div id="toggle">
+              <slot name="input"></slot>
+              <slot name="button"></slot>
+            </div>
+            <div ?hidden=${this.#canControlLightDom} id="listbox" role="listbox">
+              <option id="placeholder"
+                      aria-disabled="true"
+                      aria-hidden="${String(!!(this.#combobox.selected.length)) as 'true' | 'false'}">
+                Select an Option
+              </option>
+              ${this.#combobox.renderItemsToShadowRoot()}
+            </div>
+          </div>
+            <div ?hidden=${!this.#canControlLightDom}>
+              <slot></slot>
+            </div>
+          `;
+      }
+    }
+
+    beforeEach(async function() {
+      element = await fixture<XComboboxLight>(html`
+        <x-combobox-light>
+          <input slot="input">
+          <button slot="button">Show Options</button>
+          ${Array.from({ length: 10 }, (_, i) => html`<option>${i + 1}</option>`)}å
+        </x-combobox-light>
+      `);
+      await element.updateComplete;
+    });
+
+    describe('tabbing to the combobox', function() {
+      beforeEach(press('Tab'));
+      it('focuses the combobox', async function() {
+        const snapshot = await a11ySnapshot();
+        expect(querySnapshot(snapshot, { focused: true })?.role).to.equal('combobox');
+      });
+      describe('tabbing out of the combobox', function() {
+        beforeEach(press('Tab'));
+        it('does not focus the toggle button', async function() {
+          const snapshot = await a11ySnapshot();
+          expect(querySnapshot(snapshot, { focused: true })).to.equal(null);
         });
       });
       describe('pressing ArrowDown', function() {
@@ -101,7 +215,7 @@ describe('ComboboxController', function() {
         it('expands the listbox', async function() {
           const snapshot = await a11ySnapshot();
           expect(querySnapshot(snapshot, { role: 'listbox' })?.children, 'listbox children').to.have.length(11);
-          expect(querySnapshot(snapshot, { role: 'combobox' })?.expanded, 'expanded').to.be.true;
+          expect(querySnapshot(snapshot, { role: 'combobox' })?.expanded, 'expanded').to.equal(true);
         });
         it('maintains DOM focus on the combobox', async function() {
           const snapshot = await a11ySnapshot();
@@ -110,9 +224,24 @@ describe('ComboboxController', function() {
         it.skip('sets active state on the first option', async function() {
           // maybe this is a problem with the demo element
           await element.updateComplete;
-          expect(element.querySelector('option')?.classList.contains('active')).to.be.true;
+          expect(element.querySelector('option')?.classList.contains('active')).to.equal(true);
+        });
+        describe('pressing Escape', function() {
+          beforeEach(press('Escape'));
+          it('collapses the listbox', async function() {
+            const snapshot = await a11ySnapshot();
+            expect(querySnapshot(snapshot, { role: 'combobox' })?.expanded, 'expanded').to.equal(undefined);
+          });
+          it('maintains DOM focus on the combobox', async function() {
+            const snapshot = await a11ySnapshot();
+            expect(querySnapshot(snapshot, { focused: true })?.role).to.equal('combobox');
+          });
         });
       });
     });
+  });
+
+  describe('All options are in the shadowdom', function() {
+    /* test here */
   });
 });

d501e5b21b897bb6223cb4ece74c1f050b469fed
Benny Powers - עם ישראל חי! <bennypowers@users.noreply.github.com>
2024-08-27 12:35:50 +0300

    fix(core): improved ssr (#2833)

---
+++ core/pfe-core/ssr-shims.ts
@@ -0,0 +1,56 @@
+class ObserverShim {
+  observe(): void {
+    void 0;
+  }
+
+  disconnect(): void {
+    void 0;
+  }
+}
+
+class MiniHTMLElement {
+  innerHTML = '';
+  constructor(public tagName: string) { }
+}
+
+class MiniHTMLTemplateElement extends MiniHTMLElement {
+  content = { cloneNode: (): string => this.innerHTML };
+}
+
+class MiniDocument {
+  createElement(tagName: string): MiniHTMLElement {
+    switch (tagName) {
+      case 'template':
+        return new MiniHTMLTemplateElement(tagName);
+      default:
+        return new MiniHTMLElement(tagName);
+    }
+  }
+}
+
+// @ts-expect-error: this runs in node
+globalThis.window ??= globalThis;
+// @ts-expect-error: this runs in node
+globalThis.document ??= new MiniDocument();
+// @ts-expect-error: this runs in node
+globalThis.navigator ??= { userAgent: '' };
+// @ts-expect-error: this runs in node
+globalThis.ErrorEvent ??= Event;
+// @ts-expect-error: this runs in node
+globalThis.IntersectionObserver ??= ObserverShim;
+// @ts-expect-error: this runs in node
+globalThis.MutationObserver ??= ObserverShim;
+// @ts-expect-error: this runs in node
+globalThis.getComputedStyle ??= function() {
+  return {
+    getPropertyPriority() {
+      return '';
+    },
+    getPropertyValue() {
+      return '';
+    },
+  };
+}
+
+;
+

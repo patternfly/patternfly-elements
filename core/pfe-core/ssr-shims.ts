0088148e6ef1f4f7c71476edeacb59d03ad519e7
Benny Powers <web@bennypowers.com>
2025-01-13 14:52:11 +0200

    fix(core): rely on lit's dom-shim

--- core/pfe-core/ssr-shims.ts
+++ core/pfe-core/ssr-shims.ts
@@ -1,3 +1,5 @@
+import { installWindowOnGlobal } from '@lit-labs/ssr/lib/dom-shim.js';
+
 class ObserverShim {
   observe(): void {
     void 0;
@@ -17,35 +19,7 @@ class MiniHTMLTemplateElement extends MiniHTMLElement {
   content = { cloneNode: (): string => this.innerHTML };
 }
 
-class MiniDocument {
-  createElement(tagName: string): MiniHTMLElement {
-    switch (tagName) {
-      case 'template':
-        return new MiniHTMLTemplateElement(tagName);
-      default:
-        return new MiniHTMLElement(tagName);
-    }
-  }
-}
-
-// @ts-expect-error: this runs in node
-globalThis.window ??= globalThis;
-// @ts-expect-error: this runs in node
-globalThis.document ??= new MiniDocument();
-// @ts-expect-error: this runs in node
-globalThis.navigator ??= { userAgent: '' };
-// @ts-expect-error: opt in to event support in ssr
-globalThis.litSsrCallConnectedCallback = true;
-// @ts-expect-error: this runs in node
-globalThis.ErrorEvent ??= Event;
-// @ts-expect-error: this runs in node
-globalThis.IntersectionObserver ??= ObserverShim;
-// @ts-expect-error: this runs in node
-globalThis.MutationObserver ??= ObserverShim;
-// @ts-expect-error: this runs in node
-globalThis.ResizeObserver ??= ObserverShim;
-// @ts-expect-error: this runs in node
-globalThis.getComputedStyle ??= function() {
+function getComputedStyle() {
   return {
     getPropertyPriority() {
       return '';
@@ -54,6 +28,27 @@ globalThis.getComputedStyle ??= function() {
       return '';
     },
   };
-}
+};
 
-;
+// @ts-expect-error: opt in to event support in ssr
+globalThis.litSsrCallConnectedCallback = true;
+
+installWindowOnGlobal({
+  ErrorEvent: Event,
+  IntersectionObserver: ObserverShim,
+  MutationObserver: ObserverShim,
+  ResizeObserver: ObserverShim,
+  getComputedStyle,
+});
+
+// @ts-expect-error: this runs in node
+globalThis.navigator.userAgent ??= '@lit-labs/ssr';
+
+globalThis.document.createElement = function createElement(tagName: string): HTMLElement {
+  switch (tagName) {
+    case 'template':
+      return new MiniHTMLTemplateElement(tagName) as unknown as HTMLElement;
+    default:
+      return new MiniHTMLElement(tagName) as HTMLElement;
+  }
+};

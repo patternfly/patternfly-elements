36875aab0269f21fb0fc87e12031935503e5449e
ArathyKumar <ask6295@gmail.com>
2025-03-20 13:25:25 +0530

    chore: pf-search input created

--- elements/pf-search-input/pf-search-input-option.ts
+++ elements/pf-search-input/pf-search-input-option.ts
@@ -3,7 +3,9 @@ import { customElement } from 'lit/decorators/custom-element.js';
 import { queryAssignedNodes } from 'lit/decorators/query-assigned-nodes.js';
 import { property } from 'lit/decorators/property.js';
 import { classMap } from 'lit/directives/class-map.js';
+
 import { InternalsController } from '@patternfly/pfe-core/controllers/internals-controller.js';
+
 import { observes } from '@patternfly/pfe-core/decorators/observes.js';
 import styles from './pf-search-input-option.css';
 
@@ -15,71 +17,77 @@ import styles from './pf-search-input-option.css';
 export class PfSearchInputOption extends LitElement {
   static readonly styles: CSSStyleSheet[] = [styles];
 
-    /** form value for this option */
-    @property({ reflect: true })
-    get value() {
-      return (this.#value ?? this.textContent ?? '').trim();
-    }
-  
-    set value(v: string) {
-      this.#value = v;
-    }
-  
-    /** whether option is selected */
-    @property({ type: Boolean, reflect: true }) selected = false;
-  
-    /** whether option is active descendant */
-    @property({ type: Boolean, reflect: true }) active = false;
+   /** whether option is disabled */
+  @property({ type: Boolean, reflect: true }) disabled = false;
 
-    @queryAssignedNodes({ slot: '', flatten: true })
-    private _slottedText!: Node[];
-  
-    /**
-     * this option's position relative to the other options
-     */
-    set posInSet(posInSet: number | null) {
-      this.#internals.ariaPosInSet = `${Math.max(0, posInSet ?? 0)}`;
-    }
-  
-    get posInSet() {
-      const parsed = parseInt(this.#internals.ariaPosInSet ?? '0');
-      return Number.isNaN(parsed) ? null : parsed;
-    }
-  
-    /**
-     * total number of options
-     */
-    set setSize(setSize: number | null) {
-      this.#internals.ariaSetSize = `${Math.max(0, setSize ?? 0)}`;
-    }
-  
-    get setSize() {
-      try {
-        const int = parseInt(this.#internals.ariaSetSize ?? '0');
-        if (Number.isNaN(int)) {
-          return 0;
-        } else {
-          return int;
-        }
-      } catch {
+  /** form value for this option */
+  @property({ reflect: true })
+  get value() {
+    return (this.#value ?? this.textContent ?? '').trim();
+  }
+
+  set value(v: string) {
+    this.#value = v;
+  }
+
+  /** whether option is selected */
+  @property({ type: Boolean, reflect: true }) selected = false;
+
+  /** whether option is active descendant */
+  @property({ type: Boolean, reflect: true }) active = false;
+
+  /** Optional option description; overridden by description slot. */
+  @property() description = '';
+
+  @queryAssignedNodes({ slot: '', flatten: true })
+  private _slottedText!: Node[];
+
+  /**
+   * this option's position relative to the other options
+   */
+  set posInSet(posInSet: number | null) {
+    this.#internals.ariaPosInSet = `${Math.max(0, posInSet ?? 0)}`;
+  }
+
+  get posInSet() {
+    const parsed = parseInt(this.#internals.ariaPosInSet ?? '0');
+    return Number.isNaN(parsed) ? null : parsed;
+  }
+
+  /**
+   * total number of options
+   */
+  set setSize(setSize: number | null) {
+    this.#internals.ariaSetSize = `${Math.max(0, setSize ?? 0)}`;
+  }
+
+  get setSize() {
+    try {
+      const int = parseInt(this.#internals.ariaSetSize ?? '0');
+      if (Number.isNaN(int)) {
         return 0;
+      } else {
+        return int;
       }
+    } catch {
+      return 0;
     }
-  
-    #value?: string;
-  
-    #internals = InternalsController.of(this, { role: 'option' });
-  
+  }
+
+  #value?: string;
+
+  #internals = InternalsController.of(this, { role: 'option' });
 
   render(): TemplateResult<1> {
-    const { active, selected } = this;
+    const { disabled, active, selected } = this;
     return html`
-      <div id="outer" class="${classMap({ active, selected })}">
+      <div id="outer" class="${classMap({ active, disabled, selected })}">
         <input type="checkbox"
                inert
                role="presentation"
                tabindex="-1"
-               ?checked="${this.selected}">
+               ?checked="${this.selected}"
+               ?disabled="${this.disabled}">
         <slot name="icon"></slot>
         <span>
           <slot name="create"></slot>
@@ -91,6 +99,7 @@ export class PfSearchInputOption extends LitElement {
              aria-hidden="true">
           <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
         </svg>
+        <slot id="description" name="description">${this.description ?? ''}</slot>
       </div>
     `;
   }
@@ -98,6 +107,11 @@ export class PfSearchInputOption extends LitElement {
   @observes('selected')
   private selectedChanged() {
     this.#internals.ariaSelected = String(!!this.selected);
+  }
+
+  @observes('disabled')
+  private disabledChanged() {
+    this.#internals.ariaDisabled = String(!!this.disabled);
   }
 
   /**
--- elements/pf-search-input/pf-search-input.ts
+++ elements/pf-search-input/pf-search-input.ts
@@ -1,21 +1,24 @@
-import { LitElement, html, isServer, type TemplateResult } from 'lit';
+import type { Placement } from '@patternfly/pfe-core/controllers/floating-dom-controller.js';
+import type { TemplateResult } from 'lit';
+
+import { LitElement, html, isServer } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
-import { ComboboxController } from '@patternfly/pfe-core/controllers/combobox-controller.js';
-import { query } from 'lit/decorators/query.js';
 import { property } from 'lit/decorators/property.js';
-import { observes } from '@patternfly/pfe-core/decorators/observes.js';
+import { query } from 'lit/decorators/query.js';
+import { repeat } from 'lit/directives/repeat.js';
 import { styleMap } from 'lit/directives/style-map.js';
-import {
-  FloatingDOMController,
-  type Placement,
-} from '@patternfly/pfe-core/controllers/floating-dom-controller.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+import { ComboboxController } from '@patternfly/pfe-core/controllers/combobox-controller.js';
 import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller.js';
 import { InternalsController } from '@patternfly/pfe-core/controllers/internals-controller.js';
-import '@patternfly/elements/pf-text-input/pf-text-input.js';
+import { FloatingDOMController } from '@patternfly/pfe-core/controllers/floating-dom-controller.js';
+
 import { arraysAreEquivalent } from '@patternfly/pfe-core/functions/arraysAreEquivalent.js';
-import '@patternfly/elements/pf-icon/pf-icon.js';
+import { observes } from '@patternfly/pfe-core/decorators/observes.js';
+
 import { PfSearchInputOption } from './pf-search-input-option.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import styles from './pf-search-input.css';
 
 export class PfSelectChangeEvent extends Event {
@@ -32,46 +35,94 @@ export class PfSelectChangeEvent extends Event {
 export class PfSearchInput extends LitElement {
   static readonly styles: CSSStyleSheet[] = [styles];
 
+  static readonly formAssociated = true;
+
+  static override readonly shadowRootOptions: ShadowRootInit = {
+    ...LitElement.shadowRootOptions,
+    delegatesFocus: true,
+  };
+
+  /** Variant of rendered Select */
+  @property() variant: 'single' | 'checkbox' | 'typeahead' | 'typeaheadmulti' = 'single';
+
+  /** Accessible label for the select */
+  @property({ attribute: 'accessible-label' }) accessibleLabel?: string;
+
+  /** Accessible label for chip group used to describe chips */
+  @property({
+    attribute: 'accessible-current-selections-label',
+  }) accessibleCurrentSelectionsLabel = 'Current selections';
+
+  /** Multi listbox button text */
+  @property({ attribute: 'items-selected-text' }) itemsSelectedText = 'items selected';
+
+  /** Whether the select is disabled */
+  @property({ type: Boolean, reflect: true }) disabled = false;
+
+  /** Whether the select listbox is expanded */
   @property({ type: Boolean, reflect: true }) expanded = false;
-  @property({ reflect: true }) position: Placement = 'bottom';
+
+  /**
+   * Enable to flip listbox when it reaches boundary
+   */
   @property({ attribute: 'enable-flip', type: Boolean }) enableFlip = false;
-   /** Current form value */
-   @property() value?: string;
-   @property() variant: 'single' | 'checkbox' | 'typeahead' | 'typeaheadmulti' = 'single';
-  @query('#listbox') listbox!: HTMLElement;
-  @query('#button') button!: HTMLButtonElement;
-  @query('#combobox') combobox!: HTMLInputElement;
-  @query('#placeholder') placeholder!: PfSearchInputOption;
+
+  /** Current form value */
+  @property() value?: string;
+
+  /** Placeholder entry. Overridden by the `placeholder` slot */
+  @property() placeholder?: string;
+
+  /**
+   * Indicates initial popover position.
+   * There are 6 options: `bottom`, `top`, `top-start`, `top-end`, `bottom-start`, `bottom-end`.
+   * Default is `bottom`.
+   */
+  @property({ reflect: true }) position: Placement = 'bottom';
+
+  /** Flag indicating if selection badge should be hidden for checkbox variant,default false */
+  @property({
+    attribute: 'checkbox-selection-badge-hidden',
+    type: Boolean,
+  }) checkboxSelectionBadgeHidden = false;
+
+  //@query('pf-chip-group') private _chipGroup?: PfChipGroup;
+
+  @query('#toggle-input') private _toggleInput?: HTMLInputElement;
+
+  @query('#toggle-button') private _toggleButton?: HTMLButtonElement;
+
+  @query('#listbox') private _listbox?: HTMLElement;
+
   @query('#listbox-container') private _listboxContainer?: HTMLElement;
 
-  #isNotPlaceholderOption = (option: PfSearchInputOption) => option !== this.placeholder;
+  @query('#placeholder') private _placeholder?: PfSearchInputOption;
 
-
-  #float = new FloatingDOMController(this, { content: () => this._listboxContainer });
+  #isNotPlaceholderOption = (option: PfSearchInputOption) => option !== this._placeholder;
 
   #internals = InternalsController.of(this);
 
+  #float = new FloatingDOMController(this, { content: () => this._listboxContainer });
+
   #slots = new SlotController(this, null, 'placeholder');
-
-
-
-  // static template: TemplateResult<1> = html`
-  //   <pf-text-input-autocomplete></pf-text-input-autocomplete>`;
 
   #combobox = ComboboxController.of(this, {
     multi: this.variant === 'typeaheadmulti' || this.variant === 'checkbox',
     getItems: () => this.options,
-    isItem: item => item instanceof PfSearchInputOption,
-    getFallbackLabel: () => 'options',
-    getListboxElement: () => this.listbox ?? null,
-    getToggleButton: () => this.combobox ?? null,
-    getComboboxInput: () => this.combobox ?? null,
+    getFallbackLabel: () => this.accessibleLabel
+                         || this.#internals.computedLabelText
+                         || this.placeholder
+                         || this.#slots.getSlotted('placeholder').map(x => x.textContent).join(''),
+    getListboxElement: () => this._listbox ?? null,
+    getToggleButton: () => this._toggleButton ?? null,
+    getComboboxInput: () => this._toggleInput ?? null,
     isExpanded: () => this.expanded,
     requestShowListbox: () => void (this.expanded ||= true),
     requestHideListbox: () => void (this.expanded &&= false),
-    setItemActive: (item, active) => item.classList.toggle('active', active),
+    setItemHidden: (item, hidden) => (item.id !== 'placeholder') && void (item.hidden = hidden),
+    isItem: item => item instanceof PfSearchInputOption,
+    setItemActive: (item, active) => item.active = active,
     setItemSelected: (item, selected) => item.selected = selected,
-    setItemHidden: (item, hidden) => (item.id !== 'placeholder') && void (item.hidden = hidden)
   });
 
   /**
@@ -79,12 +130,14 @@ export class PfSearchInput extends LitElement {
    * or array of select option values for multi select.
    */
   @property({ hasChanged: (a, b) => !arraysAreEquivalent(a, b) })
-  set selected(selected: any) {
+  set selected(selected: PfSearchInputOption | PfSearchInputOption[]) {
     const list = Array.isArray(selected) ? selected : [selected];
     this.#combobox.selected = list;
   }
 
-
+  get selected(): PfSearchInputOption[] {
+    return this.#combobox.selected;
+  }
 
   /** List of options */
   get options(): PfSearchInputOption[] {
@@ -92,46 +145,53 @@ export class PfSearchInput extends LitElement {
       return []; // TODO: expose a DOM property to allow setting options in SSR scenarios
     } else {
       return [
-        this.placeholder,
+        this._placeholder,
         ...Array.from(this.querySelectorAll('pf-search-input-option')),
       ].filter((x): x is PfSearchInputOption => !!x && !x.hidden);
     }
   }
 
-  get selected(): PfSearchInputOption[] {
-    return this.#combobox.selected;
+  /** Whether select has badge for number of selected items */
+  get #hasBadge() {
+    // NOTE: revisit this in v5
+    return this.variant === 'checkbox' && !this.checkboxSelectionBadgeHidden;
   }
 
-  // get activeOption(): HTMLOptionElement | undefined {
-  //   return this.options.find(x => x.classList.contains('active'));
-  // }
+  get #buttonLabel(): string {
+    const { selected } = this.#combobox;
+    switch (this.variant) {
+      case 'typeaheadmulti':
+        return `${selected?.length ?? 0} ${this.itemsSelectedText}`;
+      case 'checkbox':
+        return this.#computePlaceholderText()
+          || 'Options';
+      default:
+        return (selected ? this.value : '')
+          || this.#computePlaceholderText()
+          || 'Select a value';
+    }
+  }
 
-  //<input id="combobox">
-  render(): TemplateResult<1> {
-
-    const { expanded, placeholder, variant } = this;
+  override render(): TemplateResult<1> {
+    const { disabled, expanded, variant, placeholder } = this;
     const { anchor = 'bottom', alignment = 'start', styles = {} } = this.#float;
     const { height, width } = this.getBoundingClientRect?.() || {};
-    const placeholderIsInert = !placeholder && this.#slots.isEmpty('placeholder');
-    const hasSelection = !!(Array.isArray(this.selected) ? this.selected.length : this.selected);
+    const hasBadge = this.#hasBadge;
+    const selectedOptions = this.#combobox.selected ?? [];
     const typeahead = variant.startsWith('typeahead');
-   const hideLightDomItems = typeahead && !ComboboxController.supportsCrossRootActiveDescendant;
+    const checkboxes = variant === 'checkbox';
+    const badge = hasBadge && 'badge';
+    const hasSelection = !!(Array.isArray(this.selected) ? this.selected.length : this.selected);
+    const hideLightDomItems = typeahead && !ComboboxController.supportsCrossRootActiveDescendant;
+    const placeholderIsInert = !placeholder && this.#slots.isEmpty('placeholder');
 
     return html`
-      <pf-text-input id="combobox" type="text" placeholder="Placeholder"></pf-text-input> 
-        <pf-button disabled plain label="Close">
-          <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 352 512">
-            <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 
-            0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 
-            189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 
-            0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 
-            356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 
-            12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 
-            44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
-            </path>
-        </svg>
-      </pf-button>
-      <pf-button disabled plain label="Search">
+      <div id="outer"
+           style="${styleMap(styles)}"
+           class="${classMap({ disabled, typeahead, expanded, [anchor]: !!anchor, [alignment]: !!alignment })}">
+        <div id="toggle">
+          ${!typeahead ? '' : html`
+          <pf-button disabled plain label="Search">
         <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 512 512">
           <path d="M256.233,5.756c-71.07,15.793-141.44,
           87.863-155.834,159.233c-11.495,57.076,0.3,111.153,
@@ -146,13 +206,40 @@ export class PfSearchInput extends LitElement {
           </path>
         </svg>
       </pf-button>
-      <div id="listbox-container"  ?hidden="${!expanded}"
+          <input id="toggle-input"
+                 ?hidden="${!typeahead}"
+                 ?disabled="${disabled}"
+                 placeholder="${placeholder || this.#buttonLabel}">`}
+          <button id="toggle-button">
+            <span id="button-text" style="display: contents;">
+              <span id="toggle-text"
+                    class="${classMap({ 'visually-hidden': !!typeahead, badge })}">${this.#buttonLabel}</span>${!hasBadge ? '' : html`
+              <span id="toggle-badge">
+                <pf-badge number="${selectedOptions.length}">${selectedOptions.length}</pf-badge>
+              </span>`}
+            </span>
+          </button>
+            <pf-button disabled plain label="Close">
+            <svg fill="currentColor" height="1em" width="1em" viewBox="0 0 352 512">
+              <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 
+              0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 
+              189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 
+              0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 
+              356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 
+              12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 
+              44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
+              </path>
+          </svg>
+        </pf-button>
+        </div>
+        <div id="listbox-container"
+             ?hidden="${!expanded}"
              style="${styleMap({
                marginTop: `${height || 0}px`,
                width: width ? `${width}px` : 'auto',
              })}">
-        <div id="listbox">
-             <pf-search-input-option id="placeholder"
+          <div id="listbox" class="${classMap({ checkboxes })}">
+            <pf-search-input-option id="placeholder"
                        disabled
                        ?inert="${placeholderIsInert}"
                        aria-hidden="${ifDefined(placeholderIsInert ? undefined : String(!!hasSelection))}"
@@ -160,18 +247,15 @@ export class PfSearchInput extends LitElement {
             ><slot name="placeholder">${placeholder ?? ''}</slot></pf-search-input-option>
             ${this.#combobox.renderItemsToShadowRoot()}
             <slot ?hidden="${hideLightDomItems}"></slot>
+          </div>
         </div>
       </div>
     `;
   }
 
-  @observes('selected')
-  private async selectedChanged(_: PfSearchInputOption[], selected: PfSearchInputOption[]) {
-    this.value = selected.map(x => x.value).join();
-    await this.updateComplete;
-    this.hide();
-   // this._toggleButton?.focus();
-    
+  @observes('disabled')
+  private disabledChanged() {
+    this.#combobox.disabled = this.disabled;
   }
 
   @observes('expanded')
@@ -183,6 +267,53 @@ export class PfSearchInput extends LitElement {
         this.#doCollapse();
       }
     }
+  }
+
+  @observes('selected')
+  private async selectedChanged(_: PfSearchInputOption[], selected: PfSearchInputOption[]) {
+    this.value = selected.map(x => x.value).join();
+    await this.updateComplete;
+    switch (this.variant) {
+      case 'single':
+        this.hide();
+        this._toggleButton?.focus();
+        break;
+      case 'typeahead':
+        this._toggleInput!.value = this.value;
+    }
+  }
+
+  @observes('variant')
+  private async variantChanged() {
+    this.#combobox.hostDisconnected();
+    this.#combobox.multi = this.variant === 'typeaheadmulti' || this.variant === 'checkbox';
+    this.#combobox.hostConnected();
+    if (this.variant === 'checkbox') {
+      import('@patternfly/elements/pf-badge/pf-badge.js');
+    }
+  }
+
+  @observes('value')
+  private valueChanged() {
+    this.#internals.setFormValue(this.value ?? '');
+    this.dispatchEvent(new PfSelectChangeEvent());
+  }
+
+  @observes('variant')
+  @observes('value')
+  private focusChips(): void {
+    // whether select has removable chips for selected items
+    // NOTE: revisit this in v5
+    // reset input if chip has been added
+    // const hasChips = this.variant === 'typeaheadmulti';
+    // if (hasChips && this._toggleInput?.value) {
+    //   // const chip =
+    //   //   this.shadowRoot?.querySelector(`pf-chip#chip-${this._toggleInput?.value}`) as PfChip;
+    //   // if (chip && this._chipGroup) {
+    //   //   this._chipGroup.focusOnChip(chip);
+    //   //   this._toggleInput.value = '';
+    //   // }
+    // }
   }
 
   async #doExpand() {
@@ -201,6 +332,31 @@ export class PfSearchInput extends LitElement {
     } catch {
       return false;
     }
+  }
+
+  /**
+   * handles chip's remove button clicking
+   * @param event remove event
+   * @param opt pf-option
+   */
+  // #onChipRemove(opt: PfOption, event: Event) {
+  //   if (event instanceof PfChipRemoveEvent) {
+  //     opt.selected = false;
+  //     this._toggleInput?.focus();
+  //   }
+  // }
+
+  #computePlaceholderText() {
+    return this.placeholder
+      || this.querySelector?.<HTMLSlotElement>('[slot=placeholder]')
+          ?.assignedNodes()
+          ?.reduce((acc, node) => `${acc}${node.textContent}`, '')
+          ?.trim()
+      || this.#combobox.items
+          .filter(this.#isNotPlaceholderOption)
+          .at(0)
+          ?.value
+      || '';
   }
 
   /**
@@ -228,12 +384,6 @@ export class PfSearchInput extends LitElement {
     } else {
       await this.show();
     }
-  }
-
-  @observes('value')
-  private valueChanged() {
-   // this.#internals.setFormValue(this.value ?? '');
-    this.dispatchEvent(new PfSelectChangeEvent());
   }
 }
 

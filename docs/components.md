35b5d8b71f4076b2e7211199a7ce25c021d8a329
Benny Powers <web@bennypowers.com>
2025-07-16 20:39:12 +0300

    docs: fix build

--- docs/components.md
+++ docs/components.md
@@ -1,20 +1,3 @@
----js
-{
-  layout: 'layout-basic.njk',
-  templateEngineOverride: 'njk,md',
-  tags: ['component'],
-  permalink: '/components/{{ element.slug }}/index.html',
-  package: '{{ element.package }}',
-  description: '{{ element.description }}',
-  pagination: {
-    data: 'elements',
-    size: 1,
-    alias: 'element',
-    before: xs => xs.filter(x => !!x.docsTemplatePath),
-  }
-}
----
-
 <script type="module">
   const CLASS_KEY = 'html-lit-react-snippets';
   const LS_KEY = `${CLASS_KEY}-index`;
--- docs/components/demos.11tydata.cjs
+++ docs/components/demos.11tydata.cjs
@@ -1,3 +1,4 @@
+const { dirname } = require('path');
 module.exports = {
   templateEngineOverride: 'njk',
   permalink: '{{ demo.permalink }}',
@@ -5,6 +6,19 @@ module.exports = {
     data: 'demos',
     alias: 'demo',
     size: 1,
-    before: xs => xs.filter(x => x.permalink),
+    // somehow the main pf-icon demo is being printed twice.
+    // so we'll group by tag name, and only take the first of each.
+    before: demos => Object.values(Object.groupBy(demos
+        .filter(demo => demo.permalink?.includes(demo.tagName))
+        .map(demo => {
+          if (demo.filePath?.endsWith(`${demo.tagName}.html`)) {
+            return {
+              ...demo,
+              permalink: `${dirname(demo.permalink)}/`,
+            };
+          } else {
+            return demo;
+          }
+        }), demo => demo.tagName)).map(([x]) => x),
   },
 };

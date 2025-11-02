67ce07496723ae363f78cce5e8e74b9921decc99
Avigail <avigail7820@gmail.com>
2025-11-02 14:10:15 +0200

    chore(monitor)Creating a description list element

---
+++ elements/pf-description-list/test/pf-description-list.spec.ts
@@ -0,0 +1,21 @@
+import { expect, html } from '@open-wc/testing';
+import { createFixture } from '@patternfly/pfe-tools/test/create-fixture.js';
+import { PfDescriptionList } from '@patternfly/elements/pf-description-list/pf-description-list.js';
+
+describe('<pf-description-list>', function() {
+  describe('simply instantiating', function() {
+    let element: PfDescriptionList;
+    it('imperatively instantiates', function() {
+      expect(document.createElement('pf-description-list')).to.be.an.instanceof(PfDescriptionList);
+    });
+
+    it('should upgrade', async function() {
+      element = await createFixture<PfDescriptionList>(html`<pf-description-list></pf-description-list>`);
+      const klass = customElements.get('pf-description-list');
+      expect(element)
+        .to.be.an.instanceOf(klass)
+        .and
+        .to.be.an.instanceOf(PfDescriptionList);
+    });
+  })
+});

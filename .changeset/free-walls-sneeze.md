a13e2590207f702d6acbb85fba917d467ba9c4f2
Benny Powers - עם ישראל חי! <bennypowers@users.noreply.github.com>
2025-12-25 12:59:04 +0200

    chore: update deps (#2957)
    * chore: update deps
    
    * chore: sync package lock
    
    * chore: update patch
    
    * chore: downgrade playwright
    
    and fix koa types
    
    * test(back-to-top): fix tests
    
    * fix(modal): remove HTMLDialogElement interface
    
    We'd have to add requestClose, so for now let's just remove this
    typescript-only feature
    
    * chore: downgrade jspm generator
    
    * chore: update to rhds 4
    
    * chore: downgrade playwright in tests

---
+++ .changeset/free-walls-sneeze.md
@@ -0,0 +1,10 @@
+---
+"@patternfly/create-element": patch
+"@patternfly/eslint-config-elements": patch
+"@patternfly/pfe-tools": patch
+"@patternfly/pfe-core": patch
+"@patternfly/elements": patch
+---
+
+Update dependencies
+  
--- docs/main.mjs
+++ docs/main.mjs
@@ -1,4 +1,4 @@
-import '@rhds/elements/rh-footer/rh-global-footer.js';
+import '@rhds/elements/rh-footer/rh-footer-universal.js';
 import 'element-internals-polyfill';
 import { PfIcon } from '@patternfly/elements/pf-icon/pf-icon.js';
 
